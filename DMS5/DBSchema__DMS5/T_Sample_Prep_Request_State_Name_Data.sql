63f0181a75597a692d35ab05b8bcd3c5cda05724
Gary Kiebel <grkiebel@pnnl.gov>
2006-08-24 00:24:11 +0000

    Removed file/folder

--- DMS5/DBSchema__DMS5/T_Sample_Prep_Request_State_Name_Data.sql
+++
@@ -1,9 +0,0 @@
-/****** Object:  Table [T_Sample_Prep_Request_State_Name] ******/
-/****** RowCount: 6 ******/
-/****** Columns: State_ID, State_Name ******/
-INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (0,'(not a valid state)')
-INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (1,'New')
-INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (2,'Open')
-INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (3,'Prep in Progress')
-INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (4,'Prep Complete')
-INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (5,'Closed')
