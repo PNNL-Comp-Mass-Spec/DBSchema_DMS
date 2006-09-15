70d0436feceaedffa038b0cd653f8ec78861aeb4
Gary Kiebel <grkiebel@pnnl.gov>
2006-09-15 18:50:58 +0000

    Changes to DMS database schema for ticket #231

--- DMS5/Trunk/T_Event_Target.sql
+++ DMS5/Trunk/T_Event_Target.sql
@@ -6,6 +6,9 @@ GO
 CREATE TABLE [dbo].[T_Event_Target](
 	[ID] [int] NOT NULL,
 	[Name] [varchar](32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Target_Table] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Target_ID_Column] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Target_State_Column] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
  CONSTRAINT [PK_T_Event_Target] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
--- DMS5/Trunk/T_Event_Target_Data.sql
+++ DMS5/Trunk/T_Event_Target_Data.sql
@@ -1,11 +1,11 @@
 /****** Object:  Table [T_Event_Target] ******/
 /****** RowCount: 8 ******/
-/****** Columns: ID, Name ******/
-INSERT INTO [T_Event_Target] VALUES (0,'(none)')
-INSERT INTO [T_Event_Target] VALUES (1,'Campaign')
-INSERT INTO [T_Event_Target] VALUES (2,'Cell Culture')
-INSERT INTO [T_Event_Target] VALUES (3,'Experiment')
-INSERT INTO [T_Event_Target] VALUES (4,'Dataset')
-INSERT INTO [T_Event_Target] VALUES (5,'Analysis Job')
-INSERT INTO [T_Event_Target] VALUES (6,'Archive')
-INSERT INTO [T_Event_Target] VALUES (7,'Archive Update')
+/****** Columns: ID, Name, Target_Table, Target_ID_Column, Target_State_Column ******/
+INSERT INTO [T_Event_Target] VALUES (0,'(none)','(none)','(none)','(none)')
+INSERT INTO [T_Event_Target] VALUES (1,'Campaign','T_Campaign','Campaign_ID','(none)')
+INSERT INTO [T_Event_Target] VALUES (2,'Cell Culture','T_Cell_Culture','CC_ID','(none)')
+INSERT INTO [T_Event_Target] VALUES (3,'Experiment','T_Experiments','Exp_ID','(none)')
+INSERT INTO [T_Event_Target] VALUES (4,'Dataset','T_Dataset','Dataset_ID','DS_state_ID')
+INSERT INTO [T_Event_Target] VALUES (5,'Analysis Job','T_Analysis_Job','AJ_jobID','AJ_StateID')
+INSERT INTO [T_Event_Target] VALUES (6,'Archive','T_Dataset_Archive','AS_Dataset_ID','AS_state_ID')
+INSERT INTO [T_Event_Target] VALUES (7,'Archive Update','T_Dataset_Archive','AS_Dataset_ID','AS_update_state_ID')
