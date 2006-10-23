5771cee1ff462ab5d9221b2fc5345f5e9ab86e8d
Gary Kiebel <grkiebel@pnnl.gov>
2006-10-23 16:32:31 +0000

    Changes for ticket #227

--- DMS5/Trunk/V_Requested_Run_Batch_Detail_Report.sql
+++ DMS5/Trunk/V_Requested_Run_Batch_Detail_Report.sql
@@ -3,20 +3,12 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE  VIEW dbo.V_Requested_Run_Batch_Detail_Report
+CREATE VIEW dbo.V_Requested_Run_Batch_Detail_Report
 AS
-SELECT R.ID, R.Batch AS Name, R.Description, 
-       dbo.GetBatchRequestedRunList(R.ID) AS Requests, U.U_Name + ' (' + U.U_PRN + ')' AS Owner, 
-       R.Created, R.Locked, 
-       R.Last_Ordered AS [Last Ordered],
-       R.Requested_Batch_Priority AS [Requested Batch Priority],
-       R.Actual_Batch_Priority AS [Actual Batch Priority],
-       R.Requested_Completion_Date AS [Requested Completion Date],
-       R.Justification_for_High_Priority AS [Justification for High Priority],
-       R.Comment AS [Comment]
-FROM T_Requested_Run_Batches R 
-     JOIN T_Users U ON R.Owner = U.ID
-
+SELECT     R.ID, R.Batch AS Name, R.Description, dbo.GetBatchRequestedRunList(R.ID) AS Requests, U.U_Name + ' (' + U.U_PRN + ')' AS Owner, R.Created, 
+                      R.Locked, R.Last_Ordered AS [Last Ordered], R.Requested_Batch_Priority AS [Requested Batch Priority], 
+                      R.Requested_Completion_Date AS [Requested Completion Date], R.Justification_for_High_Priority AS [Justification for High Priority], R.Comment
+FROM         dbo.T_Requested_Run_Batches R INNER JOIN
+                      dbo.T_Users U ON R.Owner = U.ID
 
 GO
--- DMS5/Trunk/V_Requested_Run_Batch_List_Report.sql
+++ DMS5/Trunk/V_Requested_Run_Batch_List_Report.sql
@@ -6,8 +6,7 @@ GO
 CREATE VIEW dbo.V_Requested_Run_Batch_List_Report
 AS
 SELECT     dbo.T_Requested_Run_Batches.ID, dbo.T_Requested_Run_Batches.Batch AS Name, T.Requests, H.Runs, 
-                      dbo.T_Requested_Run_Batches.Requested_Batch_Priority AS [Requested Priority], 
-                      dbo.T_Requested_Run_Batches.Actual_Batch_Priority AS [Actual Priority], dbo.T_Requested_Run_Batches.Description, 
+                      dbo.T_Requested_Run_Batches.Requested_Batch_Priority AS [Requested Priority], dbo.T_Requested_Run_Batches.Description, 
                       dbo.T_Users.U_Name AS Owner, dbo.T_Requested_Run_Batches.Created, dbo.T_Requested_Run_Batches.Locked, 
                       dbo.T_Requested_Run_Batches.Justification_for_High_Priority, dbo.T_Requested_Run_Batches.Comment
 FROM         dbo.T_Requested_Run_Batches LEFT OUTER JOIN
