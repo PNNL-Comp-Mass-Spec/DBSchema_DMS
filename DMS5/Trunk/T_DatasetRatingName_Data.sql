9aa4e67519b1ddba6de820cbce13bd18ea388def
Matthew Monroe <matthew.monroe@pnnl.gov>
2006-10-31 19:42:34 +0000

    Changes for ticket #306

--- DMS5/Trunk/T_DatasetRatingName_Data.sql
+++ DMS5/Trunk/T_DatasetRatingName_Data.sql
@@ -1,7 +1,8 @@
 /****** Object:  Table [T_DatasetRatingName] ******/
-/****** RowCount: 4 ******/
+/****** RowCount: 5 ******/
 /****** Columns: DRN_state_ID, DRN_name ******/
-INSERT INTO [T_DatasetRatingName] VALUES (-1,'No Data')
+INSERT INTO [T_DatasetRatingName] VALUES (-2,'Data Files Missing')
+INSERT INTO [T_DatasetRatingName] VALUES (-1,'No Data (Blank/Bad)')
 INSERT INTO [T_DatasetRatingName] VALUES (1,'No Interest')
 INSERT INTO [T_DatasetRatingName] VALUES (2,'Unknown')
 INSERT INTO [T_DatasetRatingName] VALUES (3,'Interest')
