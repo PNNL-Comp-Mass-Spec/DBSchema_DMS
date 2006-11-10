3fc92d38b42fefd3773e44f53c5e1aae203ece8c
Matthew Monroe <matthew.monroe@pnnl.gov>
2006-11-10 19:19:55 +0000

    Changes for ticket #310

--- DMS5/Trunk/V_Run_Planning_Report.sql
+++ DMS5/Trunk/V_Run_Planning_Report.sql
@@ -4,16 +4,27 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-create VIEW dbo.V_Run_Planning_Report
+CREATE VIEW dbo.V_Run_Planning_Report
 AS
-SELECT     TOP 100 PERCENT RA.Instrument, MIN(RA.Request) AS [Min Request], RA.[Work Package], RA.Proposal, RA.Requester, 
-                      CASE WHEN RA.Batch = 0 THEN LEFT(RA.Experiment, 10) + CASE WHEN LEN(RA.Experiment) 
-                      > 10 THEN '...' ELSE '' END ELSE RRB.Batch END AS [Batch/Experiment], MIN(CONVERT(datetime, FLOOR(CONVERT(float, RA.Created)))) 
-                      AS [Date  Created], RA.Comment, COUNT(RA.Name) AS [Run Count]
-FROM         dbo.V_Run_Assignment RA INNER JOIN
-                      dbo.T_Requested_Run_Batches RRB ON RA.Batch = RRB.ID
-GROUP BY RA.[Work Package], RA.Proposal, RA.Requester, RA.Comment, RA.Instrument, RRB.Batch, CASE WHEN RA.Batch = 0 THEN LEFT(RA.Experiment, 10) 
-                      + CASE WHEN len(RA.Experiment) > 10 THEN '...' ELSE '' END ELSE RRB.Batch END
+SELECT TOP 100 PERCENT 
+    RA.Instrument, 
+    MIN(RA.Request) AS [Min Request], 
+    RA.[Work Package], 
+    RA.Proposal, 
+    RA.Requester, 
+    CASE WHEN RA.Batch = 0 OR LEFT(RRB.Batch, 4) <> LEFT(RA.Experiment, 4)
+    THEN LEFT(RA.Experiment, 10) + CASE WHEN LEN(RA.Experiment) > 10 THEN '...' ELSE '' END 
+    ELSE RRB.Batch 
+    END AS [Batch/Experiment],
+    MIN(CONVERT(datetime, FLOOR(CONVERT(float, RA.Created)))) AS [Date Created], 
+    RA.Comment, 
+    COUNT(RA.Name) AS [Run Count]
+FROM dbo.V_Run_Assignment RA INNER JOIN
+     dbo.T_Requested_Run_Batches RRB ON RA.Batch = RRB.ID
+GROUP BY RA.[Work Package], RA.Proposal, RA.Requester, 
+    RA.Comment, RA.Instrument, RRB.Batch, 
+    CASE WHEN RA.Batch = 0 OR LEFT(RRB.Batch,4) <> LEFT(RA.Experiment,4) 
+    THEN LEFT(RA.Experiment, 10) + CASE WHEN LEN(RA.Experiment) > 10 THEN '...' ELSE '' END ELSE RRB.Batch END
 ORDER BY RA.Instrument, MIN(RA.Request)
 
 GO
