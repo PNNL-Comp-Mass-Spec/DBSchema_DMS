8b22d8cac8c64fa9239279d59ab39bfd4dbcef97
Matthew Monroe <matthew.monroe@pnnl.gov>
2006-11-14 18:18:15 +0000

    Changes for ticket #325

--- DMS5/Trunk/V_Find_User_Proposal_Dataset.sql
+++ DMS5/Trunk/V_Find_User_Proposal_Dataset.sql
@@ -5,11 +5,17 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE view V_Find_User_Proposal_Dataset
 AS
-SELECT     T_Requested_Run_History.RDS_EUS_Proposal_ID AS User_Proposal_ID, T_Dataset.Dataset_Num AS Dataset_Name, T_Dataset.Dataset_ID, 
-                      T_Instrument_Name.IN_name AS Instrument_Name, T_Dataset.Acq_Time_Start AS Acquisition_Start, 
-                      T_Dataset.Acq_Time_End AS Acquisition_End
-FROM         T_Dataset INNER JOIN
-                      T_Requested_Run_History ON T_Dataset.Dataset_ID = T_Requested_Run_History.DatasetID INNER JOIN
-                      T_Instrument_Name ON T_Dataset.DS_instrument_name_ID = T_Instrument_Name.Instrument_ID
-
+SELECT dbo.T_Requested_Run_History.RDS_EUS_Proposal_ID AS User_Proposal_ID,
+     dbo.T_Dataset.Dataset_Num AS Dataset_Name, 
+    dbo.T_Dataset.Dataset_ID, 
+    dbo.T_Instrument_Name.IN_name AS Instrument_Name, 
+    dbo.T_Dataset.Acq_Time_Start AS Acquisition_Start, 
+    dbo.T_Dataset.Acq_Time_End AS Acquisition_End, 
+    dbo.T_Dataset.Scan_Count
+FROM dbo.T_Dataset INNER JOIN
+    dbo.T_Requested_Run_History ON 
+    dbo.T_Dataset.Dataset_ID = dbo.T_Requested_Run_History.DatasetID
+     INNER JOIN
+    dbo.T_Instrument_Name ON 
+    dbo.T_Dataset.DS_instrument_name_ID = dbo.T_Instrument_Name.Instrument_ID
 GO
