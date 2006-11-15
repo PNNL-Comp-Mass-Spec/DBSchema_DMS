a1cde041250dcde135f5804e4e9d0133dcf4e67c
Matthew Monroe <matthew.monroe@pnnl.gov>
2006-11-15 01:50:33 +0000

    Changes for ticket #327

--- DMS5/Trunk/V_Analysis_Job_Duration_Precise.sql
+++ DMS5/Trunk/V_Analysis_Job_Duration_Precise.sql
@@ -5,17 +5,18 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW V_Analysis_Job_Duration_Precise
 AS
-SELECT StartQ.Job, T_Analysis_Job.AJ_StateID AS Job_State, 
-    DATEDIFF(second, StartQ.Entered, EndQ.Entered)  / 60.0 AS Job_Length_Minutes
+SELECT StartQ.Job, StartQ.Entered AS Job_Start, EndQ.Entered AS Job_Finish,
+       DATEDIFF(second, StartQ.Entered, EndQ.Entered) / 60.0 AS Job_Length_Minutes
 FROM (SELECT Target_ID AS Job, MAX(entered) AS Entered
       FROM T_Event_Log
       WHERE (Target_type = 5) AND (Target_State = 2)
       GROUP BY Target_ID
-     ) StartQ INNER JOIN T_Analysis_Job ON 
-     StartQ.Job = T_Analysis_Job.AJ_jobID LEFT OUTER JOIN
+     ) StartQ LEFT OUTER JOIN
      (SELECT Target_ID AS Job, MAX(entered) AS Entered
       FROM T_Event_Log
-      WHERE (Target_type = 5) AND (Prev_Target_State = 2) AND Target_state NOT IN (1, 2, 8)
+      WHERE (Target_type = 5) AND (Prev_Target_State = 2) AND 
+            Target_state NOT IN (1, 2, 8)
       GROUP BY Target_ID
      ) EndQ ON StartQ.Job = EndQ.Job AND StartQ.Entered < EndQ.Entered
+
 GO
