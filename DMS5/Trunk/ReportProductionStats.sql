ada937300d007f42275e96799f70b19f166dcb53
Gary Kiebel <grkiebel@pnnl.gov>
2006-12-19 17:24:14 +0000

    Changes for ticket #335.

--- DMS5/Trunk/ReportProductionStats.sql
+++ DMS5/Trunk/ReportProductionStats.sql
@@ -12,7 +12,8 @@ CREATE PROCEDURE ReportProductionStats
 **
 **		Auth: grk
 **		Date: 2/25/2005
-**             3/1/2005 grk added column for instrument name at end
+**             3/1/2005   grk added column for instrument name at end
+**             12/19/2005 added "MD" and "TS" prefixes (ticket #345)
 **    
 *****************************************************/
 	@startDate varchar(24),
@@ -40,6 +41,8 @@ AS
 		convert(decimal(5,1), [Total]/@daysInRange) as [Datasets per day],
 		[Blank] as [Blank Datasets],
 		[QC] as [QC Datasets],
+		[MD] as [Method Dev.],
+		[TS] as [Troubleshooting],
 		[Bad] as [Bad Datasets],
 		convert(decimal(5,1), ([QC] * 100.0/[Total])) as [% QC Datasets],
 		convert(decimal(5,1), ([Bad] * 100.0/[Total])) as [% Bad Datasets],
@@ -53,7 +56,9 @@ AS
 		COUNT(*) AS [Total],
 		SUM(CASE WHEN T_Dataset.Dataset_Num LIKE 'Blank%' THEN 1 ELSE 0 END) AS [Blank],
 		SUM(CASE WHEN T_Dataset.Dataset_Num LIKE 'Bad%' THEN 1 ELSE 0 END) AS [Bad],
-		SUM(CASE WHEN T_Dataset.Dataset_Num LIKE 'QC%' THEN 1 ELSE 0 END) AS [QC]
+		SUM(CASE WHEN T_Dataset.Dataset_Num LIKE 'QC%' THEN 1 ELSE 0 END) AS [QC],
+		SUM(CASE WHEN T_Dataset.Dataset_Num LIKE 'MD%' THEN 1 ELSE 0 END) AS [MD],
+		SUM(CASE WHEN T_Dataset.Dataset_Num LIKE 'TS%' THEN 1 ELSE 0 END) AS [TS]
 		FROM
 			T_Dataset INNER JOIN
 			T_Instrument_Name ON T_Dataset.DS_instrument_name_ID = T_Instrument_Name.Instrument_ID
