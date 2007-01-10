0e75173069322fbb04f9997ecbbeb30be1d6f643
Gary Kiebel <grkiebel@pnnl.gov>
2007-01-10 20:44:50 +0000

    Database changes for ticket #357

--- DMS5/Trunk/V_Organism_List_Report.sql
+++ DMS5/Trunk/V_Organism_List_Report.sql
@@ -3,12 +3,10 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW dbo.V_Organism_List_Report
 AS
-SELECT TOP 100 PERCENT Organism_ID AS ID, 
-    OG_name AS Name, OG_description AS Description, 
-    OG_created AS Created, OG_Active AS Active
-FROM dbo.T_Organisms
+SELECT     TOP 100 PERCENT Organism_ID AS ID, OG_name AS Name, OG_Genus AS Genus, OG_Species AS Species, OG_Strain AS Strain, 
+                      OG_description AS Description, OG_created AS Created, OG_Active AS Active
+FROM         dbo.T_Organisms
 
 GO
