fed6d438cd609d860965ea36a8664d4a455221a0
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-03-15 18:47:38 +0000

    Changes for ticket #406

---
+++ DMS5/Trunk/V_Dataset_Archive_Path.sql
@@ -0,0 +1,18 @@
+/****** Object:  View [dbo].[V_Dataset_Archive_Path] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW dbo.V_Dataset_Archive_Path
+AS
+SELECT dbo.T_Dataset_Archive.AS_Dataset_ID AS Dataset_ID, 
+    ISNULL(REPLACE(REPLACE(dbo.T_Archive_Path.AP_archive_path,
+     '/nwfs/dmsarch/', '\\n2.emsl.pnl.gov\dmsarch\'), '/', '\'), 
+    'unassigned') AS Archive_Path
+FROM dbo.T_Archive_Path INNER JOIN
+    dbo.T_Dataset_Archive ON 
+    dbo.T_Archive_Path.AP_path_ID = dbo.T_Dataset_Archive.AS_storage_path_ID
+
+
+GO
--- DMS5/Trunk/V_Dataset_Folder_Paths.sql
+++ DMS5/Trunk/V_Dataset_Folder_Paths.sql
@@ -3,15 +3,21 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE VIEW dbo.V_Dataset_Folder_Paths
 AS
-SELECT     dbo.T_Dataset.Dataset_Num AS Dataset, dbo.T_Dataset.Dataset_ID, 
-                      dbo.t_storage_path.SP_vol_name_client + dbo.t_storage_path.SP_path + dbo.T_Dataset.Dataset_Num AS Dataset_Folder_Path, 
-                      REPLACE(REPLACE(dbo.T_Archive_Path.AP_archive_path, '/nwfs/dmsarch/', '\\n2.emsl.pnl.gov\dmsarch\'), '/', '\') 
-                      + '\' + dbo.T_Dataset.Dataset_Num AS Archive_Folder_Path
-FROM         dbo.T_Dataset INNER JOIN
-                      dbo.t_storage_path ON dbo.T_Dataset.DS_storage_path_ID = dbo.t_storage_path.SP_path_ID LEFT OUTER JOIN
-                      dbo.T_Dataset_Archive ON dbo.T_Dataset.Dataset_ID = dbo.T_Dataset_Archive.AS_Dataset_ID LEFT OUTER JOIN
-                      dbo.T_Archive_Path ON dbo.T_Dataset_Archive.AS_storage_path_ID = dbo.T_Archive_Path.AP_path_ID
+SELECT dbo.T_Dataset.Dataset_Num AS Dataset, 
+    dbo.T_Dataset.Dataset_ID, 
+    ISNULL(dbo.t_storage_path.SP_vol_name_client + dbo.t_storage_path.SP_path
+     + dbo.T_Dataset.Dataset_Num, '') AS Dataset_Folder_Path, 
+    ISNULL(dbo.V_Dataset_Archive_Path.Archive_Path + '\' + dbo.T_Dataset.Dataset_Num,
+     '') AS Archive_Folder_Path
+FROM dbo.T_Dataset INNER JOIN
+    dbo.t_storage_path ON 
+    dbo.T_Dataset.DS_storage_path_ID = dbo.t_storage_path.SP_path_ID
+     LEFT OUTER JOIN
+    dbo.V_Dataset_Archive_Path ON 
+    dbo.T_Dataset.Dataset_ID = dbo.V_Dataset_Archive_Path.Dataset_ID
+
 
 GO
