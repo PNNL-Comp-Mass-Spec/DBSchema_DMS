5994070a4897744ba731dee8662b50f2d9a27711
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-03-16 20:54:19 +0000

    Changes for ticket #409

--- DMS5/Trunk/V_Analysis_Job_and_Dataset_Archive_State.sql
+++ DMS5/Trunk/V_Analysis_Job_and_Dataset_Archive_State.sql
@@ -5,14 +5,13 @@ SET QUOTED_IDENTIFIER ON
 GO
 create VIEW V_Analysis_Job_and_Dataset_Archive_State
 AS
-SELECT AJ.AJ_jobID AS Job, CASE WHEN AJ.AJ_StateID = 1 AND 
-    DA.AS_state_ID IN (4, 7) 
-    THEN ASN.AJS_name + ' (Dataset Purged)' WHEN AJ.AJ_StateID
-     = 1 AND 
-    DA.AS_state_ID < 3 THEN ASN.AJS_name + ' (Dataset Not Archived)'
-     WHEN AJ.AJ_StateID = 1 AND 
-    DA.AS_state_ID > 3 THEN ASN.AJS_name + ' (Dataset ' + DASN.DASN_StateName
-     + ')' ELSE ASN.AJS_name END AS Job_State, 
+SELECT AJ.AJ_jobID AS Job, 
+	CASE WHEN AJ.AJ_StateID = 1 AND DA.AS_update_state_ID = 3 THEN ASN.AJS_name + ' (Archive Update in Progress)'
+	WHEN AJ.AJ_StateID = 1 AND DA.AS_state_ID IN (3, 4, 10) THEN ASN.AJS_name
+	WHEN AJ.AJ_StateID = 1 AND DA.AS_state_ID < 3 THEN ASN.AJS_name + ' (Dataset Not Archived)'
+    WHEN AJ.AJ_StateID = 1 AND DA.AS_state_ID > 3 THEN ASN.AJS_name + ' (Dataset ' + DASN.DASN_StateName + ')' 
+	ELSE ASN.AJS_name 
+	END AS Job_State, 
     DASN.DASN_StateName AS Dataset_Archive_State
 FROM dbo.T_Analysis_Job AJ INNER JOIN
     dbo.T_Analysis_State_Name ASN ON 
