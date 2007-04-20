b2f8a553e9d79682b709402b6fe1ce6c587838a4
Gary Kiebel <grkiebel@pnnl.gov>
2007-04-20 21:46:53 +0000

    Changes for Ticket #442

--- DMS5/Trunk/V_Experiment_Detail_Report_Ex.sql
+++ DMS5/Trunk/V_Experiment_Detail_Report_Ex.sql
@@ -9,10 +9,10 @@ SELECT     dbo.T_Experiments.Experiment_Num AS Experiment, dbo.T_Users.U_Name + 
                       dbo.T_Organisms.OG_name AS Organism, dbo.T_Experiments.EX_reason AS [Reason for Experiment], dbo.T_Experiments.EX_comment AS Comment, 
                       dbo.T_Experiments.EX_created AS Created, dbo.T_Experiments.EX_sample_concentration AS [Sample Concentration], 
                       dbo.T_Enzymes.Enzyme_Name AS [Digestion Enzyme], dbo.T_Experiments.EX_lab_notebook_ref AS [Lab Notebook], 
-                      dbo.T_Campaign.Campaign_Num AS Campaign, dbo.T_Experiments.Exp_ID AS ID, dbo.T_Experiments.EX_cell_culture_list AS [Cell Cultures], 
+                      dbo.T_Campaign.Campaign_Num AS Campaign, dbo.T_Experiments.EX_cell_culture_list AS [Cell Cultures], 
                       dbo.T_Experiments.EX_Labelling AS Labelling, dbo.T_Internal_Standards.Name AS [Predigest Int Std], 
-                      T_Internal_Standards_1.Name AS [Postdigest Int Std], M.Group_ID AS [Fraction Group], A.Datasets, 
-                      dbo.T_Experiments.EX_sample_prep_request_ID AS Request
+                      T_Internal_Standards_1.Name AS [Postdigest Int Std], dbo.T_Experiments.EX_sample_prep_request_ID AS Request, 
+                      m.Group_Id AS [Experiment Group], a.DataSets, dbo.T_Experiments.Exp_ID AS ID
 FROM         dbo.T_Experiments INNER JOIN
                       dbo.T_Campaign ON dbo.T_Experiments.EX_campaign_ID = dbo.T_Campaign.Campaign_ID INNER JOIN
                       dbo.T_Users ON dbo.T_Experiments.EX_researcher_PRN = dbo.T_Users.U_PRN INNER JOIN
@@ -20,13 +20,13 @@ FROM         dbo.T_Experiments INNER JOIN
                       dbo.T_Internal_Standards ON dbo.T_Experiments.EX_internal_standard_ID = dbo.T_Internal_Standards.Internal_Std_Mix_ID INNER JOIN
                       dbo.T_Internal_Standards T_Internal_Standards_1 ON 
                       dbo.T_Experiments.EX_postdigest_internal_std_ID = T_Internal_Standards_1.Internal_Std_Mix_ID INNER JOIN
-                      dbo.T_Organisms ON dbo.T_Experiments.Ex_organism_ID = dbo.T_Organisms.Organism_ID LEFT OUTER JOIN
-                          (SELECT     T_Experiment_Group_Members.Exp_ID, T_Experiment_Group_Members.Group_ID
-                            FROM          T_Experiment_Group_Members INNER JOIN
-                                                   T_Experiment_Groups ON T_Experiment_Group_Members.Group_ID = T_Experiment_Groups.Group_ID
-                            WHERE      (T_Experiment_Groups.EG_Group_Type = 'Fraction')) M ON M.Exp_ID = dbo.T_Experiments.Exp_ID LEFT OUTER JOIN
-                          (SELECT     COUNT(*) AS Datasets, Exp_ID
-                            FROM          T_Dataset
-                            GROUP BY Exp_ID) A ON A.Exp_ID = dbo.T_Experiments.Exp_ID
+                      dbo.T_Organisms ON dbo.T_Experiments.EX_organism_ID = dbo.T_Organisms.Organism_ID LEFT OUTER JOIN
+                          (SELECT     T_ExperimenT_Group_Members.exp_Id, T_ExperimenT_Group_Members.Group_Id
+                            FROM          T_ExperimenT_Group_Members INNER JOIN
+                                                   T_ExperimenT_Groups ON T_ExperimenT_Group_Members.Group_Id = T_ExperimenT_Groups.Group_Id) m ON 
+                      m.exp_Id = dbo.T_Experiments.Exp_ID LEFT OUTER JOIN
+                          (SELECT     COUNT(*) AS DataSets, exp_Id
+                            FROM          T_DataSet
+                            GROUP BY exp_Id) a ON a.exp_Id = dbo.T_Experiments.Exp_ID
 
 GO
