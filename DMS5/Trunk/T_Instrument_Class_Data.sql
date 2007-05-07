5f8a490b5f01028b251aaf317beda90554bbba97
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-05-07 20:22:18 +0000

    Table data updates

--- DMS5/Trunk/T_Instrument_Class_Data.sql
+++ DMS5/Trunk/T_Instrument_Class_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_Instrument_Class] ******/
-/****** RowCount: 11 ******/
+/****** RowCount: 12 ******/
 /****** Columns: IN_class, is_purgable, raw_data_type, requires_preparation ******/
 INSERT INTO [T_Instrument_Class] VALUES ('Agilent_Ion_Trap',0,'dot_d_folders',0)
 INSERT INTO [T_Instrument_Class] VALUES ('Agilent_TOF',0,'dot_wiff_files',0)
@@ -12,3 +12,4 @@ INSERT INTO [T_Instrument_Class] VALUES ('IMS_Sciex_TOF',0,'dot_wiff_files',0)
 INSERT INTO [T_Instrument_Class] VALUES ('LTQ_FT',1,'dot_raw_files',0)
 INSERT INTO [T_Instrument_Class] VALUES ('Micromass_QTOF',0,'dot_raw_folder',0)
 INSERT INTO [T_Instrument_Class] VALUES ('QStar_QTOF',0,'dot_wiff_files',0)
+INSERT INTO [T_Instrument_Class] VALUES ('Triple_Quad',1,'dot_raw_files',0)
