5413cc8d2d9c985f0035f7d1f276a956d3c5abb7
Gary Kiebel <grkiebel@pnnl.gov>
2007-05-30 22:32:12 +0000

    Changes for Ticket #226

--- DMS5/Trunk/V_Experiment_Groups_Detail_Report.sql
+++ DMS5/Trunk/V_Experiment_Groups_Detail_Report.sql
@@ -3,9 +3,9 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW V_Experiment_Groups_Detail_Report
+CREATE VIEW dbo.V_Experiment_Groups_Detail_Report
 AS
-SELECT     TOP 100 PERCENT dbo.T_Experiment_Groups.Group_ID AS ID, dbo.T_Experiment_Groups.EG_Group_Type AS Group_Type, 
+SELECT     dbo.T_Experiment_Groups.Group_ID AS ID, dbo.T_Experiment_Groups.EG_Group_Type AS Group_Type, 
                       dbo.T_Experiments.Experiment_Num AS Parent_Experiment, COUNT(dbo.T_Experiment_Group_Members.Exp_ID) AS Members, 
                       dbo.T_Experiment_Groups.EG_Description AS Description, dbo.T_Experiment_Groups.EG_Created AS Created
 FROM         dbo.T_Experiment_Groups LEFT OUTER JOIN
