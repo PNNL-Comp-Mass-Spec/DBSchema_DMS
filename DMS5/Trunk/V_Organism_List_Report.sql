d6a7e5997a9c3ebb9d3debd0a47efaf0ac2a997a
Gary Kiebel <grkiebel@pnnl.gov>
2007-05-31 16:35:23 +0000

    Changes for Ticket #226

--- DMS5/Trunk/V_Organism_List_Report.sql
+++ DMS5/Trunk/V_Organism_List_Report.sql
@@ -5,8 +5,8 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW dbo.V_Organism_List_Report
 AS
-SELECT     TOP 100 PERCENT Organism_ID AS ID, OG_name AS Name, OG_Genus AS Genus, OG_Species AS Species, OG_Strain AS Strain, 
-                      OG_description AS Description, OG_created AS Created, OG_Active AS Active
+SELECT     Organism_ID AS ID, OG_name AS Name, OG_Genus AS Genus, OG_Species AS Species, OG_Strain AS Strain, OG_description AS Description, 
+                      OG_created AS Created, OG_Active AS Active
 FROM         dbo.T_Organisms
 
 GO
--- DMS5/Trunk/V_Protein_Collection_Name.sql
+++ DMS5/Trunk/V_Protein_Collection_Name.sql
@@ -3,22 +3,13 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW dbo.V_Protein_Collection_Name
 AS
-SELECT TOP 100 PERCENT Name, Type, Description, Entries, 
-    [Organism Name], ID
-FROM (SELECT Name, Type, Description, Entries, 
-          CASE WHEN Type = 'Internal_Standard' OR
-          Type = 'contaminant' THEN '' ELSE Organism_Name END
-           AS [Organism Name], ID, 
-          CASE WHEN type = 'internal_standard' THEN 1 WHEN type
-           = 'contaminant' THEN 2 ELSE 0 END AS TypeSortOrder
-      FROM Protein_Sequences.dbo.V_Collection_Picker) 
-    LookupQ
-GROUP BY Name, Type, Description, Entries, [Organism Name], ID, 
-    TypeSortOrder
-ORDER BY TypeSortOrder, Name
-
+SELECT     Name, Type, Description, Entries, [Organism Name], ID
+FROM         (SELECT     Name, Type, Description, Entries, CASE WHEN Type = 'Internal_Standard' OR
+                                              Type = 'contaminant' THEN '' ELSE Organism_Name END AS [Organism Name], ID, 
+                                              CASE WHEN type = 'internal_standard' THEN 1 WHEN type = 'contaminant' THEN 2 ELSE 0 END AS TypeSortOrder
+                       FROM          Protein_Sequences.dbo.V_Collection_Picker) AS LookupQ
+GROUP BY Name, Type, Description, Entries, [Organism Name], ID, TypeSortOrder
 
 GO
