19684ef78fe21cdd0ecf9ccfed91f79f1f6bc28c
Gary Kiebel <grkiebel@pnnl.gov>
2007-05-31 16:07:43 +0000

    Changes for Ticket #226

--- DMS5/Trunk/V_Predefined_Analysis_Disabled_List_Report.sql
+++ DMS5/Trunk/V_Predefined_Analysis_Disabled_List_Report.sql
@@ -3,33 +3,17 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW dbo.V_Predefined_Analysis_Disabled_List_Report
 AS
-SELECT TOP 100 PERCENT PA.AD_ID AS ID, 
-    PA.AD_instrumentClassCriteria AS [Instrument Class], 
-    PA.AD_level AS [Level], PA.AD_sequence AS [Seq.], 
-    PA.AD_nextLevel AS [Next Lvl.], 
-    PA.AD_analysisToolName AS [Analysis Tool], 
-    PA.AD_instrumentNameCriteria AS [Instrument Crit.], 
-    PA.AD_organismNameCriteria AS [Organism Crit.], 
-    PA.AD_campaignNameCriteria AS [Campaign Crit.], 
-    PA.AD_experimentNameCriteria AS [Experiment Crit.], 
-    PA.AD_datasetNameCriteria AS [DatasetCrit.], 
-    PA.AD_expCommentCriteria AS [ExpCommentCrit.], 
-    PA.AD_labellingInclCriteria AS [ExpLabelingCrit.], 
-    PA.AD_parmFileName AS [Parm File], 
-    PA.AD_settingsFileName AS [Settings File], 
-    Org.OG_name AS Organism, 
-    PA.AD_organismDBName AS [Organism DB], 
-    PA.AD_proteinCollectionList AS [Protein Collection List], 
-    PA.AD_proteinOptionsList AS [Protein Options List], 
-    PA.AD_priority AS priority
-FROM T_Predefined_Analysis PA INNER JOIN
-     T_Organisms Org ON PA.AD_organism_ID = Org.Organism_ID
-WHERE (PA.AD_enabled = 0)
-ORDER BY PA.AD_instrumentClassCriteria, PA.AD_level, 
-    PA.AD_sequence, PA.AD_ID
-
+SELECT     PA.AD_ID AS ID, PA.AD_instrumentClassCriteria AS [Instrument Class], PA.AD_level AS [Level], PA.AD_sequence AS [Seq.], 
+                      PA.AD_nextLevel AS [Next Lvl.], PA.AD_analysisToolName AS [Analysis Tool], PA.AD_instrumentNameCriteria AS [Instrument Crit.], 
+                      PA.AD_organismNameCriteria AS [Organism Crit.], PA.AD_campaignNameCriteria AS [Campaign Crit.], 
+                      PA.AD_experimentNameCriteria AS [Experiment Crit.], PA.AD_datasetNameCriteria AS [DatasetCrit.], PA.AD_expCommentCriteria AS [ExpCommentCrit.], 
+                      PA.AD_labellingInclCriteria AS [ExpLabelingCrit.], PA.AD_parmFileName AS [Parm File], PA.AD_settingsFileName AS [Settings File], 
+                      Org.OG_name AS Organism, PA.AD_organismDBName AS [Organism DB], PA.AD_proteinCollectionList AS [Protein Collection List], 
+                      PA.AD_proteinOptionsList AS [Protein Options List], PA.AD_priority AS priority
+FROM         dbo.T_Predefined_Analysis AS PA INNER JOIN
+                      dbo.T_Organisms AS Org ON PA.AD_organism_ID = Org.Organism_ID
+WHERE     (PA.AD_enabled = 0)
 
 GO
--- DMS5/Trunk/V_Predefined_Analysis_Scheduling_Rules_List_Report.sql
+++ DMS5/Trunk/V_Predefined_Analysis_Scheduling_Rules_List_Report.sql
@@ -3,22 +3,12 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW dbo.V_Predefined_Analysis_Scheduling_Rules_List_Report
 AS
-SELECT TOP 100 PERCENT PASR.ID, 
-    PASR.SR_evaluationOrder AS [Evaluation Order], 
-    PASR.SR_instrumentClass AS [Instrument Class], 
-    PASR.SR_instrument_Name AS Instrument, 
-    PASR.SR_dataset_Name AS Dataset, 
-    PASR.SR_analysisToolName AS [Analysis Tool], 
-    PASR.SR_priority AS Priority, ISNULL(AJPG.Group_Name, '') 
-    AS [Processor Group]
-FROM dbo.T_Predefined_Analysis_Scheduling_Rules PASR LEFT OUTER
-     JOIN
-    dbo.T_Analysis_Job_Processor_Group AJPG ON 
-    PASR.SR_processorGroupID = AJPG.ID
-ORDER BY PASR.SR_evaluationOrder
-
+SELECT     PASR.ID, PASR.SR_evaluationOrder AS [Evaluation Order], PASR.SR_instrumentClass AS [Instrument Class], 
+                      PASR.SR_instrument_Name AS Instrument, PASR.SR_dataset_Name AS Dataset, PASR.SR_analysisToolName AS [Analysis Tool], 
+                      PASR.SR_priority AS Priority, ISNULL(AJPG.Group_Name, '') AS [Processor Group]
+FROM         dbo.T_Predefined_Analysis_Scheduling_Rules AS PASR LEFT OUTER JOIN
+                      dbo.T_Analysis_Job_Processor_Group AS AJPG ON PASR.SR_processorGroupID = AJPG.ID
 
 GO
