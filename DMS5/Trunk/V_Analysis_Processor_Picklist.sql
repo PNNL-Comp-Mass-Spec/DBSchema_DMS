6aee492037818fd7a571f795fc4a370f839f6098
Gary Kiebel <grkiebel@pnnl.gov>
2007-05-31 18:44:06 +0000

    Changes for Ticket #226

--- DMS5/Trunk/V_Analysis_Processor_Picklist.sql
+++ DMS5/Trunk/V_Analysis_Processor_Picklist.sql
@@ -5,9 +5,8 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW dbo.V_Analysis_Processor_Picklist
 AS
-SELECT     TOP 100 PERCENT Processor_Name AS val, '' AS ex
+SELECT     Processor_Name AS val, '' AS ex
 FROM         dbo.T_Analysis_Job_Processors
 WHERE     (State = 'E')
-ORDER BY val
 
 GO
--- DMS5/Trunk/V_Wellplate_Picklist.sql
+++ DMS5/Trunk/V_Wellplate_Picklist.sql
@@ -3,14 +3,9 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW dbo.V_Wellplate_Picklist
 AS
-SELECT     TOP 100 PERCENT [Well Plate] + ' ' + Cast(ISNULL(Description, '') as Char(24)) AS val, [Well Plate] AS ex
+SELECT     [Well Plate] + ' ' + CAST(ISNULL(Description, '') AS Char(24)) AS val, [Well Plate] AS ex
 FROM         dbo.V_Run_Assignment_Wellplate_List_Report
-ORDER BY [Well Plate]
-
-
-
 
 GO
