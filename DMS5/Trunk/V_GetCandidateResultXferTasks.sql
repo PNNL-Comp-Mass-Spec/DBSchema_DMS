e1de843907ed66c462f62e1f983fb7d24250d2be
Gary Kiebel <grkiebel@pnnl.gov>
2007-06-22 21:17:04 +0000

    Changes for Ticket #493.

---
+++ DMS5/Trunk/V_GetCandidateResultXferTasks.sql
@@ -0,0 +1,14 @@
+/****** Object:  View [dbo].[V_GetCandidateResultXferTasks] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW dbo.V_GetCandidateResultXferTasks
+AS
+SELECT     AJ.AJ_jobID AS JobID, AJ.AJ_StateID AS JobStateID, StoragePath.SP_machine_name AS ServerName, AJ.AJ_Last_Affected AS Last_Affected
+FROM         dbo.T_Analysis_Job AS AJ INNER JOIN
+                      dbo.T_Dataset AS DS ON AJ.AJ_datasetID = DS.Dataset_ID INNER JOIN
+                      dbo.t_storage_path AS StoragePath ON DS.DS_storage_path_ID = StoragePath.SP_path_ID
+WHERE     (AJ.AJ_StateID = 3)
+
+GO
