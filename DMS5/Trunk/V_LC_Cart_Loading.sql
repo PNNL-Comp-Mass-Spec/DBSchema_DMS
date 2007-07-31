7f451b38a9e7c3cf5bfcb221a1284d2d1d769c7e
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-07-31 20:29:32 +0000

    Changes for ticket #505

--- DMS5/Trunk/V_LC_Cart_Loading.sql
+++ DMS5/Trunk/V_LC_Cart_Loading.sql
@@ -5,14 +5,15 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW dbo.V_LC_Cart_Loading
 AS
-SELECT     dbo.T_Requested_Run.ID AS Request, '' AS [Sel.], dbo.T_Requested_Run.RDS_Name AS Name, dbo.T_LC_Cart.Cart_Name AS [LC Cart], 
-                      dbo.T_Requested_Run.RDS_Cart_Col AS [Column], dbo.T_Requested_Run.RDS_instrument_name AS Instrument, 
-                      dbo.T_Requested_Run.RDS_BatchID AS Batch, dbo.T_Requested_Run.RDS_Block AS Block, dbo.T_Requested_Run.RDS_Run_Order AS [Run Order], 
-                      dbo.T_Requested_Run.RDS_comment AS Comment, dbo.T_Experiments.Experiment_Num AS Experiment, 
-                      dbo.T_Requested_Run.RDS_priority AS Priority, dbo.T_Users.U_Name AS Requester, dbo.T_Requested_Run.RDS_created AS Created
-FROM         dbo.T_Requested_Run INNER JOIN
-                      dbo.T_Users ON dbo.T_Requested_Run.RDS_Oper_PRN = dbo.T_Users.U_PRN INNER JOIN
-                      dbo.T_Experiments ON dbo.T_Requested_Run.Exp_ID = dbo.T_Experiments.Exp_ID INNER JOIN
-                      dbo.T_LC_Cart ON dbo.T_Requested_Run.RDS_Cart_ID = dbo.T_LC_Cart.ID
-
+SELECT RR.ID AS Request, '' AS [Sel.], RR.RDS_Name AS [Name], 
+       LCCart.Cart_Name AS [LC Cart], RR.RDS_Cart_Col AS [Column], 
+       RR.RDS_instrument_name AS Instrument, DSType.DST_Name AS [Run Type],
+       RR.RDS_BatchID AS Batch, RR.RDS_Block AS Block, RR.RDS_Run_Order AS [Run Order],
+       RR.RDS_comment AS Comment, E.Experiment_Num AS Experiment, RR.RDS_priority AS Priority,
+       U.U_Name AS Requester, RR.RDS_created AS Created
+FROM dbo.T_Requested_Run RR INNER JOIN 
+     dbo.T_Users U ON RR.RDS_Oper_PRN = U.U_PRN INNER JOIN 
+     dbo.T_Experiments E ON RR.Exp_ID = E.Exp_ID INNER JOIN 
+     dbo.T_LC_Cart LCCart ON RR.RDS_Cart_ID = LCCart.ID INNER JOIN 
+     dbo.T_DatasetTypeName DSType ON RR.RDS_type_ID = DSType.DST_Type_ID
 GO
