c52dce438fe78552e45ba6cfdbafd3d3148fd1f3
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-08-07 17:25:30 +0000

    Changes for ticket #511

--- DMS5/Trunk/V_Req_Run_Instrument_Picklist.sql
+++ DMS5/Trunk/V_Req_Run_Instrument_Picklist.sql
@@ -3,12 +3,14 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW V_Req_Run_Instrument_Picklist
+CREATE VIEW dbo.V_Req_Run_Instrument_Picklist
 AS
 SELECT DISTINCT 
-CASE WHEN (IN_name LIKE 'LCQ%') THEN 'LCQ' ELSE IN_name + ' ' + IN_usage END AS val, 
-CASE WHEN (IN_name LIKE 'LCQ%') THEN 'LCQ' ELSE IN_name END AS ex
-FROM         dbo.T_Instrument_Name
-WHERE     (NOT (IN_name LIKE 'SW_%')) AND (IN_status = 'active')
+    CASE WHEN (IN_name LIKE 'LCQ%') THEN 'LCQ' ELSE IN_name + ' ' + IN_usage END AS val, 
+    CASE WHEN (IN_name LIKE 'LCQ%') THEN 'LCQ' ELSE IN_name END AS ex
+FROM dbo.T_Instrument_Name
+WHERE (NOT (IN_name LIKE 'SW_%')) AND 
+      (IN_status = 'active') AND 
+      (IN_operations_role <> 'QC')
 
 GO
--- DMS5/Trunk/V_Req_Run_Instrument_Picklist_Ex.sql
+++ DMS5/Trunk/V_Req_Run_Instrument_Picklist_Ex.sql
@@ -5,10 +5,12 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW dbo.V_Req_Run_Instrument_Picklist_Ex
 AS
-SELECT     IN_name AS val, '' AS ex
-FROM         T_Instrument_Name
-WHERE     (NOT (IN_name LIKE 'SW_%')) AND (IN_status = 'active')
+SELECT IN_name AS val, '' AS ex
+FROM T_Instrument_Name
+WHERE (NOT (IN_name LIKE 'SW_%')) AND 
+      (IN_status = 'active') AND 
+      (IN_operations_role <> 'QC')
 UNION
-SELECT     'LCQ' AS val, '' AS ex
+SELECT 'LCQ' AS val, '' AS ex
 
 GO
