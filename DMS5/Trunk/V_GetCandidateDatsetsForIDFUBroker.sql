ce6c56d87bf9c80eedceee703ca07985444d77b6
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-08-28 21:44:45 +0000

    Update to include archived 9T and 11T datasets

--- DMS5/Trunk/V_GetCandidateDatsetsForIDFUBroker.sql
+++ DMS5/Trunk/V_GetCandidateDatsetsForIDFUBroker.sql
@@ -3,16 +3,22 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-/* get datasets that have entered the archive complete state (
- from the busy state) in the last N months*/
 CREATE VIEW dbo.V_GetCandidateDatsetsForIDFUBroker
 AS
-SELECT     dbo.T_Dataset.Dataset_ID, dbo.T_Dataset.Dataset_Num AS DatasetName, dbo.T_Event_Log.Entered, 
-                      dbo.T_Dataset.DS_instrument_name_ID AS InstrumentID
-FROM         dbo.T_Dataset INNER JOIN
-                      dbo.T_Dataset_Archive ON dbo.T_Dataset.Dataset_ID = dbo.T_Dataset_Archive.AS_Dataset_ID INNER JOIN
-                      dbo.T_Event_Log ON dbo.T_Dataset.Dataset_ID = dbo.T_Event_Log.Target_ID
-WHERE     (dbo.T_Event_Log.Target_Type = 6) AND (dbo.T_Event_Log.Target_State = 3) AND (dbo.T_Dataset_Archive.AS_state_ID = 3) AND 
-                      (dbo.T_Event_Log.Prev_Target_State = 2) AND (DATEDIFF(MONTH, dbo.T_Event_Log.Entered, GETDATE()) < 4)
+SELECT DS.Dataset_ID,
+       DS.Dataset_Num AS DatasetName,
+       Max(EL.Entered) AS Entered,
+       DS.DS_instrument_name_ID AS InstrumentID
+FROM dbo.T_Dataset DS
+     INNER JOIN dbo.T_Dataset_Archive DA
+       ON DS.Dataset_ID = DA.AS_Dataset_ID
+     INNER JOIN dbo.T_Event_Log EL
+       ON DS.Dataset_ID = EL.Target_ID
+WHERE (EL.Target_Type = 6) AND
+      (EL.Target_State = 3) AND
+      (EL.Prev_Target_State IN (2, 12)) AND
+      (DA.AS_state_ID IN (3, 4, 10)) AND
+      (DATEDIFF(MONTH, EL.Entered, GETDATE()) < 4)
+GROUP BY DS.Dataset_ID, DS.Dataset_Num, DS.DS_instrument_name_ID
 
 GO
