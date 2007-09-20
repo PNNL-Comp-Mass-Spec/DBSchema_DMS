412eb188ddaec2b2501f25b63880e29c6715b6c5
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-09-20 20:48:57 +0000

    Changes for ticket #534

---
+++ DMS5/Trunk/CreateAnalysisJobRequest.sql
@@ -0,0 +1,280 @@
+/****** Object:  UserDefinedFunction [dbo].[CreateAnalysisJobFromRequestList] ******/
+SET QUOTED_IDENTIFIER ON 
+GO
+SET ANSI_NULLS ON 
+GO
+
+CREATE PROCEDURE dbo.CreateAnalysisJobFromRequestList
+/****************************************************
+**
+**	Desc: 
+**	Creates analysis jobs for a given list of
+**	analysis job requests
+**
+**	Return values: 0: success, otherwise, error code
+**
+**	Parameters: 
+**
+**	Auth:	grk
+**	Date:	09/17/2007 grk - Initial version (Ticket #534)
+**			09/20/2007 mem - Now checks for existing jobs if @mode <> 'add'
+**
+*****************************************************/
+(
+    @mode varchar(32) = 'preview',					-- 'add' or 'preview'
+    @jobRequestList varchar(4096),					-- Comma separated list of analysis job requests
+    @priority int = 2,								-- Priority
+    @associatedProcessorGroup varchar(64) = '',		-- Processor group name
+    @propagationMode varchar(24) = 'Export'			-- 'Export' or 'No Export'
+)
+As
+	declare @myError int
+	set @myError = 0
+
+	declare @myRowCount int
+	set @myRowCount = 0
+	
+	declare
+		@requestID int,
+		@toolName varchar(64),
+		@parmFileName varchar(255),
+		@settingsFileName varchar(64),
+		@organismDBName varchar(64),
+		@organismName varchar(64),
+		@datasetList varchar(6000),
+		@comment varchar(255),
+		@ownerPRN varchar(32),
+		@protCollNameList varchar(512),
+		@protCollOptionsList varchar(256),
+		@message varchar(512)
+	
+	-------------------------------------------------
+	-- Temporary table to hold job requests
+	-------------------------------------------------
+
+	CREATE TABLE #TRL (
+		requestID int,
+		toolName varchar(64),
+		parmFileName varchar(255),
+		settingsFileName varchar(64),
+		organismDBName varchar(64),
+		organismName varchar(64),
+		datasetList varchar(6000),
+		comment varchar(255),
+		ownerPRN varchar(32),
+		protCollNameList varchar(512),
+		protCollOptionsList varchar(256),
+		stateName varchar(24)
+	)
+	--	
+	SELECT @myError = @@error, @myRowCount = @@rowcount
+	--
+
+	-------------------------------------------------
+	-- Get particulars for requests in list
+	-------------------------------------------------
+	INSERT INTO #TRL (
+		requestID,
+		toolName,
+		parmFileName,
+		settingsFileName,
+		organismDBName,
+		organismName,
+		datasetList,
+		comment,
+		ownerPRN,
+		protCollNameList,
+		protCollOptionsList,
+		stateName
+	)
+	SELECT
+	  AJR_requestID,
+	  AJR_analysisToolName,
+	  AJR_parmFileName,
+	  AJR_settingsFileName,
+	  AJR_organismDBName,
+	  AJR_organismName,
+	  AJR_datasets,
+	  AJR_comment,
+	  requestor,
+	  protCollNameList,
+	  protCollOptionsList,
+	  State
+	FROM   
+	  V_Analysis_Job_Request_Entry
+	WHERE AJR_requestID IN (SELECT Item FROM dbo.MakeTableFromList(@jobRequestList))
+	--	
+	SELECT @myError = @@error, @myRowCount = @@rowcount
+	--
+
+	-------------------------------------------------
+	-- Temp table to hold results for each request
+	-------------------------------------------------
+	CREATE TABLE #XRS (
+		requestID int,
+		result int,
+		description varchar(512)
+	)
+	--	
+	SELECT @myError = @@error, @myRowCount = @@rowcount
+	--
+
+	-------------------------------------------------
+	-- Verify all requests in "new" state
+	-------------------------------------------------
+	INSERT INTO #XRS (requestID, result, description)
+	SELECT requestID, -1, 'Request not in state new; unable to process'
+	FROM #TRL INNER JOIN
+		 dbo.T_Analysis_Job_Request AJR ON #TRL.requestID = AJR.AJR_requestID
+	WHERE AJR.AJR_state <> 1
+	--	
+	SELECT @myError = @@error, @myRowCount = @@rowcount
+	--
+
+	If @myRowCount > 0
+	Begin
+		-- Remove the invalid rows from #TRL
+		DELETE #TRL
+		FROM #TRL INNER JOIN 
+			 #XRS ON #TRL.requestID = #XRS.requestID
+			 
+		-- Continue only if @mode is <> 'add'
+		if @mode = 'add'
+			Goto ReportResults
+	End
+		
+
+	-------------------------------------------------
+	-- Cycle through each request,
+	-- and make a bunch of jobs for it
+	-------------------------------------------------
+
+	declare @done int
+	declare @result int
+	declare @ExistingJobMsg varchar(128)
+	declare @ExistingJobCount int
+
+	set @done = 0
+	set @result = 0
+	set @requestID = 0
+	
+	while @done = 0
+	begin -- <a>
+		-------------------------------------------------
+		-- Add group of jobs for request
+		-------------------------------------------------
+
+		-------------------------------------------------
+		-- Get next request to process
+		-------------------------------------------------
+		SELECT TOP 1 @requestID = requestID
+		FROM #TRL 
+		WHERE requestID > @requestID
+		ORDER BY requestID
+		--	
+		SELECT @myError = @@error, @myRowCount = @@rowcount		
+		
+		-------------------------------------------------
+		-- done, if no more requests
+		-------------------------------------------------
+		If @myRowCount = 0
+			set @done = 1
+		Else
+		Begin -- <b>
+			-------------------------------------------------
+			-- Get key information from request
+			-------------------------------------------------
+			SELECT
+				@toolName =  toolName,
+				@parmFileName = parmFileName,
+				@settingsFileName = settingsFileName,
+				@organismDBName = organismDBName,
+				@organismName = organismName,
+				@datasetList = datasetList,
+				@comment =  comment,
+				@ownerPRN =  ownerPRN,
+				@protCollNameList = protCollNameList,
+				@protCollOptionsList = protCollOptionsList
+			FROM #TRL
+			WHERE requestID = @requestID
+			--	
+			SELECT @myError = @@error, @myRowCount = @@rowcount
+			--
+
+			-------------------------------------------------
+			-- Check for existing jobs
+			-------------------------------------------------
+			Set @ExistingJobCount = 0
+			
+			SELECT @ExistingJobCount = COUNT(*)
+			FROM dbo.T_Analysis_Job
+			WHERE AJ_RequestID = @requestID
+
+			If @ExistingJobCount > 0
+			Begin
+				If @ExistingJobCount = 1
+					Set @ExistingJobMsg = 'Note: 1 job'
+				Else
+					Set @ExistingJobMsg = 'Note: ' + Convert(varchar(12), @ExistingJobCount) + ' jobs'
+				Set @ExistingJobMsg = @ExistingJobMsg + ' found matching this request''s parameters'
+			End
+			Else
+				Set @ExistingJobMsg = ''
+			
+
+			-------------------------------------------------
+			-- Use it to make a bunch of jobs
+			-------------------------------------------------
+			exec @result = AddAnalysisJobGroup
+								@datasetList,
+								@priority,
+								@toolName,
+								@parmFileName,
+								@settingsFileName,
+								@organismDBName,
+								@organismName,
+								@protCollNameList,
+								@protCollOptionsList,
+								@ownerPRN,
+								@comment,
+								@requestID,
+								@associatedProcessorGroup,
+								@propagationMode,
+								@mode, 
+								@message  output
+
+			Set @message = IsNull(@message, '')
+			If @ExistingJobCount > 0
+				Set @message = @ExistingJobMsg + '; ' + @message
+			
+			-------------------------------------------------
+			-- Keep track of results
+			-------------------------------------------------
+			INSERT INTO #XRS
+				(requestID,result,description)
+			VALUES     (@requestID, @result, @message)
+		end -- </b>
+
+			
+	end -- </a>
+
+ReportResults:
+
+	-------------------------------------------------
+	-- Report results
+	-------------------------------------------------
+	SELECT * 
+	FROM #XRS
+	ORDER BY requestID
+	
+
+	return @myError
+
+GO
+SET QUOTED_IDENTIFIER OFF 
+GO
+SET ANSI_NULLS ON 
+GO
+
+GRANT  EXECUTE  ON [dbo].[CreateAnalysisJobFromRequestList]  TO [DMS_Analysis]
+GO
