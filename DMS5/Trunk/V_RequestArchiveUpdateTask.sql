bc60f8bba68736108401e24d901ce004cdae8516
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-11-16 03:24:40 +0000

    Changes for ticket #546

--- DMS5/Trunk/V_RequestArchiveUpdateTask.sql
+++ DMS5/Trunk/V_RequestArchiveUpdateTask.sql
@@ -10,7 +10,7 @@ SELECT     dbo.T_Dataset.Dataset_Num AS Dataset, dbo.T_Dataset.Dataset_ID AS Dat
                       dbo.T_Archive_Path.AP_archive_path AS ArchivePath, dbo.T_Archive_Path.AP_Server_Name AS ArchiveServer, 
                       dbo.t_storage_path.SP_vol_name_client AS StorageVolExternal, dbo.T_Instrument_Name.IN_class AS InstrumentClass, 
                       dbo.T_Instrument_Name.IN_name AS InstrumentName, dbo.T_Dataset_Archive.AS_last_update AS LastUpdate, 
-                      dbo.ConvertFtpArchivePathToSamba(dbo.T_Archive_Path.AP_archive_path) AS ArchivePathSamba
+                      dbo.T_Archive_Path.AP_network_share_path AS ArchivePathSamba
 FROM         dbo.T_Dataset INNER JOIN
                       dbo.T_Dataset_Archive ON dbo.T_Dataset.Dataset_ID = dbo.T_Dataset_Archive.AS_Dataset_ID INNER JOIN
                       dbo.T_Archive_Path ON dbo.T_Dataset_Archive.AS_storage_path_ID = dbo.T_Archive_Path.AP_path_ID INNER JOIN
