443d348bcf933431c7642ed495d6861fa0362b0c
Matthew Monroe <matthew.monroe@pnnl.gov>
2007-12-20 01:07:59 +0000

    Exclude unreviewed datasets from purging

--- DMS5/Trunk/V_Purgable_Datasets.sql
+++ DMS5/Trunk/V_Purgable_Datasets.sql
@@ -5,19 +5,33 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW dbo.V_Purgable_Datasets
 AS
-SELECT     TD.Dataset_ID, dbo.t_storage_path.SP_machine_name AS StorageServerName, dbo.t_storage_path.SP_vol_name_server AS ServerVol, 
-                      MAX(dbo.T_Analysis_Job.AJ_created) AS MostRecentJob, dbo.T_Instrument_Class.raw_data_type
-FROM         dbo.T_Dataset TD INNER JOIN
-                      dbo.T_Dataset_Archive ON TD.Dataset_ID = dbo.T_Dataset_Archive.AS_Dataset_ID INNER JOIN
-                      dbo.T_Instrument_Name ON TD.DS_instrument_name_ID = dbo.T_Instrument_Name.Instrument_ID INNER JOIN
-                      dbo.T_Analysis_Job ON TD.Dataset_ID = dbo.T_Analysis_Job.AJ_datasetID INNER JOIN
-                      dbo.t_storage_path ON TD.DS_storage_path_ID = dbo.t_storage_path.SP_path_ID INNER JOIN
-                      dbo.T_Instrument_Class ON dbo.T_Instrument_Name.IN_class = dbo.T_Instrument_Class.IN_class
-WHERE     (dbo.T_Instrument_Class.is_purgable > 0) AND (dbo.T_Dataset_Archive.AS_state_ID = 3) AND (TD.DS_rating <> - 2) AND 
-                      (ISNULL(dbo.T_Dataset_Archive.AS_purge_holdoff_date, GETDATE()) <= GETDATE()) AND (NOT EXISTS
-                          (SELECT     *
-                            FROM          T_Analysis_Job
-                            WHERE      AJ_StateID IN (1, 2, 3, 8, 9, 10, 11, 12) AND AJ_datasetID = TD.Dataset_ID)) AND (dbo.T_Dataset_Archive.AS_update_state_ID = 4)
-GROUP BY TD.Dataset_ID, dbo.t_storage_path.SP_machine_name, dbo.t_storage_path.SP_vol_name_server, dbo.T_Instrument_Class.raw_data_type
+SELECT DS.Dataset_ID,
+       SPath.SP_machine_name AS StorageServerName,
+       SPath.SP_vol_name_server AS ServerVol,
+       MAX(AJ.AJ_created) AS MostRecentJob,
+       InstClass.raw_data_type
+FROM dbo.T_Dataset AS DS
+     INNER JOIN dbo.T_Dataset_Archive AS DA
+       ON DS.Dataset_ID = DA.AS_Dataset_ID
+     INNER JOIN dbo.T_Instrument_Name AS InstName
+       ON DS.DS_instrument_name_ID = InstName.Instrument_ID
+     INNER JOIN dbo.T_Analysis_Job AS AJ
+       ON DS.Dataset_ID = AJ.AJ_datasetID
+     INNER JOIN dbo.t_storage_path AS SPath
+       ON DS.DS_storage_path_ID = SPath.SP_path_ID
+     INNER JOIN dbo.T_Instrument_Class AS InstClass
+       ON InstName.IN_class = InstClass.IN_class
+WHERE (InstClass.is_purgable > 0) AND
+      (DA.AS_state_ID = 3) AND
+      (DS.DS_rating NOT IN (-2, -10)) AND
+      (ISNULL(DA.AS_purge_holdoff_date, GETDATE()) <= GETDATE()) AND
+      (DA.AS_update_state_ID = 4) AND
+      (NOT EXISTS ( SELECT AJ_jobID
+                    FROM dbo.T_Analysis_Job
+                    WHERE (AJ_StateID IN (1, 2, 3, 8, 9, 10, 11, 12, 16, 17)) AND
+                          (AJ_datasetID = DS.Dataset_ID) ))
+GROUP BY DS.Dataset_ID, SPath.SP_machine_name, SPath.SP_vol_name_server, InstClass.raw_data_type
+
+
 
 GO
--- DMS5/Trunk/V_Purgable_Datasets_NoInterest.sql
+++ DMS5/Trunk/V_Purgable_Datasets_NoInterest.sql
@@ -5,15 +5,25 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW dbo.V_Purgable_Datasets_NoInterest
 AS
-SELECT     dbo.T_Dataset.Dataset_ID, dbo.t_storage_path.SP_machine_name AS StorageServerName, dbo.t_storage_path.SP_vol_name_server AS ServerVol, 
-                      dbo.T_Dataset.DS_created AS Created, dbo.T_Instrument_Class.raw_data_type
-FROM         dbo.T_Dataset INNER JOIN
-                      dbo.T_Dataset_Archive ON dbo.T_Dataset.Dataset_ID = dbo.T_Dataset_Archive.AS_Dataset_ID INNER JOIN
-                      dbo.t_storage_path ON dbo.T_Dataset.DS_storage_path_ID = dbo.t_storage_path.SP_path_ID INNER JOIN
-                      dbo.T_Instrument_Name ON dbo.T_Dataset.DS_instrument_name_ID = dbo.T_Instrument_Name.Instrument_ID INNER JOIN
-                      dbo.T_Instrument_Class ON dbo.T_Instrument_Name.IN_class = dbo.T_Instrument_Class.IN_class
-WHERE     (dbo.T_Instrument_Class.is_purgable > 0) AND (dbo.T_Dataset_Archive.AS_state_ID = 3) AND (dbo.T_Dataset.DS_rating <> - 2) AND 
-                      (dbo.T_Dataset.DS_rating < 2) AND (ISNULL(dbo.T_Dataset_Archive.AS_purge_holdoff_date, GETDATE()) <= GETDATE()) AND 
-                      (dbo.T_Dataset_Archive.AS_update_state_ID = 4)
+SELECT DS.Dataset_ID,
+       SPath.SP_machine_name AS StorageServerName,
+       SPath.SP_vol_name_server AS ServerVol,
+       DS.DS_created AS Created,
+       InstClass.raw_data_type
+FROM dbo.T_Dataset AS DS
+     INNER JOIN dbo.T_Dataset_Archive AS DA
+       ON DS.Dataset_ID = DA.AS_Dataset_ID
+     INNER JOIN dbo.t_storage_path AS SPath
+       ON DS.DS_storage_path_ID = SPath.SP_path_ID
+     INNER JOIN dbo.T_Instrument_Name AS InstName
+       ON DS.DS_instrument_name_ID = InstName.Instrument_ID
+     INNER JOIN dbo.T_Instrument_Class AS InstClass
+       ON InstName.IN_class = InstClass.IN_class
+WHERE (InstClass.is_purgable > 0) AND
+      (DA.AS_state_ID = 3) AND
+      (DS.DS_rating NOT IN (-2, -10)) AND
+      (ISNULL(DA.AS_purge_holdoff_date, GETDATE()) <= GETDATE()) AND
+      (DA.AS_update_state_ID = 4) AND
+      (DS.DS_rating < 2)
 
 GO
