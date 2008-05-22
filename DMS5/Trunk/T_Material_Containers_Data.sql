0baa332f2fa2d241b9d4730f9adfe0522aaee079
Matthew Monroe <matthew.monroe@pnnl.gov>
2008-05-22 03:08:21 +0000

    Table definition and data updates

---
+++ DMS5/Trunk/T_Material_Containers_Data.sql
@@ -0,0 +1,5 @@
+/****** Object:  Table [T_Material_Containers] ******/
+/****** RowCount: 1 ******/
+SET IDENTITY_INSERT [T_Material_Containers] ON
+INSERT INTO [T_Material_Containers] (ID, Tag, Type, Comment, Barcode, Location_ID, Created, Status) VALUES (1,'na','na','For material that is not in any container','',1,'3/11/2008 11:54:13 AM','Active')
+SET IDENTITY_INSERT [T_Material_Containers] OFF
