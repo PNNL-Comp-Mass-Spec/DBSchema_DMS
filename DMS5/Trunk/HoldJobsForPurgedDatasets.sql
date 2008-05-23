3362305e9f1c34e404336a6f52c2c06f826b058c
Matthew Monroe <matthew.monroe@pnnl.gov>
2008-05-23 01:25:01 +0000

    Changes for Ticket #670

---
+++ DMS5/Trunk/HoldJobsForPurgedDatasets.sql
@@ -0,0 +1,104 @@
+/****** Object:  StoredProcedure [dbo].[HoldJobsForPurgedDatasets] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE Procedure dbo.HoldJobsForPurgedDatasets
+/****************************************************
+**
+**	Desc:	Updates the job state to 8=Holding for jobs
+**			associated with purged dataset
+**
+**	Return values: 0: success, otherwise, error code
+**
+**	Parameters:
+**
+**	Auth:	mem
+**	Date:	05/15/2008 (Ticket #670)
+**			05/22/2008 mem - Now updating comment for any jobs that are set to state 8 (Ticket #670)
+**
+*****************************************************/
+(
+	@InfoOnly tinyint = 0,
+	@message varchar(512) = '' output
+)
+As
+	set nocount on
+
+	declare @myError int
+	set @myError = 0
+
+	declare @myRowCount int
+	set @myRowCount = 0
+	
+	set @message = ''
+	
+	Declare @HoldMessage varchar(128)
+	Set @HoldMessage = '; holding since dataset purged'
+	
+	CREATE TABLE #Tmp_JobsToUpdate (
+		Job int NOT NULL,
+	)
+	
+	INSERT INTO #Tmp_JobsToUpdate (Job)
+	SELECT AJ_JobID
+	FROM dbo.T_Analysis_Job
+	WHERE (AJ_StateID = 1) AND
+	      (AJ_datasetID IN ( SELECT DISTINCT Target_ID
+	                         FROM dbo.T_Event_Log
+	                         WHERE (Target_Type = 6) AND
+	                               (Target_State = 4) ))
+	--
+	SELECT @myError = @@error, @myRowCount = @@rowcount
+
+
+	If @myRowCount = 0
+	Begin
+		If @InfoOnly <> 0
+			SELECT 'No jobs having purged datasets were found with state 1=New' AS Message
+	End
+	Else
+	Begin
+		If @InfoOnly <> 0
+		Begin
+			SELECT AJ.AJ_jobID AS Job,
+			       AJ.AJ_created AS Created,
+			       AJ.AJ_analysisToolID AS AnalysisToolID,
+			       IsNull(AJ.AJ_comment, '') + @HoldMessage AS Comment,
+			       AJ.AJ_StateID AS StateID,
+			       DS.Dataset_Num AS Dataset,
+			       DS.DS_created AS Dataset_Created,
+			       DFP.Dataset_Folder_Path,
+			       DFP.Archive_Folder_Path
+			FROM #Tmp_JobsToUpdate JTU
+			     INNER JOIN dbo.T_Analysis_Job AJ
+			       ON JTU.Job = AJ.AJ_JobID AND
+			          AJ.AJ_StateID = 1
+			     INNER JOIN dbo.T_Dataset DS
+			       ON AJ.AJ_datasetID = DS.Dataset_ID
+			     INNER JOIN dbo.V_Dataset_Folder_Paths DFP
+			       ON DS.Dataset_ID = DFP.Dataset_ID
+		End
+   		Else
+   		Begin
+			UPDATE dbo.T_Analysis_Job
+			SET AJ_StateID = 8,
+				AJ_Comment = AJ_Comment + @HoldMessage
+			FROM dbo.T_Analysis_Job AJ
+			     INNER JOIN #Tmp_JobsToUpdate JTU
+			       ON JTU.Job = AJ.AJ_JobID AND
+			          AJ.AJ_StateID = 1
+			--
+			SELECT @myError = @@error, @myRowCount = @@rowcount
+
+			If @myRowCount > 0 
+				Set @message = 'Placed ' + Convert(varchar(12), @myRowCount) + ' jobs on hold since their associated dataset is purged'
+		End
+	End
+	
+Done:
+	return @myError
+
+GO
+GRANT EXECUTE ON [dbo].[HoldJobsForPurgedDatasets] TO [D3L243]
+GO
---
+++ DMS5/Trunk/ReindexDatabase.sql
@@ -0,0 +1,169 @@
+/****** Object:  StoredProcedure [dbo].[ReindexDatabase] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE Procedure dbo.ReindexDatabase
+/****************************************************
+**
+**	Desc: 
+**		Reindexes the key tables in the database
+**
+**	Return values: 0:  success, otherwise, error code
+**
+**	Auth:	mem
+**	Date:	05/21/2008
+**    
+*****************************************************/
+(
+	@message varchar(512) = '' output
+)
+As
+	set nocount on
+	
+	declare @myError int
+	declare @myRowcount int
+	set @myRowcount = 0
+	set @myError = 0
+	
+	Declare @TableCount int
+	Set @TableCount = 0
+
+	declare @UpdateEnabled tinyint
+	
+	Set @message = ''
+	
+	-----------------------------------------------------------
+	-- Reindex the data tables
+	-----------------------------------------------------------
+	DBCC DBREINDEX (T_Event_Log, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_AuxInfo_Value, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Analysis_Job_Status_History, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Analysis_Job, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Dataset, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Dataset_Archive, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Requested_Run_History, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Requested_Run_History_EUS_Users, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Experiments, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Experiment_Cell_Cultures, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Analysis_Job_Processor_Group_Associations, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Experiment_Group_Members, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Health_Entries, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Analysis_Log, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Log_Entries, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Filter_Set_Criteria, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Material_Locations, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Analysis_Job_Request, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Sample_Prep_Request_Updates, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Requested_Run_EUS_Users, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Requested_Run, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Analysis_Job_Batches, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Param_File_Mass_Mods, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Experiment_Groups, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Param_Entries, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_LC_Column, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Cell_Culture, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Param_Files, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_EUS_Proposals, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Sample_Prep_Request, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Requested_Run_Batches, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Organism_DB_File, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_EUS_Proposal_Users, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Campaign, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Filter_Set_Criteria_Groups, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Predefined_Analysis, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Users, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Experiment_Annotations, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_EUS_Users, '', 90)
+	Set @TableCount = @TableCount + 1
+	
+	DBCC DBREINDEX (T_Organisms, '', 90)
+	Set @TableCount = @TableCount + 1
+			
+	-----------------------------------------------------------
+	-- Log the reindex
+	-----------------------------------------------------------
+	
+	Set @message = 'Reindexed ' + Convert(varchar(12), @TableCount) + ' tables'
+	Exec PostLogEntry 'Normal', @message, 'ReindexDatabase'
+
+Done:
+	Return @myError
+
+GO
