8eb1d83b263af9ce01a7c58654361981b8655660
Matthew Monroe <matthew.monroe@pnnl.gov>
2009-01-19 23:53:46 +0000

    Changes for Ticket #716

---
+++ DMS5/Trunk/V_GetPipelineJobPriority.sql
@@ -0,0 +1,15 @@
+/****** Object:  View [dbo].[V_GetPipelineJobPriority] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_GetPipelineJobPriority]
+AS
+SELECT AJ.AJ_jobID AS Job,
+       AJ.AJ_priority AS Priority
+FROM dbo.T_Analysis_Job AS AJ
+WHERE (AJ.AJ_StateID IN (1, 2, 8))
+
+
+GO
--- DMS5/Trunk/V_GetPipelineJobProcessors.sql
+++ DMS5/Trunk/V_GetPipelineJobProcessors.sql
@@ -3,15 +3,27 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-create view V_GetPipelineJobProcessors
+
+CREATE view [dbo].[V_GetPipelineJobProcessors]
 as
-SELECT     TAJ.AJ_jobID AS Job, P.Processor_Name AS Processor, SUM(CASE WHEN PG.Available_For_General_Processing = 'Y' THEN 1 ELSE 0 END) 
-                      AS General_Processing
-FROM         dbo.T_Analysis_Job AS TAJ INNER JOIN
-                      dbo.T_Analysis_Job_Processor_Group_Associations AS PGA ON TAJ.AJ_jobID = PGA.Job_ID INNER JOIN
-                      dbo.T_Analysis_Job_Processor_Group AS PG ON PGA.Group_ID = PG.ID INNER JOIN
-                      dbo.T_Analysis_Job_Processor_Group_Membership AS PGM ON PG.ID = PGM.Group_ID INNER JOIN
-                      dbo.T_Analysis_Job_Processors AS P ON PGM.Processor_ID = P.ID
-WHERE     (PG.Group_Enabled = 'Y') AND (PGM.Membership_Enabled = 'Y') AND (TAJ.AJ_StateID = 1)
+SELECT TAJ.AJ_jobID AS Job,
+       P.Processor_Name AS Processor,
+       SUM(CASE
+               WHEN PG.Available_For_General_Processing = 'Y' THEN 1
+               ELSE 0
+           END) AS General_Processing
+FROM dbo.T_Analysis_Job AS TAJ
+     INNER JOIN dbo.T_Analysis_Job_Processor_Group_Associations AS PGA
+       ON TAJ.AJ_jobID = PGA.Job_ID
+     INNER JOIN dbo.T_Analysis_Job_Processor_Group AS PG
+       ON PGA.Group_ID = PG.ID
+     INNER JOIN dbo.T_Analysis_Job_Processor_Group_Membership AS PGM
+       ON PG.ID = PGM.Group_ID
+     INNER JOIN dbo.T_Analysis_Job_Processors AS P
+       ON PGM.Processor_ID = P.ID
+WHERE (PG.Group_Enabled = 'Y') AND
+      (PGM.Membership_Enabled = 'Y') AND
+      (TAJ.AJ_StateID IN (1,2,8))
 GROUP BY TAJ.AJ_jobID, P.Processor_Name
+
 GO
