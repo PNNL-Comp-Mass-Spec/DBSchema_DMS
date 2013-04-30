585fb74c4b7870b57b4dfd5f8098d1adc3295df7
Matthew Monroe <matthew.monroe@pnnl.gov>
2013-04-30 22:03:11 +0000

    New View

---
+++ DMS5/V_Dataset_Archive.sql
@@ -0,0 +1,51 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_Dataset_Archive]
+AS
+SELECT DA.AS_Dataset_ID,
+       DS.Dataset_Num,
+       DA.AS_state_ID,
+       DASN.DASN_StateName,
+       DA.AS_state_Last_Affected,
+       DA.AS_storage_path_ID,
+       DA.AS_datetime,
+       DA.AS_last_update,
+       DA.AS_last_verify,
+       DA.AS_update_state_ID,
+       AUSN.AUS_name,
+       DA.AS_update_state_Last_Affected,
+       DA.AS_purge_holdoff_date,
+       DA.AS_archive_processor,
+       DA.AS_update_processor,
+       DA.AS_verification_processor,
+       DA.AS_instrument_data_purged,
+       DA.AS_Last_Successful_Archive,
+       DA.AS_StageMD5_Required,
+       DA.QC_Data_Purged,
+       DA.Purge_Policy,
+       DA.Purge_Priority,
+       AP.AP_archive_path,
+       AP.AP_network_share_path
+FROM T_Dataset_Archive DA
+     INNER JOIN T_Dataset DS
+       ON DA.AS_Dataset_ID = DS.Dataset_ID
+     INNER JOIN T_DatasetArchiveStateName DASN
+       ON DA.AS_state_ID = DASN.DASN_StateID
+     INNER JOIN T_Archive_Update_State_Name AUSN
+       ON DA.AS_update_state_ID = AUSN.AUS_stateID
+     INNER JOIN T_Archive_Path AP
+       ON DA.AS_storage_path_ID = AP.AP_path_ID
+
+GO
+
+GRANT VIEW DEFINITION ON [dbo].[V_Dataset_Archive] TO [PNL\D3M578] AS [dbo]
+GO
+
+GRANT VIEW DEFINITION ON [dbo].[V_Dataset_Archive] TO [PNL\D3M580] AS [dbo]
+GO
+
+
