d609c2a5ab90d09de3a4d1e2ea27bc9db27adbd4
alchemistmatt <matthew.monroe@pnnl.gov>
2014-09-06 01:04:08 -0700

    2014-09-06 auto-commit DMS5

--- DMS5/V_Material_Log_List_Report.sql
+++ DMS5/V_Material_Log_List_Report.sql
@@ -3,13 +3,21 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Material_Log_List_Report
+
+CREATE VIEW [dbo].[V_Material_Log_List_Report]
 AS
-SELECT     dbo.T_Material_Log.ID, dbo.T_Material_Log.Date, dbo.T_Material_Log.Type, dbo.T_Material_Log.Item, dbo.T_Material_Log.Initial_State AS Initial, 
-                      dbo.T_Material_Log.Final_State AS Final, dbo.T_Users.U_Name + ' (' + dbo.T_Material_Log.User_PRN + ')' AS [User], 
-                      dbo.T_Material_Log.Comment
-FROM         dbo.T_Material_Log LEFT OUTER JOIN
-                      dbo.T_Users ON dbo.T_Material_Log.User_PRN = dbo.T_Users.U_PRN
+SELECT  TML.ID ,
+        TML.Date ,
+        TML.Type ,
+        TML.Item ,
+        TML.Initial_State AS Initial ,
+        TML.Final_State AS Final ,
+        TU.U_Name + ' (' + TML.User_PRN + ')' AS [User] ,
+        TML.Comment,
+        TMC.Comment AS [Container Comment]
+FROM    dbo.T_Material_Log TML
+        LEFT OUTER JOIN dbo.T_Users TU ON TML.User_PRN = TU.U_PRN
+        LEFT OUTER JOIN dbo.T_Material_Containers TMC ON TML.Item = TMC.Tag
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Material_Log_List_Report] TO [PNL\D3M578] AS [dbo]
