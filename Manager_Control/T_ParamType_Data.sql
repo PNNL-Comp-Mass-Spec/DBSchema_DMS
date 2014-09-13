463206a20add8828834278e52a18ec9742bfa867
alchemistmatt <matthew.monroe@pnnl.gov>
2014-09-12 17:09:45 -0700

    2014-09-12 auto-commit Manager_Control

--- Manager_Control/T_ParamType_Data.sql
+++ Manager_Control/T_ParamType_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_ParamType] ******/
-/****** RowCount: 162 ******/
+/****** RowCount: 163 ******/
 SET IDENTITY_INSERT [T_ParamType] ON
 INSERT INTO [T_ParamType] (ParamID, ParamName, PicklistName, Comment) VALUES (1,'connectionstring','','')
 INSERT INTO [T_ParamType] (ParamID, ParamName, PicklistName, Comment) VALUES (2,'username','','')
@@ -163,4 +163,5 @@ INSERT INTO [T_ParamType] (ParamID, ParamName, PicklistName, Comment) VALUES (17
 INSERT INTO [T_ParamType] (ParamID, ParamName, PicklistName, Comment) VALUES (177,'DeconPeakDetectorProgLoc','','Path to HammerOrDeconSimplePeakDetector.exe')
 INSERT INTO [T_ParamType] (ParamID, ParamName, PicklistName, Comment) VALUES (178,'MSPathFinderProgLoc','','Path to MSPathFinder')
 INSERT INTO [T_ParamType] (ParamID, ParamName, PicklistName, Comment) VALUES (179,'PbfGenProgLoc','','Path to PbfGen')
+INSERT INTO [T_ParamType] (ParamID, ParamName, PicklistName, Comment) VALUES (180,'MzRefineryProgLoc','','Path to MzRefinery')
 SET IDENTITY_INSERT [T_ParamType] OFF
