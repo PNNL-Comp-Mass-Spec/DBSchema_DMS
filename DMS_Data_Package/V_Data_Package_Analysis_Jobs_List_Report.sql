51a0ea91ceb07fb2b22da80c6b81aa19a5ae4a34
alchemistmatt <matthew.monroe@pnnl.gov>
2014-10-18 01:03:48 -0700

    2014-10-18 auto-commit DMS_Data_Package

--- DMS_Data_Package/V_Data_Package_Analysis_Jobs_List_Report.sql
+++ DMS_Data_Package/V_Data_Package_Analysis_Jobs_List_Report.sql
@@ -3,13 +3,34 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Data_Package_Analysis_Jobs_List_Report
+
+CREATE VIEW [dbo].[V_Data_Package_Analysis_Jobs_List_Report]
 AS
-SELECT        DPJ.Data_Package_ID AS ID, DPJ.Job, DPJ.Dataset, DPJ.Tool, DPJ.[Package Comment], AJL.Campaign, AJL.Experiment, AJL.Instrument, AJL.[Parm File], 
-                         AJL.Settings_File, AJL.Organism, AJL.[Organism DB], AJL.[Protein Collection List], AJL.[Protein Options], AJL.Finished, AJL.Runtime, AJL.[Job Request], 
-                         AJL.[Results Folder], AJL.[Archive Folder Path], DPJ.[Item Added], AJL.Comment AS Comment
-FROM            dbo.T_Data_Package_Analysis_Jobs AS DPJ INNER JOIN
-                         dbo.S_V_Analysis_Job_List_Report_2 AS AJL ON DPJ.Job = AJL.Job
+SELECT DPJ.Data_Package_ID AS ID,
+       DPJ.Job,
+       DPJ.Dataset,
+       DPJ.Tool,
+       DPJ.[Package Comment],
+       AJL.Campaign,
+       AJL.Experiment,
+       AJL.Instrument,
+       AJL.[Parm File],
+       AJL.Settings_File,
+       AJL.Organism,
+       AJL.[Organism DB],
+       AJL.[Protein Collection List],
+       AJL.[Protein Options],
+       AJL.Finished,
+       AJL.Runtime,
+       AJL.[Job Request],
+       AJL.[Results Folder],
+       AJL.[Results Folder Path],
+       DPJ.[Item Added],
+       AJL.Comment AS [Comment]
+FROM dbo.T_Data_Package_Analysis_Jobs AS DPJ
+     INNER JOIN dbo.S_V_Analysis_Job_List_Report_2 AS AJL
+       ON DPJ.Job = AJL.Job
+
 
 GO
 GRANT SELECT ON [dbo].[V_Data_Package_Analysis_Jobs_List_Report] TO [DMS_SP_User] AS [dbo]
