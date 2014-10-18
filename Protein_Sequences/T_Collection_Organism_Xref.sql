202d938c2c329606e19deadf9bc641243de9de57
alchemistmatt <matthew.monroe@pnnl.gov>
2014-10-18 01:05:09 -0700

    2014-10-18 auto-commit Protein_Sequences

--- Protein_Sequences/T_Collection_Organism_Xref.sql
+++ Protein_Sequences/T_Collection_Organism_Xref.sql
@@ -14,3 +14,10 @@ CREATE TABLE [dbo].[T_Collection_Organism_Xref](
 ) ON [PRIMARY]
 
 GO
+/****** Object:  Index [IX_T_Collection_Organism_Xref_ProtCollectionID_OrganismID] ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_T_Collection_Organism_Xref_ProtCollectionID_OrganismID] ON [dbo].[T_Collection_Organism_Xref]
+(
+	[Protein_Collection_ID] ASC,
+	[Organism_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
