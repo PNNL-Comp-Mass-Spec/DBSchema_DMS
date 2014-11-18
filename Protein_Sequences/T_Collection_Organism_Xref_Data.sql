22a52660e7eb5828dcceac1a1691767233b1d39d
alchemistmatt <matthew.monroe@pnnl.gov>
2014-11-18 01:04:53 -0800

    2014-11-18 auto-commit Protein_Sequences

--- Protein_Sequences/T_Collection_Organism_Xref_Data.sql
+++ Protein_Sequences/T_Collection_Organism_Xref_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_Collection_Organism_Xref] ******/
-/****** RowCount: 1364 ******/
+/****** RowCount: 1366 ******/
 SET IDENTITY_INSERT [T_Collection_Organism_Xref] ON
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (42,1001,1003)
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (12,1002,1002)
@@ -1365,4 +1365,6 @@ INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (1424,2357,1356)
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (1423,2358,1356)
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (1425,2359,1356)
+INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (1426,2360,1297)
+INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (1427,2361,3)
 SET IDENTITY_INSERT [T_Collection_Organism_Xref] OFF
