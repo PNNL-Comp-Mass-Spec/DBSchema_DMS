40bee4a9585763ed4993d7aabe380abd14af5741
alchemistmatt <matthew.monroe@pnnl.gov>
2015-01-15 01:03:40 -0800

    2015-01-15 auto-commit DMS5

--- DMS5/V_Users_NWFs_Samba.sql
+++ DMS5/V_Users_NWFs_Samba.sql
@@ -3,11 +3,15 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Users_NWFs_Samba
+
+CREATE VIEW [dbo].[V_Users_NWFs_Samba]
 AS
-SELECT     U_PRN AS [Payroll Num], U_Name AS Name
-FROM         dbo.T_Users
-WHERE     (U_active = 'Y')
+SELECT U_PRN AS [Payroll Num],
+       U_Name AS Name
+FROM dbo.T_Users
+WHERE (U_active = 'Y') AND
+      (U_PRN NOT IN ('svc-dms')) -- Exclude this user to prevent it from being mapped to the dmsarch-ro2 group by a nightly cron job
+
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Users_NWFs_Samba] TO [PNL\D3M578] AS [dbo]
