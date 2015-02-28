e4872a113c812b2d5a4b2c691ab9c42a65ee8066
alchemistmatt <matthew.monroe@pnnl.gov>
2015-02-28 01:03:20 -0800

    2015-02-28 auto-commit DMS_Pipeline

--- DMS_Pipeline/UpdateCPULoading.sql
+++ DMS_Pipeline/UpdateCPULoading.sql
@@ -3,7 +3,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE dbo.UpdateCPULoading
+
+CREATE PROCEDURE [dbo].[UpdateCPULoading]
 /****************************************************
 **
 **	Desc:
@@ -21,6 +22,7 @@ CREATE PROCEDURE dbo.UpdateCPULoading
 **			09/10/2010 mem - Now using READUNCOMMITTED when querying T_Job_Steps
 **			10/17/2011 mem - Now populating Memory_Available
 **			09/24/2014 mem - Removed reference to Machine in T_Job_Steps
+**			02/26/2015 mem - Split the Update query into two parts
 **    
 *****************************************************/
 (
@@ -41,25 +43,26 @@ As
 	-- Find job steps that are currently busy 
 	-- and sum up cpu counts and memory uage for tools by machine
 	-- Update T_Machines
+	--
+	-- This is a two-step query to avoid locking T_Job_Steps
 	---------------------------------------------------
+	--
+	SELECT M.Machine, 
+			SUM(CASE WHEN JS.State = 4 THEN JS.CPU_Load ELSE 0 END) AS CPUs_used,
+			SUM(CASE WHEN JS.State = 4 THEN JS.Memory_Usage_MB ELSE 0 END) AS Memory_Used
+	Into #Tmp_MachineStats
+	FROM T_Machines M
+		    LEFT OUTER JOIN T_Local_Processors LP
+		    ON M.Machine = LP.Machine
+		    LEFT OUTER JOIN T_Job_Steps JS WITH ( READUNCOMMITTED )
+		    ON LP.Processor_Name = JS.Processor
+	GROUP BY M.Machine
 	--	
 	UPDATE T_Machines
-	SET    CPUs_Available = T_Machines.Total_CPUs - TX.CPUs_used,
-	       Memory_Available = T_Machines.Total_Memory_MB - TX.Memory_Used
-	FROM   T_Machines
-	INNER JOIN 
-	(
-		SELECT M.Machine, 
-			   SUM(CASE WHEN JS.State = 4 THEN JS.CPU_Load ELSE 0 END) AS CPUs_used,
-			   SUM(CASE WHEN JS.State = 4 THEN JS.Memory_Usage_MB ELSE 0 END) AS Memory_Used
-		FROM T_Machines M
-		     LEFT OUTER JOIN T_Local_Processors LP
-		       ON M.Machine = LP.Machine
-		     LEFT OUTER JOIN T_Job_Steps JS WITH ( READUNCOMMITTED )
-		       ON LP.Processor_Name = JS.Processor
-		GROUP BY M.Machine
-	) AS TX
-	ON TX.Machine = T_Machines.Machine
+	SET    CPUs_Available = M.Total_CPUs - TX.CPUs_used,
+	       Memory_Available = M.Total_Memory_MB - TX.Memory_Used
+	FROM   T_Machines M INNER JOIN #Tmp_MachineStats As TX
+	        ON TX.Machine = M.Machine
 	--
 	SELECT @myError = @@error, @myRowCount = @@rowcount
      --
@@ -77,6 +80,7 @@ Done:
 	return @myError
 
  
+
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[UpdateCPULoading] TO [Limited_Table_Write] AS [dbo]
