55dae83d23683eec0269f55ea0df01a61739666a
alchemistmatt <matthew.monroe@pnnl.gov>
2015-03-05 01:02:33 -0800

    2015-03-05 auto-commit DMS_Capture

--- DMS_Capture/MakeTableFromList.sql
+++ DMS_Capture/MakeTableFromList.sql
@@ -3,7 +3,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE FUNCTION [dbo].[MakeTableFromList]
+CREATE FUNCTION dbo.MakeTableFromList
 /****************************************************
 **
 **	Desc: 
@@ -14,12 +14,12 @@ CREATE FUNCTION [dbo].[MakeTableFromList]
 **	Parameters:
 **	
 **
-**		Auth: grk
-**		Date: 1/12/2006
+**	Auth: grk
+**	Date: 1/12/2006
 **      
-**		03/05/2008 jds -- added the line to convert null 
-**			list to empty string if value is null 
-**		08/25/2008 grk -- increased size of input @list 
+**		03/05/2008 jds - Added the line to convert null list to empty string if value is null 
+**		08/25/2008 grk - Increased size of input @list 
+**		03/04/2015 mem - Update to use udfParseDelimitedList
 **    
 *****************************************************/
 (
@@ -31,77 +31,12 @@ RETURNS @theTable TABLE
    )
 AS
 BEGIN
-		declare @delimiter char(1)
-		set @delimiter = ','
-
-		declare @EOL int
-		declare @count int
-
-		declare @myError int
-		set @myError = 0
-
-		declare @myRowCount int
-		set @myRowCount = 0
-		--
-		declare @id int
-		--
-		declare @curPos int
-		set @curPos = 1
-		declare @field varchar(128)
-
-		--if @list is null set to empty string
-		Set @list = isnull(@list, '')
-
-		-- process lists into rows
-		-- and insert into DB table
-		--
-		set @count = 0
-		set @EOL = 0
-		declare @EndOfField int
-
-		while @EOL = 0
-		begin
-			set @count = @count + 1
-
-			-- process the  next field from the list
-			--
-			set @field = ''
-			set @EOL = 0
-			
-			-- find position of delimiter
-			--
-			set @EndOfField = charindex(@delimiter, @list, @curPos)
-
-			-- if delimiter not found, field contains rest of string
-			-- and end-of-line condition is set
-			--
-			if @EndOfField = 0
-			begin
-				set @EndOfField = LEN(@list) + 1
-				set @EOL = 1
-			end
-			
-			-- extract field based on positions
-			--
-			set @field = ltrim(rtrim(substring(@list, @curPos, @EndOfField - @curPos)))
-
-			-- advance current starting position beyond current field
-			-- and set end-of-line condidtion if it is past the end of the line
-			--
-			set @curPos = @EndOfField + 1
-			if @curPos > LEN(@list)
-				set @EOL = 1
-			
-			if @field <> ''
-			begin
-				INSERT INTO @theTable
-					(Item)
-				VALUES     
-					(@field)
-			end
-		end
-
+		
+		INSERT INTO @theTable
+			(Item)
+		SELECT Value
+		FROM dbo.udfParseDelimitedList(@list, ',')
 		RETURN
-	END
+END
 
 GO
--- DMS_Capture/V_Job_Steps_Stale_and_Failed.sql
+++ DMS_Capture/V_Job_Steps_Stale_and_Failed.sql
@@ -4,6 +4,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+
 CREATE VIEW [dbo].[V_Job_Steps_Stale_and_Failed] 
 AS
 SELECT Warning_Message,
@@ -22,11 +23,9 @@ SELECT Warning_Message,
        Step,
        Completion_Message,
        Evaluation_Message
-FROM ( SELECT  CASE -- WHEN (JS.State = 4 AND JS.LastCPUStatus_Minutes >= 4*60 )                              THEN 'No status update for 4 hours'
-                    -- WHEN (JS.State = 4 AND RunTime_Predicted_Hours >= 36 )                                 THEN 'Job predicted to run over 36 hours'
-                    WHEN (JS.State = 4 AND DATEDIFF(day, JS.Start, GetDate()) >= 4 )                          THEN 'Job step running over 4 days'
-                    WHEN (JS.State = 6 AND JS.Start >= DATEADD(day, -14, GETDATE()) AND JS.Job_State <> 101 ) THEN 'Job step failed within the last 14 days'
-                    WHEN (NOT FailedJobQ.Job IS Null)                                                         THEN 'Overall job state is "failed"'
+FROM ( SELECT  CASE WHEN (JS.State = 4 AND DATEDIFF(hour, JS.Start, GetDate()) >= 5 )                               THEN 'Job step running over 5 hours'
+                    WHEN (JS.State = 6 AND JS.Start >= DATEADD(day, -14, GETDATE()) AND JS.Job_State <> 101 )       THEN 'Job step failed within the last 14 days'
+                    WHEN (NOT FailedJobQ.Job IS Null)                                                               THEN 'Overall job state is "failed"'
                     ELSE ''
                     END AS Warning_Message,
               JS.Job,
@@ -74,6 +73,7 @@ FROM ( SELECT  CASE -- WHEN (JS.State = 4 AND JS.LastCPUStatus_Minutes >= 4*60 )
               ON JS.Processor = LP.Processor_Name
    ) DataQ
 WHERE Warning_Message <> ''
+
 
 
 GO
