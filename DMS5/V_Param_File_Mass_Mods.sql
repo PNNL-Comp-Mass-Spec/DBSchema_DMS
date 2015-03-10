ec7570b38cd873871530fd74eec704885b892c58
alchemistmatt <matthew.monroe@pnnl.gov>
2015-03-10 01:03:29 -0700

    2015-03-10 auto-commit DMS5

--- DMS5/V_Param_File_Mass_Mods.sql
+++ DMS5/V_Param_File_Mass_Mods.sql
@@ -3,7 +3,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Param_File_Mass_Mods
+
+CREATE VIEW [dbo].[V_Param_File_Mass_Mods]
 AS
 (
 SELECT PFMM.*,
@@ -13,8 +14,17 @@ SELECT PFMM.*,
        SLS.Local_Symbol,
        R.Description AS Residue_Desc,
        PF.Param_File_Name,
-       PF.Param_File_Description
-       
+       PF.Param_File_Description,
+       ' || Mod_Type || Residue || Mod Name || Mod Mass ||' AS TableCode_Header,
+	   ' | ' + CASE PFMM.Mod_Type_Symbol 
+	           WHEN 'S' THEN 'Static'
+	           WHEN 'D' THEN 'Dynamic'
+	           ELSE PFMM.Mod_Type_Symbol 
+			   END +
+	   ' | ' + R.Description + 
+	   ' | ' + MCF.Mass_Correction_Tag + 
+	   ' | ' + Cast(MCF.Monoisotopic_Mass_Correction as varchar(12)) + 
+	   ' | ' AS TableCode_Row
 FROM T_Param_File_Mass_Mods PFMM
      INNER JOIN T_Residues R
        ON PFMM.Residue_ID = R.Residue_ID
