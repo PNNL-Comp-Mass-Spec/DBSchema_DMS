3197689509b80c6eec68d338114ce445229e01da
alchemistmatt <matthew.monroe@pnnl.gov>
2015-03-17 01:06:03 -0700

    2015-03-17 auto-commit DMS5

--- DMS5/T_Param_File_Mass_Mods.sql
+++ DMS5/T_Param_File_Mass_Mods.sql
@@ -25,6 +25,15 @@ GRANT SELECT ON [dbo].[T_Param_File_Mass_Mods] TO [DMS_ParamFile_Admin] AS [dbo]
 GO
 GRANT UPDATE ON [dbo].[T_Param_File_Mass_Mods] TO [DMS_ParamFile_Admin] AS [dbo]
 GO
+/****** Object:  Index [IX_T_Param_File_Mass_Mods] ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_T_Param_File_Mass_Mods] ON [dbo].[T_Param_File_Mass_Mods]
+(
+	[Param_File_ID] ASC,
+	[Local_Symbol_ID] ASC,
+	[Residue_ID] ASC,
+	[Mass_Correction_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
 ALTER TABLE [dbo].[T_Param_File_Mass_Mods]  WITH CHECK ADD  CONSTRAINT [FK_T_Param_File_Mass_Mods_T_Mass_Correction_Factors] FOREIGN KEY([Mass_Correction_ID])
 REFERENCES [dbo].[T_Mass_Correction_Factors] ([Mass_Correction_ID])
 GO
