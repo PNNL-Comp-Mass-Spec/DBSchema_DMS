1270a9dfe86b3e661b8ae6c0411dfee0158690ea
alchemistmatt <matthew.monroe@pnnl.gov>
2015-04-08 16:20:29 -0700

    Table data updates

--- DMS5/T_Instrument_Group_Data.sql
+++ DMS5/T_Instrument_Group_Data.sql
@@ -1,7 +1,8 @@
 /****** Object:  Table [T_Instrument_Group] ******/
-/****** RowCount: 31 ******/
+/****** RowCount: 32 ******/
 /****** Columns: IN_Group, Usage, Comment, Active, Default_Dataset_Type, Allocation_Tag ******/
 INSERT INTO [T_Instrument_Group] VALUES ('11T','Research','',0,1,'')
+INSERT INTO [T_Instrument_Group] VALUES ('5500XL_SOLiD','Genomic Sequencing','',1,33,'SEQ')
 INSERT INTO [T_Instrument_Group] VALUES ('Agilent_FTMS','Research','',1,1,'FT')
 INSERT INTO [T_Instrument_Group] VALUES ('Agilent_GC-MS','Metabolomics','',1,18,'GC')
 INSERT INTO [T_Instrument_Group] VALUES ('Agilent_Ion_Trap','Research','',0,2,'')
