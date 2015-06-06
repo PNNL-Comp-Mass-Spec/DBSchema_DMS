9a6410a2d4f27c1daf47737d3d6315fc9c8f2690
alchemistmatt <matthew.monroe@pnnl.gov>
2015-06-06 01:04:07 -0700

    2015-06-06 auto-commit DMS5

---
+++ DMS5/V_Instrument_Info_LCMSNet.sql
@@ -0,0 +1,22 @@
+/****** Object:  View [dbo].[V_Instrument_Info_LCMSNet] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_Instrument_Info_LCMSNet] AS 
+SELECT TIN.IN_name AS Instrument,
+	   TIN.IN_name + CASE WHEN IN_usage = '' THEN '' ELSE + ' ' + IN_usage END AS NameAndUsage,
+       TIN.IN_status as Status,
+	   TP.SP_machine_name AS HostName,
+       TP.SP_vol_name_server AS ServerPath,
+       TP.SP_path AS SharePath,
+       TIN.IN_capture_method AS CaptureMethod
+FROM T_Instrument_Name AS TIN
+     INNER JOIN t_storage_path AS TP
+       ON TIN.IN_source_path_ID = TP.SP_path_ID
+WHERE (NOT (TIN.IN_name LIKE 'SW_%')) AND (TIN.IN_status = 'active') AND (TIN.IN_operations_role <> 'QC')
+
+
+
+GO
