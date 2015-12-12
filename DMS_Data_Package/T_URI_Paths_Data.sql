926d8fcfe13a9daaeaed4c385619fe49379aa476
alchemistmatt <matthew.monroe@pnnl.gov>
2015-12-12 01:03:38 -0800

    2015-12-12 auto-commit DMS_Data_Package

--- DMS_Data_Package/T_URI_Paths_Data.sql
+++ DMS_Data_Package/T_URI_Paths_Data.sql
@@ -1,6 +1,7 @@
 /****** Object:  Table [T_URI_Paths] ******/
-/****** RowCount: 2 ******/
+/****** RowCount: 3 ******/
 SET IDENTITY_INSERT [T_URI_Paths] ON
 INSERT INTO [T_URI_Paths] (URI_PathID, URI_Path, Entered) VALUES (1,'Undefined','4/2/2012 6:11:26 PM')
 INSERT INTO [T_URI_Paths] (URI_PathID, URI_Path, Entered) VALUES (100,'https://a4.my.emsl.pnl.gov/myemsl/cgi-bin/status/','9/25/2013 7:05:00 PM')
+INSERT INTO [T_URI_Paths] (URI_PathID, URI_Path, Entered) VALUES (101,'https://ingest.my.emsl.pnl.gov/myemsl/cgi-bin/status/','12/10/2015 10:18:22 PM')
 SET IDENTITY_INSERT [T_URI_Paths] OFF
