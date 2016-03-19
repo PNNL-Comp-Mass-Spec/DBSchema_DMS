c043f8f62e5deb4f27bf333e234ea09793a80d83
alchemistmatt <matthew.monroe@pnnl.gov>
2016-03-19 01:05:55 -0700

    2016-03-19 auto-commit Protein_Sequences

--- Protein_Sequences/T_Archived_Output_File_Collections_XRef.sql
+++ Protein_Sequences/T_Archived_Output_File_Collections_XRef.sql
@@ -18,13 +18,13 @@ GO
 CREATE NONCLUSTERED INDEX [IX_T_Archived_Output_File_Collections_XRef_Archived_File_ID] ON [dbo].[T_Archived_Output_File_Collections_XRef]
 (
 	[Archived_File_ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
 /****** Object:  Index [IX_T_Archived_Output_File_Collections_XRef_Protein_Collection_ID] ******/
 CREATE NONCLUSTERED INDEX [IX_T_Archived_Output_File_Collections_XRef_Protein_Collection_ID] ON [dbo].[T_Archived_Output_File_Collections_XRef]
 (
 	[Protein_Collection_ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_Archived_Output_File_Collections_XRef]  WITH CHECK ADD  CONSTRAINT [FK_T_Archived_Output_File_Collections_XRef_T_Archived_Output_Files] FOREIGN KEY([Archived_File_ID])
 REFERENCES [dbo].[T_Archived_Output_Files] ([Archived_File_ID])
