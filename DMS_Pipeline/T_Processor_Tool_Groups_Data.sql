af02ef730e868c2d37f407e0eb0fd660c8b2fad5
alchemistmatt <matthew.monroe@pnnl.gov>
2016-04-16 01:05:13 -0700

    2016-04-16 auto-commit DMS_Pipeline

--- DMS_Pipeline/T_Processor_Tool_Groups_Data.sql
+++ DMS_Pipeline/T_Processor_Tool_Groups_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_Processor_Tool_Groups] ******/
-/****** RowCount: 25 ******/
+/****** RowCount: 27 ******/
 /****** Columns: Group_ID, Group_Name, Enabled, Comment ******/
 INSERT INTO [T_Processor_Tool_Groups] VALUES (0,'Disabled Processors',0,'')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (1,'Proto_Storage_Servers',1,'')
@@ -7,9 +7,9 @@ INSERT INTO [T_Processor_Tool_Groups] VALUES (2,'Mash_Prep_Servers',-10,'Old box
 INSERT INTO [T_Processor_Tool_Groups] VALUES (3,'Poweredge 1750 XP Pubs',-10,'Old boxes; excessed')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (4,'Poweredge 1750 XP Pubs with ICR2LS',-10,'Old boxes; excessed')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (5,'Poweredge 1850 XP Pubs',-10,'')
-INSERT INTO [T_Processor_Tool_Groups] VALUES (6,'Poweredge 1850 Windows 7 Pubs with ICR2LS-Lite',1,'ICR2LS installed 2/19/2014')
-INSERT INTO [T_Processor_Tool_Groups] VALUES (7,'Poweredge 1950 quad core, 8 GB Pubs',1,'Running 64-bit Windows 7')
-INSERT INTO [T_Processor_Tool_Groups] VALUES (8,'Poweredge 1950 quad core, 8 GB Pubs',1,'Running 64-bit Windows 7')
+INSERT INTO [T_Processor_Tool_Groups] VALUES (6,'Poweredge 1850 Windows 7 Pubs with ICR2LS-Lite',-10,'ICR2LS installed 2/19/2014')
+INSERT INTO [T_Processor_Tool_Groups] VALUES (7,'Poweredge 1950 quad core, 8 GB Pubs',1,'Running 64-bit Windows 7; ICR2LS installed 4/14/2016')
+INSERT INTO [T_Processor_Tool_Groups] VALUES (8,'Poweredge 1950 quad core, 8 GB Pubs',1,'Running 64-bit Windows 7; ICR2LS installed 4/14/2016')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (9,'R610 Pubs, Group 1, Decon2LS_V2',1,'Have .NET 4; running LCMSFeatureFinder and Decon2LS_V2; 12 GB of ram')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (10,'Sequest Clusters',1,'')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (11,'Old single core XP pubs',-10,'Old boxes; excessed')
@@ -21,8 +21,10 @@ INSERT INTO [T_Processor_Tool_Groups] VALUES (17,'R610 Pubs, Group 2, Decon2LS_V
 INSERT INTO [T_Processor_Tool_Groups] VALUES (18,'Crowell Development Box',1,'')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (19,'R610 Pub-88, Decon2LS_V2',1,'Pub-88 is the only system with Bruker DataAnalysis and NOMSI')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (20,'R610 Pubs, Group 4, R 2.15.3',1,'Have .NET 4; running LCMSFeatureFinder and Decon2LS_V2; 24 GB of ram; have R 2.15.3')
-INSERT INTO [T_Processor_Tool_Groups] VALUES (21,'R610 Pubs, Group 3, Decon2LS_V2',1,'Have .NET 4; running LCMSFeatureFinder and Decon2LS_V2; 24 GB of ram')
+INSERT INTO [T_Processor_Tool_Groups] VALUES (21,'R610 Pubs, Group 3, Decon2LS_V2',1,'Have .NET 4; running LCMSFeatureFinder and Decon2LS_V2; 24 GB of ram (or more)')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (23,'Mallard',-10,'Has .NET 4; running LCMSFeatureFinder and Decon2LS_V2; 24 GB of ram; old since Mallard now has Group_ID=21')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (24,'PIC Pubs',-10,'VMs running on PIC.  Have .NET 4, R 2.15.3, and CompassXtract (thus supporting MSXML_Bruker). 24 GB of ram')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (25,'Kiebel Development Box',1,'')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (26,'Proto_Storage_Servers with MSGF+',1,'Proto-4, Proto-5, Proto-6, and Proto-11; each has 24 or 32 GB of ram and 8 or 12 cores')
+INSERT INTO [T_Processor_Tool_Groups] VALUES (27,'Supermicro 2022TG',1,'Supermicro Pubs with 32 cores, 128 GB of ram, and a single hard drive')
+INSERT INTO [T_Processor_Tool_Groups] VALUES (28,'Supermicro 6028TR',1,'Supermicro Pubs with 16 cores, 64 GB of ram, a 128 GB SSD, and two 1 TB drives')
