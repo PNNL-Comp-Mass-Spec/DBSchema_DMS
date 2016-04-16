8b4a6d9e0088336891d4776c0829d27868c77f71
alchemistmatt <matthew.monroe@pnnl.gov>
2016-04-16 01:05:43 -0700

    2016-04-16 auto-commit DMS5

--- DMS5/T_Cell_Culture_Type_Name_Data.sql
+++ DMS5/T_Cell_Culture_Type_Name_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_Cell_Culture_Type_Name] ******/
-/****** RowCount: 6 ******/
+/****** RowCount: 7 ******/
 SET IDENTITY_INSERT [T_Cell_Culture_Type_Name] ON
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (1,'Prokaryote')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (2,'Eukaryote')
@@ -7,4 +7,5 @@ INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (101,'Viral')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (102,'Protein/peptide standards')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (103,'Metabolite standards')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (104,'Community')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (105,'Biofluid')
 SET IDENTITY_INSERT [T_Cell_Culture_Type_Name] OFF
