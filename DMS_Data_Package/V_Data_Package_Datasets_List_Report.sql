371bdbf64895955e15757945e8f3910bbe229775
alchemistmatt <matthew.monroe@pnnl.gov>
2016-04-23 01:05:42 -0700

    2016-04-23 auto-commit DMS_Data_Package

--- DMS_Data_Package/V_Data_Package_Datasets_List_Report.sql
+++ DMS_Data_Package/V_Data_Package_Datasets_List_Report.sql
@@ -3,13 +3,48 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Data_Package_Datasets_List_Report
+
+
+CREATE VIEW [dbo].[V_Data_Package_Datasets_List_Report]
 AS
-SELECT        DPD.Data_Package_ID AS ID, DPD.Dataset, DPD.Dataset_ID, DPD.Experiment, DPD.Instrument, DPD.[Package Comment], DL.Campaign, DL.State, DL.Created, 
-                         DL.Rating, DL.[Dataset Folder Path], DL.[Archive Folder Path], DL.[Acq Start], DL.[Acq. End], DL.[Acq Length], DL.[Scan Count], DL.[LC Column], DL.[Separation Type], 
-                         DL.[Blocking Factor], DL.Block, DL.[Run Order], DL.Request, DPD.[Item Added], DL.Comment, DL.[Dataset Type] AS Type, DL.[EMSL Proposal]
-FROM            dbo.T_Data_Package_Datasets AS DPD INNER JOIN
-                         dbo.S_V_Dataset_List_Report_2 AS DL ON DPD.Dataset_ID = DL.ID
+SELECT DPD.Data_Package_ID AS ID,
+       DPD.Dataset,
+       DPD.Dataset_ID,
+       DPD.Experiment,
+       DPD.Instrument,
+       DPD.[Package Comment],
+       DL.Campaign,
+       DL.[State],
+       DL.Created,
+       DL.Rating,
+       DL.[Dataset Folder Path],
+       DL.[Archive Folder Path],
+       DL.[Acq Start],
+       DL.[Acq. End],
+       DL.[Acq Length],
+       DL.[Scan Count],
+       DL.[LC Column],
+       DL.[Separation Type],
+       DL.[Blocking Factor],
+       DL.[Block],
+       DL.[Run Order],
+       DL.Request,
+       DPD.[Item Added],
+       DL.[Comment],
+       DL.[Dataset Type] AS [Type],
+       DL.[EMSL Proposal],
+	   PSM.Jobs AS [PSM Jobs],
+	   PSM.Max_Total_PSMs AS [Max Total PSMs],
+	   PSM.Max_Unique_Peptides AS [Max Unique Peptides],
+	   PSM.Max_Unique_Proteins AS [Max Unique Proteins],
+	   PSM.Max_Unique_Peptides_FDR_Filter AS [Max Unique Peptides FDR Filter]
+FROM dbo.T_Data_Package_Datasets AS DPD
+     INNER JOIN dbo.S_V_Dataset_List_Report_2 AS DL
+       ON DPD.Dataset_ID = DL.ID
+	 LEFT OUTER JOIN dbo.S_V_Analysis_Job_PSM_Summary_Export PSM
+	   ON DPD.Dataset_ID = PSM.Dataset_ID
+
+
 
 GO
 GRANT SELECT ON [dbo].[V_Data_Package_Datasets_List_Report] TO [DMS_SP_User] AS [dbo]
