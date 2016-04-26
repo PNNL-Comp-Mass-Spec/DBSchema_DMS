2578058d0fa4960395f4cad9d0ca5e775226fd11
alchemistmatt <matthew.monroe@pnnl.gov>
2016-04-26 01:05:35 -0700

    2016-04-26 auto-commit DMS5

--- DMS5/T_MiscPaths_Data.sql
+++ DMS5/T_MiscPaths_Data.sql
@@ -5,7 +5,7 @@ INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (1,'na','DMS
 INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (3,'E:\DMS_InstSourceDirScans\','','InstrumentSourceScanDir         ')
 INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (4,'','http://gigasax/LC_Cart_Config/','LCCartConfigDocs                ')
 INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (5,'E:\DIM_Trigger\','','DIMTriggerFileDir               ')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (6,'\\proto-1\DB_Backups\Gigasax_Backup\','\\proto-1\DB_Backups\Gigasax_Backup\','Database Backup Path            ')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (6,'\\protoapps\DB_Backups\Gigasax_Backup\','\\protoapps\DB_Backups\Gigasax_Backup\','Database Backup Path            ')
 INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (8,'G:\SqlServerBackup\','G:\SqlServerBackup\','Database Backup Log Path        ')
 INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (9,'','','Redgate Backup Transfer Folder  ')
 INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function) VALUES (10,'\\gigasax\DMS_Organism_Files\','F:\DMS_Organism_Files\','DMSOrganismFiles                ')
