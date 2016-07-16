dc3e8fc206206adf1aee6d82e4fdb495cfebe299
alchemistmatt <matthew.monroe@pnnl.gov>
2016-07-16 01:04:48 -0700

    2016-07-16 auto-commit DMS5

--- DMS5/V_User_Entry.sql
+++ DMS5/V_User_Entry.sql
@@ -8,7 +8,7 @@ CREATE VIEW [dbo].[V_User_Entry]
 AS
 SELECT U_PRN AS Username,
        U_HID AS HanfordIDNum,
-       'Payroll, Last Name/First Name, and Email are auto-updated when "User Update" = Y' As EntryNote,
+       'Last Name/First Name, Email, and Payroll are auto-updated when "User Update" = Y' As EntryNote,
        U_Payroll AS Payroll,
        U_Name AS LastNameFirstName,
        U_email as Email,       
--- DMS5/V_User_List_Report_2.sql
+++ DMS5/V_User_List_Report_2.sql
@@ -4,7 +4,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_User_List_Report_2]
 AS
 -- Note that a few DMS Users have multiple EUS Person_ID values
@@ -13,21 +12,20 @@ AS
 SELECT U.ID,
        U.U_PRN AS [Username],
        U.U_HID AS [Hanford ID],
-       U.U_Payroll AS Payroll,
        U.U_Name AS Name,
        U.U_Status AS Status,
        dbo.GetUserOperationsList(U.ID) AS [Operations List],
        U.U_Comment as [Comment],
        U.U_created AS Created_DMS,
-       EU.PERSON_ID AS EUS_Person_ID,
-       ESS.Name AS EUS_Site_Status
+	   U.U_Payroll AS Payroll,	   
+       EU.PERSON_ID AS EUS_ID,
+       ESS.Name AS EUS_Status,
+	   CASE When Len(U.U_email) < 25 THEN U_email Else Substring(U.U_email, 1, 22) + '...' END AS EMail
 FROM T_EUS_Site_Status ESS
      INNER JOIN T_EUS_Users EU
        ON ESS.ID = EU.Site_Status
      RIGHT OUTER JOIN T_Users U
        ON EU.HID = U.U_HID
-
-
 
 
 GO
