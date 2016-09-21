54bce59ab3d5433b2b03c256daa6fecc8be94cf9
alchemistmatt <matthew.monroe@pnnl.gov>
2016-09-21 01:05:00 -0700

    2016-09-21 auto-commit DMS_Capture

--- DMS_Capture/T_Processor_Instrument_Data.sql
+++ DMS_Capture/T_Processor_Instrument_Data.sql
@@ -1,10 +1,9 @@
 /****** Object:  Table [T_Processor_Instrument] ******/
-/****** RowCount: 16 ******/
+/****** RowCount: 15 ******/
 /****** Columns: Processor_Name, Instrument_Name, Enabled, Comment ******/
-INSERT INTO [T_Processor_Instrument] VALUES ('Monroe_CTM','12T_FTICR_B',1,'Bruker')
-INSERT INTO [T_Processor_Instrument] VALUES ('Monroe_CTM','15T_FTICR',1,'Bruker')
-INSERT INTO [T_Processor_Instrument] VALUES ('Monroe_CTM','15T_FTICR_Imaging',1,'Bruker')
-INSERT INTO [T_Processor_Instrument] VALUES ('Monroe_CTM','IMS04_AgTOF05',1,'IMS Instrument')
+INSERT INTO [T_Processor_Instrument] VALUES ('Monroe_CTM','12T_FTICR_B',0,'Bruker')
+INSERT INTO [T_Processor_Instrument] VALUES ('Monroe_CTM','15T_FTICR',0,'Bruker')
+INSERT INTO [T_Processor_Instrument] VALUES ('Monroe_CTM','15T_FTICR_Imaging',0,'Bruker')
 INSERT INTO [T_Processor_Instrument] VALUES ('Proto-11_CTM_2','12T_FTICR_B',1,'Bruker')
 INSERT INTO [T_Processor_Instrument] VALUES ('Proto-11_CTM_2','15T_FTICR',1,'Bruker')
 INSERT INTO [T_Processor_Instrument] VALUES ('Proto-11_CTM_2','15T_FTICR_Imaging',1,'Bruker Imaging')
