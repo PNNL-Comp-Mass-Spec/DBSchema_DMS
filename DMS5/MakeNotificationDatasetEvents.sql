ba053f9c7a0d24acae7be10079a71bea98655c20
alchemistmatt <matthew.monroe@pnnl.gov>
2016-11-05 01:05:01 -0700

    2016-11-05 auto-commit DMS5

--- DMS5/MakeNotificationDatasetEvents.sql
+++ DMS5/MakeNotificationDatasetEvents.sql
@@ -14,56 +14,51 @@ CREATE PROCEDURE dbo.MakeNotificationDatasetEvents
 **
 **  Parameters:
 **
-**  Auth: grk
-**  Date: 04/02/2010 grk - Initial Release
-**        04/02/2010 mem - Updated the "Not Released" check to cover Dataset Rating -9 to 1
-**                       - Now also looking for "Released" datasets
+**  Auth:	grk
+**  Date:	04/02/2010 grk - Initial Release
+**			04/02/2010 mem - Updated the "Not Released" check to cover Dataset Rating -9 to 1
+**                         - Now also looking for "Released" datasets
+**			11/03/2016 mem - Fix bug that was failing to remove events of type 20 (Dataset Not Released) from T_Notification_Event
 **    
 *****************************************************/
 As
 	set nocount on
 
 	declare @myError int
-	set @myError = 0
-
 	declare @myRowCount int
+	set @myError = 0
 	set @myRowCount = 0
 
 	---------------------------------------------------
 	-- window for dataset activity
 	---------------------------------------------------
 	--
-	DECLARE @window DATETIME 
-	SET @window = DATEADD(DAY, -7, GETDATE())
+	DECLARE @window DATETIME = DATEADD(DAY, -7, GETDATE())
 
 	---------------------------------------------------
 	-- window for batch creation date
 	---------------------------------------------------
 	--
-	DECLARE @threshold DATETIME
-	SET @threshold = DATEADD(DAY, -365, GETDATE())
+	DECLARE @threshold DATETIME = DATEADD(DAY, -365, GETDATE())
 
 	---------------------------------------------------
 	-- earlier than batch creation window 
 	-- (default for datasets with null start time)
 	---------------------------------------------------
 	--
-	DECLARE @now datetime
-	SET  @now = GETDATE()
+	DECLARE @now datetime = GETDATE()
 	--
-	DECLARE @past DATETIME 
-	SET @past = '1/1/2000'
+	DECLARE @past DATETIME = '1/1/2000'
 	--
-	DECLARE @future DATETIME 
-	SET @future = DATEADD(MONTH, 3, @now)
+	DECLARE @future DATETIME = DATEADD(MONTH, 3, @now)
 
-	DECLARE @eventTypetID INT
+	DECLARE @eventTypeID INT
 
 	---------------------------------------------------
 	-- Temp table for events to be added
 	---------------------------------------------------
 	--
-	CREATE TABLE #ENV (
+	CREATE TABLE #Tmp_NewEvents (
 		Target_ID INT,
 		Event_Type int
 	)
@@ -72,60 +67,54 @@ As
 	-- Look for Datasets that were not released, are corrupt/bad, or are marked for "Rerun"
 	---------------------------------------------------
 	--
-	SET @eventTypetID = 20 -- 'Dataset Not Released'
+	SET @eventTypeID = 20 -- 'Dataset Not Released'
 
-	INSERT INTO #ENV
+	INSERT INTO #Tmp_NewEvents
 		( Target_ID,
 		  Event_Type 
 		)
 	SELECT
 	  T_Dataset.Dataset_ID,
-	  @eventTypetID
+	  @eventTypeID
 	FROM
 	  T_Dataset
 	WHERE
 	  ( T_Dataset.DS_rating BETWEEN -9 AND 1 )
 	AND 
 	  T_Dataset.DS_created BETWEEN @window AND @now
-		AND NOT EXISTS ( SELECT
-						*
-					   FROM
-						dbo.T_Notification_Event AS TNE
-					   WHERE
-						TNE.Target_ID = T_Dataset.Dataset_ID
-						AND TNE.Event_Type = @eventTypetID )
+		AND NOT EXISTS ( SELECT *
+					     FROM dbo.T_Notification_Event AS TNE
+					     WHERE TNE.Target_ID = T_Dataset.Dataset_ID
+						   AND TNE.Event_Type = @eventTypeID )
 	  
 
 	---------------------------------------------------
-	-- Look for Datasets that released
+	-- Look for Datasets that are released
 	---------------------------------------------------
 	--
-	SET @eventTypetID = 21 -- 'Dataset Released'
+	SET @eventTypeID = 21 -- 'Dataset Released'
 
-	INSERT INTO #ENV
+	INSERT INTO #Tmp_NewEvents
 		( Target_ID,
 		  Event_Type 
 		)
 	SELECT
 	  T_Dataset.Dataset_ID,
-	  @eventTypetID
+	  @eventTypeID
 	FROM
 	  T_Dataset
 	WHERE
 	  ( T_Dataset.DS_rating >= 2 )
 	AND 
 	  T_Dataset.DS_created BETWEEN @window AND @now
-		AND NOT EXISTS ( SELECT
-						*
-					   FROM
-						dbo.T_Notification_Event AS TNE
-					   WHERE
-						TNE.Target_ID = T_Dataset.Dataset_ID
-						AND TNE.Event_Type = @eventTypetID )
+		AND NOT EXISTS ( SELECT *
+					     FROM dbo.T_Notification_Event AS TNE
+					     WHERE TNE.Target_ID = T_Dataset.Dataset_ID
+						   AND TNE.Event_Type = @eventTypeID )
 
 
 /*
- SELECT * FROM #ENV
+ SELECT * FROM #Tmp_NewEvents
 */
 	---------------------------------------------------
 	-- add new events to table
@@ -136,28 +125,24 @@ As
 			  Target_ID
 			)
 	SELECT
-	  #ENV.Event_Type,
-	  #ENV.Target_ID
+	  #Tmp_NewEvents.Event_Type,
+	  #Tmp_NewEvents.Target_ID
 	FROM
-	  #ENV
+	  #Tmp_NewEvents
 	WHERE
-	  NOT EXISTS ( SELECT
-					*
-				   FROM
-					T_Notification_Event TNE
-				   WHERE
-					TNE.Target_ID = #ENV.Target_ID
-					AND TNE.Event_Type = #ENV.Event_Type )
+	  NOT EXISTS ( SELECT *
+				   FROM T_Notification_Event TNE
+				   WHERE TNE.Target_ID = #Tmp_NewEvents.Target_ID
+					 AND TNE.Event_Type = #Tmp_NewEvents.Event_Type )
 					
 	---------------------------------------------------
 	-- clean out batch events older than window
 	---------------------------------------------------
 	--
-	DELETE FROM
-		T_Notification_Event
-	WHERE
-		Event_Type = @eventTypetID
-		AND Entered < @window
+	DELETE FROM T_Notification_Event
+	WHERE Event_Type IN (20, 21) AND
+	      Entered < @window
+
 	
 
 GO
