88aa4ca6cce711c5f16f43bb983c1a79b5e3cd6d
alchemistmatt <matthew.monroe@pnnl.gov>
2016-12-07 01:03:45 -0800

    2016-12-07 auto-commit DMS5

--- DMS5/RenameDataset.sql
+++ DMS5/RenameDataset.sql
@@ -15,6 +15,7 @@ CREATE PROCEDURE dbo.RenameDataset
 **	Auth:	mem
 **			01/25/2013 mem - Initial version
 **			07/08/2016 mem - Now show old/new names and jobs even when @infoOnly is 0
+**			12/06/2016 mem - Include file rename statements
 **    
 *****************************************************/
 (
@@ -32,9 +33,19 @@ AS
 	set @myRowCount = 0
 
 	Declare @DatasetID int = 0
+	Declare @folderPath varchar(255) = ''
+
 	Declare @JobsToUpdate table (Job int not null)
 	Declare @Job int = 0
+	
+	Declare @suffixID int
+	Declare @fileSuffix varchar(64)
+	
 	Declare @Continue tinyint
+	Declare @Continue2 tinyint
+	
+	Declare @toolBaseName varchar(64)
+	Declare @resultsFolder varchar(128)
 
 	--------------------------------------------
 	-- Validate the inputs
@@ -87,7 +98,11 @@ AS
 		Set @message = 'Dataset not found using either the old name or the new name (' +  @DatasetNameOld + ' or ' + @DatasetNameNew + ')'
 		Goto Done
 	End
-
+			
+	-- Lookup the share folder for this dataset
+	SELECT @folderPath = Dataset_Folder_Path
+	FROM V_Dataset_Folder_Paths
+	WHERE Dataset_ID = @DatasetID
 
 	If @InfoOnly = 0 
 	Begin
@@ -103,12 +118,16 @@ AS
 			       DS_Created
 			FROM T_Dataset
 			WHERE Dataset_Num IN (@DatasetNameOld, @DatasetNameNew)
+			--
+			SELECT @myError = @@error, @myRowCount = @@rowcount
 			
 			UPDATE T_Dataset
 			SET Dataset_Num = @DatasetNameNew,
 			    DS_folder_name = @DatasetNameNew
-			WHERE Dataset_Num = @DatasetNameOld
-			
+			WHERE Dataset_ID = @DatasetID AND Dataset_Num = @DatasetNameOld
+			--
+			SELECT @myError = @@error, @myRowCount = @@rowcount
+
 			Set @message = 'Renamed dataset "' + @DatasetNameOld + '" to "' + @DatasetNameNew + '"'
 			print @message
 				
@@ -135,6 +154,12 @@ AS
 			WHERE Dataset_Num IN (@DatasetNameOld, @DatasetNameNew)
 	End
 
+	--------------------------------------------
+	-- Show example commands for renaming a .raw file
+	--------------------------------------------
+	--
+	Print 'Pushd ' + @folderPath
+	Print 'Move ' + @DatasetNameOld + '.raw ' + @DatasetNameNew + '.raw'
 
 	--------------------------------------------
 	-- Update jobs in the DMS_Capture database
@@ -147,12 +172,16 @@ AS
 	FROM DMS_Capture.dbo.T_Jobs 
 	WHERE dataset = @DatasetNameOld
 	ORDER BY Job
+	--
+	SELECT @myError = @@error, @myRowCount = @@rowcount
 
 	SELECT Job AS Capture_Job, Script, State, Dataset, @DatasetNameNew as Dataset_Name_New, Dataset_ID, Imported
 	FROM DMS_Capture.dbo.T_Jobs 
 	WHERE Job In (Select Job from @JobsToUpdate)
-		
-	If @InfoOnly = 0
+	--
+	SELECT @myError = @@error, @myRowCount = @@rowcount
+
+	If @InfoOnly = 0 And Exists (Select * From @JobsToUpdate)
 	Begin
 		Set @Continue = 1
 		Set @Job = 0
@@ -161,15 +190,21 @@ AS
 	Begin
 		Set @Continue = 0
 	End
-		
+
+	--------------------------------------------
+	-- Update analysis jobs in DMS_Capture if @InfoOnly is 0
+	--------------------------------------------
+	--
 	While @Continue = 1
 	Begin
 		SELECT TOP 1 @Job = Job
 		FROM @JobsToUpdate
 		WHERE Job > @Job
 		ORDER BY Job		
+		--
+		SELECT @myError = @@error, @myRowCount = @@rowcount
 		
-		If @@RowCount = 0
+		If @myRowCount = 0
 			Set @Continue = 0
 		Else
 		Begin
@@ -179,7 +214,10 @@ AS
 			
 			UPDATE DMS_Capture.dbo.T_Jobs 
 			Set Dataset = @DatasetNameNew
-			WHERE Job = @Job			
+			WHERE Job = @Job
+			--
+			SELECT @myError = @@error, @myRowCount = @@rowcount
+
 		End
 
 	End
@@ -195,12 +233,16 @@ AS
 	FROM DMS_Pipeline.dbo.T_Jobs 
 	WHERE Dataset = @DatasetNameOld
 	ORDER BY Job
+	--
+	SELECT @myError = @@error, @myRowCount = @@rowcount
 
 	SELECT Job AS Pipeline_Job, Script, State, Dataset, @DatasetNameNew as Dataset_Name_New, Dataset_ID, Imported
 	FROM DMS_Pipeline.dbo.T_Jobs 
 	WHERE Job In (Select Job from @JobsToUpdate)
+	--
+	SELECT @myError = @@error, @myRowCount = @@rowcount
 
-	If @InfoOnly = 0
+	If @InfoOnly = 0 And Exists (Select * From @JobsToUpdate)
 	Begin
 		Set @Continue = 1
 		Set @Job = 0
@@ -216,8 +258,10 @@ AS
 		FROM @JobsToUpdate
 		WHERE Job > @Job
 		ORDER BY Job		
+		--
+		SELECT @myError = @@error, @myRowCount = @@rowcount
 		
-		If @@RowCount = 0
+		If @myRowCount = 0
 			Set @Continue = 0
 		Else
 		Begin
@@ -227,11 +271,149 @@ AS
 			
 			UPDATE DMS_Pipeline.dbo.T_Jobs 
 			Set Dataset = @DatasetNameNew
-			WHERE Job = @Job			
-		End
+			WHERE Job = @Job
+			--
+			SELECT @myError = @@error, @myRowCount = @@rowcount
 
+		End
 	End
+
+	--------------------------------------------
+	-- Show example commands for renaming the job files
+	--------------------------------------------
+	--
+	CREATE TABLE #Tmp_Extensions (
+		SuffixID int identity(1,1),
+		FileSuffix varchar(64) NOT null
+	)
+
+	CREATE UNIQUE CLUSTERED INDEX #IX_Tmp_Extensions_ID on #Tmp_Extensions(SuffixID)
+	CREATE UNIQUE INDEX #IX_Tmp_Extensions_Suffix on #Tmp_Extensions(FileSuffix)	
+
+	DELETE FROM @JobsToUpdate
+	
+	INSERT INTO @JobsToUpdate (Job)
+	SELECT Job 
+	FROM V_Analysis_Job_Export 
+	WHERE Dataset = @DatasetNameOld
+	ORDER BY Job
+	
+	Set @Continue = 1
+	Set @Job = 0
+	
+	Declare @jobFileUpdateCount int = 0
+	
+	While @Continue > 0
+	Begin
+		SELECT TOP 1 @Job = Job
+		FROM @JobsToUpdate
+		WHERE Job > @Job
+		ORDER BY Job		
+		--
+		SELECT @myError = @@error, @myRowCount = @@rowcount
+
+		Truncate Table #Tmp_Extensions
+		
+		If @myRowCount = 0
+		Begin
+			--------------------------------------------
+			-- Show example commands for renaming QC files
+			--------------------------------------------
+			--
+			Set @Continue = 0
+			Set @resultsFolder = 'QC'
 			
+			Insert Into #Tmp_Extensions (FileSuffix) Values 
+				('_BPI_MS.png'),('_BPI_MSn.png'),
+				('_HighAbu_LCMS.png'),('_HighAbu_LCMS_MSn.png'),
+				('_LCMS.png'),('_LCMS_MSn.png'),
+				('_TIC.png'),('_DatasetInfo.xml')
+		End
+		Else
+		Begin
+			SELECT @toolBaseName = Tool.AJT_toolBasename,
+				    @resultsFolder = ResultsFolder
+			FROM V_Analysis_Job_Export AJE
+				    INNER JOIN T_Analysis_Tool Tool
+				    ON AJE.AnalysisTool = Tool.AJT_toolName
+			WHERE Job = @Job
+			--
+			SELECT @myError = @@error, @myRowCount = @@rowcount
+
+			If @myRowCount > 0
+			Begin
+				If @toolBaseName = 'Decon2LS'
+				Begin
+					Insert Into #Tmp_Extensions (FileSuffix) Values 
+						('_isos.csv'), ('_scans.csv'),
+						('_BPI_MS.png'), ('_HighAbu_LCMS.png'), ('_HighAbu_LCMS_zoom.png'),
+						('_LCMS.png'), ('_LCMS_zoom.png'),
+						('_TIC.png'), ('_log.txt')
+				End
+				
+				If @toolBaseName = 'MASIC'
+				Begin
+					Insert Into #Tmp_Extensions (FileSuffix) Values 
+						('_MS_scans.csv'), ('_MSMS_scans.csv'),('_MSMethod.txt'), 
+						('_ScanStats.txt'), ('_ScanStatsConstant.txt'), ('_ScanStatsEx.txt'), 
+						('_SICstats.txt'),('_DatasetInfo.xml'),('_SICs.zip')
+				End
+
+				If @toolBaseName Like 'MSGFPlus%'
+				Begin
+					Insert Into #Tmp_Extensions (FileSuffix) Values 
+						('_msgfplus.mzid.gz'),('_msgfplus_fht.txt'), ('_msgfplus_fht_MSGF.txt'),
+						('_msgfplus_PepToProtMap.txt'), ('_msgfplus_PepToProtMapMTS.txt'),
+						('_msgfplus_syn.txt'), ('_msgfplus_syn_ModDetails.txt'),
+						('_msgfplus_syn_ModSummary.txt'),('_msgfplus_syn_MSGF.txt'),
+						('_msgfplus_syn_ProteinMods.txt'),('_msgfplus_syn_ResultToSeqMap.txt'),
+						('_msgfplus_syn_SeqInfo.txt'),('_msgfplus_syn_SeqToProteinMap.txt'),
+						('_ScanType.txt'),('_pepXML.zip')
+
+				End
+			End
+		End
+		
+		Print ''
+		Print 'cd ' + @resultsFolder
+		
+		If Exists (Select * From #Tmp_Extensions)
+			Set @Continue2 = 1
+		Else
+			Set @Continue2 = 0
+			
+		Set @suffixID = 0
+		Set @fileSuffix = ''
+		
+		While @Continue2 = 1
+		Begin
+			SELECT TOP 1 @suffixID = SuffixID,
+					     @fileSuffix = FileSuffix
+			FROM #Tmp_Extensions
+			WHERE SuffixID > @suffixID
+			ORDER BY SuffixID
+			--
+			SELECT @myError = @@error, @myRowCount = @@rowcount
+
+			If @myRowCount = 0
+				Set @Continue2 = 0
+			Else
+			Begin
+				Print 'Move ' + @DatasetNameOld + @fileSuffix + ' ' + @DatasetNameNew + @fileSuffix
+				Set @jobFileUpdateCount = @jobFileUpdateCount + 1
+			End
+
+		End
+		
+		Print 'cd ..'
+		
+	End
+
+	If @jobFileUpdateCount > 0
+	Begin
+		Select 'See the console output for ' + Cast(@jobFileUpdateCount as varchar(9)) + ' job file update commands' as Comment
+	End
+	
  	---------------------------------------------------
 	-- Done
  	---------------------------------------------------
