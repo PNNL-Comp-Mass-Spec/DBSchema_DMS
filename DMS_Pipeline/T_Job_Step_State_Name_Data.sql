c40a94be1c43ed467da0db4b9cb22d5f32730372
alchemistmatt <matthew.monroe@pnnl.gov>
2017-04-12 01:04:52 -0700

    2017-04-12 auto-commit DMS_Pipeline

--- DMS_Pipeline/T_Job_Step_State_Name_Data.sql
+++ DMS_Pipeline/T_Job_Step_State_Name_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_Job_Step_State_Name] ******/
-/****** RowCount: 8 ******/
+/****** RowCount: 9 ******/
 /****** Columns: ID, Name, Description ******/
 INSERT INTO [T_Job_Step_State_Name] VALUES (1,'Waiting','Step has not been run yet, and it cannot be assigned yet.')
 INSERT INTO [T_Job_Step_State_Name] VALUES (2,'Enabled','Step can be run because all its dependencies have been satisfied')
@@ -8,4 +8,5 @@ INSERT INTO [T_Job_Step_State_Name] VALUES (4,'Running','Step has been assigned 
 INSERT INTO [T_Job_Step_State_Name] VALUES (5,'Completed','Manager has successfully completed step')
 INSERT INTO [T_Job_Step_State_Name] VALUES (6,'Failed','Manager could not complete step successfully')
 INSERT INTO [T_Job_Step_State_Name] VALUES (7,'Holding','Established and removed manually when deus ex machina is necessary')
+INSERT INTO [T_Job_Step_State_Name] VALUES (9,'Running_Remote','Job is running on a remote resource (Linux or Cloud)')
 INSERT INTO [T_Job_Step_State_Name] VALUES (10,'Holding_Staging','Waiting for the Aurora data archive to stage the required files')
