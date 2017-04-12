a028cd92f7cbdcc9ec46d31f0e55aa48d3207efe
alchemistmatt <matthew.monroe@pnnl.gov>
2017-04-12 16:39:41 -0700

    Add V_Instrument_Usage_Report_Export

---
+++ DMS5/V_Instrument_Usage_Report_Export.sql
@@ -0,0 +1,32 @@
+/****** Object:  View [dbo].[V_Instrument_Usage_Report_Export] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_Instrument_Usage_Report_Export]
+AS
+SELECT InstUsage.EMSL_Inst_ID,
+       InstName.IN_Name AS [Instrument],
+       InstUsage.[Type],
+       InstUsage.[Start],
+       InstUsage.[Minutes],
+       InstUsage.Proposal,
+       InstUsageType.[Name] AS [Usage],
+       InstUsage.Users,
+       InstUsage.Operator,
+       InstUsage.[Comment],
+       InstUsage.[Year],
+       InstUsage.[Month],
+       InstUsage.ID,
+	   InstUsage.Seq,       
+       InstUsage.Updated,
+	   InstUsage.UpdatedBy
+FROM T_EMSL_Instrument_Usage_Report InstUsage
+     INNER JOIN T_Instrument_Name InstName
+       ON InstUsage.DMS_Inst_ID = InstName.Instrument_ID
+     LEFT OUTER JOIN T_EMSL_Instrument_Usage_Type InstUsageType
+       ON InstUsage.Usage_Type = InstUsageType.ID
+
+
+GO
