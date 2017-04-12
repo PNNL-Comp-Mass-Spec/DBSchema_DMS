673f83c4a5d1bbcbf8d9172876f58b5da33e9dd7
alchemistmatt <matthew.monroe@pnnl.gov>
2017-04-12 14:13:58 -0700

    Remove references to old synonyms

--- DMS_Capture/V_DMS_PrepLC_Job_Parameters.sql
+++ DMS_Capture/V_DMS_PrepLC_Job_Parameters.sql
@@ -7,27 +7,29 @@ GO
 CREATE view [dbo].[V_DMS_PrepLC_Job_Parameters] AS
 SELECT TPR.ID,
        TPR.Instrument,
-       CASE WHEN TSrc.SP_path_ID IS NULL THEN 'ProteomicsData\' ELSE TSrc.SP_path END AS sourcePath,
-       CASE WHEN TSrc.SP_path_ID IS NULL THEN '\\Not_Defined_see_T_Prep_Instrument_Storage.bionet\' ELSE TSrc.SP_vol_name_server END AS sourceVol,
-       CASE WHEN TSrc.SP_path_ID IS NULL THEN 0 ELSE TSrc.SP_path_ID END As SP_path_ID,
-       CASE WHEN TStor.SP_path_ID IS NULL THEN 0 ELSE TStor.SP_path_ID END AS Storage_Path_ID,
-       CASE WHEN TStor.SP_path_ID IS NULL THEN 'Not_Defined_see_T_Prep_Instrument_Storage' Else TStor.SP_machine_name END AS Storage_Server_Name,
-       CASE WHEN TStor.SP_path_ID IS NULL THEN '\\Not_Defined_see_T_Prep_Instrument_Storage\' ELSE TStor.SP_vol_name_server END AS Storage_Vol,
-       CASE WHEN TStor.SP_path_ID IS NULL THEN 'Sample_Prep_Repository\HPLC_Run_Seq\' + TPR.Instrument + '\' Else TStor.SP_path END AS Storage_Path,
-       CASE WHEN TStor.SP_path_ID IS NULL THEN '\\Not_Defined_see_T_Prep_Instrument_Storage\' ELSE TStor.SP_vol_name_client END AS Storage_Vol_External,
+       -- CASE WHEN TSrc.SP_path_ID IS NULL THEN 'ProteomicsData\' ELSE TSrc.SP_path END AS sourcePath,
+       -- CASE WHEN TSrc.SP_path_ID IS NULL THEN '\\Not_Defined_see_T_Prep_Instrument_Storage.bionet\' ELSE TSrc.SP_vol_name_server END AS sourceVol,
+       -- CASE WHEN TSrc.SP_path_ID IS NULL THEN 0 ELSE TSrc.SP_path_ID END As SP_path_ID,
+       -- CASE WHEN TStor.SP_path_ID IS NULL THEN 0 ELSE TStor.SP_path_ID END AS Storage_Path_ID,
+       -- CASE WHEN TStor.SP_path_ID IS NULL THEN 'Not_Defined_see_T_Prep_Instrument_Storage' Else TStor.SP_machine_name END AS Storage_Server_Name,
+       -- CASE WHEN TStor.SP_path_ID IS NULL THEN '\\Not_Defined_see_T_Prep_Instrument_Storage\' ELSE TStor.SP_vol_name_server END AS Storage_Vol,
+       -- CASE WHEN TStor.SP_path_ID IS NULL THEN 'Sample_Prep_Repository\HPLC_Run_Seq\' + TPR.Instrument + '\' Else TStor.SP_path END AS Storage_Path,
+       -- CASE WHEN TStor.SP_path_ID IS NULL THEN '\\Not_Defined_see_T_Prep_Instrument_Storage\' ELSE TStor.SP_vol_name_client END AS Storage_Vol_External,
+       'ProteomicsData\' AS sourcePath,
+       '\\Not_Defined_see_T_Prep_Instrument_Storage.bionet\' AS sourceVol,
+       0 As SP_path_ID,
+       0 AS Storage_Path_ID,
+       'Not_Defined_see_T_Prep_Instrument_Storage' AS Storage_Server_Name,
+       '\\Not_Defined_see_T_Prep_Instrument_Storage\' AS Storage_Vol,
+       'Sample_Prep_Repository\HPLC_Run_Seq\' + TPR.Instrument + '\' AS Storage_Path,
+       '\\Not_Defined_see_T_Prep_Instrument_Storage\' AS Storage_Vol_External,
        '' AS SourceFolderName,
        '' AS Comment,
        '' AS Job,
-       TPI.Capture_Method
+       'fso' as Capture_Method
 FROM S_DMS_T_Prep_LC_Run AS TPR
-     INNER JOIN S_DMS_T_Prep_Instruments AS TPI
-       ON TPI.Name = TPR.Instrument
-     LEFT OUTER JOIN S_DMS_T_Prep_Instrument_Storage AS TSrc
-       ON TPR.Instrument = TSrc.SP_instrument_name AND
-          TSrc.SP_function = 'inbox'
-     LEFT OUTER JOIN S_DMS_T_Prep_Instrument_Storage AS TStor
-       ON TPR.Instrument = TStor.SP_instrument_name AND
-          TStor.SP_function = 'raw-storage'
+     -- LEFT OUTER JOIN S_DMS_T_Prep_Instrument_Storage AS TSrc  ON TPR.Instrument = TSrc.SP_instrument_name  AND TSrc.SP_function  = 'inbox'
+     -- LEFT OUTER JOIN S_DMS_T_Prep_Instrument_Storage AS TStor ON TPR.Instrument = TStor.SP_instrument_name AND TStor.SP_function = 'raw-storage'
 
 
 GO
