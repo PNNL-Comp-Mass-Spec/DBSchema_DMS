fed111487a352ec305a6c6cf400f67e0d2e02322
alchemistmatt <matthew.monroe@pnnl.gov>
2017-04-19 01:04:20 -0700

    2017-04-19 auto-commit Protein_Sequences

--- Protein_Sequences/T_Collection_Organism_Xref_Data.sql
+++ Protein_Sequences/T_Collection_Organism_Xref_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_Collection_Organism_Xref] ******/
-/****** RowCount: 1941 ******/
+/****** RowCount: 1942 ******/
 SET IDENTITY_INSERT [T_Collection_Organism_Xref] ON
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (42,1001,1003)
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (12,1002,1002)
@@ -1942,4 +1942,5 @@ INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (2000,2935,1556)
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (2001,2936,1155)
 INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (2002,2937,1131)
+INSERT INTO [T_Collection_Organism_Xref] (ID, Protein_Collection_ID, Organism_ID) VALUES (2003,2938,8)
 SET IDENTITY_INSERT [T_Collection_Organism_Xref] OFF
