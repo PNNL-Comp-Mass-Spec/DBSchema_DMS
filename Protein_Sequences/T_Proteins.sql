9166557e11a622c55482c1c7561c8e561e324deb
alchemistmatt <matthew.monroe@pnnl.gov>
2017-05-27 01:03:51 -0700

    2017-05-27 auto-commit Protein_Sequences

--- Protein_Sequences/T_Proteins.sql
+++ Protein_Sequences/T_Proteins.sql
@@ -22,18 +22,6 @@ CREATE TABLE [dbo].[T_Proteins](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
-/****** Object:  Index [_dta_index_T_Proteins_18_117575457__K1] ******/
-CREATE NONCLUSTERED INDEX [_dta_index_T_Proteins_18_117575457__K1] ON [dbo].[T_Proteins]
-(
-	[Protein_ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_T_Proteins_Average_Mass] ******/
-CREATE NONCLUSTERED INDEX [IX_T_Proteins_Average_Mass] ON [dbo].[T_Proteins]
-(
-	[Average_Mass] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
-GO
 /****** Object:  Index [IX_T_Proteins_DateCreated] ******/
 CREATE NONCLUSTERED INDEX [IX_T_Proteins_DateCreated] ON [dbo].[T_Proteins]
 (
