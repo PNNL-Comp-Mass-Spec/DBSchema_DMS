c8a3ee9a626a7690f512920efc75bdcb01659e49
alchemistmatt <matthew.monroe@pnnl.gov>
2017-06-13 15:11:17 -0700

    Update the index on T_File_Attachments to include Entity_ID

--- DMS5/T_File_Attachment.sql
+++ DMS5/T_File_Attachment.sql
@@ -30,12 +30,13 @@ GO
 SET ANSI_PADDING ON
 
 GO
-/****** Object:  Index [IX_T_File_Attachment_EntityType_Active] ******/
-CREATE NONCLUSTERED INDEX [IX_T_File_Attachment_EntityType_Active] ON [dbo].[T_File_Attachment]
+/****** Object:  Index [IX_T_File_Attachment_EntityType_Active_include_EntityID] ******/
+CREATE NONCLUSTERED INDEX [IX_T_File_Attachment_EntityType_Active_include_EntityID] ON [dbo].[T_File_Attachment]
 (
 	[Entity_Type] ASC,
 	[Active] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
+)
+INCLUDE ( 	[Entity_ID]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_File_Attachment] ADD  CONSTRAINT [DF_T_File_Attachment_Created]  DEFAULT (getdate()) FOR [Created]
 GO
