0df5509701dc33593447f5ef5f8781bdf5796e0b
alchemistmatt <matthew.monroe@pnnl.gov>
2017-06-13 10:48:07 -0700

    Change special Sample Prep Request state "Closed (containers and material)" to have ID 99

--- DMS5/V_Sample_Prep_Request_State_Picklist.sql
+++ DMS5/V_Sample_Prep_Request_State_Picklist.sql
@@ -3,10 +3,16 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-create view V_Sample_Prep_Request_State_Picklist as 
-SELECT State_Name AS val, State_Name AS ex,  State_ID FROM T_Sample_Prep_Request_State_Name WHERE (State_ID > 0) 
+
+CREATE view [dbo].[V_Sample_Prep_Request_State_Picklist] as 
+SELECT State_Name AS val,
+       State_Name AS ex,
+       State_ID
+FROM T_Sample_Prep_Request_State_Name
+WHERE (Active = 1)
 UNION
-SELECT 'Closed (containers and material)' AS val, 'Closed (containers and material)' AS ex, 6 AS State_ID
+SELECT 'Closed (containers and material)' AS val, 'Closed (containers and material)' AS ex, 99 AS State_ID
+
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Sample_Prep_Request_State_Picklist] TO [DDL_Viewer] AS [dbo]
