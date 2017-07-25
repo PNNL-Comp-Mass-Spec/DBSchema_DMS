dc12291c5045184bd898acefb46a904636c3a9ee
alchemistmatt <matthew.monroe@pnnl.gov>
2017-07-24 20:01:20 -0700

    Move query logic from V_Run_Assignment into V_Run_Planning_Report

--- DMS5/V_Run_Planning_Report.sql
+++ DMS5/V_Run_Planning_Report.sql
@@ -4,29 +4,30 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE view [dbo].[V_Run_Planning_Report] as
-SELECT  GroupQ.[Inst. Group] ,
-        GroupQ.[DS Type] ,
-        GroupQ.[Run Count] ,
-        GroupQ.Blocked ,
-        GroupQ.BlkMissing ,
+CREATE VIEW [dbo].[V_Run_Planning_Report] as
+SELECT  GroupQ.[Inst. Group],
+        GroupQ.[DS Type],
+        GroupQ.[Run Count],
+        GroupQ.Blocked,
+        GroupQ.BlkMissing,
         GroupQ.[Request Name],
-        RequestLookupQ.RDS_BatchID AS Batch ,
-        GroupQ.Requester ,
-        DATEDIFF(DAY, GroupQ.[Date Created], GETDATE()) AS [Days in Queue] ,
-        GroupQ.[Days in Prep Queue] ,
+        RequestLookupQ.RDS_BatchID AS Batch,
+        GroupQ.Requester,
+        DATEDIFF(DAY, GroupQ.[Date Created], GETDATE()) AS [Days in Queue],
+        GroupQ.[Days in Prep Queue],
         Convert(decimal(10, 1), TAC.Actual_Hours) As Actual_Hours,
-        TIGA.Allocated_Hours ,
-        GroupQ.[Separation Group] ,
-        RequestLookupQ.RDS_comment AS [Comment] ,
-        GroupQ.[Min Request] ,
-        GroupQ.[Work Package] ,
+        TIGA.Allocated_Hours,
+        GroupQ.[Separation Group],
+        RequestLookupQ.RDS_comment AS [Comment],
+        GroupQ.[Min Request],
+        GroupQ.[Work Package],
 		GroupQ.[WP State],
-        GroupQ.Proposal ,
-        TEUT.Name AS [Usage] ,
-        GroupQ.Locked ,
-        GroupQ.[Last Ordered] ,
-        GroupQ.[Request Name Code] ,
+        GroupQ.Proposal,
+		GroupQ.[Proposal Type],
+        TEUT.Name AS [Usage],
+        GroupQ.Locked,
+        GroupQ.[Last Ordered],
+        GroupQ.[Request Name Code],
         CASE WHEN DATEDIFF(DAY, GroupQ.[Date Created], GETDATE()) <= 30
              THEN 30  -- Request is 0 to 30 days old
              WHEN DATEDIFF(DAY, GroupQ.[Date Created], GETDATE()) <= 60
@@ -36,76 +37,95 @@ SELECT  GroupQ.[Inst. Group] ,
              ELSE 120                                                            -- Request is over 90 days old
         END AS #DaysInQueue,
 		WPActivationState AS #WPActivationState
-FROM    ( SELECT    [Inst. Group] ,
-                    MIN(RequestID) AS [Min Request] ,
-                    COUNT(RequestName) AS [Run Count] ,
+FROM    ( SELECT    [Inst. Group],
+                    MIN(RequestID) AS [Min Request],
+                    COUNT(RequestName) AS [Run Count],
                     MIN(Request_Prefix) AS [Request Name],
-                    Requester ,
-                    MIN(Request_Created) AS [Date Created] ,
-                    [Separation Group] ,
-                    [DS Type] ,
-                    [Work Package] ,
-					[WP State] ,
-					WPActivationState ,
-                    Proposal ,
-                    Locked ,
-                    [Last Ordered] ,
-                    [Request Name Code] ,
-                    MAX([Days in Prep Queue]) AS [Days in Prep Queue] ,
-                    SUM(BlkMissing) AS BlkMissing ,
-                    SUM(Blocked) AS Blocked					
-          FROM      ( SELECT    RA.Instrument AS [Inst. Group] ,
-                                RA.[Separation Group] ,
-                                RA.[Type] AS [DS Type] ,
-                                RA.Request AS RequestID ,
-                                RA.Name AS RequestName ,
-                                LEFT(RA.Name, 20)
-                                          + CASE WHEN LEN(RA.Name) > 20
-                                                 THEN '...'
-                                                 ELSE ''
-                                            END AS [Request_Prefix] ,
-                                RA.[Request Name Code] ,
-                                RA.Requester ,
-                                RA.Created AS Request_Created ,
-                                RA.[Work Package] ,
-								ISNULL(CC.Activation_State_Name, '') AS [WP State],
-								CC.Activation_State AS WPActivationState,
-                                RA.Proposal ,
-                                RRB.Locked ,
-                                RA.Batch,
-                                CONVERT(DATETIME, FLOOR(CONVERT(FLOAT, RRB.Last_Ordered))) AS [Last Ordered] ,
-                                CASE WHEN SPR.ID = 0 THEN NULL
-                                     ELSE QT.[Days In Queue]
-                                END AS [Days in Prep Queue] ,
-                                CASE WHEN ISNULL(SPR.BlockAndRandomizeRuns, '') = 'yes'
-                                          AND ( ISNULL(RA.Block, '') = ''
-                                                OR ISNULL(RA.[Run Order], '') = ''
-                                              ) THEN 1
-                                     ELSE 0
-                                END AS BlkMissing ,
-                                CASE WHEN ISNULL(RA.Block, '') <> ''
-                                          AND ISNULL(RA.[Run Order], '') <> ''
-                                     THEN 1
-                                     ELSE 0
-                                END AS Blocked
-                      FROM      V_Run_Assignment AS RA
-                                INNER JOIN T_Requested_Run_Batches AS RRB ON RA.Batch = RRB.ID
-                                INNER JOIN T_Experiments AS E ON RA.[Experiment ID] = E.Exp_ID
-                                INNER JOIN T_Sample_Prep_Request AS SPR ON E.EX_sample_prep_request_ID = SPR.ID
-                                LEFT OUTER JOIN V_Sample_Prep_Request_Queue_Times AS QT ON SPR.ID = QT.Request_ID
-								LEFT OUTER JOIN V_Charge_Code_Status AS CC ON RA.[Work Package] = CC.Charge_Code                                
-                      WHERE     ( RA.Status = 'Active' )
-                    ) AS RequestQ
-          GROUP BY  [Inst. Group] ,
-                    [Separation Group] ,
-                    [DS Type] ,
-                    [Request Name Code] ,
-                    Requester ,
-                    [Work Package] ,
-					[WP State] ,
-					WPActivationState ,
+                    Requester,
+                    MIN(Request_Created) AS [Date Created],
+                    [Separation Group],
+                    [DS Type],
+                    [Work Package],
+					[WP State],
+					WPActivationState,
                     Proposal,
-                    Locked ,
+					[Proposal Type],
+                    Locked,
+                    [Last Ordered],
+                    [Request Name Code],
+                    MAX([Days in Prep Queue]) AS [Days in Prep Queue],
+                    SUM(BlkMissing) AS BlkMissing,
+                    SUM(Blocked) AS Blocked					
+          FROM      ( SELECT RR.RDS_instrument_name AS [Inst. Group],
+                             RR.RDS_Sec_Sep AS [Separation Group],
+                             DTN.DST_Name AS [DS Type],
+                             RR.ID AS RequestID,
+                             RR.RDS_Name AS RequestName,
+                             LEFT(RR.RDS_Name, 20) + CASE
+                                                         WHEN LEN(RR.RDS_Name) > 20 THEN '...'
+                                                         ELSE ''
+                                                     END AS [Request_Prefix],
+                             RR.RDS_NameCode AS [Request Name Code],
+                             U.U_Name AS Requester,
+                             RR.RDS_created AS Request_Created,
+                             RR.RDS_WorkPackage AS [Work Package],
+                             ISNULL(CC.Activation_State_Name, '') AS [WP State],
+                             CC.Activation_State AS WPActivationState,
+                             RR.RDS_EUS_Proposal_ID AS Proposal,
+                             EPT.Abbreviation AS [Proposal Type],
+                             RRB.Locked,
+                             RR.RDS_BatchID AS Batch,
+                             CONVERT(datetime, FLOOR(CONVERT(float, RRB.Last_Ordered))) AS [Last Ordered],
+                             CASE
+                                 WHEN SPR.ID = 0 THEN NULL
+                                 ELSE QT.[Days In Queue]
+                             END AS [Days in Prep Queue],
+                             CASE
+                                 WHEN ISNULL(SPR.BlockAndRandomizeRuns, '') = 'yes' AND
+                                      (ISNULL(RR.RDS_Block, '') = '' OR
+                                       ISNULL(RR.RDS_Run_Order, '') = '') THEN 1
+                                 ELSE 0
+                             END AS BlkMissing,
+                             CASE
+                                 WHEN ISNULL(RR.RDS_Block, '') <> '' AND
+                                      ISNULL(RR.RDS_Run_Order, '') <> '' THEN 1
+                                 ELSE 0
+                             END AS Blocked
+                      FROM T_DatasetTypeName AS DTN
+                           INNER JOIN T_Requested_Run AS RR
+                             ON DTN.DST_Type_ID = RR.RDS_type_ID
+                           INNER JOIN T_Users AS U
+                             ON RR.RDS_Oper_PRN = U.U_PRN
+                           INNER JOIN T_Experiments AS E
+                             ON RR.Exp_ID = E.Exp_ID
+                           INNER JOIN T_EUS_UsageType AS EUT
+                             ON RR.RDS_EUS_UsageType = EUT.ID
+                           INNER JOIN T_Requested_Run_Batches AS RRB
+                             ON RR.RDS_BatchID = RRB.ID
+                           INNER JOIN T_Sample_Prep_Request AS SPR
+                             ON E.EX_sample_prep_request_ID = SPR.ID
+                           LEFT OUTER JOIN V_Sample_Prep_Request_Queue_Times AS QT
+                             ON SPR.ID = QT.Request_ID
+                           LEFT OUTER JOIN V_Charge_Code_Status AS CC
+                             ON RR.RDS_WorkPackage = CC.Charge_Code
+                           LEFT OUTER JOIN T_EUS_Proposals AS EUP
+                             ON RR.RDS_EUS_Proposal_ID = EUP.Proposal_ID
+                           LEFT OUTER JOIN T_EUS_Proposal_Type EPT
+                             ON EUP.Proposal_Type = EPT.Proposal_Type
+                      WHERE RR.RDS_Status = 'Active' AND
+                            RR.DatasetID IS NULL
+                    ) AS RequestQ
+          GROUP BY  [Inst. Group],
+                    [Separation Group],
+                    [DS Type],
+                    [Request Name Code],
+                    Requester,
+                    [Work Package],
+					[WP State],
+					WPActivationState,
+                    Proposal,
+					[Proposal Type],
+                    Locked,
                     [Last Ordered],
                     Batch
         ) AS GroupQ
@@ -113,8 +133,8 @@ FROM    ( SELECT    [Inst. Group] ,
         INNER JOIN T_EUS_UsageType AS TEUT ON RequestLookupQ.RDS_EUS_UsageType = TEUT.ID
         LEFT OUTER JOIN T_Cached_Instrument_Usage_by_Proposal AS TAC ON TAC.IN_Group = GroupQ.[Inst. Group]
                                     AND TAC.EUS_Proposal_ID = GroupQ.Proposal
-        LEFT OUTER JOIN ( SELECT    QG.IN_Group ,
-                                    QIA.Proposal_ID ,
+        LEFT OUTER JOIN ( SELECT    QG.IN_Group,
+                                    QIA.Proposal_ID,
                                     QIA.Allocated_Hours
                           FROM      T_Instrument_Group AS QG
                                     INNER JOIN T_Instrument_Allocation AS QIA ON QG.Allocation_Tag = QIA.Allocation_Tag
