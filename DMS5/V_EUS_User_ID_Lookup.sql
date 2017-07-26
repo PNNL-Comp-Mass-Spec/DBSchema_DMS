b7abab570f399f138dbd9f6726fbff0e07a773c7
alchemistmatt <matthew.monroe@pnnl.gov>
2017-07-25 18:12:46 -0700

    Assure that HID is not null in T_EUS_Users

--- DMS5/V_EUS_User_ID_Lookup.sql
+++ DMS5/V_EUS_User_ID_Lookup.sql
@@ -12,22 +12,24 @@ SELECT U.U_PRN AS [Username],
        U.ID AS DMS_User_ID,
        U.U_created AS Created_DMS,
 	   U.U_Status AS DMS_Status,
-       EU.EUS_Person_ID,
-	   EU.EUS_Name,
-       EU.EUS_Site_Status
+       LookupQ.EUS_Person_ID,
+	   LookupQ.EUS_Name,
+       LookupQ.EUS_Site_Status
 FROM T_Users U
      INNER JOIN ( -- A few users have multiple EUS Person_ID values
-                       -- We use EU.RowRank = 1 when joining to this subquery to just keep one of those rows
-                       SELECT EU.PERSON_ID AS EUS_Person_ID,
-							  EU.NAME_FM AS EUS_Name,
-                              ESS.Name AS EUS_Site_Status,
-							  EU.HID AS EUS_Hanford_ID,                               
-                              ROW_NUMBER() Over (Partition By EU.HID ORDER BY EU.PERSON_ID Desc) as RowRank
-                       FROM T_EUS_Site_Status ESS
-                            INNER JOIN T_EUS_Users EU
-                              ON ESS.ID = EU.Site_Status ) EU
-       ON EU.EUS_Hanford_ID = U.U_HID AND EU.RowRank = 1
-
+                  -- We use LookupQ.RowRank = 1 when joining to this subquery to just keep one of those rows
+                  -- This logic is also used by V_User_Detail_Report
+                  SELECT EU.PERSON_ID AS EUS_Person_ID,
+                         EU.NAME_FM AS EUS_Name,
+                         ESS.Name AS EUS_Site_Status,
+                         EU.HID AS EUS_Hanford_ID,                               
+                         ROW_NUMBER() Over (Partition By EU.HID ORDER BY EU.PERSON_ID Desc) as RowRank
+                  FROM T_EUS_Site_Status ESS
+                       INNER JOIN T_EUS_Users EU
+                         ON ESS.ID = EU.Site_Status 
+                  WHERE NOT EU.HID IS Null 
+                ) LookupQ
+       ON LookupQ.EUS_Hanford_ID = U.U_HID AND LookupQ.RowRank = 1
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_EUS_User_ID_Lookup] TO [DDL_Viewer] AS [dbo]
--- DMS5/V_User_Detail_Report.sql
+++ DMS5/V_User_Detail_Report.sql
@@ -17,12 +17,13 @@ SELECT U.U_PRN AS [Username],
        U.U_comment AS [Comment],
        U.ID,
        U.U_created AS Created_DMS,
-       EU.EUS_Person_ID,
-       EU.EUS_Site_Status,
-       EU.EUS_Last_Affected
+       LookupQ.EUS_Person_ID,
+       LookupQ.EUS_Site_Status,
+       LookupQ.EUS_Last_Affected
 FROM T_Users U
      LEFT OUTER JOIN ( -- A few users have multiple EUS Person_ID values
-                       -- We use EU.RowRank = 1 when joining to this subquery to just keep one of those rows
+                       -- We use LookupQ.RowRank = 1 when joining to this subquery to just keep one of those rows
+                       -- This logic is also used by V_EUS_User_ID_Lookup
                        SELECT EU.HID, 
                               EU.PERSON_ID AS EUS_Person_ID,
                               ESS.Name AS EUS_Site_Status,
@@ -30,9 +31,10 @@ FROM T_Users U
                               ROW_NUMBER() Over (Partition By EU.HID ORDER BY EU.PERSON_ID Desc) as RowRank
                        FROM T_EUS_Site_Status ESS
                             INNER JOIN T_EUS_Users EU
-                              ON ESS.ID = EU.Site_Status ) EU
-       ON EU.HID = U.U_HID AND EU.RowRank = 1
-
+                              ON ESS.ID = EU.Site_Status 
+                       WHERE NOT EU.HID IS Null 
+                     ) LookupQ
+       ON LookupQ.HID = U.U_HID AND LookupQ.RowRank = 1
 
 
 GO
