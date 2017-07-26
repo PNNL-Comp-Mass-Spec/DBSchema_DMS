fe4c6d3922fa3fcb267df44ff1c21ce4b0f5140a
alchemistmatt <matthew.monroe@pnnl.gov>
2017-07-25 18:09:27 -0700

    Include NAME_FM in index IX_T_EUS_Users_SiteStatus

--- DMS5/T_EUS_Users.sql
+++ DMS5/T_EUS_Users.sql
@@ -30,12 +30,13 @@ GO
 SET ANSI_PADDING ON
 
 GO
-/****** Object:  Index [IX_T_EUS_Users_SiteStatus_include_PersonID_HID] ******/
-CREATE NONCLUSTERED INDEX [IX_T_EUS_Users_SiteStatus_include_PersonID_HID] ON [dbo].[T_EUS_Users]
+/****** Object:  Index [IX_T_EUS_Users_SiteStatus_include_PersonID_NameFM_HID] ******/
+CREATE NONCLUSTERED INDEX [IX_T_EUS_Users_SiteStatus_include_PersonID_NameFM_HID] ON [dbo].[T_EUS_Users]
 (
 	[Site_Status] ASC
 )
 INCLUDE ( 	[PERSON_ID],
+	[NAME_FM],
 	[HID]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_EUS_Users] ADD  CONSTRAINT [DF_T_EUS_Users_PERSON_ID]  DEFAULT ('0') FOR [PERSON_ID]
