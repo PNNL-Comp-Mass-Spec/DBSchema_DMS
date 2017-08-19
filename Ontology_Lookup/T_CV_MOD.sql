8792e6e4994b72fb2f1779fb8fbf7170a541da83
alchemistmatt <matthew.monroe@pnnl.gov>
2017-08-18 18:35:03 -0700

    Add/update indices in the Ontology_Lookup database

--- Ontology_Lookup/T_CV_MOD.sql
+++ Ontology_Lookup/T_CV_MOD.sql
@@ -44,6 +44,16 @@ GO
 SET ANSI_PADDING ON
 
 GO
+/****** Object:  Index [IX_T_CV_MOD_Identifier] ******/
+CREATE NONCLUSTERED INDEX [IX_T_CV_MOD_Identifier] ON [dbo].[T_CV_MOD]
+(
+	[Identifier] ASC
+)
+INCLUDE ( 	[Term_Name]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+
+GO
 /****** Object:  Index [IX_T_CV_MOD_Parent_Term_Name] ******/
 CREATE NONCLUSTERED INDEX [IX_T_CV_MOD_Parent_Term_Name] ON [dbo].[T_CV_MOD]
 (
