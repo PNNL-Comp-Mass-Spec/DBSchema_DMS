4287e5dbb27e59a851f59598e810fc88171dd5bc
alchemistmatt <matthew.monroe@pnnl.gov>
2017-08-24 21:54:38 -0700

    Add view for the Tissue Ontology page family

---
+++ Ontology_Lookup/V_BTO_Tissue_Report.sql
@@ -0,0 +1,22 @@
+/****** Object:  View [dbo].[V_BTO_Tissue_Report] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE VIEW [dbo].[V_BTO_Tissue_Report]
+AS
+    SELECT Term_Name AS Tissue,
+	   identifier AS Identifier,
+	   Is_Leaf AS [Is Leaf],
+	   Parent_term_name AS [Parent Tissue],
+	   Parent_term_ID AS [Parent ID],
+	   GrandParent_term_name AS [Grandparent Tissue],
+	   GrandParent_term_ID AS [Grandparent ID],
+	   [Synonyms] AS [Synonyms],
+	   Entry_ID       
+    FROM T_CV_BTO
+
+
+GO
