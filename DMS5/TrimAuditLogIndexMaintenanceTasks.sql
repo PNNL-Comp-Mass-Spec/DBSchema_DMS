61c7e6ee278a1db773f09448604d088d8e6791f9
alchemistmatt <matthew.monroe@pnnl.gov>
2017-08-30 18:11:00 -0700

    Remove old Alter Procedure, Alter Table, Alter View, etc. entries from SchemaChangeLog

--- DMS5/TrimAuditLogIndexMaintenanceTasks.sql
+++ DMS5/TrimAuditLogIndexMaintenanceTasks.sql
@@ -10,11 +10,15 @@ CREATE Procedure TrimAuditLogIndexMaintenanceTasks
 **		Trims entries from table SchemaChangeLog related to
 **		automated index maintenance tasks
 **
+**		Also removes old Alter Object entries
+**		ALTER_PROCEDURE, ALTER_TABLE, ALTER_VIEW, etc.
+**
 **	Return values: 0: success, otherwise, error code
 **
 **	Auth:	mem
 **	Date:	10/27/2016
 **			10/28/2016 mem - Auto-determine the LoginName to filter on
+**			08/30/2017 mem - Remove old Alter Procedure, Alter Table, Alter View, etc. entries
 **    
 *****************************************************/
 (
@@ -39,7 +43,9 @@ As
 	Declare @LoginName varchar(128)
 	
 	Declare @SchemaChangeLogTable nvarchar(128)
-
+	Declare @CreateDateFilter nvarchar(128)
+	Declare @SqlEventFilter nvarchar(255)
+	
 	Declare @currentIsland int = -1
 	Declare @StartSeqNo int = 0
 	Declare @EndSeqNo int = -1
@@ -80,6 +86,12 @@ As
 	)
 
 	CREATE UNIQUE CLUSTERED INDEX #IX_Tmp_LogEntriesToUpdate ON #Tmp_LogEntriesToUpdate (IslandNumber)
+	
+	CREATE TABLE #Tmp_AlterEventsToRemove (
+		SchemaChangeLogID int not null
+	)	
+	
+	CREATE UNIQUE CLUSTERED INDEX #IX_Tmp_AlterEventsToRemove ON #Tmp_AlterEventsToRemove (SchemaChangeLogID)	
 	
 	---------------------------------------------------
 	-- Find databases to process
@@ -149,7 +161,7 @@ As
 			
 			Set @Sql = ''
 			Set @Sql = @Sql + ' SELECT TOP 1 @LoginName = LoginName'
-			Set @Sql = @Sql + ' FROM SchemaChangeLog'
+			Set @Sql = @Sql + ' FROM ' + @SchemaChangeLogTable
 			Set @Sql = @Sql + ' WHERE (SQLEvent IN (''ALTER_INDEX'', ''UPDATE_STATISTICS''))'
 			Set @Sql = @Sql + ' GROUP BY LoginName'
 			Set @Sql = @Sql + ' ORDER BY COUNT(*) DESC'
@@ -208,17 +220,23 @@ As
 			-- associated with each island
 			---------------------------------------------------
 			--
-			UPDATE #Tmp_LogEntriesToUpdate
-			SET Date_Avg = DateQ.Date_Avg
-			FROM #Tmp_LogEntriesToUpdate Target
-				INNER JOIN ( SELECT IslandNumber,
-									Cast(Avg(Cast(SCL.CreateDate As Float)) As datetime) AS Date_Avg
-							FROM #Tmp_LogEntriesToUpdate U
-								INNER JOIN SchemaChangeLog SCL
-									ON SCL.SchemaChangeLogID BETWEEN U.StartSeqNo AND U.EndSeqNo
-							GROUP BY IslandNumber
-							) DateQ
-				ON Target.IslandNumber = DateQ.IslandNumber
+			Set @Sql = ''
+			Set @Sql = @Sql + ' UPDATE #Tmp_LogEntriesToUpdate'
+			Set @Sql = @Sql + ' SET Date_Avg = DateQ.Date_Avg'
+			Set @Sql = @Sql + ' FROM #Tmp_LogEntriesToUpdate Target'
+			Set @Sql = @Sql + ' 	INNER JOIN ( SELECT IslandNumber,'
+			Set @Sql = @Sql + ' 						Cast(Avg(Cast(SCL.CreateDate As Float)) As DateTime) AS Date_Avg'
+			Set @Sql = @Sql + ' 				FROM #Tmp_LogEntriesToUpdate U'
+			Set @Sql = @Sql + ' 					INNER JOIN ' + @SchemaChangeLogTable + ' SCL'
+			Set @Sql = @Sql + ' 						ON SCL.SchemaChangeLogID BETWEEN U.StartSeqNo AND U.EndSeqNo'
+			Set @Sql = @Sql + ' 				GROUP BY IslandNumber'
+			Set @Sql = @Sql + ' 				) DateQ'
+			Set @Sql = @Sql + ' 	ON Target.IslandNumber = DateQ.IslandNumber'
+						
+			If @previewSql > 0
+				Print @Sql
+			
+			exec sp_executesql @Sql
 			--
 			SELECT @myError = @@error, @myRowCount = @@rowcount
 			
@@ -233,7 +251,52 @@ As
 				--
 				SELECT @myError = @@error, @myRowCount = @@rowcount				
 			End
+
+
+			---------------------------------------------------
+			-- Find old alter object entries to remove
+			-- We keep all of the events in recent weeks (determined via @exclusionWeeks)
+			-- Plus also the last two change events prior to that; grouping change events by day
+			---------------------------------------------------
+			--
+			TRUNCATE TABLE #Tmp_AlterEventsToRemove			
 			
+			Set @CreateDateFilter = 'DateAdd(week, -' + Cast(@exclusionWeeks as varchar(6)) + ', GetDate())'
+			Set @SqlEventFilter = 'SQLEvent IN (''ALTER_PROCEDURE'', ''ALTER_TABLE'', ''ALTER_FUNCTION'', ''ALTER_VIEW'', ''ALTER_TRIGGER'', ''ALTER_INDEX'')'
+			
+			Set @Sql = ''
+			Set @Sql = @Sql + ' INSERT INTO #Tmp_AlterEventsToRemove( SchemaChangeLogID )'
+			Set @Sql = @Sql + ' SELECT SchemaChangeLogID'
+			Set @Sql = @Sql + ' FROM ' + @SchemaChangeLogTable
+			Set @Sql = @Sql + ' WHERE ' + @SqlEventFilter + ' AND '
+			Set @Sql = @Sql + '       CreateDate < ' + @CreateDateFilter + ' AND '
+			Set @Sql = @Sql + '       NOT SchemaChangeLogID IN '
+			Set @Sql = @Sql + '           ( SELECT SchemaChangeLogID'
+			Set @Sql = @Sql + '             FROM ( SELECT ObjectName,'
+			Set @Sql = @Sql + '                           SchemaChangeLogID,'
+			Set @Sql = @Sql + '                           Row_Number() OVER ( PARTITION BY ObjectName '
+			Set @Sql = @Sql + '                                               ORDER BY SchemaChangeLogID DESC ) AS DateRank'
+			Set @Sql = @Sql + '                    FROM ( SELECT ObjectName,'
+			Set @Sql = @Sql + '                                  Cast(CreateDate AS date) AS CreateDate,'
+			Set @Sql = @Sql + '                                  SchemaChangeLogID,'
+			Set @Sql = @Sql + '                                  Row_Number() OVER ( PARTITION BY ObjectName, Cast(CreateDate AS date) '
+			Set @Sql = @Sql + '                                                      ORDER BY SchemaChangeLogID DESC ) AS SameDayRank'
+			Set @Sql = @Sql + '                           FROM ' + @SchemaChangeLogTable
+			Set @Sql = @Sql + '                           WHERE ' + @SqlEventFilter + ' AND '
+			Set @Sql = @Sql + '                                 CreateDate < ' + @CreateDateFilter
+			Set @Sql = @Sql + '                         ) RankQ'
+			Set @Sql = @Sql + '                   WHERE SameDayRank = 1 '
+			Set @Sql = @Sql + '                  ) OuterRankQ'
+			Set @Sql = @Sql + '             WHERE DateRank <= 2 )'           
+			
+			If @previewSql > 0
+				Print @Sql
+			
+			exec sp_executesql @Sql
+			--
+			SELECT @myError = @@error, @myRowCount = @@rowcount
+			
+
 			If @infoOnly = 1
 			Begin -- <c1>
 			
@@ -282,6 +345,43 @@ As
 					SELECT @myError = @@error, @myRowCount = @@rowcount
 					
 				End -- </d1>
+				
+				If Not Exists (Select * FROM #Tmp_AlterEventsToRemove)
+				Begin
+					If @previewSql = 0
+					Begin
+						If @exclusionWeeks = 0
+							SELECT @currentDbName as DB, 
+								'No Alter Object entries to remove' as Message, 
+								'Date filter not applied since @exclusionWeeks = 0' as Detail
+
+						Else
+							SELECT @currentDbName as DB, 
+								'No Alter Object entries to remove' as Message, 
+								'No entries to remove prior to ' + Cast(DateAdd(WEEK, -@exclusionWeeks, GetDate()) as varchar(30)) as Detail
+					End
+				End
+				Else
+				Begin
+					---------------------------------------------------
+					-- Preview the entries to remove
+					---------------------------------------------------
+					--
+					Set @sql = ''
+					Set @sql = @Sql + ' SELECT ''' + @currentDbName + ''' AS DB, ''Old entry to delete'' AS Action, SCL.*'
+					Set @sql = @Sql + ' FROM #Tmp_AlterEventsToRemove R'
+					Set @sql = @Sql +      ' INNER JOIN ' + @SchemaChangeLogTable + ' SCL'
+					Set @sql = @Sql + ' ON SCL.SchemaChangeLogID = R.SchemaChangeLogID'
+					Set @sql = @Sql + ' ORDER BY SCL.SchemaChangeLogID'
+
+					If @previewSql > 0
+						Print @Sql
+					Else
+						exec sp_executesql @Sql
+					--
+					SELECT @myError = @@error, @myRowCount = @@rowcount
+				End
+				
 			End -- </c1>
 			Else
 			Begin -- <c2>
@@ -375,7 +475,20 @@ As
 
 					End -- </e>
 				End -- </d2>
-				
+			
+				Set @SQL = ''
+				Set @SQL = @SQL + ' DELETE ' + @SchemaChangeLogTable
+				Set @SQL = @SQL + ' FROM ' + @SchemaChangeLogTable + ' Target'
+				Set @SQL = @SQL +     ' INNER JOIN #Tmp_AlterEventsToRemove Src '
+				Set @SQL = @SQL +     ' ON Target.SchemaChangeLogID = Src.SchemaChangeLogID '
+				--
+				SELECT @myError = @@error, @myRowCount = @@rowcount
+									
+				If @previewSql > 0
+					Print @Sql
+				Else
+					exec sp_executesql @Sql
+							
 			End -- </c2>
 
 		End -- </b>
