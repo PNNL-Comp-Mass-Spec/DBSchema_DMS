ffc2bf8f619983534e1d17fe93112a99c0cc7fe2
alchemistmatt <matthew.monroe@pnnl.gov>
2017-09-01 18:53:26 -0700

    Add index to T_CV_BTO

--- Ontology_Lookup/T_CV_BTO.sql
+++ Ontology_Lookup/T_CV_BTO.sql
@@ -38,6 +38,16 @@ GO
 SET ANSI_PADDING ON
 
 GO
+/****** Object:  Index [IX_T_CV_BTO_Cached_Names_Term_Name] ******/
+CREATE NONCLUSTERED INDEX [IX_T_CV_BTO_Cached_Names_Term_Name] ON [dbo].[T_CV_BTO]
+(
+	[Term_Name] ASC
+)
+INCLUDE ( 	[Identifier]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+
+GO
 /****** Object:  Index [IX_T_CV_BTO_GrandParent_Term_Name] ******/
 CREATE NONCLUSTERED INDEX [IX_T_CV_BTO_GrandParent_Term_Name] ON [dbo].[T_CV_BTO]
 (
