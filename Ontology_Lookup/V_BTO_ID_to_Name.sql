616d6d2bbe8eeabbb4d18057610acea5f09475ea
alchemistmatt <matthew.monroe@pnnl.gov>
2017-09-01 18:53:39 -0700

    Use T_CV_BTO_Cached_Names instead of T_CV_BTO

--- Ontology_Lookup/V_BTO_ID_to_Name.sql
+++ Ontology_Lookup/V_BTO_ID_to_Name.sql
@@ -4,12 +4,11 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_BTO_ID_to_Name]
 AS
-	SELECT identifier AS Identifier, Min(Term_Name) AS Tissue
-	FROM T_CV_BTO
-	GROUP BY identifier
+SELECT Identifier,
+       Term_Name AS Tissue
+FROM T_CV_BTO_Cached_Names
 
 
 GO
