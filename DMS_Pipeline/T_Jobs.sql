615ae36ed68bf88971f63a393e7113ad8d78be9e
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-03 15:09:18 -0700

    Update index on T_Jobs to include Storage Server

--- DMS_Pipeline/T_Jobs.sql
+++ DMS_Pipeline/T_Jobs.sql
@@ -51,14 +51,18 @@ CREATE NONCLUSTERED INDEX [IX_T_Jobs_State] ON [dbo].[T_Jobs]
 	[State] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
-/****** Object:  Index [IX_T_Jobs_State_include_Job_Priority_ArchiveBusy] ******/
-CREATE NONCLUSTERED INDEX [IX_T_Jobs_State_include_Job_Priority_ArchiveBusy] ON [dbo].[T_Jobs]
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_T_Jobs_State_include_Job_Priority_ArchiveBusy_StorageServer] ******/
+CREATE NONCLUSTERED INDEX [IX_T_Jobs_State_include_Job_Priority_ArchiveBusy_StorageServer] ON [dbo].[T_Jobs]
 (
 	[State] ASC
 )
 INCLUDE ( 	[Job],
 	[Priority],
-	[Archive_Busy]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
+	[Archive_Busy],
+	[Storage_Server]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_Jobs] ADD  CONSTRAINT [DF_T_Jobs_State]  DEFAULT ((0)) FOR [State]
 GO
