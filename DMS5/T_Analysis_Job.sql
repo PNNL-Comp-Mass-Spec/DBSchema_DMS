9e2cd065ccedbb4bfbe51f7f9b96c331faa9f424
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-11 09:52:44 -0700

    Add index and check constraint to T_Analysis_Job

--- DMS5/T_Analysis_Job.sql
+++ DMS5/T_Analysis_Job.sql
@@ -97,6 +97,13 @@ CREATE NONCLUSTERED INDEX [IX_T_Analysis_Job_AJ_ToolNameCached] ON [dbo].[T_Anal
 	[AJ_ToolNameCached] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
+/****** Object:  Index [IX_T_Analysis_Job_BatchID_include_JobID] ******/
+CREATE NONCLUSTERED INDEX [IX_T_Analysis_Job_BatchID_include_JobID] ON [dbo].[T_Analysis_Job]
+(
+	[AJ_batchID] ASC
+)
+INCLUDE ( 	[AJ_jobID]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
 /****** Object:  Index [IX_T_Analysis_Job_Created_include_Job_StateID_Progress] ******/
 CREATE NONCLUSTERED INDEX [IX_T_Analysis_Job_Created_include_Job_StateID_Progress] ON [dbo].[T_Analysis_Job]
 (
@@ -268,6 +275,10 @@ ALTER TABLE [dbo].[T_Analysis_Job]  WITH CHECK ADD  CONSTRAINT [FK_T_Analysis_Jo
 REFERENCES [dbo].[T_YesNo] ([Flag])
 GO
 ALTER TABLE [dbo].[T_Analysis_Job] CHECK CONSTRAINT [FK_T_Analysis_Job_T_YesNo]
+GO
+ALTER TABLE [dbo].[T_Analysis_Job]  WITH CHECK ADD  CONSTRAINT [CK_T_Analysis_Job_PropagationMode] CHECK  (([AJ_propagationMode]=(1) OR [AJ_propagationMode]=(0)))
+GO
+ALTER TABLE [dbo].[T_Analysis_Job] CHECK CONSTRAINT [CK_T_Analysis_Job_PropagationMode]
 GO
 /****** Object:  Trigger [dbo].[trig_d_AnalysisJob] ******/
 SET ANSI_NULLS ON
