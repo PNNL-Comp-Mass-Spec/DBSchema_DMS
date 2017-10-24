38f8a187116ebc44f7c96872d952576a61b97b87
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-24 11:37:54 -0700

    Add Includes_Contaminants to V_Protein_Collections_By_Organism and update that view to query tables instead of other views

--- Protein_Sequences/V_Protein_Collections_By_Organism.sql
+++ Protein_Sequences/V_Protein_Collections_By_Organism.sql
@@ -4,32 +4,36 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
 CREATE VIEW [dbo].[V_Protein_Collections_By_Organism]
 AS
-SELECT DISTINCT VPC.Protein_Collection_ID,
-                VPC.Display,
-                VPC.Description,
+SELECT DISTINCT PC.Protein_Collection_ID,
+                PC.[FileName] + ' (' + CAST(PC.NumProteins AS varchar) + ' Entries)' AS [Display],
+                PC.[Description],
                 PC.Collection_State_ID,
+                PCS.State AS State_Name,
                 PC.Collection_Type_ID,
+                PCTypes.[Type],
                 PC.NumProteins,
-				PC.NumResidues,
+                PC.NumResidues,
                 PC.Authentication_Hash,
-                VPC.FileName,
+                PC.[FileName] AS [Filename],
                 OrgXref.Organism_ID,
-                VPC.Primary_Annotation_Type_ID AS Authority_ID,
-                OrgPicker.Short_Name AS Organism_Name,
-                VPC.Contents_Encrypted AS Contents_Encrypted				
+                PC.Primary_Annotation_Type_ID AS Authority_ID,
+                OrgList.[Name] AS Organism_Name,
+                PC.Contents_Encrypted,
+                PC.Includes_Contaminants,
+                AOF.Filesize
 FROM T_Protein_Collections PC
      INNER JOIN T_Collection_Organism_Xref OrgXref
        ON PC.Protein_Collection_ID = OrgXref.Protein_Collection_ID
-     INNER JOIN V_Protein_Collections VPC
-       ON PC.Protein_Collection_ID = VPC.Protein_Collection_ID
-     INNER JOIN V_Organism_Picker OrgPicker
-       ON OrgXref.Organism_ID = OrgPicker.ID
-
-
+     INNER JOIN MT_Main.dbo.T_DMS_Organisms OrgList
+       ON OrgXref.Organism_ID = OrgList.Organism_ID
+     INNER JOIN T_Protein_Collection_Types AS PCTypes
+       ON PC.Collection_Type_ID = PCTypes.Collection_Type_ID
+     INNER JOIN T_Protein_Collection_States PCS
+       ON PC.Collection_State_ID = PCS.Collection_State_ID
+     LEFT OUTER JOIN dbo.T_Archived_Output_Files AS AOF
+       ON PC.Authentication_Hash = AOF.Authentication_Hash
 
 
 GO
