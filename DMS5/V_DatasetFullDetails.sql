687bc2fe5baefb89827138abdea9d03b24ebf3c2
alchemistmatt <matthew.monroe@pnnl.gov>
2017-11-29 20:22:12 -0800

    Update V_DatasetFullDetails to use T_Cached_Experiment_Components
    This view is used by synonym S_DMS_V_DatasetFullDetails in the DMS_Capture database

--- DMS5/V_DatasetFullDetails.sql
+++ DMS5/V_DatasetFullDetails.sql
@@ -5,23 +5,49 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW dbo.V_DatasetFullDetails
 AS
-SELECT     dbo.T_Dataset.Dataset_Num, dbo.T_Dataset.DS_created, dbo.T_Instrument_Name.IN_name, dbo.T_DatasetStateName.DSS_name, 
-                      ISNULL(dbo.T_Dataset.DS_comment, '') AS DS_comment, dbo.T_Dataset.DS_folder_name, dbo.t_storage_path.SP_path, 
-                      dbo.t_storage_path.SP_vol_name_server, dbo.t_storage_path.SP_vol_name_client, dbo.T_DatasetTypeName.DST_name, dbo.T_Dataset.DS_sec_sep, 
-                      ISNULL(dbo.T_Dataset.DS_well_num, 'na') AS DS_Well_num, dbo.T_Dataset.DS_Oper_PRN, dbo.T_Dataset.Dataset_ID, 
-                      dbo.T_Experiments.Experiment_Num, ISNULL(dbo.T_Experiments.EX_reason, '') AS EX_Reason, dbo.T_Organisms.OG_name AS EX_organism_name, 
-                      ISNULL(dbo.T_Experiments.EX_cell_culture_list, '') AS EX_cell_culture_list, dbo.T_Experiments.EX_researcher_PRN, 
-                      ISNULL(dbo.T_Experiments.EX_comment, '') AS EX_comment, ISNULL(dbo.T_Experiments.EX_lab_notebook_ref, 'na') AS EX_lab_notebook_ref, 
-                      dbo.T_Campaign.Campaign_Num, dbo.T_Campaign.CM_Project_Num, ISNULL(dbo.T_Campaign.CM_comment, '') AS CM_Comment, 
-                      dbo.T_Campaign.CM_created, ISNULL(dbo.T_Experiments.EX_sample_concentration, 'na') AS EX_sample_concentration
-FROM         dbo.T_Dataset INNER JOIN
-                      dbo.T_Experiments ON dbo.T_Dataset.Exp_ID = dbo.T_Experiments.Exp_ID INNER JOIN
-                      dbo.T_Campaign ON dbo.T_Experiments.EX_campaign_ID = dbo.T_Campaign.Campaign_ID INNER JOIN
-                      dbo.T_DatasetStateName ON dbo.T_Dataset.DS_state_ID = dbo.T_DatasetStateName.Dataset_state_ID INNER JOIN
-                      dbo.T_Instrument_Name ON dbo.T_Dataset.DS_instrument_name_ID = dbo.T_Instrument_Name.Instrument_ID INNER JOIN
-                      dbo.t_storage_path ON dbo.T_Dataset.DS_storage_path_ID = dbo.t_storage_path.SP_path_ID INNER JOIN
-                      dbo.T_DatasetTypeName ON dbo.T_Dataset.DS_type_ID = dbo.T_DatasetTypeName.DST_Type_ID INNER JOIN
-                      dbo.T_Organisms ON dbo.T_Experiments.Ex_organism_ID = dbo.T_Organisms.Organism_ID
+SELECT DS.Dataset_Num,
+       DS.DS_created,
+       InstName.IN_name,
+       DSN.DSS_name,
+       ISNULL(DS.DS_comment, '') AS DS_comment,
+       DS.DS_folder_name,
+       SPath.SP_path,
+       SPath.SP_vol_name_server,
+       SPath.SP_vol_name_client,
+       DTN.DST_name,
+       DS.DS_sec_sep,
+       ISNULL(DS.DS_well_num, 'na') AS DS_Well_num,
+       DS.DS_Oper_PRN,
+       DS.Dataset_ID,
+       E.Experiment_Num,
+       ISNULL(E.EX_reason, '') AS EX_Reason,
+       Org.OG_name AS EX_organism_name,
+       ISNULL(CCE.Cell_Culture_List, '') AS EX_cell_culture_list,
+       E.EX_researcher_PRN,
+       ISNULL(E.EX_comment, '') AS EX_comment,
+       ISNULL(E.EX_lab_notebook_ref, 'na') AS EX_lab_notebook_ref,
+       C.Campaign_Num,
+       C.CM_Project_Num,
+       ISNULL(C.CM_comment, '') AS CM_Comment,
+       C.CM_created,
+       ISNULL(E.EX_sample_concentration, 'na') AS EX_sample_concentration
+FROM T_Dataset DS
+     INNER JOIN T_Experiments E
+       ON DS.Exp_ID = E.Exp_ID
+     INNER JOIN T_Campaign C
+       ON E.EX_campaign_ID = C.Campaign_ID
+     INNER JOIN T_DatasetStateName DSN
+       ON DS.DS_state_ID = DSN.Dataset_state_ID
+     INNER JOIN T_Instrument_Name InstName
+       ON DS.DS_instrument_name_ID = InstName.Instrument_ID
+     INNER JOIN T_Storage_Path SPath
+       ON DS.DS_storage_path_ID = SPath.SP_path_ID
+     INNER JOIN T_DatasetTypeName DTN
+       ON DS.DS_type_ID = DTN.DST_Type_ID
+     INNER JOIN T_Organisms Org
+       ON E.EX_organism_ID = Org.Organism_ID
+     LEFT OUTER JOIN T_Cached_Experiment_Components CCE
+       ON E.Exp_ID = CCE.Exp_ID
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_DatasetFullDetails] TO [DDL_Viewer] AS [dbo]
