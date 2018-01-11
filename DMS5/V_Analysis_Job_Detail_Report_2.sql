c44f2ead0c4376c2473ef255a09f9a7dccfe8380
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-11 14:40:14 -0800

    Show experiment tissue and organism on Job List report and Detail report

--- DMS5/V_Analysis_Job_Detail_Report_2.sql
+++ DMS5/V_Analysis_Job_Detail_Report_2.sql
@@ -4,8 +4,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE VIEW [dbo].[V_Analysis_Job_Detail_Report_2]
-AS
+CREATE VIEW [dbo].[V_Analysis_Job_Detail_Report_2] AS
 SELECT AJ.AJ_jobID AS JobNum,
        DS.Dataset_Num AS Dataset,
        E.Experiment_Num AS Experiment,
@@ -20,20 +19,22 @@ SELECT AJ.AJ_jobID AS JobNum,
        AJ.AJ_parmFileName AS [Parm File],
        AnalysisTool.AJT_parmFileStoragePath AS [Parm File Storage Path],
        AJ.AJ_settingsFileName AS [Settings File],
-       Org.OG_name AS Organism,
+       ExpOrg.OG_Name As [Organism],
+       BTO.Tissue AS [Experiment Tissue],
+       JobOrg.OG_name AS [Job Organism],
        AJ.AJ_organismDBName AS [Organism DB],
-       dbo.GetFASTAFilePath(AJ.AJ_organismDBName, Org.OG_name) AS [Organism DB Storage Path],
+       dbo.GetFASTAFilePath(AJ.AJ_organismDBName, JobOrg.OG_name) AS [Organism DB Storage Path],
        AJ.AJ_proteinCollectionList AS [Protein Collection List],
        AJ.AJ_proteinOptionsList AS [Protein Options List],
-	   CASE WHEN AJ.AJ_StateID = 2 THEN ASN.AJS_name + ': ' + 
-			  CAST(CAST(AJ.Progress AS DECIMAL(9,2)) AS VARCHAR(12)) + '%, ETA ' + 
-			  CASE 
-				WHEN AJ.ETA_Minutes > 3600 THEN CAST(CAST(AJ.ETA_Minutes/1440.0 AS DECIMAL(18,1)) AS VARCHAR(12)) + ' days'
-				WHEN AJ.ETA_Minutes > 90 THEN CAST(CAST(AJ.ETA_Minutes/60.0 AS DECIMAL(18,1)) AS VARCHAR(12)) + ' hours'
-				ELSE CAST(CAST(AJ.ETA_Minutes AS DECIMAL(18,1)) AS VARCHAR(12)) + ' minutes'
-			  END
-	       ELSE ASN.AJS_name
-		   END AS State,
+       CASE WHEN AJ.AJ_StateID = 2 THEN ASN.AJS_name + ': ' + 
+              CAST(CAST(AJ.Progress AS DECIMAL(9,2)) AS VARCHAR(12)) + '%, ETA ' + 
+              CASE 
+                WHEN AJ.ETA_Minutes > 3600 THEN CAST(CAST(AJ.ETA_Minutes/1440.0 AS DECIMAL(18,1)) AS VARCHAR(12)) + ' days'
+                WHEN AJ.ETA_Minutes > 90 THEN CAST(CAST(AJ.ETA_Minutes/60.0 AS DECIMAL(18,1)) AS VARCHAR(12)) + ' hours'
+                ELSE CAST(CAST(AJ.ETA_Minutes AS DECIMAL(18,1)) AS VARCHAR(12)) + ' minutes'
+              END
+           ELSE ASN.AJS_name
+           END AS State,
        CONVERT(decimal(9, 2), AJ.AJ_ProcessingTimeMinutes) AS [Runtime Minutes],
        AJ.AJ_owner AS Owner,
        AJ.AJ_comment AS [Comment],
@@ -62,7 +63,7 @@ SELECT AJ.AJ_jobID AS JobNum,
        AJ.AJ_start AS [Started],
        AJ.AJ_finish AS Finished,
        AJ.AJ_requestID AS Request,
-       AJ.AJ_priority AS Priority,
+       AJ.AJ_priority AS [Priority],
        AJ.AJ_assignedProcessorName AS [Assigned Processor],
        AJ.AJ_Analysis_Manager_Error AS [AM Code],
        dbo.GetDEMCodeString(AJ.AJ_Data_Extraction_Error) AS [DEM Code],
@@ -72,49 +73,54 @@ SELECT AJ.AJ_jobID AS JobNum,
        END AS [Export Mode],
        T_YesNo.Description AS [Dataset Unreviewed],
        T_MyEMSLState.StateName AS [MyEMSL State],
-	   AJPG.Group_Name AS [Processor Group]
-FROM T_Analysis_Job_Processor_Group AJPG
-     INNER JOIN T_Analysis_Job_Processor_Group_Associations AJPJA
-       ON AJPG.ID = AJPJA.Group_ID
-     RIGHT OUTER JOIN T_Analysis_Job AJ
-                      INNER JOIN T_Dataset DS
+      AJPG.Group_Name AS [Processor Group]       
+FROM S_V_BTO_ID_to_Name AS BTO
+     RIGHT OUTER JOIN T_Analysis_Job AS AJ
+                      INNER JOIN T_Dataset AS DS
                         ON AJ.AJ_datasetID = DS.Dataset_ID
-                      INNER JOIN T_Experiments E
+                      INNER JOIN T_Experiments AS E
                         ON DS.Exp_ID = E.Exp_ID
-                      INNER JOIN V_Dataset_Folder_Paths DFP
+                      INNER JOIN T_Organisms ExpOrg
+                        ON E.EX_organism_ID = ExpOrg.Organism_ID
+                      INNER JOIN V_Dataset_Folder_Paths AS DFP
                         ON DFP.Dataset_ID = DS.Dataset_ID
-                      INNER JOIN T_Storage_Path SPath
+                      INNER JOIN T_Storage_Path AS SPath
                         ON DS.DS_storage_path_ID = SPath.SP_path_ID
-                      INNER JOIN T_Analysis_Tool AnalysisTool
+                      INNER JOIN T_Analysis_Tool AS AnalysisTool
                         ON AJ.AJ_analysisToolID = AnalysisTool.AJT_toolID
-                      INNER JOIN T_Analysis_State_Name ASN
+                      INNER JOIN T_Analysis_State_Name AS ASN
                         ON AJ.AJ_StateID = ASN.AJS_stateID
-                      INNER JOIN T_Instrument_Name InstName
+                      INNER JOIN T_Instrument_Name AS InstName
                         ON DS.DS_instrument_name_ID = InstName.Instrument_ID
-                      INNER JOIN T_Organisms Org
-                        ON Org.Organism_ID = AJ.AJ_organismID
+                      INNER JOIN T_Organisms AS JobOrg
+                        ON JobOrg.Organism_ID = AJ.AJ_organismID
                       INNER JOIN T_YesNo
                         ON AJ.AJ_DatasetUnreviewed = T_YesNo.Flag
                       INNER JOIN T_MyEMSLState
                         ON AJ.AJ_MyEMSLState = T_MyEMSLState.MyEMSLState
-       ON AJPJA.Job_ID = AJ.AJ_jobID
+       ON BTO.Identifier = E.EX_Tissue_ID
+     LEFT OUTER JOIN T_Analysis_Job_Processor_Group AS AJPG
+                     INNER JOIN T_Analysis_Job_Processor_Group_Associations AS AJPJA
+                       ON AJPG.ID = AJPJA.Group_ID
+       ON AJ.AJ_jobID = AJPJA.Job_ID
      LEFT OUTER JOIN ( SELECT Job,
                               COUNT(*) AS MT_DB_Count
-                       FROM dbo.T_MTS_MT_DB_Jobs_Cached
-                       GROUP BY Job ) MTSMT
+                       FROM T_MTS_MT_DB_Jobs_Cached
+                       GROUP BY Job ) AS MTSMT
        ON AJ.AJ_jobID = MTSMT.Job
      LEFT OUTER JOIN ( SELECT Job,
                               COUNT(*) AS PT_DB_Count
-                       FROM dbo.T_MTS_PT_DB_Jobs_Cached
-                       GROUP BY Job ) MTSPT
+                       FROM T_MTS_PT_DB_Jobs_Cached
+                       GROUP BY Job ) AS MTSPT
        ON AJ.AJ_jobID = MTSPT.Job
      LEFT OUTER JOIN ( SELECT DMS_Job,
                               COUNT(*) AS PMTasks
-                       FROM dbo.T_MTS_Peak_Matching_Tasks_Cached AS PM
-                       GROUP BY DMS_Job ) PMTaskCountQ
+                       FROM T_MTS_Peak_Matching_Tasks_Cached AS PM
+                       GROUP BY DMS_Job ) AS PMTaskCountQ
        ON PMTaskCountQ.DMS_Job = AJ.AJ_jobID
-     LEFT OUTER JOIN T_Dataset_Archive DA
+     LEFT OUTER JOIN T_Dataset_Archive AS DA
        ON DS.Dataset_ID = DA.AS_Dataset_ID
+
 
 
 GO
--- DMS5/V_Analysis_Job_List_Report_2.sql
+++ DMS5/V_Analysis_Job_List_Report_2.sql
@@ -4,47 +4,57 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-CREATE VIEW [dbo].[V_Analysis_Job_List_Report_2] as
-SELECT  AJ.AJ_jobID AS Job ,
-        AJ.AJ_priority AS Pri ,
-        AJ.AJ_StateNameCached AS State ,
-		AJ.Aj_ToolNameCached AS Tool,
-        DS.Dataset_Num AS Dataset ,
-        C.Campaign_Num AS Campaign ,
-        E.Experiment_Num AS Experiment ,
-        InstName.IN_name AS Instrument ,
-        AJ.AJ_parmFileName AS [Parm File] ,
-        AJ.AJ_settingsFileName AS Settings_File ,
-        Org.OG_name AS Organism ,
-        AJ.AJ_organismDBName AS [Organism DB] ,
-        AJ.AJ_proteinCollectionList AS [Protein Collection List] ,
-        AJ.AJ_proteinOptionsList AS [Protein Options] ,
-        AJ.AJ_comment AS Comment,
-        AJ.AJ_created AS Created ,
-        AJ.AJ_start AS Started ,
-        AJ.AJ_finish AS Finished ,
-        CAST(AJ.AJ_ProcessingTimeMinutes AS DECIMAL(9, 2)) AS Runtime ,
-		CAST(AJ.Progress AS DECIMAL(9,2)) AS Progress,
-	    CAST(AJ.ETA_Minutes AS DECIMAL(18,1)) AS ETA_Minutes,
-        AJ.AJ_requestID AS [Job Request] ,
-        ISNULL(AJ.AJ_resultsFolderName, '(none)') AS [Results Folder] ,
-        CASE WHEN AJ.AJ_Purged = 0
-        THEN SPath.SP_vol_name_client + SPath.SP_path + ISNULL(DS.DS_folder_name, DS.Dataset_Num) + '\' + AJ.AJ_resultsFolderName 
-        ELSE 'Purged'
-        END AS [Results Folder Path],
-        AJ.AJ_Last_Affected AS Last_Affected,
-        DR.DRN_name AS Rating
-FROM T_Analysis_Job AJ INNER JOIN
-   T_Dataset DS ON AJ.AJ_datasetID = DS.Dataset_ID INNER JOIN
-   T_Storage_Path SPath ON DS.DS_storage_path_ID = SPath.SP_path_ID INNER JOIN
-   T_DatasetRatingName DR ON DS.DS_rating = DR.DRN_state_ID INNER JOIN
-   T_Organisms Org ON AJ.AJ_organismID = Org.Organism_ID INNER JOIN
-   T_Instrument_Name InstName ON DS.DS_instrument_name_ID = InstName.Instrument_ID INNER JOIN
-   T_Experiments E ON DS.Exp_ID = E.Exp_ID INNER JOIN
-   T_Campaign C ON E.EX_campaign_ID = C.Campaign_ID
-
-
+CREATE VIEW [dbo].[V_Analysis_Job_List_Report_2] AS
+SELECT AJ.AJ_jobID AS Job,
+       AJ.AJ_priority AS Pri,
+       AJ.AJ_StateNameCached AS State,
+       AJ.Aj_ToolNameCached AS Tool,
+       DS.Dataset_Num AS Dataset,
+       C.Campaign_Num AS Campaign,
+       E.Experiment_Num AS Experiment,
+       InstName.IN_name AS Instrument,
+       AJ.AJ_parmFileName AS [Parm File],
+       AJ.AJ_settingsFileName AS Settings_File,
+       ExpOrg.OG_Name As [Organism],
+       BTO.Tissue AS Tissue,
+       JobOrg.OG_name AS [Job Organism],
+       AJ.AJ_organismDBName AS [Organism DB],
+       AJ.AJ_proteinCollectionList AS [Protein Collection List],
+       AJ.AJ_proteinOptionsList AS [Protein Options],
+       AJ.AJ_comment AS Comment,
+       AJ.AJ_created AS Created,
+       AJ.AJ_start AS Started,
+       AJ.AJ_finish AS Finished,
+       CAST(AJ.AJ_ProcessingTimeMinutes AS DECIMAL(9, 2)) AS Runtime,
+       CAST(AJ.Progress AS DECIMAL(9,2)) AS Progress,
+       CAST(AJ.ETA_Minutes AS DECIMAL(18,1)) AS ETA_Minutes,
+       AJ.AJ_requestID AS [Job Request],
+       ISNULL(AJ.AJ_resultsFolderName, '(none)') AS [Results Folder],
+       CASE WHEN AJ.AJ_Purged = 0
+       THEN SPath.SP_vol_name_client + SPath.SP_path + ISNULL(DS.DS_folder_name, DS.Dataset_Num) + '\' + AJ.AJ_resultsFolderName 
+       ELSE 'Purged'
+       END AS [Results Folder Path],
+       AJ.AJ_Last_Affected AS Last_Affected,
+       DR.DRN_name AS Rating
+FROM T_Analysis_Job AJ
+     INNER JOIN T_Dataset DS
+       ON AJ.AJ_datasetID = DS.Dataset_ID
+     INNER JOIN T_Storage_Path SPath
+       ON DS.DS_storage_path_ID = SPath.SP_path_ID
+     INNER JOIN T_DatasetRatingName DR
+       ON DS.DS_rating = DR.DRN_state_ID
+     INNER JOIN T_Organisms JobOrg
+       ON AJ.AJ_organismID = JobOrg.Organism_ID
+     INNER JOIN T_Instrument_Name InstName
+       ON DS.DS_instrument_name_ID = InstName.Instrument_ID
+     INNER JOIN T_Experiments E
+       ON DS.Exp_ID = E.Exp_ID
+     INNER JOIN T_Organisms ExpOrg
+       ON E.EX_organism_ID = ExpOrg.Organism_ID
+     INNER JOIN T_Campaign C
+       ON E.EX_campaign_ID = C.Campaign_ID
+     LEFT OUTER JOIN S_V_BTO_ID_to_Name BTO
+       ON BTO.Identifier = E.EX_Tissue_ID
 
 
 GO
