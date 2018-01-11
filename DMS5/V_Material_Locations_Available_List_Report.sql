f40bf08bb881fed87bac7fcade171087604a979f
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-11 14:49:36 -0800

    Remove barcode from Material locations list report

--- DMS5/V_Material_Locations_Available_List_Report.sql
+++ DMS5/V_Material_Locations_Available_List_Report.sql
@@ -3,12 +3,24 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Material_Locations_Available_List_Report
-AS
-SELECT     Location, Freezer, Shelf, Rack, Row, Col, Barcode, Comment, Limit, Containers, Available, 'New Container' AS Action, [#ID]
-FROM         dbo.V_Material_Locations_List_Report
-WHERE     (Available > 0) AND (Status = 'Active')
 
+CREATE VIEW [dbo].[V_Material_Locations_Available_List_Report]
+AS
+SELECT Location,
+       Freezer,
+       Shelf,
+       Rack,
+       Row,
+       Col,
+       [Comment],
+       Limit,
+       Containers,
+       Available,
+       'New Container' AS [Action],
+       [#ID]
+FROM dbo.V_Material_Locations_List_Report
+WHERE Available > 0 AND
+      Status = 'Active'
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Material_Locations_Available_List_Report] TO [DDL_Viewer] AS [dbo]
 GO
