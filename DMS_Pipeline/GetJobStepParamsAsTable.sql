9e42bac1080cbe5585272068c843c2bd341bb8e7
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-11 14:33:55 -0800

    Add @section, @paramName, and @firstParameterValue to job step params retrieval procedures

--- DMS_Pipeline/GetJobStepParamsAsTable.sql
+++ DMS_Pipeline/GetJobStepParamsAsTable.sql
@@ -11,17 +11,20 @@ CREATE PROCEDURE dbo.GetJobStepParamsAsTable
 **
 **	Note: Data comes from table T_Job_Parameters in the DMS_Pipeline DB, not from DMS5
 **
-**
 **	Return values: 0: success, otherwise, error code
 **
 **	Auth:	mem
-**			12/04/2009 mem - initial release
+**			12/04/2009 mem - Initial release
+**			01/05/2018 mem - Add parameters @section, @paramName, and @firstParameterValue
 **    
 *****************************************************/
 (
 	@jobNumber int,
 	@stepNumber int,
+	@section varchar(128) = '',			-- Optional section name to filter on, for example: JobParameters
+	@paramName varchar(128) = '',		-- Optional parameter name to filter on, for example: SourceJob
     @message varchar(512) = '' output,
+    @firstParameterValue varchar(1024) = '' output,		-- The value of the first parameter in the retrieved job parameters; useful when using both @section and @paramName
     @DebugMode tinyint = 0
 )
 AS
@@ -33,6 +36,14 @@ AS
 	set @myRowCount = 0
 	--
 	set @message = ''
+	set @firstParameterValue = ''
+	
+	---------------------------------------------------
+	-- Validate the inputs
+	---------------------------------------------------
+	
+	Set @section = IsNull(@section, '')
+	Set @paramName = IsNull(@paramName, '')
 	
 	---------------------------------------------------
 	-- Temporary table to hold job parameters
@@ -51,6 +62,30 @@ AS
 	exec @myError = GetJobStepParamsWork @jobNumber, @stepNumber, @message output, @DebugMode
 	if @myError <> 0
 		Goto Done
+
+	---------------------------------------------------
+	-- Possibly filter the parameters
+	---------------------------------------------------
+		
+	If @section <> ''
+	Begin
+		DELETE FROM #Tmp_JobParamsTable
+		WHERE [Section] <> @section
+	End
+
+	If @paramName <> ''
+	Begin
+		DELETE FROM #Tmp_JobParamsTable
+		WHERE [Name] <> @paramName
+	End
+	
+	---------------------------------------------------
+	-- Cache the first parameter value (sorting on section name then parameter name)
+	---------------------------------------------------
+	
+	SELECT TOP 1 @firstParameterValue = [Value]
+	FROM #Tmp_JobParamsTable
+	ORDER BY [Section], [Name]
 	
 	---------------------------------------------------
 	-- Return the contents of #Tmp_JobParamsTable
@@ -61,7 +96,6 @@ AS
 	ORDER BY [Section], [Name], [Value]
 	--
 	SELECT @myError = @@error, @myRowCount = @@rowcount
-	
 	
 	---------------------------------------------------
 	-- Exit
