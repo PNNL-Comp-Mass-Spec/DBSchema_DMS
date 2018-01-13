dfce917c9c58f787c63b695c9f6e26cdbf48fb24
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-13 01:03:23 -0800

    2018-01-13 auto-commit Manager_Control

--- Manager_Control/V_MgrParams.sql
+++ Manager_Control/V_MgrParams.sql
@@ -4,15 +4,15 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_MgrParams]
 AS
-SELECT dbo.T_Mgrs.M_ID AS ManagerID,
+SELECT dbo.T_ParamValue.MgrID AS ManagerID,
        dbo.T_Mgrs.M_Name AS ManagerName,
        dbo.T_MgrTypes.MT_TypeName AS ManagerType,
        dbo.T_ParamType.ParamName AS ParameterName,
        dbo.T_ParamValue.Value AS ParameterValue,
        dbo.T_ParamValue.Comment,
+       dbo.T_ParamValue.Entry_ID,
        dbo.T_ParamValue.Last_Affected,
        dbo.T_ParamValue.Entered_By
 FROM dbo.T_Mgrs
@@ -22,8 +22,6 @@ FROM dbo.T_Mgrs
        ON dbo.T_Mgrs.M_ID = dbo.T_ParamValue.MgrID
      INNER JOIN dbo.T_ParamType
        ON dbo.T_ParamValue.TypeID = dbo.T_ParamType.ParamID
-
-
 
 
 GO
