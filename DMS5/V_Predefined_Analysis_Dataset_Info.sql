7c4607fc4e7c61ead5f843f7fd4c5f079aa136d6
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-22 12:41:15 -0800

    Use Left Outer Join to T_Secondary_Sep

--- DMS5/V_Predefined_Analysis_Dataset_Info.sql
+++ DMS5/V_Predefined_Analysis_Dataset_Info.sql
@@ -7,11 +7,11 @@ GO
 
 CREATE VIEW [dbo].[V_Predefined_Analysis_Dataset_Info]
 AS
-SELECT Cmpgn.Campaign_Num AS Campaign,
-       Exp.Experiment_Num AS Experiment,
-       Exp.EX_comment AS Experiment_Comment,
-       Exp.EX_Labelling AS Experiment_Labelling,
-       dbo.T_Organisms.OG_name AS Organism,
+SELECT C.Campaign_Num AS Campaign,
+       E.Experiment_Num AS Experiment,
+       E.EX_comment AS Experiment_Comment,
+       E.EX_Labelling AS Experiment_Labelling,
+       Org.OG_name AS Organism,
        InstName.IN_name AS Instrument,
        InstName.IN_class AS InstrumentClass,
        DS.DS_comment AS Dataset_Comment,
@@ -22,22 +22,22 @@ SELECT Cmpgn.Campaign_Num AS Campaign,
        DSTypeName.DST_Name AS Dataset_Type,
        SepType.SS_name AS Separation_Type,
        ISNULL(DS.Acq_Time_Start, DS.DS_created) AS DS_Date,
-	   DS.Scan_Count
+       DS.Scan_Count
 FROM dbo.T_Dataset DS
      INNER JOIN dbo.T_Instrument_Name InstName
        ON DS.DS_instrument_name_ID = InstName.Instrument_ID
-     INNER JOIN dbo.T_Experiments Exp
-       ON DS.Exp_ID = Exp.Exp_ID
-     INNER JOIN dbo.T_Campaign Cmpgn
-       ON Exp.EX_campaign_ID = Cmpgn.Campaign_ID
+     INNER JOIN dbo.T_Experiments E
+       ON DS.Exp_ID = E.Exp_ID
+     INNER JOIN dbo.T_Campaign C
+       ON E.EX_campaign_ID = C.Campaign_ID
      INNER JOIN dbo.T_DatasetTypeName DSTypeName
        ON DS.DS_type_ID = DSTypeName.DST_Type_ID
-     INNER JOIN dbo.T_Organisms
-       ON Exp.EX_organism_ID = dbo.T_Organisms.Organism_ID
-     INNER JOIN dbo.T_Secondary_Sep SepType
-       ON DS.DS_sec_sep = SepType.SS_name
+     INNER JOIN dbo.T_Organisms Org
+       ON E.EX_organism_ID = Org.Organism_ID
      INNER JOIN T_DatasetRatingName DRN
        ON DS.DS_rating = DRN.DRN_state_ID
+     LEFT OUTER JOIN dbo.T_Secondary_Sep SepType
+       ON DS.DS_sec_sep = SepType.SS_name
 
 
 
