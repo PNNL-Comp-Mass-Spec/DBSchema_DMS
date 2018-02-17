6ce5c303878789819262ea54a5ea80d2499e009f
alchemistmatt <matthew.monroe@pnnl.gov>
2018-02-16 21:07:23 -0800

    Update views

---
+++ DMS5/V_MyEMSL_Main_Metadata.sql
@@ -0,0 +1,23 @@
+/****** Object:  View [dbo].[V_MyEMSL_Main_Metadata] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_MyEMSL_Main_Metadata] 
+AS 
+SELECT DS.[Dataset_ID] AS [dataset_id],
+       DS.[Dataset_Num] AS [dataset_name],
+       DS.[DS_Oper_PRN] AS [submitter_prn],
+       COALESCE(RR.[RDS_EUS_Proposal_ID], '17797') AS [proposal_id],
+       COALESCE(DMSInstMap.[EUS_Instrument_ID], 34127) AS [instrument_id],
+       DS.[DS_created] AS [dataset_ctime],
+       COALESCE(DS.[File_Info_Last_Modified], DS.[DS_created]) AS [dataset_mtime]
+FROM dbo.[T_Dataset] AS DS
+     LEFT OUTER JOIN dbo.[T_Requested_Run] AS RR
+       ON RR.DatasetID = DS.[Dataset_ID]
+     LEFT OUTER JOIN dbo.[T_EMSL_DMS_Instrument_Mapping] AS DMSInstMap
+       ON DMSInstMap.[DMS_Instrument_ID] = DS.[DS_instrument_name_ID]
+
+
+GO
--- DMS5/V_MyEMSL_Supplemental_Metadata.sql
+++ DMS5/V_MyEMSL_Supplemental_Metadata.sql
@@ -4,8 +4,8 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE VIEW [dbo].[V_MyEMSL_Supplemental_Metadata]
-AS
+CREATE VIEW [dbo].[V_MyEMSL_Supplemental_Metadata] 
+AS 
 SELECT DS.Dataset_ID AS [omics.dms.dataset_id],
        DS.Dataset_Num AS [omics.dms.dataset_name],
        E.Exp_ID AS [omics.dms.experiment_id],
@@ -22,16 +22,15 @@ SELECT DS.Dataset_ID AS [omics.dms.dataset_id],
        DTN.DST_name AS [omics.dms.dataset_type],
        RR.ID AS [omics.dms.requested_run_id]
 FROM dbo.T_Campaign AS C
-     INNER JOIN dbo.T_Experiments AS E
+     LEFT OUTER JOIN dbo.T_Experiments AS E
        ON C.Campaign_ID = E.EX_campaign_ID
-     INNER JOIN dbo.T_Dataset AS DS
+     LEFT OUTER JOIN dbo.T_Dataset AS DS
        ON E.Exp_ID = DS.Exp_ID
-     INNER JOIN dbo.T_DatasetTypeName AS DTN
+     LEFT OUTER JOIN dbo.T_DatasetTypeName AS DTN
        ON DS.DS_type_ID = DTN.DST_Type_ID
-     INNER JOIN dbo.T_Requested_Run AS RR
+     LEFT OUTER JOIN dbo.T_Requested_Run AS RR
        ON DS.Dataset_ID = RR.DatasetID
-     INNER JOIN dbo.T_Organisms AS Org
+     LEFT OUTER JOIN dbo.T_Organisms AS Org
        ON E.EX_organism_ID = Org.Organism_ID
-
 
 GO
