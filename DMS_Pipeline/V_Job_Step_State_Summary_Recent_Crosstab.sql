7ac45e957affd9c889ec9a71cdf3f5e9d0fd41d8
alchemistmatt <matthew.monroe@pnnl.gov>
2018-03-06 19:05:58 -0800

    Update tools in the job step state summary crosstab

--- DMS_Pipeline/V_Job_Step_State_Summary_Recent_Crosstab.sql
+++ DMS_Pipeline/V_Job_Step_State_Summary_Recent_Crosstab.sql
@@ -8,32 +8,30 @@ CREATE VIEW [dbo].[V_Job_Step_State_Summary_Recent_Crosstab]
 AS
 SELECT PivotData.State,
        PivotData.StateName AS Job_State,
-       IsNull([DTA_Gen], 0) AS [DTA_Gen],
-       IsNull([Sequest], 0) AS [Sequest],
-       IsNull([XTandem], 0) AS [XTandem],
-       IsNull([Inspect], 0) AS [Inspect],
-       IsNull([Decon2LS], 0) AS [Decon2LS],
-       IsNull([MASIC_Finnigan], 0) AS [MASIC_Finnigan],
-       IsNull([Results_Transfer], 0) AS [Results_Transfer],
        IsNull([DataExtractor], 0) AS [DataExtractor],
-       IsNull([InspectResultsAssembly], 0) AS [InspectResultsAssembly],
-       IsNull([MSXML_Gen], 0) AS [MSXML_Gen]
+       IsNull([Decon2LS_V2], 0) AS [Decon2LS_V2],
+       IsNull([DTA_Gen], 0) AS [DTA_Gen],
+       IsNull([DTA_Refinery], 0) AS [DTA_Refinery],
+       IsNull([MASIC_Finnigan], 0) AS [MASIC_Finnigan],
+       IsNull([MSGFPlus], 0) AS [MSGFPlus],
+       IsNull([MSPathFinder], 0) AS [MSPathFinder],
+       IsNull([MSXML_Gen], 0) AS [MSXML_Gen],
+       IsNull([Mz_Refinery], 0) AS [Mz_Refinery],
+       IsNull([PBF_Gen], 0) AS [PBF_Gen],
+       IsNull([Phospho_FDR_Aggregator], 0) AS [Phospho_FDR_Aggregator],
+       IsNull([PRIDE_Converter], 0) AS [PRIDE_Converter],
+       IsNull([ProMex], 0) AS [ProMex],
+       IsNull([SMAQC], 0) AS [SMAQC]
 FROM ( SELECT Step_Tool,
               State,
               StateName,
               StepCount
        FROM V_Job_Step_State_Summary_Recent ) AS SourceTable
-     PIVOT ( SUM(StepCount)
+     PIVOT ( Sum(StepCount)
              FOR Step_Tool
-             IN ( [DTA_Gen], 
-                  [Sequest], [XTandem], [Inspect], 
-                  [DataExtractor], [InspectResultsAssembly], 
-                  [Decon2LS],
-                  [MASIC_Finnigan],
-                  [Results_Transfer], 
-                  [MSXML_Gen] 
-             ) ) AS PivotData
-
+             IN ( [DataExtractor], [Decon2LS_V2], [DTA_Gen], [DTA_Refinery], [MASIC_Finnigan], 
+             [MSGFPlus], [MSPathFinder], [MSXML_Gen], [Mz_Refinery], [PBF_Gen], 
+             [Phospho_FDR_Aggregator], [PRIDE_Converter], [ProMex], [SMAQC] ) ) AS PivotData
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Job_Step_State_Summary_Recent_Crosstab] TO [DDL_Viewer] AS [dbo]
