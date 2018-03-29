77d65772a193d5bcc4ef06795f0d2fb9be433b9c
alchemistmatt <matthew.monroe@pnnl.gov>
2018-03-28 23:16:36 -0700

    Query tables directly

--- Protein_Sequences/V_Collection_Picker.sql
+++ Protein_Sequences/V_Collection_Picker.sql
@@ -6,21 +6,26 @@ GO
 
 CREATE VIEW [dbo].[V_Collection_Picker]
 AS
-SELECT PCByOrg.[FileName] AS [Name],
-       PCByOrg.Protein_Collection_ID AS ID,
-       PCByOrg.[Description],
-       PCByOrg.NumProteins AS Entries,
-       PCByOrg.NumResidues AS Residues,
-       PCByOrg.Organism_Name,
+SELECT PC.[FileName] AS [Name],
+       PC.Protein_Collection_ID AS ID,
+       PC.[Description],
+       PC.[Source],
+       PC.NumProteins AS Entries,
+       PC.NumResidues AS Residues,
+       OrgList.[Name] AS Organism_Name,
        PCTypes.[Type],
        AOF.Filesize,
-       PCByOrg.Organism_ID
-FROM dbo.V_Protein_Collections_By_Organism AS PCByOrg
-     INNER JOIN dbo.T_Protein_Collection_Types AS PCTypes
-       ON PCByOrg.Collection_Type_ID = PCTypes.Collection_Type_ID
+       OrgXref.Organism_ID
+FROM T_Protein_Collections PC
+     INNER JOIN T_Collection_Organism_Xref OrgXref
+       ON PC.Protein_Collection_ID = OrgXref.Protein_Collection_ID
+     INNER JOIN MT_Main.dbo.T_DMS_Organisms OrgList
+       ON OrgXref.Organism_ID = OrgList.Organism_ID
+     INNER JOIN T_Protein_Collection_Types AS PCTypes
+       ON PC.Collection_Type_ID = PCTypes.Collection_Type_ID
      LEFT OUTER JOIN dbo.T_Archived_Output_Files AS AOF
-       ON PCByOrg.Authentication_Hash = AOF.Authentication_Hash
-WHERE (PCByOrg.Collection_State_ID BETWEEN 1 AND 3)
+       ON PC.Authentication_Hash = AOF.Authentication_Hash
+WHERE PC.Collection_State_ID BETWEEN 1 AND 3
 
 
 GO
