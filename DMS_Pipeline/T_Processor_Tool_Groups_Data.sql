e4c0c23361c474dd5e8b301bb119d54f00dfeb14
alchemistmatt <matthew.monroe@pnnl.gov>
2018-03-29 22:42:51 -0700

    Configure managers on Pubs 14 through 17 to push jobs to a remote host

--- DMS_Pipeline/T_Processor_Tool_Groups_Data.sql
+++ DMS_Pipeline/T_Processor_Tool_Groups_Data.sql
@@ -27,5 +27,5 @@ INSERT INTO [T_Processor_Tool_Groups] VALUES (24,'PIC Pubs',-10,'VMs running on 
 INSERT INTO [T_Processor_Tool_Groups] VALUES (25,'Kiebel Development Box',1,'')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (26,'Proto_Storage_Servers with MSGF+, 1x',1,'Proto-5, Proto-6, and Proto-11; each has at least 24 GB of ram and at least 8 cores')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (27,'Supermicro 2022TG',1,'Supermicro Pubs 02-09 with AMD chips, 32 cores, 128 GB of ram, and a single hard drive; also supports MSXML_Bruker; have R 3.2.3')
-INSERT INTO [T_Processor_Tool_Groups] VALUES (28,'Supermicro 6028TR',1,'Supermicro Pubs 14-17 with Intel chips, 16 cores, 64 GB of ram, a 128 GB SSD, and two 1 TB drives; also supports MSXML_Bruker; have R 3.2.3')
+INSERT INTO [T_Processor_Tool_Groups] VALUES (28,'Supermicro 6028TR',1,'Supermicro Pubs 14-17 with Intel chips, 16 cores, 64 GB of ram, a 128 GB SSD, and two 1 TB drives; also supports MSXML_Bruker; have R 3.2.3.  AnalysisManager2 on each box pushes jobs to Linux boxes to be processed by offline AnalysisManagers. AnalysisManager2 instances also monitor jobs running remotely and retrieve results.')
 INSERT INTO [T_Processor_Tool_Groups] VALUES (29,'Proto_Storage_Servers with MSGF+, 2x',1,'Proto-3, Proto-4; can run two copies of MSGF+; each has at least 32 GB of ram and at least 12 cores')
