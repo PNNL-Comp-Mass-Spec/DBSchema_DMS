2588f4a503b5adbaddbccb8973976105c4e95fa4
alchemistmatt <matthew.monroe@pnnl.gov>
2018-04-25 14:01:02 -0700

    Update data package objects to join on Dataset_ID instead of dataset name

--- DMS_Data_Package/CheckDataPackageDatasetJobCoverage.sql
+++ DMS_Data_Package/CheckDataPackageDatasetJobCoverage.sql
@@ -7,96 +7,83 @@ CREATE FUNCTION dbo.CheckDataPackageDatasetJobCoverage
 /****************************************************
 **
 **  Desc: 
-**  Returns a table
+**  Returns a table of dataset job coverage
 **
 **  Return values: 
 **
 **  Parameters:
-**	
-**  Auth: grk
-**  Date: 05/22/2010
+**    
+**  Auth:   grk
+**  Date:   05/22/2010
+**          04/25/2018 - Now joining T_Data_Package_Datasets and T_Data_Package_Analysis_Jobs on Dataset_ID
 **    
 *****************************************************/
 (
-	@packageID INT,
-	@tool VARCHAR(128),
-	@mode VARCHAR(32)
+    @packageID INT,
+    @tool VARCHAR(128),
+    @mode VARCHAR(32)
 )
 RETURNS @table_variable TABLE (Dataset VARCHAR(128), Num int)
 AS
-	BEGIN
+BEGIN
 
-	-- package datasets with no package jobs for tool
-	--
-	IF @mode = 'NoPackageJobs'
-	BEGIN 
-		INSERT INTO @table_variable
-			( Dataset, Num )
-		SELECT
-		  Dataset, NULL AS Num
-		FROM
-		  T_Data_Package_Datasets AS TD
-		WHERE
-		  ( Data_Package_ID = @packageID )
-		  AND ( NOT EXISTS ( SELECT
-							  Dataset
-							 FROM
-							  T_Data_Package_Analysis_Jobs AS TA
-							 WHERE
-							  ( Tool = @tool )
-							  AND ( TD.Dataset = Dataset )
-							  AND ( TD.Data_Package_ID = Data_Package_ID ) )
-			  )
-	END
-		  
-		-- package datasets with no dms jobs for tool
-		--
-	IF @mode = 'NoDMSJobs'
-	BEGIN 
-		INSERT INTO @table_variable
-			( Dataset, Num )
-		SELECT
-		  Dataset, NULL AS Num
-		FROM
-		  T_Data_Package_Datasets AS TD
-		WHERE
-		  ( Data_Package_ID = @packageID )
-		  AND ( NOT EXISTS ( SELECT
-							  Dataset
-							 FROM
-							  S_V_Analysis_Job_List_Report_2 AS TA
-							 WHERE
-							  ( Tool = @tool )
-							  AND ( TD.Dataset = Dataset )
-							  AND ( TD.Data_Package_ID = Data_Package_ID ) )
-		  )
-	END
+    -- Package datasets with no package jobs for tool
+    --
+    IF @mode = 'NoPackageJobs'
+    BEGIN 
+        INSERT INTO @table_variable
+            ( Dataset, Num )
+        SELECT Dataset,
+               NULL AS Num
+        FROM T_Data_Package_Datasets AS TD
+        WHERE (Data_Package_ID = @packageID) AND
+              (NOT EXISTS ( SELECT Dataset
+                            FROM T_Data_Package_Analysis_Jobs AS TA
+                            WHERE Tool = @tool AND
+                                  TD.Dataset = Dataset AND
+                                  TD.Data_Package_ID = Data_Package_ID 
+                          ))
+    END
+          
+    -- Package datasets with no dms jobs for tool
+    --
+    IF @mode = 'NoDMSJobs'
+    BEGIN 
+        INSERT INTO @table_variable
+            ( Dataset, Num )
+        SELECT Dataset,
+               NULL AS Num
+        FROM T_Data_Package_Datasets AS TD
+        WHERE (Data_Package_ID = @packageID) AND
+              (NOT EXISTS ( SELECT Dataset
+                            FROM S_V_Analysis_Job_List_Report_2 AS TA
+                            WHERE Tool = @tool AND
+                                  TD.Dataset = Dataset AND
+                                  TD.Data_Package_ID = Data_Package_ID 
+              ))
+    END
   
-	IF @mode = 'PackageJobCount'
-	BEGIN 
-		INSERT INTO @table_variable
-			( Dataset, Num )
-		SELECT
-		  TD.Dataset,
-		  SUM(CASE WHEN TA.Job IS NULL THEN 0
-				   ELSE 1
-			  END) AS Num
-		FROM
-		  T_Data_Package_Datasets AS TD
-		  LEFT OUTER JOIN T_Data_Package_Analysis_Jobs AS TA ON TD.Dataset = TA.Dataset
-																AND TD.Data_Package_ID = TA.Data_Package_ID
+    IF @mode = 'PackageJobCount'
+    BEGIN 
+        INSERT INTO @table_variable
+            ( Dataset, Num )
+        SELECT TD.Dataset,
+               SUM(CASE
+                       WHEN TJ.Job IS NULL THEN 0
+                       ELSE 1
+                   END) AS Num
+        FROM T_Data_Package_Datasets AS TD
+             LEFT OUTER JOIN T_Data_Package_Analysis_Jobs AS TJ
+               ON TD.Dataset_ID = TJ.Dataset_ID AND
+                  TD.Data_Package_ID = TJ.Data_Package_ID
+        GROUP BY TD.Data_Package_ID, TD.Dataset, TJ.Tool
+        HAVING TD.Data_Package_ID = @packageID AND
+               TJ.Tool = @tool
+    END
 
-		GROUP BY
-		  TD.Data_Package_ID,
-		  TD.Dataset,
-		  TA.Tool
-		HAVING
-		  TD.Data_Package_ID = @packageID 
-		  AND TA.Tool = @tool
-		END
+    RETURN
+END
 
-	RETURN
-	END
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[CheckDataPackageDatasetJobCoverage] TO [DDL_Viewer] AS [dbo]
--- DMS_Data_Package/V_Data_Package_Aggregation_List_Report.sql
+++ DMS_Data_Package/V_Data_Package_Aggregation_List_Report.sql
@@ -4,10 +4,9 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_Data_Package_Aggregation_List_Report]
-AS
-SELECT dbo.GetXMLRow(TD.Data_Package_ID, 'Job', TM.Job) AS [Sel.],
+As
+SELECT dbo.GetXMLRow(TD.Data_Package_ID, 'Job', TJ.Job) AS [Sel.],
        TM.Job,
        TM.State,
        TM.Tool,
@@ -26,15 +25,14 @@ SELECT dbo.GetXMLRow(TD.Data_Package_ID, 'Job', TM.Job) AS [Sel.],
        DS.Rating,
 	   DS.Instrument
 FROM T_Data_Package_Datasets AS TD
-     INNER JOIN S_V_Dataset_List_Report_2 AS DS
+     LEFT OUTER JOIN S_V_Dataset_List_Report_2 AS DS
        ON TD.Dataset_ID = DS.ID
      LEFT OUTER JOIN S_V_Analysis_Job_List_Report_2 AS TM
-       ON TD.Dataset = TM.Dataset
+       ON TD.Dataset_ID = TM.Dataset_ID
      LEFT OUTER JOIN T_Data_Package_Analysis_Jobs AS TJ
        ON TJ.Job = TM.Job AND
+          TJ.Dataset_ID = TD.Dataset_ID AND
           TJ.Data_Package_ID = TD.Data_Package_ID
-
-
 
 
 GO
