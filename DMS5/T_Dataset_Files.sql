7199131fed974e8fe693660db3dea53bdb9c7f88
alchemistmatt <matthew.monroe@pnnl.gov>
2018-06-17 21:38:50 -0700

    Add a new clustered index to T_Dataset_Files

--- DMS5/T_Dataset_Files.sql
+++ DMS5/T_Dataset_Files.sql
@@ -11,12 +11,22 @@ CREATE TABLE [dbo].[T_Dataset_Files](
 	[File_Hash] [varchar](64) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[Allow_Duplicates] [bit] NULL,
 	[Deleted] [bit] NULL,
- CONSTRAINT [PK_T_Dataset_Files] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_T_Dataset_Files] PRIMARY KEY NONCLUSTERED 
 (
 	[Dataset_File_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_T_Dataset_Files_DatasetID_FilePath] ******/
+CREATE UNIQUE CLUSTERED INDEX [IX_T_Dataset_Files_DatasetID_FilePath] ON [dbo].[T_Dataset_Files]
+(
+	[Dataset_ID] ASC,
+	[File_Path] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_Dataset_Files] ADD  CONSTRAINT [DF_T_Dataset_Files_Allow_Duplicates]  DEFAULT ((0)) FOR [Allow_Duplicates]
 GO
