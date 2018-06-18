fef5f7b95e3d6eaa8279f28bd6773180df23c147
alchemistmatt <matthew.monroe@pnnl.gov>
2018-06-17 21:39:24 -0700

    Add new cell culture types

--- DMS5/T_Cell_Culture_Type_Name_Data.sql
+++ DMS5/T_Cell_Culture_Type_Name_Data.sql
@@ -1,9 +1,12 @@
 /****** Object:  Table [T_Cell_Culture_Type_Name] ******/
-/****** RowCount: 7 ******/
+/****** RowCount: 10 ******/
 SET IDENTITY_INSERT [T_Cell_Culture_Type_Name] ON
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (105,'Biofluid')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (104,'Community')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (2,'Eukaryote')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (107,'Genetically modified eukaryote')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (106,'Genetically modified prokaryote')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (108,'Genetically modified virus')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (103,'Metabolite standards')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (1,'Prokaryote')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (102,'Protein/peptide standards')
