4c04d89a379ed414bdd1042660d92cda28f5b084
alchemistmatt <matthew.monroe@pnnl.gov>
2018-07-07 01:02:40 -0700

    Remove where clause and rename column "MyEMSL Transaction IDs"

--- DMS5/V_MyEMSL_Proposal_ID_Update.sql
+++ DMS5/V_MyEMSL_Proposal_ID_Update.sql
@@ -3,13 +3,12 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW [dbo].[V_MyEMSL_Proposal_ID_Update] 
-AS
+AS 
 -- This view is used by MyEMSL to check for updated EUS Proposal IDs
 SELECT RR.ID AS Request_ID,
        DS.Dataset_ID AS Dataset_ID,
-       dbo.GetDatasetMyEMSLTransactionIDs(DS.Dataset_ID) AS [MyEMSL Transaction IDs],
+       dbo.GetDatasetMyEMSLTransactionIDs(DS.Dataset_ID) AS [MyEMSL_Transaction_ID_list],
        RR.RDS_EUS_Proposal_ID,
        RR.Updated AS Updated
 FROM T_Requested_Run AS RR
@@ -17,7 +16,4 @@ FROM T_Requested_Run AS RR
        ON RR.DatasetID = DS.Dataset_ID
      LEFT OUTER JOIN T_EUS_Proposals AS EUP
        ON RR.RDS_EUS_Proposal_ID = EUP.Proposal_ID
-WHERE RR.RDS_EUS_Proposal_ID IS NOT NULL
-
-
 GO
