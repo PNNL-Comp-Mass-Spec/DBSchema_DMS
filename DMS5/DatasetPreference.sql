4c1daf31236368a4e354334eb5bf8750259808fa
alchemistmatt <matthew.monroe@pnnl.gov>
2018-08-08 18:04:37 -0700

    Auto release QC_Mam and QC_PP_MCF-7 datasets and create priority 2 jobs for them

--- DMS5/DatasetPreference.sql
+++ DMS5/DatasetPreference.sql
@@ -3,48 +3,57 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE FUNCTION dbo.DatasetPreference
+
+CREATE FUNCTION [dbo].[DatasetPreference]
 /****************************************************
 **
-**	Desc: 
-**		Determines if dataset name warrants preferential processing priority
-**		This procedure is used by AddNewDataset to auto-release QC_Shew datasets
-**		(if either the dataset name or the experiment name matches one of the
-**		 filters below, the Interest_Rating is set to 5 (Released)
+**  Desc: 
+**       Determines if dataset name warrants preferential processing priority
+**       This procedure is used by AddNewDataset to auto-release QC_Shew datasets
 **
-**	Return values: 1 if preferred, 0 if not
+**       If either the dataset name or the experiment name matches one of the
+**       filters below, the Interest_Rating is set to 5 (Released)
 **
-**	Parameters:
-**	
+**  Return values: 1 if preferred, 0 if not
 **
-**	Auth:	grk
-**	Date:	02/10/2006
-**			04/09/2007 mem - Added matching of QC_Shew datasets in addition to QC datasets (Ticket #430)
-**			04/11/2008 mem - Added matching of SE_QC_Shew datasets in addition to QC datasets
-**			05/12/2011 mem - Now excluding datasets that end in -bad
-**			01/16/2014 mem - Added QC_ShewIntact datasets
-**			12/18/2014 mem - Replace [_] with [_-]
-**			05/07/2015 mem - Added QC_Shew_TEDDY
+**  Parameters:
+**    
+**
+**  Auth:   grk
+**  Date:   02/10/2006
+**          04/09/2007 mem - Added matching of QC_Shew datasets in addition to QC datasets (Ticket #430)
+**          04/11/2008 mem - Added matching of SE_QC_Shew datasets in addition to QC datasets
+**          05/12/2011 mem - Now excluding datasets that end in -bad
+**          01/16/2014 mem - Added QC_ShewIntact datasets
+**          12/18/2014 mem - Replace [_] with [_-]
+**          05/07/2015 mem - Added QC_Shew_TEDDY
+**          08/08/2018 mem - Added QC_Mam and QC_PP_MCF-7
 **    
 *****************************************************/
 (
-	@datasetNum varchar(128)
+    @datasetNum varchar(128)
 )
 RETURNS tinyint
 AS
 BEGIN
-	declare @result tinyint
+    Declare @result tinyint
 
-	IF (@datasetNum LIKE 'QC[_][0-9][0-9]%' OR
-	    @datasetNum LIKE 'QC[_-]Shew[_-][0-9][0-9]%' OR
-	    @datasetNum LIKE 'QC[_-]ShewIntact%' OR
-	    @datasetNum LIKE 'QC[_]Shew[_]TEDDY%') AND
-	    NOT @datasetNum LIKE '%-bad'
-	    SET @result = 1
-	ELSE
-	    SET @result = 0
-	
-	RETURN @result
+    If (@datasetNum LIKE 'QC[_][0-9][0-9]%' OR
+        @datasetNum LIKE 'QC[_-]Shew[_-][0-9][0-9]%' OR
+        @datasetNum LIKE 'QC[_-]ShewIntact%' OR
+        @datasetNum LIKE 'QC[_]Shew[_]TEDDY%' OR
+        @datasetNum LIKE 'QC[_]Mam%' OR
+        @datasetNum Like 'QC[_]PP[_]MCF-7%'
+       ) AND NOT @datasetNum LIKE '%-bad'
+    Begin
+        Set @result = 1
+    End
+    Else
+    Begin
+        Set @result = 0
+    End
+    
+    Return @result
 END
 
 
