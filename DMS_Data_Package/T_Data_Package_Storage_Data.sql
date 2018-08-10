51ce19b580083bf5bc74163c0501ab88fa1a8e2d
alchemistmatt <matthew.monroe@pnnl.gov>
2018-08-10 10:56:16 -0700

    Update table data

--- DMS_Data_Package/T_Data_Package_Storage_Data.sql
+++ DMS_Data_Package/T_Data_Package_Storage_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_Data_Package_Storage] ******/
 /****** RowCount: 1 ******/
 SET IDENTITY_INSERT [T_Data_Package_Storage] ON
-INSERT INTO [T_Data_Package_Storage] (ID, Path_Local_Root, Path_Shared_Root, Path_Web_Root, Path_Archive_Root, State) VALUES (11,'F:\DataPkgs','\\protoapps\DataPkgs\','http://protoapps.pnl.gov/DataPkgs/public.php?folder=','/aurora/prismarch/DataPkgs/','Active')
+INSERT INTO [T_Data_Package_Storage] (ID, Path_Local_Root, Path_Shared_Root, Path_Web_Root, Path_Archive_Root, State) VALUES (11,'I:\DataPkgs','\\protoapps\DataPkgs\','http://protoapps.pnl.gov/DataPkgs/public.php?folder=','/aurora/prismarch/DataPkgs/','Active')
 SET IDENTITY_INSERT [T_Data_Package_Storage] OFF
--- DMS_Pipeline/T_Process_Step_Control_Data.sql
+++ DMS_Pipeline/T_Process_Step_Control_Data.sql
@@ -1,14 +1,15 @@
 /****** Object:  Table [T_Process_Step_Control] ******/
-/****** RowCount: 11 ******/
+/****** RowCount: 12 ******/
 /****** Columns: Processing_Step_Name, enabled, Last_Affected, Entered_By ******/
 INSERT INTO [T_Process_Step_Control] VALUES ('AddNewJobs',1,'2/3/2010 2:19:33 PM','pnl\D3L243')
+INSERT INTO [T_Process_Step_Control] VALUES ('AutoFixFailedJobs',1,'5/1/2015 2:23:51 PM','PNL\D3L243')
 INSERT INTO [T_Process_Step_Control] VALUES ('CreateJobSteps',1,'6/5/2009 1:56:15 PM','PNL\D3L243')
-INSERT INTO [T_Process_Step_Control] VALUES ('ImportJobProcessors',1,'6/5/2009 1:50:15 PM','PNL\D3L243')
-INSERT INTO [T_Process_Step_Control] VALUES ('ImportProcessors',1,'6/5/2009 1:50:15 PM','PNL\D3L243')
-INSERT INTO [T_Process_Step_Control] VALUES ('LogLevel',1,'6/4/2009 7:45:24 PM','PNL\D3L243')
+INSERT INTO [T_Process_Step_Control] VALUES ('ImportJobProcessors-Deprecated',0,'5/28/2015 3:14:13 PM','PNL\D3L243')
+INSERT INTO [T_Process_Step_Control] VALUES ('ImportProcessors',1,'5/28/2015 3:14:13 PM','PNL\D3L243')
+INSERT INTO [T_Process_Step_Control] VALUES ('LogLevel',1,'5/4/2015 10:42:35 AM','PNL\D3L243')
 INSERT INTO [T_Process_Step_Control] VALUES ('RemoveDMSDeletedJobs',1,'6/5/2009 1:50:15 PM','PNL\D3L243')
 INSERT INTO [T_Process_Step_Control] VALUES ('SyncJobInfo',1,'6/5/2009 1:50:15 PM','PNL\D3L243')
 INSERT INTO [T_Process_Step_Control] VALUES ('UpdateCPULoading',1,'6/5/2009 1:50:15 PM','PNL\D3L243')
 INSERT INTO [T_Process_Step_Control] VALUES ('UpdateDMS',1,'6/4/2009 7:33:44 PM','PNL\D3L243')
 INSERT INTO [T_Process_Step_Control] VALUES ('UpdateJobState',1,'6/5/2009 1:56:15 PM','PNL\D3L243')
-INSERT INTO [T_Process_Step_Control] VALUES ('UpdateStepStates',1,'6/5/2009 1:56:15 PM','PNL\D3L243')
+INSERT INTO [T_Process_Step_Control] VALUES ('UpdateStepStates',1,'9/17/2014 4:18:17 PM','PNL\D3L243')
