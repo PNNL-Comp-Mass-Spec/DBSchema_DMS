84df096d1775005d6dc461c73da7f8c3a3e44dae
alchemistmatt <matthew.monroe@pnnl.gov>
2018-08-10 18:19:08 -0700

    Call UpdateDMSFileInfoXML when flagging bad datasets that failed capture

--- DMS_Capture/HandleDatasetCaptureValidationFailure.sql
+++ DMS_Capture/HandleDatasetCaptureValidationFailure.sql
@@ -3,154 +3,180 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE Procedure dbo.HandleDatasetCaptureValidationFailure
+
+CREATE Procedure [dbo].[HandleDatasetCaptureValidationFailure]
 /****************************************************
 **
-**	Desc:	This procedure can be used with datasets that
-**			are successfully captured but fail the dataset integrity check
-**			(.Raw file too small, expected files missing, etc).
+**  Desc:   This procedure can be used with datasets that
+**          are successfully captured but fail the dataset integrity check
+**          (.Raw file too small, expected files missing, etc).
 **
-**			The procedure changes the capture job state to 101
-**			then calls HandleDatasetCaptureValidationFailure in DMS5
+**          The procedure changes the capture job state to 101
+**          then calls HandleDatasetCaptureValidationFailure in DMS5
 **
-**	Return values: 0: success, otherwise, error code
+**  Return values: 0: success, otherwise, error code
 **
-**	Parameters:
-**
-**	Auth:	mem
-**	Date:	04/28/2011
-**			09/13/2011 mem - Updated to support script 'IMSDatasetCapture' in addition to 'DatasetCapture'
-**			11/05/2012 mem - Added additional Print statement
-**			04/06/2016 mem - Now using Try_Convert to convert from text to int
+**  Auth:   mem
+**  Date:   04/28/2011
+**          09/13/2011 mem - Updated to support script 'IMSDatasetCapture' in addition to 'DatasetCapture'
+**          11/05/2012 mem - Added additional Print statement
+**          04/06/2016 mem - Now using Try_Convert to convert from text to int
+**          08/10/2018 mem - Call UpdateDMSFileInfoXML to push the dataset info into DMS5.T_Dataset_Info
 **
 *****************************************************/
 (
-	@DatasetNameOrID varchar(255),
-	@Comment varchar(255) = 'Bad .raw file',
-	@InfoOnly tinyint = 0,
-	@message varchar(512) = '' output
+    @datasetNameOrID varchar(255),
+    @comment varchar(255) = 'Bad .raw file',
+    @infoOnly tinyint = 0,
+    @message varchar(512) = '' output
 )
 As
-	set nocount on
+    set nocount on
 
-	declare @myError int
-	declare @myRowCount int
-	set @myError = 0
-	set @myRowCount = 0
-	
-	Declare @DatasetID int
-	Declare @DatasetName varchar(255)
-	
-	Set @DatasetName = ''
-	Set @DatasetID = 0
-
-	----------------------------------------
-	-- Validate the inputs
-	----------------------------------------
-
-	Set @DatasetNameOrID = IsNull(@DatasetNameOrID, '')
-	Set @Comment = IsNull(@Comment, '')
-	Set @message = ''
-	
-	If @Comment = ''
-		Set @Comment = 'Bad dataset'
-		
-	Set @DatasetID = IsNull(Try_Convert(int, @DatasetNameOrID), 0)
-	If @DatasetID <> 0
-	Begin
-		----------------------------------------
-		-- Lookup the Dataset Name
-		----------------------------------------
-		
-		Set @DatasetID = Convert(int, @DatasetNameOrID)
-		
-		SELECT @DatasetName = Dataset
-		FROM T_Jobs
-		WHERE Dataset_ID = @DatasetID AND 
-			  Script IN ('DatasetCapture', 'IMSDatasetCapture')
-		
-		If @DatasetName = ''
-		Begin
-			set @message = 'Dataset ID not found: ' + @DatasetNameOrID
-			Set @myError = 50000
-			Print @message
-		End
-
-	End
-	Else
-	Begin	
-		----------------------------------------
-		-- Lookup the dataset ID
-		----------------------------------------
-	
-		Set @DatasetName = @DatasetNameOrID
-				
-		SELECT @DatasetID = Dataset_ID
-		FROM T_Jobs
-		WHERE Dataset = @DatasetName AND 
-			  Script IN ('DatasetCapture', 'IMSDatasetCapture')
-		
-		If @DatasetName = ''
-		Begin
-			set @message = 'Dataset not found: ' + @DatasetName
-			Set @myError = 50001
-			Print @message
-		End
-	End
-	
-	If @myError = 0
-	Begin	
-		-- Make sure the DatasetCapture job has failed
-		IF NOT EXISTS (SELECT * FROM T_Jobs WHERE Dataset_ID = @DatasetID AND Script IN ('DatasetCapture', 'IMSDatasetCapture') AND State = 5)
-		Begin
-			Set @message = 'DatasetCapture job for dataset ' + @DatasetName + ' is not in State 5; unable to continue'
-			Set @myError = 50002
-			Print @message
-		End
-	End
-	
-	If @myError = 0
-	Begin
-		If @infoOnly <> 0
-		Begin
-			SELECT 'Mark dataset as bad: ' + @comment as Message, *
-			FROM T_Jobs
-			WHERE Dataset_ID = @DatasetID AND 
-			      Script IN ('DatasetCapture', 'IMSDatasetCapture') AND 
-			      State = 5
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
     
-		End
-		Else
-		Begin
-	
-			UPDATE T_Jobs
-			SET State = 101
-			WHERE Dataset_ID = @DatasetID AND 
-			      Script IN ('DatasetCapture', 'IMSDatasetCapture') AND 
-			      State = 5
-			--
-			SELECT @myError = @@error, @myRowCount = @@rowcount
+    Declare @datasetID int
+    Declare @datasetName varchar(255)
+    Declare @captureJob int
 
-			If @myRowCount = 0
-			Begin
-				Set @message = 'Unable to update dataset in T_Jobs: ' + @DatasetName
-				Set @myError = 50003
-				Print @message
-			End
-			Else
-			Begin
-				-- Mark the dataset as bad in DMS5
-				Exec DMS5.dbo.HandleDatasetCaptureValidationFailure @DatasetID, @Comment, @InfoOnly, ''
-				
-				Set @message = 'Marked dataset as bad: ' + @DatasetName
-				Print @message
-				
-			End
-		End
-	
-	End
+    Set @datasetName = ''
+    Set @datasetID = 0
+    Set @captureJob = 0
 
-	return @myError
+    ----------------------------------------
+    -- Validate the inputs
+    ----------------------------------------
+
+    Set @datasetNameOrID = IsNull(@datasetNameOrID, '')
+    Set @comment = IsNull(@comment, '')
+    Set @message = ''
+    
+    If @comment = ''
+        Set @comment = 'Bad dataset'
+        
+    Set @datasetID = IsNull(Try_Convert(int, @datasetNameOrID), 0)
+    If @datasetID <> 0
+    Begin
+        ----------------------------------------
+        -- Lookup the Dataset Name
+        ----------------------------------------
+        
+        Set @datasetID = Convert(int, @datasetNameOrID)
+        
+        SELECT @datasetName = Dataset
+        FROM T_Jobs
+        WHERE Dataset_ID = @datasetID AND 
+              Script IN ('DatasetCapture', 'IMSDatasetCapture')
+        
+        If @datasetName = ''
+        Begin
+            set @message = 'Dataset ID not found: ' + @datasetNameOrID
+            Set @myError = 50000
+            Print @message
+        End
+
+    End
+    Else
+    Begin    
+        ----------------------------------------
+        -- Lookup the dataset ID
+        ----------------------------------------
+    
+        Set @datasetName = @datasetNameOrID
+                
+        SELECT @datasetID = Dataset_ID
+        FROM T_Jobs
+        WHERE Dataset = @datasetName AND 
+              Script IN ('DatasetCapture', 'IMSDatasetCapture')
+        
+        If @datasetName = ''
+        Begin
+            set @message = 'Dataset not found: ' + @datasetName
+            Set @myError = 50001
+            Print @message
+        End
+    End
+    
+    If @myError = 0
+    Begin    
+        -- Make sure the DatasetCapture job has failed
+        SELECT @captureJob = Job
+        FROM T_Jobs
+        WHERE Dataset_ID = @datasetID AND
+              Script IN ('DatasetCapture', 'IMSDatasetCapture') AND
+              State = 5
+        --
+        SELECT @myError = @@error, @myRowCount = @@rowcount
+
+        IF @myRowCount = 0 Or @captureJob = 0
+        Begin
+            Set @message = 'DatasetCapture job for dataset ' + @datasetName + ' is not in State 5; unable to continue'
+            Set @myError = 50002
+            Print @message
+        End
+    End
+    
+    If @myError = 0
+    Begin -- <a>
+        If @infoOnly <> 0
+        Begin
+            SELECT 'Mark dataset as bad: ' + @comment as Message, *
+            FROM T_Jobs
+            WHERE Dataset_ID = @datasetID AND 
+                  Script IN ('DatasetCapture', 'IMSDatasetCapture') AND 
+                  State = 5
+    
+        End
+        Else
+        Begin -- <b>
+
+            -- Call UpdateDMSFileInfoXML to push the dataset info into DMS5.T_Dataset_Info
+            -- If a duplicate dataset is found, @myError will be 53600
+            EXEC @myError = UpdateDMSFileInfoXML @datasetID, @DeleteFromTableOnSuccess=1, @message=@message Output
+
+            If @myError = 53600
+            Begin
+                -- Use special completion code of 101
+                EXEC @myError = S_SetCaptureTaskComplete @datasetName, 101, @message OUTPUT, @failureMessage = @message
+                
+                -- Fail out the job with state 14 (Failed, Ignore Job Step States)
+                UPDATE T_Jobs
+                SET State = 14
+                WHERE Job = @captureJob
+                --
+                SELECT @myError = @@error, @myRowCount = @@rowcount
+
+                Goto Done
+            End
+            
+            UPDATE T_Jobs
+            SET State = 101
+            WHERE Job = @captureJob
+            --
+            SELECT @myError = @@error, @myRowCount = @@rowcount
+
+            If @myRowCount = 0
+            Begin
+                Set @message = 'Unable to update job ' + Cast(@captureJob As varchar(12)) + 'in T_Jobs for dataset ' + @datasetName
+                Set @myError = 50003
+                Print @message
+            End
+            Else
+            Begin
+                -- Mark the dataset as bad in DMS5
+                Exec DMS5.dbo.HandleDatasetCaptureValidationFailure @datasetID, @comment, @infoOnly, ''
+                
+                Set @message = 'Marked dataset as bad: ' + @datasetName
+                Print @message
+                
+            End
+        End -- </b>
+    
+    End -- </a>
+
+Done:
+    return @myError
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[HandleDatasetCaptureValidationFailure] TO [DDL_Viewer] AS [dbo]
