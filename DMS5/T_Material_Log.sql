5b28bc30833576baede11b1ddb3682671f43522d
alchemistmatt <matthew.monroe@pnnl.gov>
2018-08-29 01:02:53 -0700

    2018-08-29 auto-commit DMS5

--- DMS5/T_Material_Log.sql
+++ DMS5/T_Material_Log.sql
@@ -12,6 +12,8 @@ CREATE TABLE [dbo].[T_Material_Log](
 	[Final_State] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[User_PRN] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[Comment] [varchar](512) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Item_Type]  AS (case when [Type] like 'B Material%' then 'Biomaterial' when [Type] like 'Biomaterial%' then 'Biomaterial' when [Type] like 'E Material%' then 'Experiment' when [Type] like 'Experiment%' then 'Experiment' when [Type] like 'R Material%' then 'RefCompound' when [Type] like 'Reference Compound%' then 'RefCompound' when [Type] like 'Container%' then 'Container' when [Type] like '%Container' then 'Container' else [Type] end) PERSISTED NOT NULL,
+	[Type_Name_Cached]  AS (case when [Type]='B Material Move' then 'Biomaterial Move' when [Type]='B Material Retirement' then 'Biomaterial Retirement' when [Type]='E Material Move' then 'Experiment Move' when [Type]='E Material Retirement' then 'Experiment Retirement' when [Type]='R Material Move' then 'RefCompound Move' when [Type]='R Material Retirement' then 'RefCompound Retirement' else [Type] end) PERSISTED NOT NULL,
  CONSTRAINT [PK_T_Material_Log] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
@@ -20,6 +22,38 @@ CREATE TABLE [dbo].[T_Material_Log](
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[T_Material_Log] TO [DDL_Viewer] AS [dbo]
+GO
+SET ARITHABORT ON
+SET CONCAT_NULL_YIELDS_NULL ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+SET NUMERIC_ROUNDABORT OFF
+
+GO
+/****** Object:  Index [IX_T_Material_Log_Item_Type_Date] ******/
+CREATE NONCLUSTERED INDEX [IX_T_Material_Log_Item_Type_Date] ON [dbo].[T_Material_Log]
+(
+	[Item_Type] ASC,
+	[Date] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ARITHABORT ON
+SET CONCAT_NULL_YIELDS_NULL ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+SET NUMERIC_ROUNDABORT OFF
+
+GO
+/****** Object:  Index [IX_T_Material_Log_Type_Name_Cached_Date] ******/
+CREATE NONCLUSTERED INDEX [IX_T_Material_Log_Type_Name_Cached_Date] ON [dbo].[T_Material_Log]
+(
+	[Type_Name_Cached] ASC,
+	[Date] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_Material_Log] ADD  CONSTRAINT [DF_T_Material_Log_Date]  DEFAULT (getdate()) FOR [Date]
 GO
--- DMS5/V_Material_Log_List_Report.sql
+++ DMS5/V_Material_Log_List_Report.sql
@@ -4,21 +4,23 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_Material_Log_List_Report]
 AS
-SELECT  TML.ID ,
-        TML.Date ,
-        TML.Type ,
-        TML.Item ,
-        TML.Initial_State AS Initial ,
-        TML.Final_State AS Final ,
-        TU.Name_with_PRN AS [User] ,
-        TML.Comment,
-        TMC.Comment AS [Container Comment]
-FROM    dbo.T_Material_Log TML
-        LEFT OUTER JOIN dbo.T_Users TU ON TML.User_PRN = TU.U_PRN
-        LEFT OUTER JOIN dbo.T_Material_Containers TMC ON TML.Item = TMC.Tag
+SELECT ML.ID,
+       ML.[Date],
+       ML.Type_Name_Cached AS [Type],
+       ML.Item,
+       ML.Initial_State AS Initial,
+       ML.Final_State AS Final,
+       U.Name_with_PRN AS [User],
+       ML.[Comment],
+       TMC.[Comment] AS [Container Comment],
+       ML.[Item_Type]
+FROM dbo.T_Material_Log ML
+     LEFT OUTER JOIN dbo.T_Users U
+       ON ML.User_PRN = U.U_PRN
+     LEFT OUTER JOIN dbo.T_Material_Containers TMC
+       ON ML.Item = TMC.Tag
 
 
 GO
