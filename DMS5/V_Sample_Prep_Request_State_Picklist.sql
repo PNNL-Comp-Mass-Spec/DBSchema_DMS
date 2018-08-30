4e621f975be812f3ecd25f1f3ecd19fe30a87341
alchemistmatt <matthew.monroe@pnnl.gov>
2018-08-29 20:18:32 -0700

    Remove the 'Closed (containers and material)' state for sample prep requests

--- DMS5/V_Sample_Prep_Request_State_Picklist.sql
+++ DMS5/V_Sample_Prep_Request_State_Picklist.sql
@@ -4,14 +4,16 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE view [dbo].[V_Sample_Prep_Request_State_Picklist] as 
+CREATE VIEW [dbo].[V_Sample_Prep_Request_State_Picklist]
+AS 
 SELECT State_Name AS val,
        State_Name AS ex,
        State_ID
 FROM T_Sample_Prep_Request_State_Name
 WHERE (Active = 1)
-UNION
-SELECT 'Closed (containers and material)' AS val, 'Closed (containers and material)' AS ex, 99 AS State_ID
+-- Deprecated:
+-- UNION
+-- SELECT 'Closed (containers and material)' AS val, 'Closed (containers and material)' AS ex, 99 AS State_ID
 
 
 GO
