357fca7fa47b1ed8385062c4c8998cd8e5fd3565
alchemistmatt <matthew.monroe@pnnl.gov>
2018-10-03 17:29:06 -0700

    Add @comment to AddUpdateBionetHost and update parameter capitalization

--- DMS5/AddUpdateBionetHost.sql
+++ DMS5/AddUpdateBionetHost.sql
@@ -3,170 +3,175 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE AddUpdateBionetHost
+
+CREATE PROCEDURE [dbo].[AddUpdateBionetHost]
 /****************************************************
 **
-**	Desc: 
-**		Adds new or edits existing item in 
-**		T_Bionet_Hosts 
+**  Desc:   Adds new or edits existing item in T_Bionet_Hosts 
 **
-**	Return values: 0: success, otherwise, error code
+**  Return values: 0: success, otherwise, error code
 **
-**	Date:	09/08/2016 mem - Initial version
-**			04/12/2017 mem - Log exceptions to T_Log_Entries
-**			06/16/2017 mem - Restrict access using VerifySPAuthorized
-**			08/01/2017 mem - Use THROW if not authorized
+**  Date:   09/08/2016 mem - Initial version
+**          04/12/2017 mem - Log exceptions to T_Log_Entries
+**          06/16/2017 mem - Restrict access using VerifySPAuthorized
+**          08/01/2017 mem - Use THROW if not authorized
+**          10/03/2018 mem - Add @comment
 **    
 *****************************************************/
 (
-	@Host varchar(64),
-	@IP varchar(15),
-	@Alias varchar(64),
-	@Tag varchar(24),
-	@Instruments varchar(1024),
-	@Active tinyint,
-	@mode varchar(12) = 'add', -- or 'update'
-	@message varchar(512) output,
-	@callingUser varchar(128) = ''
+    @host varchar(64),
+    @ip varchar(15),
+    @alias varchar(64),
+    @tag varchar(24),
+    @instruments varchar(1024),
+    @active tinyint,
+    @comment varchar(1024),
+    @mode varchar(12) = 'add', -- or 'update'
+    @message varchar(512) output,
+    @callingUser varchar(128) = ''
 )
 As
-	Set XACT_ABORT, nocount on
+    Set XACT_ABORT, nocount on
 
-	Declare @myError int = 0
-	Declare @myRowCount int = 0
-	
-	Set @message = ''
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
+    
+    Set @message = ''
 
-	Declare @msg varchar(256)
-	
-	---------------------------------------------------
-	-- Verify that the user can execute this procedure from the given client host
-	---------------------------------------------------
-		
-	Declare @authorized tinyint = 0	
-	Exec @authorized = VerifySPAuthorized 'AddUpdateBionetHost', @raiseError = 1
-	If @authorized = 0
-	Begin
-		THROW 51000, 'Access denied', 1;
-	End
+    Declare @msg varchar(256)
+    
+    ---------------------------------------------------
+    -- Verify that the user can execute this procedure from the given client host
+    ---------------------------------------------------
+        
+    Declare @authorized tinyint = 0    
+    Exec @authorized = VerifySPAuthorized 'AddUpdateBionetHost', @raiseError = 1
+    If @authorized = 0
+    Begin;
+        THROW 51000, 'Access denied', 1;
+    End;
 
-	Begin Try 	
+    Begin Try     
 
-		---------------------------------------------------
-		-- Validate input fields
-		---------------------------------------------------
-	
-		If @mode IS NULL OR Len(@mode) < 1
-		Begin
-			Set @myError = 51002
-			RAISERROR ('@mode cannot be blank',
-				11, 1)
-		End
+        ---------------------------------------------------
+        -- Validate input fields
+        ---------------------------------------------------
+    
+        If @mode IS NULL OR Len(@mode) < 1
+        Begin
+            Set @myError = 51002
+            RAISERROR ('@mode cannot be blank',
+                11, 1)
+        End
 
-		If @Host IS NULL OR Len(@Host) < 1
-		Begin
-			Set @myError = 51002
-			RAISERROR ('@Host cannot be blank',
-				11, 1)
-		End
+        If @host IS NULL OR Len(@host) < 1
+        Begin
+            Set @myError = 51002
+            RAISERROR ('@host cannot be blank',
+                11, 1)
+        End
 
-		Set @IP = IsNull(@IP, '')
-		
-		If Len(IsNull(@Alias, '')) = 0 Set @Alias = Null
-		If Len(IsNull(@Tag, '')) = 0 Set @Tag = Null
-		If Len(IsNull(@Instruments, '')) = 0 Set @Instruments = Null
+        Set @ip = IsNull(@ip, '')
+        
+        If Len(Ltrim(Rtrim(IsNull(@alias, '')))) = 0 Set @alias = Null
+        If Len(Ltrim(Rtrim(IsNull(@tag, '')))) = 0 Set @tag = Null
+        If Len(Ltrim(Rtrim(IsNull(@instruments, '')))) = 0 Set @instruments = Null
+        If Len(Ltrim(Rtrim(IsNull(@comment, '')))) = 0 Set @comment = Null
 
-		Set @Active = IsNull(@Active, 1)
-	
-		---------------------------------------------------
-		-- Is entry already in database?
-		---------------------------------------------------
-	
-		If @mode = 'add' And Exists (SELECT * FROM T_Bionet_Hosts WHERE Host = @Host)
-		Begin
-			-- Cannot create an entry that already exists
-			--
-			Set @msg = 'Cannot add: item "' + @Host + '" is already in the database'
-			RAISERROR (@msg, 11, 1)
-			return 51004
-		End
-		
-		
-		If @mode = 'update' And Not Exists (SELECT * FROM T_Bionet_Hosts WHERE Host = @Host)
-		Begin
-			-- Cannot update a non-existent entry
-			Set @msg = 'Cannot update: item "' + @Host + '" is not in the database'
-			RAISERROR (@msg, 11, 16)
-			return 51005
-		End
-	
-		---------------------------------------------------
-		-- Action for add mode
-		---------------------------------------------------
-		--
-		If @Mode = 'add'
-		Begin
-		
-			INSERT INTO T_Bionet_Hosts( 
-				Host,
-				IP,
-				Alias,
-				Entered,
-				Instruments,
-				Active,
-				Tag 
-			)
-			VALUES(
-				@Host, 
-				@IP, 
-				@Alias, 
-				GetDate(), 
-				@Instruments, 
-				@Active, 
-				@Tag
-			)
-			--
-			SELECT @myError = @@error, @myRowCount = @@rowcount
-			--
-			If @myError <> 0
-				RAISERROR ('Insert operation failed: "%s"', 11, 7, @Host)
-	
-		End -- add mode
-	
-		---------------------------------------------------
-		-- Action for update mode
-		---------------------------------------------------
-		--
-		If @Mode = 'update' 
-		Begin
+        Set @active = IsNull(@active, 1)
+    
+        ---------------------------------------------------
+        -- Is entry already in database?
+        ---------------------------------------------------
+    
+        If @mode = 'add' And Exists (SELECT * FROM T_Bionet_Hosts WHERE Host = @host)
+        Begin
+            -- Cannot create an entry that already exists
+            --
+            Set @msg = 'Cannot add: item "' + @host + '" is already in the database'
+            RAISERROR (@msg, 11, 1)
+            return 51004
+        End
+        
+        
+        If @mode = 'update' And Not Exists (SELECT * FROM T_Bionet_Hosts WHERE Host = @host)
+        Begin
+            -- Cannot update a non-existent entry
+            Set @msg = 'Cannot update: item "' + @host + '" is not in the database'
+            RAISERROR (@msg, 11, 16)
+            return 51005
+        End
+    
+        ---------------------------------------------------
+        -- Action for add mode
+        ---------------------------------------------------
+        --
+        If @Mode = 'add'
+        Begin
+        
+            INSERT INTO T_Bionet_Hosts( 
+                Host,
+                IP,
+                Alias,
+                Entered,
+                Instruments,
+                Active,
+                Tag,
+                Comment
+            )
+            VALUES(
+                @host, 
+                @ip, 
+                @alias, 
+                GetDate(), 
+                @instruments, 
+                @active, 
+                @tag,
+                @comment
+            )
+            --
+            SELECT @myError = @@error, @myRowCount = @@rowcount
+            --
+            If @myError <> 0
+                RAISERROR ('Insert operation failed: "%s"', 11, 7, @host)
+    
+        End -- add mode
+    
+        ---------------------------------------------------
+        -- Action for update mode
+        ---------------------------------------------------
+        --
+        If @Mode = 'update' 
+        Begin
 
-			UPDATE T_Bionet_Hosts
-			SET IP = @IP,
-			    Alias = @Alias,
-			    Instruments = @Instruments,
-			    Active = @Active,
-			    Tag = @Tag
-			WHERE Host = @Host
-			--
-			SELECT @myError = @@error, @myRowCount = @@rowcount
-			--
-			If @myError <> 0
-				RAISERROR ('Update operation failed: "%s"', 11, 4, @Host)
-	
-		End -- update mode
-	
-	End Try
-	Begin Catch 
-		EXEC FormatErrorMessage @message output, @myError output
-		
-		-- Rollback any open transactions
-		If (XACT_STATE()) <> 0
-			ROLLBACK TRANSACTION;
-			
-		Exec PostLogEntry 'Error', @message, 'AddUpdateBionetHost'
-	End Catch
+            UPDATE T_Bionet_Hosts
+            SET IP = @ip,
+                Alias = @alias,
+                Instruments = @instruments,
+                Active = @active,
+                Tag = @tag,
+                Comment = @comment
+            WHERE Host = @host
+            --
+            SELECT @myError = @@error, @myRowCount = @@rowcount
+            --
+            If @myError <> 0
+                RAISERROR ('Update operation failed: "%s"', 11, 4, @host)
+    
+        End -- update mode
+    
+    End Try
+    Begin Catch 
+        EXEC FormatErrorMessage @message output, @myError output
+        
+        -- Rollback any open transactions
+        If (XACT_STATE()) <> 0
+            ROLLBACK TRANSACTION;
+            
+        Exec PostLogEntry 'Error', @message, 'AddUpdateBionetHost'
+    End Catch
 
-	return @myError
+    return @myError
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[AddUpdateBionetHost] TO [DDL_Viewer] AS [dbo]
