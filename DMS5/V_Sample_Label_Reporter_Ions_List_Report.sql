11cda3c3076bc469708cd2dfd6a88fc65e72b402
alchemistmatt <matthew.monroe@pnnl.gov>
2018-11-19 19:36:03 -0800

    Add view for viewing sample label reporter ions

---
+++ DMS5/V_Sample_Label_Reporter_Ions_List_Report.sql
@@ -0,0 +1,17 @@
+/****** Object:  View [dbo].[V_Sample_Label_Reporter_Ions_List_Report] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_Sample_Label_Reporter_Ions_List_Report]
+As
+SELECT Label,
+       Channel,
+       Tag_Name,
+       MASIC_Name,
+       Reporter_Ion_Mz
+FROM T_Sample_Labelling_Reporter_Ions
+
+
+GO
