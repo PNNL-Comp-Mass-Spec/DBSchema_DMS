28577a637f43b8db7829d155086dc5c470e65536
alchemistmatt <matthew.monroe@pnnl.gov>
2018-11-19 19:29:07 -0800

    Update ParseDelimitedList UDFs to include special handling if @delimeter is CR, LF, or CRLF

--- DMS5/udfParseDelimitedList.sql
+++ DMS5/udfParseDelimitedList.sql
@@ -3,141 +3,196 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-	
+    
 CREATE FUNCTION [dbo].[udfParseDelimitedList]
-/****************************************************	
-**	Parses the text in @DelimitedList and returns a table
-**	containing the values
+/****************************************************
 **
-**	@DelimitedList should be of the form 'Value1,Value2'
-**	Will not return empty string values, e.g. if the list is 'Value1,,Value2' or ',Value1,Value2'
-**	 then the table will only contain entries 'Value1' and 'Value2'
+**  Parses the text in @delimitedList and
+**  returns a table containing the values
 **
+**  @delimitedList should be of the form 'Value1,Value2'
 **
-**	Auth:	mem
-**	Date:	06/06/2006
-**			11/10/2006 mem - Updated to prevent blank values from being returned in the table
-**			03/14/2007 mem - Changed @DelimitedList parameter from varchar(8000) to varchar(max)
-**			04/02/2012 mem - Now removing Tab characters
-**			03/27/2013 mem - Now replacing Tab characters, carriage returns and line feeds with @Delimiter
-**			01/20/2016 mem - Add numbers table example
-**			03/17/2017 mem - Add parameter @CallingProcedure
-**						   - Add optional call to PostUsageLogEntry
+**  Will not return empty string values, e.g. if the list is 'Value1,,Value2' or ',Value1,Value2'
+**     then the table will only contain entries 'Value1' and 'Value2'
+**
+**  If @delimiter is Char(13) or Char(10), will split @delimitedList on CR or LF
+**  In this case, blank lines will not be included in table @tmpValues
+**
+**  Auth:   mem
+**  Date:   06/06/2006
+**          11/10/2006 mem - Updated to prevent blank values from being returned in the table
+**          03/14/2007 mem - Changed @delimitedList parameter from varchar(8000) to varchar(max)
+**          04/02/2012 mem - Now removing Tab characters
+**          03/27/2013 mem - Now replacing Tab characters, carriage returns and line feeds with @delimiter
+**          01/20/2016 mem - Add numbers table example
+**          03/17/2017 mem - Add parameter @callingProcedure
+**                         - Add optional call to PostUsageLogEntry
+**          11/19/2018 mem - Add special handling if @delimeter is CR, LF, or CRLF
 **  
 ****************************************************/
 (
-	@DelimitedList varchar(max),
-	@Delimiter varchar(2) = ',',
-	@CallingProcedure varchar(128)= ''
+    @delimitedList varchar(max),
+    @delimiter varchar(2) = ',',
+    @callingProcedure varchar(128)= ''
 )
 RETURNS @tmpValues TABLE(Value varchar(2048))
 AS
 BEGIN
-	
-	Declare @continue tinyint
-	Declare @StartPosition int
-	Declare @DelimiterPos int
-	
-	Declare @Value varchar(2048)
+    
+    Declare @continue tinyint
+    Declare @startPosition int
 
-	-- Declare @procedureNameForUsageLog varchar(255) = 'udfParseDelimitedList_' + @CallingProcedure
+    Declare @delimiterPos int
+    Declare @crPos int
+    Declare @lfPos int
 
-	-- Uncomment the following to log usage of this procedure to a file in C:\temp\
-	-- Requirements for this to work:
-	--  - Enable xp_cmdshell
-	--  - Enable the Server Proxy Account
-	--  - Grant execute on master..xp_cmdshell to DMSWebUser and any other user that will call this udf
-	
-	--DECLARE @SQL varchar(500)
-	--SELECT @SQL = '"C:\Program Files (x86)\Windows Resource Kits\Tools\Now.exe" ' + @procedureNameForUsageLog + ' >> C:\temp\UsageLogStats.txt'
-	--EXEC master..xp_cmdshell @SQL
+    Declare @delimiterIsCRorLF tinyint = 0
 
-	
-	Set @DelimitedList = IsNull(@DelimitedList, '')
-	
-	If Len(@DelimitedList) > 0
-	Begin -- <a>
-	
-		-- Replace any CR or LF characters with @Delimiter
-		If @DelimitedList Like '%' + Char(13) + '%'
-			Set @DelimitedList = LTrim(RTrim(Replace(@DelimitedList, Char(13),  @Delimiter)))
+    Declare @value varchar(2048)
 
-		If @DelimitedList Like '%' + Char(10) + '%'
-			Set @DelimitedList = LTrim(RTrim(Replace(@DelimitedList, Char(10),  @Delimiter)))
+    -- Declare @procedureNameForUsageLog varchar(255) = 'udfParseDelimitedList_' + @callingProcedure
 
-		If @Delimiter <> Char(9)
-		Begin
-			-- Replace any tab characters with @Delimiter
-			If @DelimitedList Like '%' + Char(9)  + '%'
-				Set @DelimitedList = LTrim(RTrim(Replace(@DelimitedList, Char(9),  @Delimiter)))
-		End
-					
-		Set @StartPosition = 1
-		Set @continue = 1
-		While @continue = 1
-		Begin -- <b>
-			Set @DelimiterPos = CharIndex(@Delimiter, @DelimitedList, @StartPosition)
-			If @DelimiterPos = 0
-			Begin
-				Set @DelimiterPos = Len(@DelimitedList) + 1
-				Set @continue = 0
-			End
+    -- Uncomment the following to log usage of this procedure to a file in C:\temp\
+    -- Requirements for this to work:
+    --  - Enable xp_cmdshell
+    --  - Enable the Server Proxy Account
+    --  - Grant execute on master..xp_cmdshell to DMSWebUser and any other user that will call this udf
+    
+    --DECLARE @SQL varchar(500)
+    --SELECT @SQL = '"C:\Program Files (x86)\Windows Resource Kits\Tools\Now.exe" ' + @procedureNameForUsageLog + ' >> C:\temp\UsageLogStats.txt'
+    --EXEC master..xp_cmdshell @SQL
 
-			If @DelimiterPos > @StartPosition
-			Begin -- <c>
-				Set @Value = LTrim(RTrim(SubString(@DelimitedList, @StartPosition, @DelimiterPos - @StartPosition)))
-			
-				If Len(@Value) > 0 
-				Begin
-					INSERT INTO @tmpValues (Value)
-					VALUES (@Value)
-				End
-				
-			End -- </c>
+    
+    Set @delimitedList = IsNull(@delimitedList, '')
+    
+    If @delimiter Like '%' + Char(13) + '%' Or
+       @delimiter Like '%' + Char(10) + '%'
+    Begin
+        Set @delimiterIsCRorLF = 1
+        
+        -- The logic below will match either CR or LF, so it doesn't matter what @delimiter is
+        Set @delimiter = char(10)
+    End
 
-			Set @StartPosition = @DelimiterPos + 1
-		End -- </b>
-		
-		/*
-		 * The following is an alternative method, using a Numbers table
-		 * This method is faster than the above Loop-based method, but it
-		 * does require that the Numbers table be pre-generated
-		 * prior to using the query
-		 *
-		 *
-		 * Numbers table creation:
-			-- DECLARE @UpperLimit INT = 1000000;
-			-- 
-			-- WITH n(rn) AS (
-			--   SELECT ROW_NUMBER() OVER (ORDER BY s1.[object_id])
-			--   FROM sys.all_columns AS s1
-			--   CROSS JOIN sys.all_columns AS s2
-			-- )
-			-- SELECT [Number] = rn
-			-- INTO dbo.T_Numbers
-			-- FROM n
-			-- WHERE rn <= @UpperLimit;
-			-- 
-			-- CREATE UNIQUE CLUSTERED INDEX IX_Numbers_Number ON dbo.T_Numbers([Number])
-			-- -- WITH (DATA_COMPRESSION = PAGE);
-		 *
-		 *
-			SELECT rn,
-		       vn = ROW_NUMBER() OVER ( PARTITION BY [Value] ORDER BY rn ),
-		       [Value]
-			FROM ( SELECT rn = ROW_NUMBER() OVER ( ORDER BY CHARINDEX(@Delim, @List + @Delim) ),
-		              [Value] = LTRIM(RTRIM(SUBSTRING(@List, [Number], 
-		                                      CHARINDEX(@Delim, @List + @Delim, [Number]) - [Number])))
-		       FROM dbo.T_Numbers
-		       WHERE [Number] <= LEN(@List) AND
-		             SUBSTRING(@Delim + @List, [Number], LEN(@Delim)) = @Delim ) AS x
-		 *
-		 *
-		 */
+    If Len(@delimitedList) > 0
+    Begin -- <a>
 
-	End -- </a>
-	
-	RETURN
+        If @delimiterIsCRorLF = 0
+        Begin
+            -- Replace any CR or LF characters with @delimiter
+            If @delimitedList Like '%' + Char(13) + '%'
+            Begin
+                Set @delimitedList = LTrim(RTrim(Replace(@delimitedList, Char(13), @delimiter)))
+            End
+
+            If @delimitedList Like '%' + Char(10) + '%'
+            Begin
+                Set @delimitedList = LTrim(RTrim(Replace(@delimitedList, Char(10), @delimiter)))
+            End
+
+            If @delimiter <> Char(9)
+            Begin
+                -- Replace any tab characters with @delimiter
+                If @delimitedList Like '%' + Char(9)  + '%'
+                    Set @delimitedList = LTrim(RTrim(Replace(@delimitedList, Char(9), @delimiter)))
+            End
+        End
+
+        Set @startPosition = 1
+        Set @continue = 1
+        While @continue = 1
+        Begin -- <b>
+            If @delimiterIsCRorLF = 1
+            Begin
+                Set @crPos = CharIndex(Char(13), @delimitedList, @startPosition)
+                Set @lfPos = CharIndex(Char(10), @delimitedList, @startPosition)
+
+                If @crPos > 0 And @crPos <= @lfPos
+                    Set @delimiterPos = @crPos
+                Else If @lfPos > 0 And @lfPos <= @crPos
+                    Set @delimiterPos = @lfPos
+                Else If @crPos > 0
+                    Set @delimiterPos = @crPos
+                Else If @lfPos > 0
+                    Set @delimiterPos = @lfPos
+                Else 
+                    Set @delimiterPos = 0
+            End
+            Else
+            Begin
+                Set @delimiterPos = CharIndex(@delimiter, @delimitedList, @startPosition)
+            End
+
+            If @delimiterPos = 0
+            Begin
+                -- Delimiter not found
+                Set @delimiterPos = Len(@delimitedList) + 1
+                Set @continue = 0
+            End
+
+            If @delimiterPos > @startPosition
+            Begin -- <c>
+                Set @value = LTrim(RTrim(SubString(@delimitedList, @startPosition, @delimiterPos - @startPosition)))
+
+                If @delimiterIsCRorLF = 1
+                Begin
+                    If @value = char(10)
+                        Set @value = ''
+                    Else If @value = char(13)
+                        Set @value = ''
+                End
+
+                If Len(@value) > 0
+                Begin
+                    INSERT INTO @tmpValues (Value)
+                    VALUES (@value)
+                End
+                
+            End -- </c>
+
+            Set @startPosition = @delimiterPos + 1
+        End -- </b>
+        
+        /*
+         * The following is an alternative method, using a Numbers table
+         * This method is faster than the above Loop-based method, but it
+         * does require that the Numbers table be pre-generated
+         * prior to using the query
+         *
+         *
+         * Numbers table creation:
+            -- DECLARE @UpperLimit INT = 1000000;
+            -- 
+            -- WITH n(rn) AS (
+            --   SELECT ROW_NUMBER() OVER (ORDER BY s1.[object_id])
+            --   FROM sys.all_columns AS s1
+            --   CROSS JOIN sys.all_columns AS s2
+            -- )
+            -- SELECT [Number] = rn
+            -- INTO dbo.T_Numbers
+            -- FROM n
+            -- WHERE rn <= @UpperLimit;
+            -- 
+            -- CREATE UNIQUE CLUSTERED INDEX IX_Numbers_Number ON dbo.T_Numbers([Number])
+            -- -- WITH (DATA_COMPRESSION = PAGE);
+         *
+         *
+            SELECT rn,
+               vn = ROW_NUMBER() OVER ( PARTITION BY [Value] ORDER BY rn ),
+               [Value]
+            FROM ( SELECT rn = ROW_NUMBER() OVER ( ORDER BY CHARINDEX(@Delim, @List + @Delim) ),
+                      [Value] = LTRIM(RTRIM(SUBSTRING(@List, [Number], 
+                                              CHARINDEX(@Delim, @List + @Delim, [Number]) - [Number])))
+               FROM dbo.T_Numbers
+               WHERE [Number] <= LEN(@List) AND
+                     SUBSTRING(@Delim + @List, [Number], LEN(@Delim)) = @Delim ) AS x
+         *
+         *
+         */
+
+    End -- </a>
+    
+    RETURN
 END
 
 
--- DMS5/udfParseDelimitedListOrdered.sql
+++ DMS5/udfParseDelimitedListOrdered.sql
@@ -4,87 +4,164 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE FUNCTION udfParseDelimitedListOrdered
-/****************************************************	
-**	Parses the text in @DelimitedList and returns a table
-**	 containing the values.  The table includes column EntryID
-**   to allow the calling procedure to sort the data based on the 
-**   data order in @DelimitedList.  The first row will have EntryID = 1
+CREATE FUNCTION [dbo].[udfParseDelimitedListOrdered]
+/****************************************************    
 **
-**  Note that if two commas in a row are encountered, then the resultant table
-**   will contain an empty cell for that row
+**  Parses the text in @delimitedList and returns a table
+**  containing the values. The table includes column EntryID
+**  to allow the calling procedure to sort the data based on the 
+**  data order in @delimitedList.  The first row will have EntryID = 1
 **
-**	@DelimitedList should be of the form 'Value1,Value2'
+**  @delimitedList should be of the form 'Value1,Value2'
 **
-**	Auth:	mem
-**	Date:	10/16/2007
-**			03/27/2013 mem - Now replacing Tab characters, carriage returns and line feeds with @Delimiter
+**  Note that if two commas in a row are encountered, the resultant table
+**  will contain an empty cell for that row
+**
+**  If @delimiter is Char(13) or Char(10), will split @delimitedList on CR or LF
+**  In this case, blank lines will not be included in table @tmpValues
+**
+**  Auth:   mem
+**  Date:   10/16/2007
+**          03/27/2013 mem - Now replacing Tab characters, carriage returns and line feeds with @delimiter
+**          11/19/2018 mem - Add special handling if @delimeter is CR, LF, or CRLF
+**                         - Add parameter @maxRows
 **  
 ****************************************************/
 (
-	@DelimitedList varchar(max),
-	@Delimiter varchar(2) = ','
+    @delimitedList varchar(max),
+    @delimiter varchar(2) = ',',
+    @maxRows int = 0               -- Optionally set this to a positive number to limit the number of rows returned in @tmpValues.
+                                   -- This is useful if you are parsing a comma-separated list of items, 
+                                   -- and the final item is a comment field, which itself might contain commas.
 )
 RETURNS @tmpValues TABLE(EntryID int NOT NULL, Value varchar(2048) NULL)
 AS
 BEGIN
-	
-	Declare @continue tinyint
-	Declare @StartPosition int
-	Declare @DelimiterPos int
-	
-	Declare @Value varchar(2048)
-	Declare @EntryID int
-	Set @EntryID = 1
-	
-	Set @DelimitedList = IsNull(@DelimitedList, '')
-	
-	If Len(@DelimitedList) > 0
-	Begin -- <a>
-		
-		-- Replace any CR or LF characters with @Delimiter
-		If @DelimitedList Like '%' + Char(13) + '%'
-			Set @DelimitedList = LTrim(RTrim(Replace(@DelimitedList, Char(13),  @Delimiter)))
+    
+    Declare @continue tinyint
+    Declare @startPosition int
 
-		If @DelimitedList Like '%' + Char(10) + '%'
-			Set @DelimitedList = LTrim(RTrim(Replace(@DelimitedList, Char(10),  @Delimiter)))
+    Declare @delimiterPos int
+    Declare @crPos int
+    Declare @lfPos int
+    
+    Declare @delimiterIsCRorLF tinyint = 0
 
-		If @Delimiter <> Char(9)
-		Begin
-			-- Replace any tab characters with @Delimiter
-			If @DelimitedList Like '%' + Char(9)  + '%'
-				Set @DelimitedList = LTrim(RTrim(Replace(@DelimitedList, Char(9),  @Delimiter)))
-		End
-		
-		Set @StartPosition = 1
-		Set @continue = 1
-		While @continue = 1
-		Begin -- <b>
-			Set @DelimiterPos = CharIndex(@Delimiter, @DelimitedList, @StartPosition)
-			If @DelimiterPos = 0
-			Begin
-				Set @DelimiterPos = Len(@DelimitedList) + 1
-				Set @continue = 0
-			End
+    Declare @value varchar(2048)
+    Declare @entryID int = 1
+    
+    Set @delimitedList = IsNull(@delimitedList, '')
+    
+    If @delimiter Like '%' + Char(13) + '%' Or
+       @delimiter Like '%' + Char(10) + '%'
+    Begin
+        Set @delimiterIsCRorLF = 1
+        
+        -- The logic below will match either CR or LF, so it doesn't matter what @delimiter is
+        Set @delimiter = char(10)
+    End
 
-			If @DelimiterPos >= @StartPosition
-			Begin -- <c>
-				If @DelimiterPos > @StartPosition
-					Set @Value = LTrim(RTrim(SubString(@DelimitedList, @StartPosition, @DelimiterPos - @StartPosition)))
-				Else
-					Set @Value = ''
-				
-				INSERT INTO @tmpValues (EntryID, Value)
-				VALUES (@EntryID, @Value)
-				
-				Set @EntryID = @EntryID + 1
-			end -- </c>
+    If Len(@delimitedList) > 0
+    Begin -- <a>
+        
+        If @delimiterIsCRorLF = 0
+        Begin
+            -- Replace any CR or LF characters with @delimiter
+            If @delimitedList Like '%' + Char(13) + '%'
+            Begin
+                Set @delimitedList = LTrim(RTrim(Replace(@delimitedList, Char(13),  @delimiter)))
+            End
 
-			Set @StartPosition = @DelimiterPos + 1
-		End -- </b>
-	End -- </a>
-	
-	RETURN
+            If @delimitedList Like '%' + Char(10) + '%'
+            Begin
+                Set @delimitedList = LTrim(RTrim(Replace(@delimitedList, Char(10),  @delimiter)))
+            End
+
+            If @delimiter <> Char(9)
+            Begin
+                -- Replace any tab characters with @delimiter
+                If @delimitedList Like '%' + Char(9)  + '%'
+                    Set @delimitedList = LTrim(RTrim(Replace(@delimitedList, Char(9),  @delimiter)))
+            End
+        End
+
+        Set @startPosition = 1
+        Set @continue = 1
+        While @continue = 1
+        Begin -- <b>
+            If @delimiterIsCRorLF = 1
+            Begin
+                Set @crPos = CharIndex(Char(13), @delimitedList, @startPosition)
+                Set @lfPos = CharIndex(Char(10), @delimitedList, @startPosition)
+
+                If @crPos > 0 And @crPos <= @lfPos
+                    Set @delimiterPos = @crPos
+                Else If @lfPos > 0 And @lfPos <= @crPos
+                    Set @delimiterPos = @lfPos
+                Else If @crPos > 0
+                    Set @delimiterPos = @crPos
+                Else If @lfPos > 0
+                    Set @delimiterPos = @lfPos
+                Else 
+                    Set @delimiterPos = 0
+            End
+            Else
+            Begin
+                Set @delimiterPos = CharIndex(@delimiter, @delimitedList, @startPosition)
+            End
+
+            If @delimiterPos = 0
+            Begin
+                -- Delimiter not found
+                Set @delimiterPos = Len(@delimitedList) + 1
+                Set @continue = 0
+            End
+
+            If @delimiterPos >= @startPosition
+            Begin -- <c>
+                If @delimiterPos > @startPosition
+                Begin
+                    Set @value = LTrim(RTrim(SubString(@delimitedList, @startPosition, @delimiterPos - @startPosition)))
+
+                    If @delimiterIsCRorLF = 1
+                    Begin
+                        If @value = char(10)
+                            Set @value = ''
+                        Else If @value = char(13)
+                            Set @value = ''
+                    End                    
+                End
+                Else
+                Begin
+                    Set @value = ''
+                End
+
+                If @delimiterIsCRorLF = 0 Or Len(@value) > 0
+                Begin
+                    If @maxRows > 0 And @entryID >= @maxRows
+                    Begin
+                        Set @continue = 0
+
+                        If @delimiterPos > 0 And @delimiterPos < Len(@delimitedList)
+                        Begin
+                            -- Append the remaining text
+                            Set @value = @value + LTrim(RTrim(SubString(@delimitedList, @delimiterPos, Len(@delimitedList))))
+                        End
+                    End
+
+                    INSERT INTO @tmpValues (EntryID, Value)
+                    VALUES (@entryID, @value)
+                
+                    Set @entryID = @entryID + 1
+                End
+            End -- </c>
+
+            Set @startPosition = @delimiterPos + 1
+
+        End -- </b>
+    End -- </a>
+    
+    RETURN
 END
 
 GO
