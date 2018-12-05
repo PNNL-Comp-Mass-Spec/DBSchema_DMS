ad1392c29e6ba6c5a1b970d762963f9b10252ab3
alchemistmatt <matthew.monroe@pnnl.gov>
2018-12-04 18:03:04 -0800

    Update plex channel type names

--- DMS5/T_Experiment_Plex_Channel_Type_Name_Data.sql
+++ DMS5/T_Experiment_Plex_Channel_Type_Name_Data.sql
@@ -1,6 +1,7 @@
 /****** Object:  Table [T_Experiment_Plex_Channel_Type_Name] ******/
-/****** RowCount: 3 ******/
+/****** RowCount: 4 ******/
 /****** Columns: Channel_Type_ID, Channel_Type_Name ******/
 INSERT INTO [T_Experiment_Plex_Channel_Type_Name] VALUES (0,'Empty')
-INSERT INTO [T_Experiment_Plex_Channel_Type_Name] VALUES (1,'Normal')
+INSERT INTO [T_Experiment_Plex_Channel_Type_Name] VALUES (1,'Sample')
 INSERT INTO [T_Experiment_Plex_Channel_Type_Name] VALUES (2,'Reference')
+INSERT INTO [T_Experiment_Plex_Channel_Type_Name] VALUES (3,'Boost')
