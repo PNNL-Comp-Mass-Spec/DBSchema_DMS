fd93cb4cd08df507fd45eabc75fb7916f61cb2f5
alchemistmatt <matthew.monroe@pnnl.gov>
2018-12-07 14:58:56 -0800

    Reformat views

--- DMS5/V_Datasets_RSS.sql
+++ DMS5/V_Datasets_RSS.sql
@@ -3,18 +3,32 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Datasets_RSS
+
+CREATE VIEW [dbo].[V_Datasets_RSS]
 AS
-SELECT     Dataset AS url_title, CONVERT(VARCHAR(12), ID) + ' - ' + Dataset AS post_title, CONVERT(VARCHAR(12), ID) AS guid, 
-                      ' Dataset:' + Dataset + '| Experiment:' + Experiment + '| Researcher:' + Researcher + '| Experiment Group:' + Exp_Group + ', Experiment Group ID:' + CONVERT(VARCHAR(12),
-                       Group_ID) AS post_body, Researcher AS U_PRN, Created AS post_date
-FROM         (SELECT     TD.Dataset_ID AS ID, TD.Dataset_Num AS Dataset, TE.Experiment_Num AS Experiment, TE.EX_researcher_PRN AS Researcher, 
-                                              dbo.T_Experiment_Groups.EG_Description AS Exp_Group, dbo.T_Experiment_Groups.Group_ID, TD.DS_created AS Created
-                       FROM          dbo.T_Dataset AS TD INNER JOIN
-                                              dbo.T_Experiments AS TE ON TD.Exp_ID = TE.Exp_ID INNER JOIN
-                                              dbo.T_Experiment_Group_Members ON TE.Exp_ID = dbo.T_Experiment_Group_Members.Exp_ID INNER JOIN
-                                              dbo.T_Experiment_Groups ON dbo.T_Experiment_Group_Members.Group_ID = dbo.T_Experiment_Groups.Group_ID
-                       WHERE      (TD.DS_created > DATEADD(DAY, - 30, GETDATE()))) AS TM
+SELECT Dataset AS url_title,
+       CONVERT(varchar(12), ID) + ' - ' + Dataset AS post_title,
+       CONVERT(varchar(12), ID) AS guid,
+       ' Dataset:' + Dataset + '| Experiment:' + Experiment + '| Researcher:' + Researcher + 
+       '| Experiment Group:' + Exp_Group + ', Experiment Group ID:' + CONVERT(varchar(12), Group_ID) 
+         AS post_body,
+       Researcher AS U_PRN,
+       Created AS post_date
+FROM ( SELECT DS.Dataset_ID AS ID,
+              DS.Dataset_Num AS Dataset,
+              E.Experiment_Num AS Experiment,
+              E.EX_researcher_PRN AS Researcher,
+              dbo.T_Experiment_Groups.EG_Description AS Exp_Group,
+              dbo.T_Experiment_Groups.Group_ID,
+              DS.DS_created AS Created
+       FROM dbo.T_Dataset AS DS
+            INNER JOIN dbo.T_Experiments AS E
+              ON DS.Exp_ID = E.Exp_ID
+            INNER JOIN dbo.T_Experiment_Group_Members
+              ON E.Exp_ID = dbo.T_Experiment_Group_Members.Exp_ID
+            INNER JOIN dbo.T_Experiment_Groups
+              ON dbo.T_Experiment_Group_Members.Group_ID = dbo.T_Experiment_Groups.Group_ID
+       WHERE (DS.DS_created > DATEADD(day, -30, GETDATE())) ) AS FilterQ
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Datasets_RSS] TO [DDL_Viewer] AS [dbo]
--- DMS5/V_Experiment_Group_Members_List_Report.sql
+++ DMS5/V_Experiment_Group_Members_List_Report.sql
@@ -3,17 +3,29 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Experiment_Group_Members_List_Report
+
+CREATE VIEW [dbo].[V_Experiment_Group_Members_List_Report]
 AS
-SELECT     dbo.T_Experiments.Experiment_Num AS Experiment, 
-                      CASE WHEN dbo.T_Experiment_Groups.Parent_Exp_ID = T_Experiments.Exp_ID THEN 'Parent' ELSE 'Child' END AS Member, 
-                      dbo.T_Experiments.EX_researcher_PRN AS Researcher, dbo.T_Organisms.OG_name AS Organism, dbo.T_Experiments.EX_reason AS Reason, 
-                      dbo.T_Experiments.EX_comment AS Comment, dbo.T_Experiment_Groups.Group_ID AS [#Group]
-FROM         dbo.T_Experiments INNER JOIN
-                      dbo.T_Campaign ON dbo.T_Experiments.EX_campaign_ID = dbo.T_Campaign.Campaign_ID INNER JOIN
-                      dbo.T_Experiment_Group_Members ON dbo.T_Experiments.Exp_ID = dbo.T_Experiment_Group_Members.Exp_ID INNER JOIN
-                      dbo.T_Organisms ON dbo.T_Experiments.Ex_organism_ID = dbo.T_Organisms.Organism_ID LEFT OUTER JOIN
-                      dbo.T_Experiment_Groups ON dbo.T_Experiment_Group_Members.Group_ID = dbo.T_Experiment_Groups.Group_ID
+SELECT E.Experiment_Num AS Experiment,
+       CASE
+           WHEN EG.Parent_Exp_ID = E.Exp_ID THEN 'Parent'
+           ELSE 'Child'
+       END AS Member,
+       E.EX_researcher_PRN AS Researcher,
+       Org.OG_name AS Organism,
+       E.EX_reason AS Reason,
+       E.EX_comment AS [Comment],
+       EG.Group_ID AS [#Group]
+FROM dbo.T_Experiments E
+     INNER JOIN dbo.T_Campaign C
+       ON E.EX_campaign_ID = C.Campaign_ID
+     INNER JOIN dbo.T_Experiment_Group_Members EGM
+       ON E.Exp_ID = EGM.Exp_ID
+     INNER JOIN dbo.T_Organisms Org
+       ON E.Ex_organism_ID = Org.Organism_ID
+     LEFT OUTER JOIN dbo.T_Experiment_Groups EG
+       ON EGM.Group_ID = EG.Group_ID
+
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Experiment_Group_Members_List_Report] TO [DDL_Viewer] AS [dbo]
