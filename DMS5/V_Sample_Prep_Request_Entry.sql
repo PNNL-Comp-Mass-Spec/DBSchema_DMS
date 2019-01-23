767bca1c184fbb2d4efa00681f816c983d7c2372
alchemistmatt <matthew.monroe@pnnl.gov>
2019-01-23 15:54:52 -0800

    Add __NoCopy__ flags to six prep request fields

--- DMS5/V_Sample_Prep_Request_Entry.sql
+++ DMS5/V_Sample_Prep_Request_Entry.sql
@@ -4,17 +4,16 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_Sample_Prep_Request_Entry]
 AS
 SELECT SPR.Request_Name AS RequestName,
         SPR.Requester_PRN AS RequesterPRN,
-        SPR.Reason,
+        SPR.Reason + '__NoCopy__' As Reason,
         SPR.Organism,
         BTO.Tissue,
         SPR.Biohazard_Level AS BiohazardLevel,
         SPR.Campaign,
-        SPR.Number_of_Samples AS NumberofSamples,
+        Cast(SPR.Number_of_Samples As Varchar(12)) + '__NoCopy__' AS NumberofSamples,
         SPR.Sample_Name_List AS SampleNameList,
         SPR.Sample_Type AS SampleType,
         SPR.Prep_Method AS PrepMethod,
@@ -22,12 +21,12 @@ SELECT SPR.Request_Name AS RequestName,
         SPR.Requested_Personnel AS RequestedPersonnel,
         SPR.Assigned_Personnel AS AssignedPersonnel,
         SPR.Estimated_Completion AS EstimatedCompletion,
-        SPR.Estimated_MS_runs AS EstimatedMSRuns,
+         Cast(SPR.Estimated_MS_runs As Varchar(12)) + '__NoCopy__' AS EstimatedMSRuns,
         SPR.Work_Package_Number AS WorkPackageNumber,
         SPR.Instrument_Group AS InstrumentGroup,
         SPR.Dataset_Type AS DatasetType,
         SPR.Instrument_Analysis_Specifications AS InstrumentAnalysisSpecifications,
-        SPR.[Comment],
+        SPR.[Comment] + '__NoCopy__' As [Comment],
         SPR.Priority,
         T_Sample_Prep_Request_State_Name.State_Name AS State,
         SPR.ID,
@@ -38,15 +37,13 @@ SELECT SPR.Request_Name AS RequestName,
         SPR.Separation_Type AS SeparationGroup,
         SPR.BlockAndRandomizeSamples,
         SPR.BlockAndRandomizeRuns,
-        SPR.Reason_For_High_Priority AS ReasonForHighPriority,
-        SPR.Material_Container_List AS MaterialContainerList
+        SPR.Reason_For_High_Priority + '__NoCopy__' AS ReasonForHighPriority,
+        SPR.Material_Container_List + '__NoCopy__' AS MaterialContainerList
 FROM T_Sample_Prep_Request AS SPR
      INNER JOIN T_Sample_Prep_Request_State_Name
        ON SPR.State = T_Sample_Prep_Request_State_Name.State_ID
      LEFT OUTER JOIN S_V_BTO_ID_to_Name BTO
        ON SPR.Tissue_ID = BTO.Identifier
-
-
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Sample_Prep_Request_Entry] TO [DDL_Viewer] AS [dbo]
