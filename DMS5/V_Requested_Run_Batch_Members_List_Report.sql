f8e31ed88d9efd06ad987c3e587f243433ab49a0
alchemistmatt <matthew.monroe@pnnl.gov>
2019-03-05 10:57:02 -0800

    Reformat views

--- DMS5/V_Requested_Run_Batch_Members_List_Report.sql
+++ DMS5/V_Requested_Run_Batch_Members_List_Report.sql
@@ -3,19 +3,32 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-create VIEW V_Requested_Run_Batch_Members_List_Report
-AS
-SELECT     dbo.T_Requested_Run.ID AS Request, dbo.T_Requested_Run.RDS_Name AS Name, dbo.T_Requested_Run_Batches.Batch, 
-                      dbo.T_Requested_Run.RDS_Blocking_Factor AS [Blocking Factor], dbo.T_Requested_Run.RDS_Block AS Block, 
-                      dbo.T_Requested_Run.RDS_Run_Order AS [Run Order], dbo.T_Experiments.Experiment_Num AS Experiment, 
-                      dbo.T_Requested_Run.RDS_instrument_name AS Instrument, dbo.T_Users.U_Name AS Requestor, dbo.T_Requested_Run.RDS_created AS Created, 
-                      dbo.T_Requested_Run.RDS_priority AS Pri, dbo.T_Requested_Run.RDS_comment AS Comment_____________, 
-                      dbo.T_Requested_Run.RDS_Well_Plate_Num AS Wellplate, dbo.T_Requested_Run.RDS_Well_Num AS Well, 
-                      dbo.T_Requested_Run.RDS_BatchID AS [#BatchID]
-FROM         dbo.T_Requested_Run INNER JOIN
-                      dbo.T_Experiments ON dbo.T_Requested_Run.Exp_ID = dbo.T_Experiments.Exp_ID INNER JOIN
-                      dbo.T_Requested_Run_Batches ON dbo.T_Requested_Run.RDS_BatchID = dbo.T_Requested_Run_Batches.ID INNER JOIN
-                      dbo.T_Users ON dbo.T_Experiments.EX_researcher_PRN = dbo.T_Users.U_PRN
+
+CREATE VIEW [dbo].[V_Requested_Run_Batch_Members_List_Report]
+As
+-- March 2019: This view appears to be unused
+SELECT RR.ID AS Request,
+       RR.RDS_Name AS Name,
+       RRB.Batch,
+       RR.RDS_Blocking_Factor AS [Blocking Factor],
+       RR.RDS_Block AS [Block],
+       RR.RDS_Run_Order AS [Run Order],
+       E.Experiment_Num AS Experiment,
+       RR.RDS_instrument_name AS Instrument,
+       U.U_Name AS Requestor,
+       RR.RDS_created AS Created,
+       RR.RDS_priority AS Pri,
+       RR.RDS_comment AS Comment_____________,
+       RR.RDS_Well_Plate_Num AS Wellplate,
+       RR.RDS_Well_Num AS Well,
+       RR.RDS_BatchID AS [#BatchID]
+FROM T_Requested_Run RR
+     INNER JOIN T_Experiments E
+       ON RR.Exp_ID = E.Exp_ID
+     INNER JOIN T_Requested_Run_Batches RRB
+       ON RR.RDS_BatchID = RRB.ID
+     INNER JOIN T_Users U
+       ON E.EX_researcher_PRN = U.U_PRN
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Requested_Run_Batch_Members_List_Report] TO [DDL_Viewer] AS [dbo]
