9843dc513b197f2f20874ef2913dc2ccbf1f66fd
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-03 19:05:05 -0700

    Tabs to spaces

--- DMS5/AddUpdateRunInterval.sql
+++ DMS5/AddUpdateRunInterval.sql
@@ -3,181 +3,179 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE AddUpdateRunInterval
+
+CREATE PROCEDURE [dbo].[AddUpdateRunInterval]
 /****************************************************
 **
-**  Desc: 
-**		 Adds new or edits existing item in T_Run_Interval 
+**  Desc:   Edits existing item in T_Run_Interval
+            This procedure cannot be used to add rows to T_Run_Interval
 **
 **  Return values: 0: success, otherwise, error code
 **
-**  Parameters:
-**
-**    Auth: grk
-**    Date: 02/15/2012 
+**  Auth:   grk
+**  Date:   02/15/2012 
 **          02/15/2012 grk - modified percentage parameters
 **          03/03/2012 grk - changed to embedded usage tags
 **          03/07/2012 mem - Now populating Last_Affected and Entered_By
 **          03/21/2012 grk - modified to handle modified ParseUsageText
-**			02/23/2016 mem - Add set XACT_ABORT on
-**			04/12/2017 mem - Log exceptions to T_Log_Entries
-**			04/28/2017 mem - Disable logging to T_Log_Entries when ParseUsageText reports an error
-**			06/16/2017 mem - Restrict access using VerifySPAuthorized
-**			08/01/2017 mem - Use THROW if not authorized
-**			08/02/2017 mem - @ID is no longer an output variable
-**						   - Add parameters @showDebug and @invalidUsage
-**						   - Pass @ID and @invalidUsage to ParseUsageText
+**          02/23/2016 mem - Add set XACT_ABORT on
+**          04/12/2017 mem - Log exceptions to T_Log_Entries
+**          04/28/2017 mem - Disable logging to T_Log_Entries when ParseUsageText reports an error
+**          06/16/2017 mem - Restrict access using VerifySPAuthorized
+**          08/01/2017 mem - Use THROW if not authorized
+**          08/02/2017 mem - @ID is no longer an output variable
+**                         - Add parameters @showDebug and @invalidUsage
+**                         - Pass @ID and @invalidUsage to ParseUsageText
+**          05/03/2019 mem - Update comments
 **   
 *****************************************************/
 (
-	@ID int ,
-	@Comment varchar(MAX),				-- Usage comment, e.g. 'User[100%], Proposal[49521], PropUser[50151]'
-	@mode varchar(12) = 'add',			-- 'add' or 'update'
-	@message varchar(512) output,
-	@callingUser varchar(128) = '',
-	@showDebug tinyint = 0,
-	@invalidUsage tinyint = 0 output	-- Set to 1 if the usage text in @comment cannot be parsed (or if the total percentage is not 100); UpdateRunOpLog uses this to skip invalid entries
+    @ID int ,
+    @Comment varchar(MAX),              -- Usage comment, e.g. 'User[100%], Proposal[49521], PropUser[50151]'
+    @mode varchar(12) = 'update',       -- 'update'; note that 'add' is not supported
+    @message varchar(512) output,
+    @callingUser varchar(128) = '',
+    @showDebug tinyint = 0,
+    @invalidUsage tinyint = 0 output    -- Set to 1 if the usage text in @comment cannot be parsed (or if the total percentage is not 100); UpdateRunOpLog uses this to skip invalid entries
 )
 As
-	Set XACT_ABORT, nocount on
+    Set XACT_ABORT, nocount on
 
-	declare @myError int = 0
-	declare @myRowCount int = 0
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
 
-	---------------------------------------------------
-	-- Validate the inputs
-	---------------------------------------------------
+    ---------------------------------------------------
+    -- Validate the inputs
+    ---------------------------------------------------
 
-	Set @ID = IsNull(@ID, -1)
-	Set @message = ''
-	Set @showDebug = IsNull(@showDebug, 0)
-	Set @invalidUsage = 0
-	
-	Declare @logErrors tinyint = 0
-	
-	Set @CallingUser = IsNull(@CallingUser, '')
-	if @CallingUser = ''
-		Set @CallingUser = suser_sname()
+    Set @ID = IsNull(@ID, -1)
+    Set @message = ''
+    Set @showDebug = IsNull(@showDebug, 0)
+    Set @invalidUsage = 0
+    
+    Declare @logErrors tinyint = 0
+    
+    Set @CallingUser = IsNull(@CallingUser, '')
+    if @CallingUser = ''
+        Set @CallingUser = suser_sname()
 
-	---------------------------------------------------
-	-- Verify that the user can execute this procedure from the given client host
-	---------------------------------------------------
-		
-	Declare @authorized tinyint = 0	
-	Exec @authorized = VerifySPAuthorized 'AddUpdateRunInterval', @raiseError = 1
-	If @authorized = 0
-	Begin
-		THROW 51000, 'Access denied', 1;
-	End
-	
-	BEGIN TRY 
+    ---------------------------------------------------
+    -- Verify that the user can execute this procedure from the given client host
+    ---------------------------------------------------
+        
+    Declare @authorized tinyint = 0    
+    Exec @authorized = VerifySPAuthorized 'AddUpdateRunInterval', @raiseError = 1
+    If @authorized = 0
+    Begin;
+        THROW 51000, 'Access denied', 1;
+    End;
+    
+    BEGIN TRY 
 
-	IF @ID < 0
-	Begin
-		Set @message = 'Invalid ID: ' + Cast(@id as varchar(9))
-		RAISERROR (@message, 11, 10)
-		Goto Done
-	End
-	
-	---------------------------------------------------
-	-- Validate usage and comment
-	-- ParseUsageText looks for special usage tags in the comment and extracts that information, returning it as XML
-	--
-	-- If @comment is 'User[100%], Proposal[49361], PropUser[50082] Extra information about interval'
-	-- after calling ParseUsageText, @cleanedComment will be 'Extra information about interval'
-	-- and @usageXML will be <u User="100" Proposal="49361" PropUser="50082" />
-	--
-	-- If @comment only has 'User[100%], Proposal[49361], PropUser[50082]', then @cleanedComment will be empty after the call to ParseUsageText
-	--
-	-- Since @validateTotal is set to 1, if the percentages do not add up to 100%, ParseUsageText will raise an error (and @usageXML will be null)
-	---------------------------------------------------
-	
-	DECLARE @usageXML XML
-	DECLARE @cleanedComment VARCHAR(MAX) = @comment
-	
-	If @showDebug > 0
-		print 'Calling ParseUsageText'
-		
-	EXEC @myError = ParseUsageText @cleanedComment output, @usageXML output, @message output, @seq=@ID, @showDebug=@showDebug, @validateTotal = 1, @invalidUsage=@invalidUsage output
-	
-	If @showDebug > 0
-		print 'ParseUsageText returned ' + Cast(@myError as varchar(9))
-		
-	IF @myError <> 0
-	Begin
-		If @myError BETWEEN 1 and 255
-			RAISERROR (@message, 11, @myError)
-		Else
-			RAISERROR (@message, 11, 12)
-	End
-	
-	If @showDebug > 0
-		print '@myError is 0 after ParseUsageText'
-	
-	Set @logErrors = 1
-	
-	---------------------------------------------------
-	-- Is entry already in database? (only applies to updates)
-	---------------------------------------------------
-	if @mode = 'update'
-	begin
-		-- cannot update a non-existent entry
-		--
-		declare @tmp int
-		set @tmp = 0
-		--
-		SELECT @tmp = ID
-		FROM  T_Run_Interval 
-		WHERE (ID = @ID)
-		--
-		SELECT @myError = @@error, @myRowCount = @@rowcount
-		--
-		if @myError <> 0 OR @tmp = 0
-			RAISERROR ('No entry could be found in database for update', 11, 16)
-	end
+    IF @ID < 0
+    Begin
+        Set @message = 'Invalid ID: ' + Cast(@id as varchar(9))
+        RAISERROR (@message, 11, 10)
+        Goto Done
+    End
+    
+    ---------------------------------------------------
+    -- Validate usage and comment
+    -- ParseUsageText looks for special usage tags in the comment and extracts that information, returning it as XML
+    --
+    -- If @comment is 'User[100%], Proposal[49361], PropUser[50082] Extra information about interval'
+    -- after calling ParseUsageText, @cleanedComment will be 'Extra information about interval'
+    -- and @usageXML will be <u User="100" Proposal="49361" PropUser="50082" />
+    --
+    -- If @comment only has 'User[100%], Proposal[49361], PropUser[50082]', then @cleanedComment will be empty after the call to ParseUsageText
+    --
+    -- Since @validateTotal is set to 1, if the percentages do not add up to 100%, ParseUsageText will raise an error (and @usageXML will be null)
+    ---------------------------------------------------
+    
+    DECLARE @usageXML XML
+    DECLARE @cleanedComment VARCHAR(MAX) = @comment
+    
+    If @showDebug > 0
+        print 'Calling ParseUsageText'
+        
+    EXEC @myError = ParseUsageText @cleanedComment output, @usageXML output, @message output, @seq=@ID, @showDebug=@showDebug, @validateTotal = 1, @invalidUsage=@invalidUsage output
+    
+    If @showDebug > 0
+        print 'ParseUsageText returned ' + Cast(@myError as varchar(9))
+        
+    IF @myError <> 0
+    Begin
+        If @myError BETWEEN 1 and 255
+            RAISERROR (@message, 11, @myError)
+        Else
+            RAISERROR (@message, 11, 12)
+    End
+    
+    If @showDebug > 0
+        print '@myError is 0 after ParseUsageText'
+    
+    Set @logErrors = 1
+    
+    ---------------------------------------------------
+    -- Is entry already in database? (only applies to updates)
+    ---------------------------------------------------
+    If @mode = 'update'
+    Begin
+        -- Cannot update a non-existent entry
+        --
+        declare @tmp Int = 0
+        --
+        SELECT @tmp = ID
+        FROM  T_Run_Interval 
+        WHERE (ID = @ID)
+        --
+        SELECT @myError = @@error, @myRowCount = @@rowcount
+        --
+        if @myError <> 0 OR @tmp = 0
+            RAISERROR ('No entry could be found in database for update', 11, 16)
+    End
+    
+    ---------------------------------------------------
+    -- Add mode is not supported
+    ---------------------------------------------------
 
+    ---------------------------------------------------
+    -- Action for update mode
+    ---------------------------------------------------
+    --
+    If @mode = 'update' 
+    Begin
+        set @myError = 0
+        --
+        UPDATE T_Run_Interval 
+        SET
+            Comment = @Comment,
+            Usage = @usageXML,
+            Last_Affected = GetDate(),
+            Entered_By = @CallingUser
+        WHERE (ID = @ID)
+        --
+        SELECT @myError = @@error, @myRowCount = @@rowcount
+        --
+        if @myError <> 0
+            RAISERROR ('Update operation failed: "%s"', 11, 4, @ID)
 
-	---------------------------------------------------
-	-- add mode is not supported
-	---------------------------------------------------
+    End -- update mode
 
-	---------------------------------------------------
-	-- action for update mode
-	---------------------------------------------------
-	--
-	if @mode = 'update' 
-	begin
-		set @myError = 0
-		--
-		UPDATE T_Run_Interval 
-		SET
-			Comment = @Comment,
-			Usage = @usageXML,
-			Last_Affected = GetDate(),
-			Entered_By = @CallingUser
-		WHERE (ID = @ID)
-		--
-		SELECT @myError = @@error, @myRowCount = @@rowcount
-		--
-		if @myError <> 0
-			RAISERROR ('Update operation failed: "%s"', 11, 4, @ID)
-
-	end -- update mode
-
-	END TRY
-	BEGIN CATCH 
-		EXEC FormatErrorMessage @message output, @myError output
-		
-		-- rollback any open transactions
-		IF (XACT_STATE()) <> 0
-			ROLLBACK TRANSACTION;
-		
-		If @logErrors > 0	
-			Exec PostLogEntry 'Error', @message, 'AddUpdateRunInterval'
-	END CATCH
+    END TRY
+    BEGIN CATCH 
+        EXEC FormatErrorMessage @message output, @myError output
+        
+        -- rollback any open transactions
+        IF (XACT_STATE()) <> 0
+            ROLLBACK TRANSACTION;
+        
+        If @logErrors > 0    
+            Exec PostLogEntry 'Error', @message, 'AddUpdateRunInterval'
+    END CATCH
 
 Done:
-	return @myError
+    return @myError
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[AddUpdateRunInterval] TO [DDL_Viewer] AS [dbo]
---
+++ DMS5/GetRunTrackingMonthlyInfoByID.sql
@@ -0,0 +1,244 @@
+/****** Object:  UserDefinedFunction [dbo].[GetRunTrackingMonthlyInfoByID] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[GetRunTrackingMonthlyInfoByID]
+/****************************************************
+**
+**  Desc:   Returns run tracking information for given EUS Instrument ID
+**          Modeled after GetRunTrackingMonthlyInfoByID
+**    
+**  Auth:   mem
+**  Date:   02/14/2012 mem - Initial release
+**    
+*****************************************************/
+(
+    @eusInstrumentId Int,
+    @year varchar(8), -- '2012'
+    @month varchar(12), -- '1'
+    @options varchar(32) = ''       -- Reserved for future use
+)
+RETURNS @TX TABLE (
+    Seq INT primary key,
+    ID INT ,
+    Dataset varchar(128) ,
+    [Day] INT,
+    Duration int NULL,
+    Interval INT NULL,
+    Time_Start datetime ,
+    Time_End datetime ,
+    Instrument varchar(128) ,
+    CommentState varchar(24),
+    Comment varchar(256)
+)
+AS
+    BEGIN
+        Declare @maxNormalInterval INT = dbo.GetLongIntervalThreshold()
+        Declare @message varchar(512) = ''
+    
+        ---------------------------------------------------
+        -- check arguments
+        ---------------------------------------------------
+
+        IF ISNULL(@year, 0) = 0 OR ISNULL(@month, 0) = 0 OR ISNULL(@eusInstrumentId, 0) = 0
+        BEGIN
+            INSERT INTO @TX (Dataset) VALUES ('Bad arguments')
+            RETURN 
+        END
+
+        ---------------------------------------------------
+        -- Validate @eusInstrumentId
+        ---------------------------------------------------
+
+        Declare @instrumentIDFirst Int
+
+        SELECT Top 1 @instrumentIDFirst = InstName.Instrument_ID
+        FROM T_Instrument_Name AS InstName
+             INNER JOIN T_EMSL_DMS_Instrument_Mapping AS InstMapping
+               ON InstName.Instrument_ID = InstMapping.DMS_Instrument_ID
+        WHERE InstMapping.EUS_Instrument_ID = @eusInstrumentId
+        Order By InstName.IN_name
+
+        IF ISNULL(@instrumentIDFirst, 0) = 0
+        BEGIN
+            INSERT INTO @TX (Dataset) VALUES ('Unrecognized EUS ID; no DMS instruments are mapped to EUS Instrument ID ' + Cast(@eusInstrumentId As Varchar(12)))
+            RETURN 
+        END
+
+        ---------------------------------------------------
+        -- Set up dates for beginning and end of month 
+        ---------------------------------------------------
+        
+        Declare @startDate varchar(16) = @month + '/1/' + @year
+        Declare @firstDayOfStartingMonth datetime = CONVERT(datetime, @startDate, 102);
+        Declare @firstDayOfTrailingMonth datetime = DATEADD(MONTH, 1, @firstDayOfStartingMonth)
+
+        ---------------------------------------------------
+        -- Get datasets whose start time falls within month 
+        ---------------------------------------------------
+        Declare @seqIncrement INT = 1
+        Declare @seqOffset INT = 0
+
+        INSERT INTO @TX
+        (
+            Seq,
+            ID ,
+            Dataset ,
+            [Day],
+            Time_Start ,
+            Time_End ,
+            Duration,
+            [Interval],
+            Instrument
+        )        
+        SELECT (@seqIncrement * ((ROW_NUMBER() OVER ( ORDER BY TD.Acq_Time_Start ASC )) - 1) + 1) + @seqOffset AS 'Seq',
+               TD.Dataset_ID AS ID,
+               TD.Dataset_Num AS Dataset,
+               DATEPART(DAY, TD.Acq_Time_Start) AS [Day],
+               TD.Acq_Time_Start AS Time_Start,
+               TD.Acq_Time_End AS Time_End,
+               TD.Acq_Length_Minutes AS Duration,
+               TD.Interval_to_Next_DS AS [Interval],
+               @instrumentIDFirst AS Instrument
+        FROM T_Dataset AS TD
+             INNER JOIN T_EMSL_DMS_Instrument_Mapping AS InstMapping
+               ON TD.DS_instrument_name_ID = InstMapping.DMS_Instrument_ID
+        WHERE InstMapping.EUS_Instrument_ID = @eusInstrumentId AND
+              @firstDayOfStartingMonth <= TD.Acq_Time_Start AND
+              TD.Acq_Time_Start < @firstDayOfTrailingMonth
+        ORDER BY TD.Acq_Time_Start
+
+        ---------------------------------------------------
+        -- Need to add some part of run or interval from
+        -- preceding month if first run in month is not
+        -- close to beginning of month
+        ---------------------------------------------------
+
+        Declare @firstRunSeq int
+        Declare @lastRunSeq int
+        SELECT @firstRunSeq = MIN(Seq), @lastRunSeq = MAX(Seq) FROM @TX
+        
+        Declare @firstStart datetime
+        SELECT @firstStart = Time_Start FROM @TX WHERE Seq = @firstRunSeq
+        
+        Declare @initialGap INT = DATEDIFF(MINUTE, @firstDayOfStartingMonth, @firstStart)
+        
+        -- get preceeding dataset (latest with starting time preceding this month)
+        --
+        IF DATEDIFF(MINUTE, @firstDayOfStartingMonth, @firstStart) > @maxNormalInterval
+        BEGIN 
+            Declare @precID INT
+            Declare @precDataset varchar(128) 
+            Declare @precStart datetime
+            Declare @precEnd datetime 
+            Declare @precDuration INT
+            Declare @precInterval INT 
+
+            SELECT TOP 1 @precID = TD.Dataset_ID,
+                         @precDataset = TD.Dataset_Num,
+                         @precStart = TD.Acq_Time_Start,
+                         @precEnd = TD.Acq_Time_End,
+                         @precDuration = TD.Acq_Length_Minutes,
+                         @precInterval = TD.Interval_to_Next_DS
+            FROM T_Dataset AS TD
+                 INNER JOIN T_EMSL_DMS_Instrument_Mapping AS InstMapping
+                   ON TD.DS_instrument_name_ID = InstMapping.DMS_Instrument_ID
+            WHERE InstMapping.EUS_Instrument_ID = @eusInstrumentId AND
+                  TD.Acq_Time_Start < @firstDayOfStartingMonth AND
+                  TD.Acq_Time_Start > DATEADD(DAY, -90, @firstDayOfStartingMonth)
+            ORDER BY TD.Acq_Time_Start DESC
+            
+            -- if preceeding dataset's end time is before start of month, 
+            -- zero the duration and truncate the interval
+            -- othewise just truncate the duration
+            --
+            IF @precEnd < @firstDayOfStartingMonth
+            BEGIN 
+                SET @precDuration = 0
+                SET @precInterval = @initialGap
+            END
+            ELSE
+            BEGIN 
+                SET @precDuration = DATEDIFF(MINUTE, @firstDayOfStartingMonth, @precStart)
+            END 
+
+            -- Add preceeding dataset record (with truncated duration/interval)
+            -- at beginning of results
+            --
+            INSERT INTO @TX( Seq,
+                             Dataset,
+                             ID,
+                             [Day],
+                             Time_Start,
+                             Time_End,
+                             Duration,
+                             [Interval],
+                             Instrument )
+            Values (@firstRunSeq - 1, 
+                    @precDataset, 
+                    @precID, 
+                    1,                -- Day of month
+                    @precStart, 
+                    @precEnd, 
+                    @precDuration,
+                    @precInterval, 
+                    @instrumentIDFirst)
+        END
+
+        ---------------------------------------------------
+        -- Need to truncate part of last run or following
+        -- interval that hangs over end of month
+        ---------------------------------------------------
+        
+        -- If end of last run hangs over start of succeeding month,
+        -- truncate duration and set interval to zero.
+
+        -- Otherwise, if interval hangs over succeeding month, truncate it
+        --
+        Declare @lastRunStart datetime 
+        Declare @lastRunEnd datetime 
+        Declare @lastRunInterval int
+
+        SELECT @lastRunStart = Time_Start,
+               @lastRunEnd = Time_End,
+               @lastRunInterval = [Interval]
+        FROM @TX
+        WHERE Seq = @lastRunSeq
+        
+        IF @lastRunEnd > @firstDayOfTrailingMonth
+        BEGIN 
+            UPDATE @TX
+            SET [Interval] = 0,
+                Duration = DATEDIFF(MINUTE, @lastRunStart, @firstDayOfTrailingMonth)
+            WHERE Seq = @lastRunSeq
+        END 
+        ELSE IF DATEADD(MINUTE, @lastRunInterval, @lastRunEnd) > @firstDayOfTrailingMonth
+        BEGIN
+            UPDATE @TX
+            SET [Interval] = DATEDIFF(MINUTE, @lastRunEnd, @firstDayOfTrailingMonth)
+            WHERE Seq = @lastRunSeq
+        END
+
+        ---------------------------------------------------
+        -- Fill in interval comment information
+        ---------------------------------------------------
+
+        UPDATE @TX
+        SET [Comment] = CONVERT(varchar(256), TRI.[Comment]),
+            CommentState = CASE
+                               WHEN TRI.Instrument IS NULL THEN 'x'
+                               ELSE (CASE
+                                         WHEN ISNULL(TRI.[Comment], '') = '' THEN '-'
+                                         ELSE '+'
+                                     END)
+                           END
+        FROM @TX T
+             LEFT OUTER JOIN T_Run_Interval TRI
+               ON T.ID = TRI.ID
+
+        RETURN
+    END
+
+GO
