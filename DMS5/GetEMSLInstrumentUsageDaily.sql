8915e960ceaf5c0c0be46d5c6d35a8bf27f96550
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-08 13:55:43 -0700

    Fix URL

--- DMS5/GetEMSLInstrumentUsageDaily.sql
+++ DMS5/GetEMSLInstrumentUsageDaily.sql
@@ -4,14 +4,14 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE FUNCTION dbo.GetEMSLInstrumentUsageDaily
+CREATE FUNCTION [dbo].[GetEMSLInstrumentUsageDaily]
 /****************************************************
 **	Desc: 
 **  Outputs contents of EMSL instrument usage report table as rollup
 **
 **	This UDF is used by the CodeIgniter instance at http://prismsupport.pnl.gov/dms2ws/
 **	Example URL:
-**		http://prismsupport.pnl.gov/dms2ws/instrument_usage_report/ws/2015/07
+**      https://prismsupport.pnl.gov/dms2ws/instrument_usage_report/daily/2019/03
 **
 **	See also /file1/www/html/prismsupport/dms2ws/application/controllers/instrument_usage_report.php
 **
