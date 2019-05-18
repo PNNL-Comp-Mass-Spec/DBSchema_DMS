9eb86dbfa8819c05ff822f7adc8d3d300ed7b220
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-17 18:23:49 -0700

    Add synonym

---
+++ DMS_Capture/S_DMS_T_Dataset_Files.sql
@@ -0,0 +1,5 @@
+/****** Object:  Synonym [dbo].[S_DMS_T_Dataset_Files] ******/
+CREATE SYNONYM [dbo].[S_DMS_T_Dataset_Files] FOR [DMS5].[dbo].[T_Dataset_Files]
+GO
+GRANT VIEW DEFINITION ON [dbo].[S_DMS_T_Dataset_Files] TO [DDL_Viewer] AS [dbo]
+GO
