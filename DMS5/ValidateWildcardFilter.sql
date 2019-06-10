dce1edac77471118d083831321629a7cf9db2274
alchemistmatt <matthew.monroe@pnnl.gov>
2019-06-10 16:09:54 -0700

    Add function ValidateWildcardFilter

---
+++ DMS5/ValidateWildcardFilter.sql
@@ -0,0 +1,39 @@
+/****** Object:  UserDefinedFunction [dbo].[ValidateWildcardFilter] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[ValidateWildcardFilter]
+/****************************************************
+**
+**  Desc:   Makes sure that @wildcardFilter contains a percent sign
+**          Adds percent signs at the beginning and end if it does not have them
+**
+**  Returns the updated wildcard filter
+**
+**  Auth:   mem
+**  Date:   06/10/2019 mem - Initial version
+**
+*****************************************************/
+(
+    @wildcardFilter varchar(4000)          -- Filter text to examine
+)
+    Returns varchar(4000)
+As
+Begin
+    Set @wildcardFilter = IsNull(@wildcardFilter, '')
+    
+    If Len(IsNull(@wildcardFilter, '')) > 0
+    Begin
+        -- Add wildcards if @wildcardFilter doesn't contain a percent sign
+        If Not @wildcardFilter Like '%[%]%'
+        Begin
+            Set @wildcardFilter = '%' + @wildcardFilter + '%'
+        End
+    End
+
+    Return @wildcardFilter
+End
+
+GO
