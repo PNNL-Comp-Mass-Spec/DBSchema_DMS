22ab2b914f8a71c575b109655ba04bf0a310d3ae
alchemistmatt <matthew.monroe@pnnl.gov>
2019-06-27 18:18:37 -0700

    Create dataset capture jobs with priority 2 for QC datasets
    Create capture jobs with priority 6 for MRM datasets

---
+++ DMS_Capture/GetDatasetCapturePriority.sql
@@ -0,0 +1,63 @@
+/****** Object:  UserDefinedFunction [dbo].[GetDatasetCapturePriority] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[GetDatasetCapturePriority]
+/****************************************************
+**
+**  Desc: 
+**      Determines if the dataset warrants preferential processing priority for dataset capture
+**      This procedure is used by MakeNewJobsFromDMS to define the capture job priority
+**
+**      If the dataset name matches one of the filters below, the capture priority will be 2 instead of 4
+**      Otherwise, if the instrument group matches one of the filters, the capture priority will be 6 instead of 4
+**
+**  Return values: 2 if high priority; 4 if medium priority, 6 if low priority
+**
+**  Auth:   mem
+**  Date:   06/27/2019 mem - Initial version
+**    
+*****************************************************/
+(
+    @datasetName varchar(128),
+    @instrumentGroup varchar(64)
+)
+RETURNS tinyint
+AS
+BEGIN
+    Declare @priority tinyint
+
+    -- These dataset names are modelled after those in GetDatasetPriority in DMS5
+    If (@datasetName LIKE 'QC[_][0-9][0-9]%' OR
+        @datasetName LIKE 'QC[_-]Shew[_-][0-9][0-9]%' OR
+        @datasetName LIKE 'QC[_-]ShewIntact%' OR
+        @datasetName LIKE 'QC[_]Shew[_]TEDDY%' OR
+        @datasetName LIKE 'QC[_]Mam%' OR
+        @datasetName Like 'QC[_]PP[_]MCF-7%'
+       ) AND NOT @datasetName LIKE '%-bad'
+    Begin
+        Set @priority = 2
+    End
+    Else
+    Begin
+        If @instrumentGroup In ('TSQ', 'Bruker_FTMS', 'MALDI-Imaging')
+        Begin
+            Set @priority = 6
+        End
+        Else
+        Begin
+            Set @priority = 4
+        End
+    End
+    
+    Return @priority
+END
+
+
+GO
+GRANT VIEW DEFINITION ON [dbo].[GetDatasetCapturePriority] TO [DDL_Viewer] AS [dbo]
+GO
+GRANT EXECUTE ON [dbo].[GetDatasetCapturePriority] TO [public] AS [dbo]
+GO
--- DMS_Capture/MakeNewJobsFromDMS.sql
+++ DMS_Capture/MakeNewJobsFromDMS.sql
@@ -3,168 +3,174 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE MakeNewJobsFromDMS
+
+CREATE PROCEDURE [dbo].[MakeNewJobsFromDMS]
 /****************************************************
 **
-**	Desc:
-**    Add dataset capture jobs from DMS for datsets that are in “New” state that aren’t
-**    already in table.  Choose script..
+**  Desc:
+**      Add dataset capture jobs for datasets in state New in DMS5
 **
-**	Auth:	grk
-**	Date:	09/02/2009 grk - Initial release (http://prismtrac.pnl.gov/trac/ticket/746)
-**			02/10/2010 dac - Removed comment stating that jobs were created from test script
-**			03/09/2011 grk - Added logic to choose different capture script based on instrument group
-**			09/17/2015 mem - Added parameter @infoOnly
-**			06/16/2017 mem - Restrict access using VerifySPAuthorized
-**			08/01/2017 mem - Use THROW if not authorized
+**  Auth:   grk
+**  Date:   09/02/2009 grk - Initial release (http://prismtrac.pnl.gov/trac/ticket/746)
+**          02/10/2010 dac - Removed comment stating that jobs were created from test script
+**          03/09/2011 grk - Added logic to choose different capture script based on instrument group
+**          09/17/2015 mem - Added parameter @infoOnly
+**          06/16/2017 mem - Restrict access using VerifySPAuthorized
+**          08/01/2017 mem - Use THROW if not authorized
+**          06/27/2019 mem - Use GetDatasetCapturePriority to determine capture job priority using dataset name and instrument group
 **    
 *****************************************************/
 (
-	@bypassDMS tinyint = 0,
-	@message varchar(512) = '' output,
-	@MaxJobsToProcess int = 0,
-	@LogIntervalThreshold int = 15,		-- If this procedure runs longer than this threshold, then status messages will be posted to the log
-	@LoggingEnabled tinyint = 0,		-- Set to 1 to immediately enable progress logging; if 0, then logging will auto-enable if @LogIntervalThreshold seconds elapse
-	@LoopingUpdateInterval int = 5,		-- Seconds between detailed logging while looping through the dependencies
-	@infoOnly tinyint = 0,				-- 1 to preview changes that would be made; 2 to add new jobs but do not create job steps
-	@DebugMode tinyint = 0				-- 0 for no debugging; 1 to see debug messages
+    @bypassDMS tinyint = 0,
+    @message varchar(512) = '' output,
+    @maxJobsToProcess int = 0,
+    @logIntervalThreshold int = 15,        -- If this procedure runs longer than this threshold, then status messages will be posted to the log
+    @loggingEnabled tinyint = 0,        -- Set to 1 to immediately enable progress logging; if 0, then logging will auto-enable if @logIntervalThreshold seconds elapse
+    @loopingUpdateInterval int = 5,        -- Seconds between detailed logging while looping through the dependencies
+    @infoOnly tinyint = 0,                -- 1 to preview changes that would be made; 2 to add new jobs but do not create job steps
+    @debugMode tinyint = 0                -- 0 for no debugging; 1 to see debug messages
 )
 As
-	set nocount on
-	
-	declare @myError int = 0
-	declare @myRowCount int = 0
+    set nocount on
+    
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
 
-	declare @currJob int
-	declare @Dataset varchar(128)
-	declare @continue tinyint
+    Declare @currJob int
+    Declare @Dataset varchar(128)
+    Declare @continue tinyint
 
-	declare @JobsProcessed int
-	Declare @JobCountToResume int
-	declare @JobCountToReset int
-	
-	Declare @MaxJobsToAddResetOrResume int
+    Declare @JobsProcessed int
+    Declare @JobCountToResume int
+    Declare @JobCountToReset int
+    
+    Declare @MaxJobsToAddResetOrResume int
 
-	declare @StartTime datetime
-	declare @LastLogTime datetime
-	declare @StatusMessage varchar(512)	
+    Declare @StartTime datetime
+    Declare @LastLogTime datetime
+    Declare @StatusMessage varchar(512)    
 
-	---------------------------------------------------
-	-- Verify that the user can execute this procedure from the given client host
-	---------------------------------------------------
-		
-	Declare @authorized tinyint = 0	
-	Exec @authorized = VerifySPAuthorized 'MakeNewJobsFromDMS', @raiseError = 1;
-	If @authorized = 0
-	Begin
-		THROW 51000, 'Access denied', 1;
-	End
-		
-	---------------------------------------------------
-	-- Validate the inputs
-	---------------------------------------------------
-	--
-	Set @infoOnly = IsNull(@infoOnly, 0)
-	Set @bypassDMS = IsNull(@bypassDMS, 0)
-	Set @DebugMode = IsNull(@DebugMode, 0)
-	Set @MaxJobsToProcess = IsNull(@MaxJobsToProcess, 0)
-	
-	set @message = ''
+    ---------------------------------------------------
+    -- Verify that the user can execute this procedure from the given client host
+    ---------------------------------------------------
+        
+    Declare @authorized tinyint = 0    
+    Exec @authorized = VerifySPAuthorized 'MakeNewJobsFromDMS', @raiseError = 1;
+    If @authorized = 0
+    Begin;
+        THROW 51000, 'Access denied', 1;
+    End;
+        
+    ---------------------------------------------------
+    -- Validate the inputs
+    ---------------------------------------------------
+    --
+    Set @infoOnly = IsNull(@infoOnly, 0)
+    Set @bypassDMS = IsNull(@bypassDMS, 0)
+    Set @debugMode = IsNull(@debugMode, 0)
+    Set @maxJobsToProcess = IsNull(@maxJobsToProcess, 0)
+    
+    set @message = ''
 
-	If @MaxJobsToProcess <= 0
-		Set @MaxJobsToAddResetOrResume = 1000000
-	Else
-		Set @MaxJobsToAddResetOrResume = @MaxJobsToProcess
+    If @maxJobsToProcess <= 0
+        Set @MaxJobsToAddResetOrResume = 1000000
+    Else
+        Set @MaxJobsToAddResetOrResume = @maxJobsToProcess
 
-	Set @StartTime = GetDate()
-	Set @LoggingEnabled = IsNull(@LoggingEnabled, 0)
-	Set @LogIntervalThreshold = IsNull(@LogIntervalThreshold, 15)
-	Set @LoopingUpdateInterval = IsNull(@LoopingUpdateInterval, 5)
-	
-	If @LogIntervalThreshold = 0
-		Set @LoggingEnabled = 1
-		
-	If @LoopingUpdateInterval < 2
-		Set @LoopingUpdateInterval = 2
+    Set @StartTime = GetDate()
+    Set @loggingEnabled = IsNull(@loggingEnabled, 0)
+    Set @logIntervalThreshold = IsNull(@logIntervalThreshold, 15)
+    Set @loopingUpdateInterval = IsNull(@loopingUpdateInterval, 5)
+    
+    If @logIntervalThreshold = 0
+        Set @loggingEnabled = 1
+        
+    If @loopingUpdateInterval < 2
+        Set @loopingUpdateInterval = 2
 
-	If @LoggingEnabled = 1 Or DateDiff(second, @StartTime, GetDate()) >= @LogIntervalThreshold
-	Begin
-		Set @StatusMessage = 'Entering (' + CONVERT(VARCHAR(12), @bypassDMS) + ')'
-		exec PostLogEntry 'Progress', @StatusMessage, 'MakeNewJobsFromDMS'
-	End
-	
-	---------------------------------------------------
-	-- Add new jobs
-	---------------------------------------------------
-	--
-	IF @bypassDMS = 0
-	BEGIN -- <AddJobs>
-	
-		If @LoggingEnabled = 1 Or DateDiff(second, @StartTime, GetDate()) >= @LogIntervalThreshold
-		Begin
-			Set @StatusMessage = 'Querying DMS'
-			exec PostLogEntry 'Progress', @StatusMessage, 'MakeNewJobsFromDMS'
-		End
+    If @loggingEnabled = 1 Or DateDiff(second, @StartTime, GetDate()) >= @logIntervalThreshold
+    Begin
+        Set @StatusMessage = 'Entering (' + CONVERT(VARCHAR(12), @bypassDMS) + ')'
+        exec PostLogEntry 'Progress', @StatusMessage, 'MakeNewJobsFromDMS'
+    End
+    
+    ---------------------------------------------------
+    -- Add new jobs
+    ---------------------------------------------------
+    --
+    IF @bypassDMS = 0
+    BEGIN -- <AddJobs>
+    
+        If @loggingEnabled = 1 Or DateDiff(second, @StartTime, GetDate()) >= @logIntervalThreshold
+        Begin
+            Set @StatusMessage = 'Querying DMS'
+            exec PostLogEntry 'Progress', @StatusMessage, 'MakeNewJobsFromDMS'
+        End
 
-		If @infoOnly = 0
-		Begin -- <InsertQuery>
-		
-			INSERT INTO T_Jobs( Script,
-								[Comment],
-								Dataset,
-								Dataset_ID )
-			SELECT CASE
-					WHEN Src.IN_Group = 'IMS' THEN 'IMSDatasetCapture'
-					ELSE 'DatasetCapture'
-				END AS Script,
-				'' AS [Comment],
-				Src.Dataset,
-				Src.Dataset_ID
-			FROM V_DMS_Get_New_Datasets Src LEFT OUTER JOIN
-			     T_Jobs Target ON Src.Dataset_ID = Target.Dataset_ID
-			WHERE Target.Dataset_ID Is Null
-			--
-			SELECT @myError = @@error, @myRowCount = @@rowcount
-			--
-			if @myError <> 0
-			begin
-				set @message = 'Error adding new DatasetCapture tasks'
-				goto Done
-			end
-			
-		End -- </InsertQuery>
-		Else
-		Begin -- <Preview>
+        If @infoOnly = 0
+        Begin -- <InsertQuery>
+        
+            INSERT INTO T_Jobs( Script,
+                                [Comment],
+                                Dataset,
+                                Dataset_ID,
+                                Priority)
+            SELECT CASE
+                       WHEN Src.IN_Group = 'IMS' THEN 'IMSDatasetCapture'
+                       ELSE 'DatasetCapture'
+                   END AS Script,
+                   '' AS [Comment],
+                   Src.Dataset,
+                   Src.Dataset_ID,
+                   dbo.GetDatasetCapturePriority(Src.Dataset, Src.IN_Group)
+            FROM V_DMS_Get_New_Datasets Src
+                 LEFT OUTER JOIN T_Jobs Target
+                   ON Src.Dataset_ID = Target.Dataset_ID
+            WHERE Target.Dataset_ID IS NULL
+            --
+            SELECT @myError = @@error, @myRowCount = @@rowcount
+            --
+            if @myError <> 0
+            begin
+                set @message = 'Error adding new DatasetCapture tasks'
+                goto Done
+            end
+            
+        End -- </InsertQuery>
+        Else
+        Begin -- <Preview>
 
-			SELECT CASE
-					WHEN Src.IN_Group = 'IMS' THEN 'IMSDatasetCapture'
-					ELSE 'DatasetCapture'
-				END AS Script,
-				'' AS [Comment],
-				Src.Dataset,
-				Src.Dataset_ID
-			FROM V_DMS_Get_New_Datasets Src LEFT OUTER JOIN
-			     T_Jobs Target ON Src.Dataset_ID = Target.Dataset_ID
-			WHERE Target.Dataset_ID Is Null
-			--
-			SELECT @myError = @@error, @myRowCount = @@rowcount
-			
-		End -- </Preview>
-		
-	END -- </AddJobs>
+            SELECT CASE
+                       WHEN Src.IN_Group = 'IMS' THEN 'IMSDatasetCapture'
+                       ELSE 'DatasetCapture'
+                   END AS Script,
+                   '' AS [Comment],
+                   Src.Dataset,
+                   Src.Dataset_ID,
+                   dbo.GetDatasetCapturePriority(Src.Dataset, Src.IN_Group) As Priority
+            FROM V_DMS_Get_New_Datasets Src
+                 LEFT OUTER JOIN T_Jobs Target
+                   ON Src.Dataset_ID = Target.Dataset_ID
+            WHERE Target.Dataset_ID IS NULL
+            --
+            SELECT @myError = @@error, @myRowCount = @@rowcount
+            
+        End -- </Preview>
+        
+    END -- </AddJobs>
 
-	---------------------------------------------------
-	-- Exit
-	---------------------------------------------------
-	--
+    ---------------------------------------------------
+    -- Exit
+    ---------------------------------------------------
+    --
 Done:
-	If @LoggingEnabled = 1 Or DateDiff(second, @StartTime, GetDate()) >= @LogIntervalThreshold
-	Begin
-		Set @StatusMessage = 'Exiting'
-		exec PostLogEntry 'Progress', @StatusMessage, 'MakeNewJobsFromDMS'
-	End
+    If @loggingEnabled = 1 Or DateDiff(second, @StartTime, GetDate()) >= @logIntervalThreshold
+    Begin
+        Set @StatusMessage = 'Exiting'
+        exec PostLogEntry 'Progress', @StatusMessage, 'MakeNewJobsFromDMS'
+    End
 
-	return @myError
+    return @myError
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[MakeNewJobsFromDMS] TO [DDL_Viewer] AS [dbo]
