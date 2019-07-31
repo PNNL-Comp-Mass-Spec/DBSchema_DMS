532195422d5009157aeab7aa06e4668dda12d138
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-30 21:08:42 -0700

    Whitespace update

--- DMS5/ValidateAnalysisJobRequestDatasets.sql
+++ DMS5/ValidateAnalysisJobRequestDatasets.sql
@@ -3,260 +3,270 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE Procedure [dbo].[ValidateAnalysisJobRequestDatasets]
 /****************************************************
 **
-**	Desc:
-**			Validates datasets in temporary table #TD 
-**			The calling procedure must create #TD and populate it with the dataset names; 
-**			 the remaining columns in the table will be populated by this procedure
+**  Desc:   Validates datasets in temporary table #TD 
+**          The calling procedure must create #TD and populate it with the dataset names; 
+**          the remaining columns in the table will be populated by this procedure
 **
-**	Return values: 
-**			0 if no problems
-**			Error code if a problem; @message will contain the error message
+**      CREATE TABLE #TD (
+**          Dataset_Num varchar(128),
+**          Dataset_ID int NULL,
+**          IN_class varchar(64) NULL, 
+**          DS_state_ID int NULL, 
+**          AS_state_ID int NULL,
+**          Dataset_Type varchar(64) NULL,
+**          DS_rating smallint NULL,
+**      )
 **
-**	Auth:	mem
-**			11/12/2012 mem - Initial version (extracted code from AddUpdateAnalysisJobRequest and ValidateAnalysisJobParameters)
-**			03/05/2013 mem - Added parameter @AutoRemoveNotReleasedDatasets
-**			08/02/2013 mem - Tweaked message for "Not Released" datasets
-**			03/30/2015 mem - Tweak warning message grammar
-**			04/23/2015 mem - Added parameter @toolName
-**			06/24/2015 mem - Added parameter @showDebugMessages
-**			07/20/2016 mem - Tweak error messages
-**			12/06/2017 mem - Add @allowNewDatasets
+**  Return values: 
+**      0 if no problems
+**      Error code if a problem; @message will contain the error message
+**
+**  Auth:   mem
+**          11/12/2012 mem - Initial version (extracted code from AddUpdateAnalysisJobRequest and ValidateAnalysisJobParameters)
+**          03/05/2013 mem - Added parameter @autoRemoveNotReleasedDatasets
+**          08/02/2013 mem - Tweaked message for "Not Released" datasets
+**          03/30/2015 mem - Tweak warning message grammar
+**          04/23/2015 mem - Added parameter @toolName
+**          06/24/2015 mem - Added parameter @showDebugMessages
+**          07/20/2016 mem - Tweak error messages
+**          12/06/2017 mem - Add @allowNewDatasets
+**          07/30/2019 mem - Tabs to spaces
 **
 *****************************************************/
 (
-	@message varchar(512) output,
-	@AutoRemoveNotReleasedDatasets tinyint = 0,			-- When 1, then automatically removes datasets from #TD if they have an invalid rating
-	@toolName varchar(64) = 'unknown',
-	@allowNewDatasets tinyint = 0,						-- When 0, all datasets must have state 3 (Complete); when 1, will also allow datasets with state 1 or 2 (New or Capture In Progress)
-	@showDebugMessages tinyint = 0						-- 1 to print @message strings; 2 to also see the contents of #TD
+    @message varchar(512) output,
+    @autoRemoveNotReleasedDatasets tinyint = 0,            -- When 1, then automatically removes datasets from #TD if they have an invalid rating
+    @toolName varchar(64) = 'unknown',
+    @allowNewDatasets tinyint = 0,                        -- When 0, all datasets must have state 3 (Complete); when 1, will also allow datasets with state 1 or 2 (New or Capture In Progress)
+    @showDebugMessages tinyint = 0                        -- 1 to print @message strings; 2 to also see the contents of #TD
 )
 As
-	set nocount on
+    set nocount on
 
-	declare @myError int
-	declare @myRowCount int
-	set @myError = 0
-	set @myRowCount = 0
-	
-	set @message = ''
-	
-	Set @AutoRemoveNotReleasedDatasets = IsNull(@AutoRemoveNotReleasedDatasets, 0)
-	Set @showDebugMessages = IsNull(@showDebugMessages, 0)
-	
-	---------------------------------------------------
-	-- Auto-delete 'Dataset' and 'Dataset_Num' from #TD
-	---------------------------------------------------
-	--
-	DELETE FROM #TD
-	WHERE Dataset_Num IN ('Dataset', 'Dataset_Num')
+    Declare @myError Int = 0
+    Declare @myRowCount Int = 0
+    
+    set @message = ''
+    
+    Set @autoRemoveNotReleasedDatasets = IsNull(@autoRemoveNotReleasedDatasets, 0)
+    Set @showDebugMessages = IsNull(@showDebugMessages, 0)
+    
+    ---------------------------------------------------
+    -- Auto-delete 'Dataset' and 'Dataset_Num' from #TD
+    ---------------------------------------------------
+    --
+    DELETE FROM #TD
+    WHERE Dataset_Num IN ('Dataset', 'Dataset_Num')
 
-	---------------------------------------------------
-	-- Update the additional info in #TD
-	---------------------------------------------------
-	--
-	UPDATE T
-	SET
-		T.Dataset_ID = T_Dataset.Dataset_ID, 
-		T.IN_class = T_Instrument_Class.IN_class, 
-		T.DS_state_ID = T_Dataset.DS_state_ID, 
-		T.AS_state_ID = isnull(T_Dataset_Archive.AS_state_ID, 0),
-		T.Dataset_Type = T_DatasetTypeName.DST_name,
-		T.DS_rating = T_Dataset.DS_Rating
-	FROM
-		#TD T INNER JOIN
-		T_Dataset ON T.Dataset_Num = T_Dataset.Dataset_Num INNER JOIN
-		T_Instrument_Name ON T_Dataset.DS_instrument_name_ID = T_Instrument_Name.Instrument_ID INNER JOIN
-		T_Instrument_Class ON T_Instrument_Name.IN_class = T_Instrument_Class.IN_class INNER JOIN
-		T_DatasetTypeName ON T_DatasetTypeName.DST_Type_ID = T_Dataset.DS_type_ID LEFT OUTER JOIN
-		T_Dataset_Archive ON T_Dataset.Dataset_ID = T_Dataset_Archive.AS_Dataset_ID	
-	--
-	SELECT @myError = @@error, @myRowCount = @@rowcount
+    ---------------------------------------------------
+    -- Update the additional info in #TD
+    ---------------------------------------------------
+    --
+    UPDATE #TD
+    SET #TD.Dataset_ID = T_Dataset.Dataset_ID,
+        #TD.IN_class = T_Instrument_Class.IN_class,
+        #TD.DS_state_ID = T_Dataset.DS_state_ID,
+        #TD.AS_state_ID = isnull(T_Dataset_Archive.AS_state_ID, 0),
+        #TD.Dataset_Type = T_DatasetTypeName.DST_name,
+        #TD.DS_rating = T_Dataset.DS_Rating
+    FROM #TD
+         INNER JOIN T_Dataset
+           ON #TD.Dataset_Num = T_Dataset.Dataset_Num
+         INNER JOIN T_Instrument_Name
+           ON T_Dataset.DS_instrument_name_ID = T_Instrument_Name.Instrument_ID
+         INNER JOIN T_Instrument_Class
+           ON T_Instrument_Name.IN_class = T_Instrument_Class.IN_class
+         INNER JOIN T_DatasetTypeName
+           ON T_DatasetTypeName.DST_Type_ID = T_Dataset.DS_type_ID
+         LEFT OUTER JOIN T_Dataset_Archive
+           ON T_Dataset.Dataset_ID = T_Dataset_Archive.AS_Dataset_ID
+    --
+    SELECT @myError = @@error, @myRowCount = @@rowcount
 
-	If @showDebugMessages > 1
-	Begin
-		SELECT *
-		FROM #TD
-		ORDER BY Dataset_Num
-	End
-			
-	---------------------------------------------------
-	-- Make sure none of the datasets has a rating of -5 (Not Released)
-	---------------------------------------------------
-	--
-	Declare @list varchar(4000)
-	Declare @NotReleasedCount int
-	--
-	Set @NotReleasedCount = 0
-	
-	SELECT @NotReleasedCount = COUNT(*)
-	FROM #TD
-	WHERE DS_Rating = -5
-	
-	If @NotReleasedCount > 0
-	Begin
-		Set @list = ''
-		
-		SELECT @list = @list + Dataset_Num + ', '
-		FROM #TD
-		WHERE DS_Rating = -5
-		ORDER BY Dataset_Num
-		
-		-- Remove the trailing comma If the length is less than 400 characters, otherwise truncate
-		If Len(@list) < 400
-			Set @list = Left(@list, Len(@list)-1)
-		Else
-			Set @list = Left(@list, 397) + '...'
-		
-		If @AutoRemoveNotReleasedDatasets = 0
-		Begin
-			if @NotReleasedCount = 1
-				set @message = 'dataset is "Not Released": ' + @list
-			else
-				set @message = Convert(varchar(12), @NotReleasedCount) + ' datasets are "Not Released": ' + @list
-				
-			if @showDebugMessages <> 0
-				print @message
-				
-			return 50101
-		End
-		Else
-		Begin
-			set @message = 'Skipped ' + Convert(varchar(12), @NotReleasedCount) + ' "Not Released" ' + dbo.CheckPlural(@NotReleasedCount, 'dataset', 'datasets') + ': ' + @list
+    If @showDebugMessages > 1
+    Begin
+        SELECT *
+        FROM #TD
+        ORDER BY Dataset_Num
+    End
+            
+    ---------------------------------------------------
+    -- Make sure none of the datasets has a rating of -5 (Not Released)
+    ---------------------------------------------------
+    --
+    Declare @list varchar(4000)
+    Declare @NotReleasedCount int = 0
+    
+    SELECT @NotReleasedCount = COUNT(*)
+    FROM #TD
+    WHERE DS_Rating = -5
+    
+    If @NotReleasedCount > 0
+    Begin
+        Set @list = ''
+        
+        SELECT @list = @list + Dataset_Num + ', '
+        FROM #TD
+        WHERE DS_Rating = -5
+        ORDER BY Dataset_Num
+        
+        -- Remove the trailing comma If the length is less than 400 characters, otherwise truncate
+        If Len(@list) < 400
+            Set @list = Left(@list, Len(@list)-1)
+        Else
+            Set @list = Left(@list, 397) + '...'
+        
+        If @autoRemoveNotReleasedDatasets = 0
+        Begin
+            if @NotReleasedCount = 1
+                set @message = 'dataset is "Not Released": ' + @list
+            else
+                set @message = Convert(varchar(12), @NotReleasedCount) + ' datasets are "Not Released": ' + @list
+                
+            if @showDebugMessages <> 0
+                print @message
+                
+            return 50101
+        End
+        Else
+        Begin
+            set @message = 'Skipped ' + Convert(varchar(12), @NotReleasedCount) + ' "Not Released" ' + dbo.CheckPlural(@NotReleasedCount, 'dataset', 'datasets') + ': ' + @list
 
-			if @showDebugMessages <> 0
-				print @message
-			
-			DELETE FROM #TD
-			WHERE DS_Rating = -5
-			
-		End
-	End
-	
-	---------------------------------------------------
-	-- Verify that datasets in list all exist
-	---------------------------------------------------
-	--
-	set @list = null
-	--
-	SELECT @list = Coalesce(@list + Dataset_Num + ', ', Dataset_Num)
-	FROM #TD
-	WHERE Dataset_ID IS NULL
-	--
-	SELECT @myError = @@error, @myRowCount = @@rowcount
-	--
-	if @myError <> 0
-	begin
-		set @message = 'Error checking dataset existence'
-		if @showDebugMessages <> 0
-			print @message
+            if @showDebugMessages <> 0
+                print @message
+            
+            DELETE FROM #TD
+            WHERE DS_Rating = -5
+            
+        End
+    End
+    
+    ---------------------------------------------------
+    -- Verify that datasets in list all exist
+    ---------------------------------------------------
+    --
+    set @list = null
+    --
+    SELECT @list = Coalesce(@list + Dataset_Num + ', ', Dataset_Num)
+    FROM #TD
+    WHERE Dataset_ID IS NULL
+    --
+    SELECT @myError = @@error, @myRowCount = @@rowcount
+    --
+    if @myError <> 0
+    begin
+        set @message = 'Error checking dataset existence'
+        if @showDebugMessages <> 0
+            print @message
 
-		return 51007
-	end
-	--
-	if IsNull(@list, '') <> ''
-	begin
-		set @message = 'The following datasets were not in the database: ' + @list
-		if @showDebugMessages <> 0
-			print @message
+        return 51007
+    end
+    --
+    if IsNull(@list, '') <> ''
+    begin
+        set @message = 'The following datasets were not in the database: ' + @list
+        if @showDebugMessages <> 0
+            print @message
 
-		return 51007
-	end	
+        return 51007
+    end    
 
-	---------------------------------------------------
-	-- Verify state of datasets
-	-- If @allowNewDatasets is 0, they must all have state Complete
-	-- If @allowNewDatasets is non-zero, we also allow New and Capture In Progress datasets
-	---------------------------------------------------
-	--
-	set @list = null
-	--
-	SELECT @list = Coalesce(@list + Dataset_Num + ', ', Dataset_Num)
-	FROM #TD
-	WHERE (@allowNewDatasets = 0 AND DS_state_ID <> 3) OR
-	      (@allowNewDatasets > 0 AND DS_state_ID NOT IN (1,2,3))
-	--
-	SELECT @myError = @@error, @myRowCount = @@rowcount
-	--
-	if @myError <> 0
-	begin
-		set @message = 'Error checking dataset state'
-		if @showDebugMessages <> 0
-			print @message
+    ---------------------------------------------------
+    -- Verify state of datasets
+    -- If @allowNewDatasets is 0, they must all have state Complete
+    -- If @allowNewDatasets is non-zero, we also allow New and Capture In Progress datasets
+    ---------------------------------------------------
+    --
+    set @list = null
+    --
+    SELECT @list = Coalesce(@list + Dataset_Num + ', ', Dataset_Num)
+    FROM #TD
+    WHERE (@allowNewDatasets = 0 AND DS_state_ID <> 3) OR
+          (@allowNewDatasets > 0 AND DS_state_ID NOT IN (1,2,3))
+    --
+    SELECT @myError = @@error, @myRowCount = @@rowcount
+    --
+    if @myError <> 0
+    begin
+        set @message = 'Error checking dataset state'
+        if @showDebugMessages <> 0
+            print @message
 
-		return 51007
-	end
-	--
-	if IsNull(@list, '') <> ''
-	begin
-		set @message = 'The following datasets were not in correct state: ' + @list
-		if @showDebugMessages <> 0
-			print @message
+        return 51007
+    end
+    --
+    if IsNull(@list, '') <> ''
+    begin
+        set @message = 'The following datasets were not in correct state: ' + @list
+        if @showDebugMessages <> 0
+            print @message
 
-		return 51007
-	end	
+        return 51007
+    end    
 
-	---------------------------------------------------
-	-- Verify rating of datasets
-	---------------------------------------------------
-	--
-	set @list = null
-	--
-	SELECT @list = Coalesce(@list + Dataset_Num + ', ', Dataset_Num)
-	FROM #TD
-	WHERE (DS_rating IN (-1, -2))
-	--
-	SELECT @myError = @@error, @myRowCount = @@rowcount
-	--
-	if @myError <> 0
-	begin
-		set @message = 'Error checking dataset rating'
-		if @showDebugMessages <> 0
-			print @message
+    ---------------------------------------------------
+    -- Verify rating of datasets
+    ---------------------------------------------------
+    --
+    set @list = null
+    --
+    SELECT @list = Coalesce(@list + Dataset_Num + ', ', Dataset_Num)
+    FROM #TD
+    WHERE (DS_rating IN (-1, -2))
+    --
+    SELECT @myError = @@error, @myRowCount = @@rowcount
+    --
+    if @myError <> 0
+    begin
+        set @message = 'Error checking dataset rating'
+        if @showDebugMessages <> 0
+            print @message
 
-		return 51007
-	end
-	--
-	if IsNull(@list, '') <> ''
-	begin
-		set @message = 'The following datasets have a rating of -1 (No Data) or -2 (Data Files Missing): ' + @list
-		if @showDebugMessages <> 0
-			print @message
+        return 51007
+    end
+    --
+    if IsNull(@list, '') <> ''
+    begin
+        set @message = 'The following datasets have a rating of -1 (No Data) or -2 (Data Files Missing): ' + @list
+        if @showDebugMessages <> 0
+            print @message
 
-		return 51007
-	end	
+        return 51007
+    end    
 
-	---------------------------------------------------
-	-- Do not allow high res datasets to be mixed with low res datasets
-	-- (though this is OK if the tool is MSXML_Gen)
-	---------------------------------------------------
-	--
-	Declare @HMSCount int = 0
-	Declare @MSCount int = 0
-	
-	SELECT @HMSCount = COUNT(*)
-	FROM #TD
-	WHERE Dataset_Type LIKE 'hms%' OR
-	      Dataset_Type LIKE 'ims-hms%'
+    ---------------------------------------------------
+    -- Do not allow high res datasets to be mixed with low res datasets
+    -- (though this is OK if the tool is MSXML_Gen)
+    ---------------------------------------------------
+    --
+    Declare @HMSCount int = 0
+    Declare @MSCount int = 0
+    
+    SELECT @HMSCount = COUNT(*)
+    FROM #TD
+    WHERE Dataset_Type LIKE 'hms%' OR
+          Dataset_Type LIKE 'ims-hms%'
 
 
-	SELECT @MSCount = COUNT(*)
-	FROM #TD
-	WHERE Dataset_Type LIKE 'MS%' OR
-	      Dataset_Type LIKE 'IMS-MS%'
-	
-	If @HMSCount > 0 And @MSCount > 0 And Not @toolName in ('MSXML_Gen')
-	Begin		
-		Set @message = 'You cannot mix high-res MS datasets with low-res datasets; create separate analysis job requests. You currently have ' + Convert(varchar(12), @HMSCount) + ' high res (HMS) and ' + Convert(varchar(12), @MSCount) + ' low res (MS)'
-		if @showDebugMessages <> 0
-			print @message
+    SELECT @MSCount = COUNT(*)
+    FROM #TD
+    WHERE Dataset_Type LIKE 'MS%' OR
+          Dataset_Type LIKE 'IMS-MS%'
+    
+    If @HMSCount > 0 And @MSCount > 0 And Not @toolName in ('MSXML_Gen')
+    Begin        
+        Set @message = 'You cannot mix high-res MS datasets with low-res datasets; create separate analysis job requests. You currently have ' + Convert(varchar(12), @HMSCount) + ' high res (HMS) and ' + Convert(varchar(12), @MSCount) + ' low res (MS)'
+        if @showDebugMessages <> 0
+            print @message
 
-		return 51009
-	End	
-		
-	return 0
+        return 51009
+    End    
+        
+    return 0
 
 
 GO
