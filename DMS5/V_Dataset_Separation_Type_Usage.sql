8ed1a833ebe0fe8c05225ec883027d81e6170dc5
alchemistmatt <matthew.monroe@pnnl.gov>
2019-11-06 16:06:18 -0800

    Add new view and include Sample Type existing separation type usage views

--- DMS5/V_Dataset_Separation_Type_Usage.sql
+++ DMS5/V_Dataset_Separation_Type_Usage.sql
@@ -4,7 +4,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_Dataset_Separation_Type_Usage]
 AS
 
@@ -12,9 +11,12 @@ SELECT U.Usage_Last12Months AS [Usage Last 12 Months],
        SS.SS_name AS [Separation Type],
        SS.Sep_Group AS [Separation Group],
        SS.SS_comment AS [Separation Type Comment],
+       SampType.Name AS [Sample Type],
        U.Usage_AllYears AS [Dataset Usage All Years],
        U.Most_Recent_Use AS [Most Recent Use]
 FROM T_Secondary_Sep SS
+     INNER JOIN T_Secondary_Sep_SampleType SampType
+       ON SS.SampleType_ID = SampType.SampleType_ID
      LEFT OUTER JOIN T_Secondary_Sep_Usage U
        ON U.SS_ID = SS.SS_ID
 WHERE (SS.SS_active <> 0)
--- DMS5/V_Separation_Type_List_Report.sql
+++ DMS5/V_Separation_Type_List_Report.sql
@@ -1,27 +1,26 @@
-/****** Object:  View [dbo].[V_Dataset_Separation_Type_Usage_2] ******/
+/****** Object:  View [dbo].[V_Separation_Type_List_Report] ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE VIEW [dbo].[V_Dataset_Separation_Type_Usage_2]
+CREATE VIEW [dbo].[V_Separation_Type_List_Report]
 AS
-SELECT 'x' AS Sel,
-       U.Usage_Last12Months AS [Usage Last 12 Months],
-       SS.SS_name AS [Separation Type],
+
+SELECT SS.SS_name AS [Separation Type],
        SS.Sep_Group AS [Separation Group],
        SS.SS_comment AS [Separation Type Comment],
        SampType.Name AS [Sample Type],
+       U.Usage_Last12Months AS [Usage Last 12 Months],
        U.Usage_AllYears AS [Dataset Usage All Years],
-       U.Most_Recent_Use AS [Most Recent Use]
+       U.Most_Recent_Use AS [Most Recent Use],
+       SS.SS_active As Active,
+       SS.SS_ID As ID
 FROM T_Secondary_Sep SS
      INNER JOIN T_Secondary_Sep_SampleType SampType
        ON SS.SampleType_ID = SampType.SampleType_ID
      LEFT OUTER JOIN T_Secondary_Sep_Usage U
        ON U.SS_ID = SS.SS_ID
-WHERE (SS.SS_active <> 0)
 
 
-GO
-GRANT VIEW DEFINITION ON [dbo].[V_Dataset_Separation_Type_Usage_2] TO [DDL_Viewer] AS [dbo]
 GO
