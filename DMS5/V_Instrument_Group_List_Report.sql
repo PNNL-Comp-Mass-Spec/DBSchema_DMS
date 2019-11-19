2f6f7ce861ef6ad4dc26d4faa2c8452f27f1899d
alchemistmatt <matthew.monroe@pnnl.gov>
2019-11-18 16:56:34 -0800

    Update views to filter instruments displayed for each group

--- DMS5/V_Instrument_Group_List_Report.sql
+++ DMS5/V_Instrument_Group_List_Report.sql
@@ -4,15 +4,15 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE view [dbo].[V_Instrument_Group_List_Report] as
+CREATE VIEW [dbo].[V_Instrument_Group_List_Report] as
 SELECT I.IN_Group AS Instrument_Group,
        I.Usage,
        I.Comment,
        I.Active,
-	   I.Sample_Prep_Visible,
+       I.Sample_Prep_Visible,
        I.Allocation_Tag,
        ISNULL(DT.DST_name, '') AS [Default Dataset Type],
-       dbo.GetInstrumentGroupMembershipList(I.IN_Group) AS Instruments,
+       dbo.GetInstrumentGroupMembershipList(I.IN_Group, 0, 0) AS Instruments,
        dbo.GetInstrumentGroupDatasetTypeList(I.IN_Group) AS Allowed_Dataset_Types
 FROM dbo.T_Instrument_Group I
      LEFT OUTER JOIN dbo.T_DatasetTypeName DT
--- DMS5/V_Instrument_Group_PickList.sql
+++ DMS5/V_Instrument_Group_PickList.sql
@@ -4,19 +4,30 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE view [dbo].[V_Instrument_Group_PickList] as
-SELECT I.IN_Group AS Instrument_Group,
-       I.Usage,
-       dbo.GetInstrumentGroupMembershipList(I.IN_Group) AS Instruments,
-       I.Comment,
-       dbo.GetInstrumentGroupDatasetTypeList(I.IN_Group) AS Allowed_Dataset_Types,
-	   I.Sample_Prep_Visible,
-       I.Requested_Run_Visible
-FROM dbo.T_Instrument_Group I
-     LEFT OUTER JOIN dbo.T_DatasetTypeName DT
-       ON I.Default_Dataset_Type = DT.DST_Type_ID
-WHERE I.Active > 0
-
+CREATE VIEW [dbo].[V_Instrument_Group_PickList] As
+SELECT Instrument_Group,
+       Usage,
+       Instruments,
+       Comment,
+       Allowed_Dataset_Types,
+       Sample_Prep_Visible,
+       Requested_Run_Visible,
+       CASE WHEN Instruments = '' 
+       THEN Instrument_Group + ' (no active instruments)' 
+       ELSE Instrument_Group + ' (' + Instruments + ')' 
+       END As Instrument_Group_and_Instruments
+From (  SELECT I.IN_Group AS Instrument_Group,
+               I.Usage,
+               dbo.GetInstrumentGroupMembershipList(I.IN_Group, 1, 64) AS Instruments,
+               I.Comment,
+               dbo.GetInstrumentGroupDatasetTypeList(I.IN_Group) AS Allowed_Dataset_Types,
+               I.Sample_Prep_Visible,
+               I.Requested_Run_Visible
+        FROM dbo.T_Instrument_Group I
+             LEFT OUTER JOIN dbo.T_DatasetTypeName DT
+               ON I.Default_Dataset_Type = DT.DST_Type_ID
+        WHERE I.Active > 0
+    ) LookupQ
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Instrument_Group_PickList] TO [DDL_Viewer] AS [dbo]
