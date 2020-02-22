16aebf40d4b5aa747c92f2b01568afa9dc7d063b
alchemistmatt <matthew.monroe@pnnl.gov>
2020-02-22 01:03:40 -0800

    2020-02-22 auto-commit DMS_Capture

---
+++ DMS_Capture/udfDecimalToDouble.sql
@@ -0,0 +1,30 @@
+/****** Object:  UserDefinedFunction [dbo].[udfDecimalToDouble] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[udfDecimalToDouble]
+/****************************************************	
+**	Test udf
+**
+**	Auth:	mem
+**	Date:	02/20/2020
+**  
+****************************************************/
+(
+	@value Decimal(9,5)
+)
+RETURNS float
+AS
+BEGIN
+    Declare @newValue Float
+
+    Set @newValue = @value
+
+	RETURN  @value
+END
+
+
+
+GO
