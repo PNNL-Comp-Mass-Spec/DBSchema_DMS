b3d0f00a696ffdd74268b87c9d501b5d1a82ea23
alchemistmatt <matthew.monroe@pnnl.gov>
2020-09-09 11:17:29 -0700

    When @AutoDefineStoragePath is positive, raise an error if any of the paths are \ or /

--- DMS5/ValidateAutoStoragePathParams.sql
+++ DMS5/ValidateAutoStoragePathParams.sql
@@ -3,7 +3,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE ValidateAutoStoragePathParams
+
+CREATE PROCEDURE [dbo].[ValidateAutoStoragePathParams]
 /****************************************************
 **
 **	Desc:	Validates that the Auto storage path parameters are correct
@@ -14,6 +15,7 @@ CREATE PROCEDURE ValidateAutoStoragePathParams
 **	Date:	05/13/2011 mem - Initial version
 **			07/05/2016 mem - Archive path is now aurora.emsl.pnl.gov\archive\dmsarch\
 **			09/02/2016 mem - Archive path is now adms.emsl.pnl.gov\dmsarch\
+**          09/08/2020 mem - When @AutoDefineStoragePath is positive, raise an error if any of the paths are \ or /
 **    
 *****************************************************/
 (
@@ -28,26 +30,31 @@ CREATE PROCEDURE ValidateAutoStoragePathParams
 )
 AS
 	Set NoCount On
-
-
-	If @AutoDefineStoragePath = 1	 
+    
+    Set @AutoSPVolNameClient = LTrim(RTrim(IsNull(@AutoSPVolNameClient, '')))
+    Set @AutoSPVolNameServer = LTrim(RTrim(IsNull(@AutoSPVolNameServer, '')))
+    Set @AutoSPPathRoot = LTrim(RTrim(IsNull(@AutoSPPathRoot, '')))
+    Set @AutoSPArchiveServerName = LTrim(RTrim(IsNull(@AutoSPArchiveServerName, '')))
+    Set @AutoSPArchivePathRoot = LTrim(RTrim(IsNull(@AutoSPArchivePathRoot, '')))
+    Set @AutoSPArchiveSharePathRoot = LTrim(RTrim(IsNull(@AutoSPArchiveSharePathRoot, '')))
+    
+	If @AutoDefineStoragePath > 0	 
 	Begin
-		If IsNull(@AutoSPVolNameClient, '') = ''
-			RAISERROR ('Auto Storage VolNameClient cannot be blank', 11, 4)
-		If IsNull(@AutoSPVolNameServer, '') = ''
-			RAISERROR ('Auto Storage VolNameServer cannot be blank', 11, 4)
-		If IsNull(@AutoSPPathRoot, '') = ''
-			RAISERROR ('Auto Storage Path Root cannot be blank', 11, 4)
-		If IsNull(@AutoSPArchiveServerName, '') = ''
-			RAISERROR ('Auto Storage Archive Server Name cannot be blank', 11, 4)
-		If IsNull(@AutoSPArchivePathRoot, '') = ''
-			RAISERROR ('Auto Storage Archive Path Root cannot be blank', 11, 4)
-		If IsNull(@AutoSPArchiveSharePathRoot, '') = ''
-			RAISERROR ('Auto Storage Archive Share Path Root cannot be blank', 11, 4)
-		   
+		If @AutoSPVolNameClient IN ('', '\', '/')
+			RAISERROR ('Auto Storage VolNameClient cannot be blank or \ or /', 11, 4)
+		If @AutoSPVolNameServer IN ('', '\', '/')
+			RAISERROR ('Auto Storage VolNameServer cannot be blank or \ or /', 11, 4)
+		If @AutoSPPathRoot IN ('', '\', '/')
+			RAISERROR ('Auto Storage Path Root cannot be blank or \ or /', 11, 4)
+		If @AutoSPArchiveServerName IN ('', '\', '/')
+			RAISERROR ('Auto Storage Archive Server Name cannot be blank or \ or /', 11, 4)
+		If @AutoSPArchivePathRoot IN ('', '\', '/')
+			RAISERROR ('Auto Storage Archive Path Root cannot be blank or \ or /', 11, 4)
+		If @AutoSPArchiveSharePathRoot IN ('', '\', '/')
+			RAISERROR ('Auto Storage Archive Share Path Root cannot be blank or \ or /', 11, 4)		   
 	End
 	
-	If IsNull(@AutoSPVolNameClient, '') <> ''
+	If @AutoSPVolNameClient <> ''
 	Begin
 		If @AutoSPVolNameClient Not Like '\\%'
 			RAISERROR ('Auto Storage VolNameClient should be a network share, for example: \\Proto-3\', 11, 4)
@@ -56,7 +63,7 @@ AS
 			RAISERROR ('Auto Storage VolNameClient must end in a backslash, for example: \\Proto-3\', 11, 4)
 	End
 	
-	If IsNull(@AutoSPVolNameServer, '') <> ''
+	If @AutoSPVolNameServer <> ''
 	Begin
 		If @AutoSPVolNameServer Not Like '[A-Z]:%'
 			RAISERROR ('Auto Storage VolNameServer should be a drive letter, for example: G:\', 11, 4)
@@ -65,19 +72,18 @@ AS
 			RAISERROR ('Auto Storage VolNameServer must end in a backslash, for example: G:\', 11, 4)
 	End
 	
-	If IsNull(@AutoSPArchivePathRoot, '') <> ''
+	If @AutoSPArchivePathRoot <> ''
 	Begin
 		If @AutoSPArchivePathRoot Not Like '/%'
 			RAISERROR ('Auto Storage Archive Path Root should be a linux path, for example: /archive/dmsarch/Broad_Orb1', 11, 4)
 		
 	End
 	
-	If IsNull(@AutoSPArchiveSharePathRoot, '') <> ''
+	If @AutoSPArchiveSharePathRoot <> ''
 	Begin
 		If @AutoSPArchiveSharePathRoot Not Like '\\%'
 			RAISERROR ('Auto Storage Archive Share Path Root should be a network share, for example: \\adms.emsl.pnl.gov\dmsarch\VOrbiETD01', 11, 4)
 	End	
-
 
 	return 0
 
