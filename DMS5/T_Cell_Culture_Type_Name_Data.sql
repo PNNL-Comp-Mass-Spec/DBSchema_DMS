94f9b2456708eb8e9dd47fe33ae65affa442ca79
alchemistmatt <matthew.monroe@pnnl.gov>
2020-10-31 01:03:27 -0700

    2020-10-31 auto-commit DMS5

--- DMS5/T_Cell_Culture_Type_Name_Data.sql
+++ DMS5/T_Cell_Culture_Type_Name_Data.sql
@@ -1,6 +1,7 @@
 /****** Object:  Table [T_Cell_Culture_Type_Name] ******/
-/****** RowCount: 10 ******/
+/****** RowCount: 17 ******/
 SET IDENTITY_INSERT [T_Cell_Culture_Type_Name] ON
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (109,'Air')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (105,'Biofluid')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (104,'Community')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (2,'Eukaryote')
@@ -8,7 +9,13 @@ INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (107,'Genetically modif
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (106,'Genetically modified prokaryote')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (108,'Genetically modified virus')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (103,'Metabolite standards')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (114,'Miscellaneous or artificial')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (113,'Plant-associated')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (1,'Prokaryote')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (102,'Protein/peptide standards')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (112,'Sediment')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (110,'Soil')
 INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (101,'Viral')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (115,'Wastewater/sludge')
+INSERT INTO [T_Cell_Culture_Type_Name] (ID, Name) VALUES (111,'Water')
 SET IDENTITY_INSERT [T_Cell_Culture_Type_Name] OFF
