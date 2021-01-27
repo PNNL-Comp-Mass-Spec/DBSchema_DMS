fed337c636f90d29771ed8bdacca724a8ad46228
alchemistmatt <matthew.monroe@pnnl.gov>
2021-01-26 20:46:19 -0800

    Only count active items when counting the number of items in a container

--- DMS5/V_Material_Containers_Detail_Report.sql
+++ DMS5/V_Material_Containers_Detail_Report.sql
@@ -4,70 +4,67 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_Material_Containers_Detail_Report] AS 
-SELECT  Container ,
-        [Type] ,
-        [Location] ,
-        Items ,
-        Comment,
-        Freezer ,
-        dbo.GetMaterialContainerCampaignList(Container_ID, Items) AS Campaigns ,
-        Barcode ,
-        Created ,
-        [Status] ,
-        Researcher ,
-        Files      
-FROM (
-    SELECT  MC.Tag AS Container ,
-            MC.[Type] ,
-            ML.Tag AS [Location] ,
-            COUNT(ContentsQ.Material_ID) AS Items ,
-            MC.Comment ,
-            ML.Freezer_Tag AS Freezer,
-            MC.Barcode ,
-            MC.Created ,
-            MC.[Status] ,
-            MC.Researcher,
-            TFA.Files ,
-            MC.ID AS Container_ID
-    FROM    T_Material_Containers MC
-            LEFT OUTER JOIN ( SELECT    CC_Container_ID AS Container_ID ,
-                                        CC_ID AS Material_ID
-                              FROM      T_Cell_Culture
-                              UNION
-                              SELECT    EX_Container_ID AS Container_ID ,
-                                        Exp_ID AS Material_ID
-                              FROM      T_Experiments
-                              UNION
-                              SELECT    Container_ID AS Container_ID ,
-                                        Compound_ID AS Material_ID
-                              FROM      T_Reference_Compound
-                            ) AS ContentsQ ON ContentsQ.Container_ID = MC.ID
-            LEFT OUTER JOIN ( SELECT    [Entity_ID] ,
-                                        COUNT(*) AS Files
-                              FROM      T_File_Attachment
-                              WHERE     Entity_Type = 'material_container' AND 
-                                        Active > 0 AND
-                                        -- Exclude the staging containers because they have thousands of items, 
-                                        -- leading to slow query times on the Material Container Detail Report
-                                        -- when this query looks for a file attachment associated with every container in the staging location
-                                        Entity_ID NOT IN ('na', 'Staging', 'Met_Staging', '-80_Staging')
-                              GROUP BY  [Entity_ID]
-                            ) AS TFA ON TFA.Entity_ID = MC.Tag                        
-            INNER JOIN T_Material_Locations ML ON MC.Location_ID = ML.ID
-    GROUP BY MC.Tag ,
-             MC.[Type] ,
-             ML.Tag ,
-             MC.Comment ,
-             MC.Barcode ,
-             MC.Created ,
-             MC.[Status] ,
-             MC.Researcher,
-             ML.Freezer_Tag,
-             TFA.Files,
-             MC.ID
-    ) ContainerQ
+SELECT Container,
+       [Type],
+       [Location],
+       Items,
+       [Comment],
+       Freezer,
+       dbo.GetMaterialContainerCampaignList(Container_ID, Items) AS Campaigns,
+       Barcode,
+       Created,
+       [Status],
+       Researcher,
+       Files
+FROM ( SELECT MC.Tag AS Container,
+              MC.[Type],
+              ML.Tag AS [Location],
+              COUNT(ContentsQ.Material_ID) AS Items,
+              MC.[Comment],
+              ML.Freezer_Tag AS Freezer,
+              MC.Barcode,
+              MC.Created,
+              MC.[Status],
+              MC.Researcher,
+              TFA.Files,
+              MC.ID AS Container_ID
+       FROM T_Material_Containers MC
+            LEFT OUTER JOIN (SELECT CC_Container_ID AS Container_ID,
+                                    CC_ID AS Material_ID
+                             FROM T_Cell_Culture
+                             WHERE CC_Material_Active = 'Active'
+                             UNION
+                             SELECT EX_Container_ID AS Container_ID,
+                                    Exp_ID AS Material_ID
+                             FROM T_Experiments
+                             WHERE Ex_Material_Active = 'Active'
+                             UNION
+                             SELECT Container_ID AS Container_ID,
+                                    Compound_ID AS Material_ID
+                             FROM T_Reference_Compound
+                             WHERE Active > 0 
+                            ) AS ContentsQ
+              ON ContentsQ.Container_ID = MC.ID
+            LEFT OUTER JOIN ( SELECT [Entity_ID],
+                                     COUNT(*) AS Files
+                              FROM T_File_Attachment
+                              WHERE Entity_Type = 'material_container' AND
+                                    Active > 0 AND
+                                    -- Exclude the staging containers because they have thousands of items, 
+
+                                    -- leading to slow query times on the Material Container Detail Report
+
+                                    -- when this query looks for a file attachment associated with every container in the staging location
+                                    Entity_ID NOT IN ('na', 'Staging', 'Met_Staging', '-80_Staging')
+                              GROUP BY [Entity_ID] 
+                            ) AS TFA
+              ON TFA.Entity_ID = MC.Tag
+            INNER JOIN T_Material_Locations ML
+              ON MC.Location_ID = ML.ID
+       GROUP BY MC.Tag, MC.[Type], ML.Tag, MC.[Comment], MC.Barcode, MC.Created, MC.[Status],
+                MC.Researcher, ML.Freezer_Tag, TFA.Files, MC.ID 
+     ) ContainerQ
 
 
 GO
--- DMS5/V_Material_Containers_List_Report.sql
+++ DMS5/V_Material_Containers_List_Report.sql
@@ -5,60 +5,60 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE VIEW [dbo].[V_Material_Containers_List_Report] AS 
-SELECT  Container,
-        [Type],
-        [Location],
-        Items,
-        FileCount AS Files,
-        Comment,
-        [Status],
-        'New Biomaterial' AS [Action],
-        Created,
-        dbo.GetMaterialContainerCampaignList([#ID], Items) AS Campaigns,
-        Researcher,
-        [#ID]
-FROM    ( SELECT    MC.Tag AS Container,
-                    MC.[Type],
-                    ML.Tag AS [Location],
-                    COUNT(ContentsQ.Material_ID) AS Items,
-                    MC.Comment,
-                    MC.[Status],
-                    -- Unused: MC.Barcode,
-                    MC.Created,
-                    MC.ID AS [#ID],
-                    MC.Researcher,
-                    TFA.FileCount
-          FROM      T_Material_Containers AS MC
-                    INNER JOIN T_Material_Locations AS ML ON MC.Location_ID = ML.ID
-                    LEFT OUTER JOIN ( SELECT    CC_Container_ID AS Container_ID,
-                                                CC_ID AS Material_ID
-                                      FROM      T_Cell_Culture
-                                      UNION
-                                      SELECT    EX_Container_ID AS Container_ID,
-                                                Exp_ID AS Material_ID
-                                      FROM      T_Experiments
-                                      UNION
-                                      SELECT    Container_ID AS Container_ID,
-                                                Compound_ID AS Material_ID
-                                      FROM      T_Reference_Compound
-                                    ) AS ContentsQ ON ContentsQ.Container_ID = MC.ID
-                    LEFT OUTER JOIN ( SELECT    Entity_ID,
-                                                COUNT(*) AS FileCount
-                                      FROM      T_File_Attachment
-                                      WHERE     ( Entity_Type = 'material_container' )
-                                                AND ( Active > 0 )
-                                      GROUP BY  Entity_ID
-                                    ) AS TFA ON TFA.Entity_ID = MC.Tag
-          GROUP BY MC.Tag,
-                   MC.[Type],
-                   ML.Tag,
-                   MC.Comment,
-                   MC.Created,
-                   MC.[Status],
-                   MC.ID,
-                   MC.Researcher,
-                   TFA.FileCount
-        ) AS ContainerQ
+SELECT Container,
+       [Type],
+       [Location],
+       Items,
+       FileCount AS Files,
+       [Comment],
+       [Status],
+       'New Biomaterial' AS [Action],
+       Created,
+       dbo.GetMaterialContainerCampaignList([#ID], Items) AS Campaigns,
+       Researcher,
+       [#ID]
+FROM ( SELECT MC.Tag AS Container,
+              MC.[Type],
+              ML.Tag AS [Location],
+              COUNT(ContentsQ.Material_ID) AS Items,
+              MC.[Comment],
+              MC.[Status],
+              -- Unused: MC.Barcode,
+              MC.Created,
+              MC.ID AS [#ID],
+              MC.Researcher,
+              TFA.FileCount
+       FROM T_Material_Containers AS MC
+            INNER JOIN T_Material_Locations AS ML
+              ON MC.Location_ID = ML.ID
+            LEFT OUTER JOIN (SELECT CC_Container_ID AS Container_ID,
+                                    CC_ID AS Material_ID
+                             FROM T_Cell_Culture
+                             WHERE CC_Material_Active = 'Active'
+                             UNION
+                             SELECT EX_Container_ID AS Container_ID,
+                                    Exp_ID AS Material_ID
+                             FROM T_Experiments
+                             WHERE Ex_Material_Active = 'Active'
+                             UNION
+                             SELECT Container_ID AS Container_ID,
+                                    Compound_ID AS Material_ID
+                             FROM T_Reference_Compound
+                             WHERE Active > 0 
+                            ) AS ContentsQ
+              ON ContentsQ.Container_ID = MC.ID
+            LEFT OUTER JOIN ( SELECT Entity_ID,
+                                     COUNT(*) AS FileCount
+                              FROM T_File_Attachment
+                              WHERE Entity_Type = 'material_container' 
+                                    AND
+                                    Active > 0
+                              GROUP BY Entity_ID 
+                             ) AS TFA
+              ON TFA.Entity_ID = MC.Tag
+       GROUP BY MC.Tag, MC.[Type], ML.Tag, MC.[Comment], MC.Created, MC.[Status], 
+                MC.ID, MC.Researcher, TFA.FileCount 
+     ) AS ContainerQ
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Material_Containers_List_Report] TO [DDL_Viewer] AS [dbo]
