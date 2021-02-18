4e673703991b7aab2caa170b1c3b29a8cf6911dc
alchemistmatt <matthew.monroe@pnnl.gov>
2021-02-18 15:12:39 -0800

    Add option to format instruments as a vertical bar separated list of instrument name and instrument ID

--- DMS5/GetInstrumentGroupMembershipList.sql
+++ DMS5/GetInstrumentGroupMembershipList.sql
@@ -16,49 +16,63 @@ CREATE FUNCTION [dbo].[GetInstrumentGroupMembershipList]
 **  Auth:   grk
 **  Date:   08/30/2010 grk - Initial version
 **          11/18/2019 mem - Add parameters @activeOnly and @maximumLength
+**          02/18/2021 mem - Add @activeOnly=2 which formats the instruments as a vertical bar separated list of instrument name and instrument ID
 **    
 *****************************************************/
 (
     @instrumentGroup varchar(64),
-    @activeOnly tinyint,             -- 0 for all instruments, 1 for only active instruments
-    @maximumLength int = 64          -- Maximum length of the returned list of instruments; if 0, all instruments, sorted alphabetically; if non-zero, sort by descending instrument iD
+    @activeOnly tinyint,             -- 0 for all instruments, 1 for only active instruments, 2 to format the instruments as a vertical bar separated list of instrument name and ID (see comments below)
+    @maximumLength int = 64          -- Maximum length of the returned list of instruments; if 0, all instruments, sorted alphabetically; if non-zero, sort by descending instrument ID
 )
 RETURNS varchar(4000)
 AS
     BEGIN
-        Declare @list varchar(4000)
+        Declare @list varchar(4000) = ''
         
-        Set @list = ''
-        
-        If @maximumLength <= 0
+        -- When @activeOnly is 2, the instrument list will be in the form:
+        -- InstrumentName:InstrumentID|InstrumentName:InstrumentID|InstrumentName:InstrumentID
+        -- Additionally, if the instrument is inactive of offsite, the instrument name will show that in parentheses, with inactive taking precedence
+        -- This is used to format instrument on the Instrument Group Detail Report page
+        -- https://dms2.pnl.gov/instrument_group/show/VelosOrbi
+
+        Declare @delimiter VARCHAR(4)
+
+        If @activeOnly = 2
         Begin
-            SELECT @list = @list + CASE WHEN @list = '' THEN '' ELSE ', ' END + IN_name     
-            FROM   T_Instrument_Name
-            WHERE  IN_Group = @InstrumentGroup And
-                   (@activeOnly = 0 Or IN_status <> 'inactive') And
-                   (@activeOnly = 0 Or IN_operations_role <> 'Offsite')
-            ORDER BY IN_name ASC
+            Set @delimiter = '|'
         End
         Else
         Begin
-            SELECT @list = @list + CASE WHEN @list = '' THEN '' ELSE ', ' END + IN_name     
-            FROM   T_Instrument_Name
-            WHERE  IN_Group = @InstrumentGroup And
-                   (@activeOnly = 0 Or IN_status <> 'inactive') And
-                   (@activeOnly = 0 Or IN_operations_role <> 'Offsite')
-            ORDER BY IN_name ASC
+            Set @delimiter  = ', '
+        End
 
-            If Len(@list) > @maximumLength
+        If @maximumLength > 0 And @maximumLength < 10
+        Begin
+            Set @maximumLength = 10
+        End
+
+        SELECT @list = @list + CASE WHEN @list = '' THEN '' ELSE @delimiter END + 
+                       IN_name + 
+                       CASE When @activeOnly = 2 AND IN_Status = 'inactive' THEN ' (' + IN_status + ')' ELSE '' END +
+                       CASE When @activeOnly = 2 AND IN_Status <> 'inactive' AND IN_operations_role = 'Offsite' THEN ' (' + IN_operations_role + ')' ELSE '' END +
+                       CASE When @activeOnly = 2 THEN ':' + CAST(Instrument_ID as VARCHAR(12)) ELSE '' END
+        FROM   T_Instrument_Name
+        WHERE  IN_Group = @InstrumentGroup And
+                (@activeOnly In (0, 2) Or IN_status <> 'inactive') And
+                (@activeOnly In (0, 2) Or IN_operations_role <> 'Offsite')
+        ORDER BY IN_name ASC
+
+        If @maximumLength > 0 And Len(@list) > @maximumLength
+        Begin
+            Set @list = Rtrim(Substring(@list, 1, @maximumLength - 3))
+
+            If @list Like '%,'
             Begin
-                Set @list = Rtrim(Substring(@list, 1, 61))
-                If @list Like '%,'
-                Begin
-                    Set @list = Substring(@list, 1, Len(@list) - 1) + '...'
-                End
-                Else
-                Begin
-                    Set @list = @list + ' ...'
-                End
+                Set @list = Substring(@list, 1, Len(@list) - 1) + '...'
+            End
+            Else
+            Begin
+                Set @list = @list + ' ...'
             End
         End
 
