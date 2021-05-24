f574a051157b1144b4dfc313ae49b51fd6c03a68
alchemistmatt <matthew.monroe@pnnl.gov>
2021-05-24 15:58:32 -0700

    Use special treatment for all QC datasets when determining job holdoff due to a pending archive task

--- DMS5/V_GetAnalysisJobsForArchiveBusy.sql
+++ DMS5/V_GetAnalysisJobsForArchiveBusy.sql
@@ -4,8 +4,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
 CREATE VIEW [dbo].[V_GetAnalysisJobsForArchiveBusy]
 AS
 SELECT AJ.AJ_jobID, DS.Dataset_ID, DS.Dataset_Num, DS.DS_created, DS.DS_state_ID
@@ -15,13 +13,13 @@ FROM dbo.T_Analysis_Job AJ
      INNER JOIN dbo.T_Dataset DS
        ON AJ.AJ_datasetID = DS.Dataset_ID
 WHERE AJ.AJ_stateid IN (1,2,3,8) AND
-      ( (Dataset_Num Like 'QC_Shew%' AND DA.AS_state_ID = 7 And DA.AS_state_Last_Affected > DateAdd(Minute, -15, GetDate()))
+      ( (Dataset_Num Like 'QC[_]%' AND DA.AS_state_ID = 7 And DA.AS_state_Last_Affected > DateAdd(Minute, -15, GetDate()))
         OR
-        (Dataset_Num Not Like 'QC_Shew%' AND DA.AS_state_ID IN (1)    And DA.AS_state_Last_Affected > DateAdd(Minute, -180, GetDate()))
+        (Dataset_Num Not Like 'QC[_]%' AND DA.AS_state_ID IN (1)    And DA.AS_state_Last_Affected > DateAdd(Minute, -180, GetDate()))
 		OR
-		(Dataset_Num Not Like 'QC_Shew%' AND DA.AS_state_ID IN (7, 8) And DA.AS_state_Last_Affected > DateAdd(Minute, -120, GetDate()))
+		(Dataset_Num Not Like 'QC[_]%' AND DA.AS_state_ID IN (7, 8) And DA.AS_state_Last_Affected > DateAdd(Minute, -120, GetDate()))
 		OR
-		(Dataset_Num Not Like 'QC_Shew%' AND DA.AS_state_ID IN (2, 6) And DA.AS_state_Last_Affected > DateAdd(Minute,  -60, GetDate()))
+		(Dataset_Num Not Like 'QC[_]%' AND DA.AS_state_ID IN (2, 6) And DA.AS_state_Last_Affected > DateAdd(Minute,  -60, GetDate()))
       )
 
 GO
