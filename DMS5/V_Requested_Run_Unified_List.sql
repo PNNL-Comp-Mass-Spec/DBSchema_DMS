81825d2ddc6615d3abace8552bc1b7992267a74d
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-02 11:08:35 -0700

    Add batch name column

--- DMS5/V_Requested_Run_Unified_List.sql
+++ DMS5/V_Requested_Run_Unified_List.sql
@@ -4,14 +4,27 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE VIEW [dbo].[V_Requested_Run_Unified_List]  as
-SELECT     T_Requested_Run.RDS_BatchID AS BatchID, T_Requested_Run.RDS_Name AS Name, T_Requested_Run.RDS_Status AS Status, 
-                      T_Requested_Run.ID AS Request, T_Requested_Run.DatasetID AS Dataset_ID, T_Dataset.Dataset_Num AS Dataset, 
-                      T_Experiments.Experiment_Num AS Experiment, T_Requested_Run.Exp_ID AS Experiment_ID, T_Requested_Run.RDS_Block AS Block, 
-                      T_Requested_Run.RDS_Run_Order AS [Run Order]
-FROM         T_Requested_Run INNER JOIN
-                      T_Experiments ON T_Requested_Run.Exp_ID = T_Experiments.Exp_ID LEFT OUTER JOIN
-                      T_Dataset ON T_Requested_Run.DatasetID = T_Dataset.Dataset_ID  
+CREATE VIEW [dbo].[V_Requested_Run_Unified_List] 
+AS
+SELECT RR.ID AS Request,
+       RR.RDS_Name AS Name,
+       RR.RDS_BatchID AS BatchID,
+       RRB.Batch AS [Batch Name],
+       RR.DatasetID AS Dataset_ID,
+       D.Dataset_Num AS Dataset,
+       RR.Exp_ID AS Experiment_ID,
+       E.Experiment_Num AS Experiment,
+       RR.RDS_Status AS Status,
+       RR.RDS_Block AS [Block],
+       RR.RDS_Run_Order AS [Run Order]
+FROM T_Requested_Run RR
+     INNER JOIN T_Experiments E
+       ON RR.Exp_ID = E.Exp_ID
+     LEFT OUTER JOIN T_Dataset D
+       ON RR.DatasetID = D.Dataset_ID
+     LEFT OUTER JOIN T_Requested_Run_Batches RRB
+       ON RR.RDS_BatchID = RRB.ID
+
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Requested_Run_Unified_List] TO [DDL_Viewer] AS [dbo]
