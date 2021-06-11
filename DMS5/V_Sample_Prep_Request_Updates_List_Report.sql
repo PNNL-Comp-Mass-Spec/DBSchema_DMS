d4e93b9a6d4ffd92a480151e186977490f1feec4
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-10 21:56:05 -0700

    Update whitespace

--- DMS5/V_Sample_Prep_Request_Updates_List_Report.sql
+++ DMS5/V_Sample_Prep_Request_Updates_List_Report.sql
@@ -3,14 +3,23 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Sample_Prep_Request_Updates_List_Report
+
+CREATE VIEW [dbo].[V_Sample_Prep_Request_Updates_List_Report]
 AS
-SELECT     dbo.T_Sample_Prep_Request_Updates.Date_of_Change, dbo.T_Sample_Prep_Request_Updates.System_Account, dbo.T_Users.U_Name AS Name, 
-                      BSN.State_Name AS [Beginning State], ESN.State_Name AS [End State], dbo.T_Sample_Prep_Request_Updates.Request_ID AS [#RequestID]
-FROM         dbo.T_Sample_Prep_Request_Updates INNER JOIN
-                      dbo.T_Sample_Prep_Request_State_Name AS BSN ON dbo.T_Sample_Prep_Request_Updates.Beginning_State_ID = BSN.State_ID INNER JOIN
-                      dbo.T_Sample_Prep_Request_State_Name AS ESN ON dbo.T_Sample_Prep_Request_Updates.End_State_ID = ESN.State_ID LEFT OUTER JOIN
-                      dbo.T_Users ON dbo.T_Sample_Prep_Request_Updates.System_Account = dbo.T_Users.U_PRN
+SELECT Updates.Date_of_Change,
+       Updates.System_Account,
+       U.U_Name AS Name,
+       BSN.State_Name AS [Beginning State],
+       ESN.State_Name AS [End State],
+       Updates.Request_ID AS [#RequestID]
+FROM dbo.T_Sample_Prep_Request_Updates AS Updates
+     INNER JOIN dbo.T_Sample_Prep_Request_State_Name AS BSN
+       ON Updates.Beginning_State_ID = BSN.State_ID
+     INNER JOIN dbo.T_Sample_Prep_Request_State_Name AS ESN
+       ON Updates.End_State_ID = ESN.State_ID
+     LEFT OUTER JOIN dbo.T_Users U
+       ON Updates.System_Account = U.U_PRN
+
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Sample_Prep_Request_Updates_List_Report] TO [DDL_Viewer] AS [dbo]
