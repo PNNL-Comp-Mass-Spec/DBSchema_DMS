030ee80c173ef85e05bb0da5a442d333627824f7
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-10 21:56:29 -0700

    Rename prep request state and disable old state

--- DMS5/T_Sample_Prep_Request_State_Name_Data.sql
+++ DMS5/T_Sample_Prep_Request_State_Name_Data.sql
@@ -3,8 +3,8 @@
 /****** Columns: State_ID, State_Name, Active ******/
 INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (0,'(not a valid state)',0)
 INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (1,'New',1)
-INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (2,'Open',1)
+INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (2,'On Hold',1)
 INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (3,'Prep in Progress',1)
-INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (4,'Prep Complete',1)
+INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (4,'Prep Complete',0)
 INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (5,'Closed',1)
 INSERT INTO [T_Sample_Prep_Request_State_Name] VALUES (6,'Pending Approval',0)
