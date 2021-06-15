67b1aa1c335a2d382226bee3b84cfd6b19f5d28b
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-15 14:25:10 -0700

    Update item counts for prep requests whose state changed within the last year

--- DMS5/UpdateAllSamplePrepRequestItems.sql
+++ DMS5/UpdateAllSamplePrepRequestItems.sql
@@ -18,15 +18,21 @@ CREATE PROCEDURE [dbo].[UpdateAllSamplePrepRequestItems]
 **          04/12/2017 mem - Log exceptions to T_Log_Entries
 **          06/16/2017 mem - Restrict access using VerifySPAuthorized
 **          08/01/2017 mem - Use THROW if not authorized
+**          06/15/2021 mem - Also update counts for prep requests whose state changed within the last year
 **
 *****************************************************/
+(
+    @daysPriorToUpdateClosedRequests int = 365,
+    @message varchar(512) = '' output
+)
 As
     Set XACT_ABORT, nocount on
 
-    declare @myError int = 0
-    declare @myRowCount int = 0
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
 
-    DECLARE @message varchar(512) = ''
+    Set @daysPriorToUpdateClosedRequests = Abs(IsNull(@daysPriorToUpdateClosedRequests, 365))
+    Set @message = ''
 
     ---------------------------------------------------
     -- Verify that the user can execute this procedure from the given client host
@@ -42,56 +48,61 @@ As
     BEGIN TRY 
     
         ---------------------------------------------------
-
+        -- Create and populate table to hold active package IDs
+        ---------------------------------------------------
+        
         CREATE TABLE #SPRS (
             ID INT
-            -- FUTURE: details about auto-update
         )
+        
+        -- Update counts for active prep requests
+        INSERT INTO #SPRS ( ID )
+        SELECT ID
+        FROM T_Sample_Prep_Request
+        WHERE State IN (2, 3, 4)
 
-        INSERT INTO #SPRS
-        ( ID )
-        SELECT [ID]
-        FROM [T_Sample_Prep_Request]
-        WHERE [State] IN (2,3,4)
+        -- Also update counts for closed prep requests where the state changed within the last year
+        INSERT INTO #SPRS ( ID )
+        SELECT ID
+        FROM T_Sample_Prep_Request
+        WHERE State = 5 And StateChanged >= DateAdd(Day, -@daysPriorToUpdateClosedRequests, GetDate())
 
         ---------------------------------------------------
         -- Cycle through active packages and do auto import
         -- for each one
         ---------------------------------------------------
-        DECLARE
-                @itemType varchar(128) = '',
-                @mode varchar(12) = 'update',
-                @callingUser varchar(128) = USER
-
-        DECLARE
-            @currentId INT = 0,
-            @prevId INT = 0,
-            @done INT = 0
-
-        WHILE @done = 0
-        BEGIN --<d>
-            SET @currentId = 0
-
+         
+        Declare @itemType varchar(128) = ''
+        Declare @mode varchar(12) = 'update'
+        Declare @callingUser varchar(128) = suser_sname() 
+        
+        Declare @currentId int = 0
+        Declare @prevId int = 0
+        Declare @continue int = 1
+        
+        While @continue = 1
+        Begin
+            Set @currentId = 0
+            
             SELECT TOP 1 @currentId = ID
             FROM #SPRS
             WHERE ID > @prevId
             ORDER BY ID
-
-            IF @currentId = 0
-            BEGIN --<e>
-                SET @done = 1
-            END --<e>
-            ELSE
-            BEGIN  --<f>
-                SET @prevId = @currentId
-
-            EXEC @myError = UpdateSamplePrepRequestItems
-                    @currentId,
-                    @mode,
-                    @message OUTPUT,
-                    @callingUser
-
-/*
+        
+            If @currentId = 0
+            Begin
+                Set @continue = 0
+            End
+            Else
+            Begin
+                Set @prevId = @currentId
+                
+                EXEC @myError = UpdateSamplePrepRequestItems
+                        @currentId,
+                        @mode,
+                        @message OUTPUT,
+                        @callingUser
+/*                    
                 EXEC @myError = UpdateOSMPackageItems
                                     @currentId,
                                     @itemType,
@@ -108,9 +119,9 @@ As
     BEGIN CATCH 
         EXEC FormatErrorMessage @message output, @myError output
         -- rollback any open transactions
-        IF (XACT_STATE()) <> 0
+        If (XACT_STATE()) <> 0
             ROLLBACK TRANSACTION;
-
+            
         Exec PostLogEntry 'Error', @message, 'UpdateAllSamplePrepRequestItems'
     END CATCH
     
