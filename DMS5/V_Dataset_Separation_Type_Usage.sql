b371bedfc9259c79fc0b372256a77638897e6ede
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-22 10:29:27 -0700

    Show all separation types on usage report

--- DMS5/V_Dataset_Separation_Type_Usage.sql
+++ DMS5/V_Dataset_Separation_Type_Usage.sql
@@ -13,13 +13,13 @@ SELECT U.Usage_Last12Months AS [Usage Last 12 Months],
        SS.SS_comment AS [Separation Type Comment],
        SampType.Name AS [Sample Type],
        U.Usage_AllYears AS [Dataset Usage All Years],
-       U.Most_Recent_Use AS [Most Recent Use]
+       U.Most_Recent_Use AS [Most Recent Use],
+       SS.SS_active AS Active
 FROM T_Secondary_Sep SS
      INNER JOIN T_Secondary_Sep_SampleType SampType
        ON SS.SampleType_ID = SampType.SampleType_ID
      LEFT OUTER JOIN T_Secondary_Sep_Usage U
        ON U.SS_ID = SS.SS_ID
-WHERE (SS.SS_active <> 0)
 
 
 GO
