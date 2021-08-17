77750f52785f637652f7dc965e946728039879a2
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-17 12:45:10 -0700

    Add view showing active connections

---
+++ Manager_Control/V_Active_Connections.sql
@@ -0,0 +1,23 @@
+/****** Object:  View [dbo].[V_Active_Connections] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_Active_Connections]
+AS
+SELECT Rtrim(Cast(hostname AS nvarchar(128))) AS Host,
+       Rtrim(Cast(program_name AS nvarchar(128))) AS Application,
+       Rtrim(Cast(loginame AS nvarchar(128))) AS LoginName,
+       DB_NAME(dbid) AS DBName,
+       spid,
+       login_time,
+       last_batch,
+       Rtrim(Cast(cmd AS nvarchar(32))) AS Cmd,
+       Rtrim(Cast(Status AS nvarchar(32))) Status
+FROM sys.sysprocesses
+WHERE dbid > 0 AND
+      IsNull(hostname, '') <> ''
+
+
+GO
