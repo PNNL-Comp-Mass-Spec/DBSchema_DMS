f2ad0c72fed23d5a1a5228792d8f563839ddb15a
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-30 19:18:09 -0700

    Show all protein collections on the protein collection list report

--- DMS5/V_Protein_Collection_List_Report.sql
+++ DMS5/V_Protein_Collection_List_Report.sql
@@ -4,11 +4,8 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_Protein_Collection_List_Report]
 AS
--- This view uses V_Collection_Picker in the Protein_Sequences database
--- That view excludes inactive protein collections (by filtering to show collections with state 1, 2, or 3)
 SELECT LookupQ.ID,
        LookupQ.[Name],
        CASE
@@ -18,8 +15,9 @@ SELECT LookupQ.ID,
                  ELSE 'PREFERRED: ' + LookupQ.[Description]
              END
            ELSE LookupQ.[Description]
-       END AS Description,
+       END AS [Description],
        LookupQ.[Organism Name],
+       LookupQ.[State],
        LookupQ.Entries,
        LookupQ.Residues,
        CASE
@@ -47,27 +45,31 @@ FROM ( SELECT [Name],
               CASE
                   WHEN IntStandardOrContaminant > 0 THEN ''
                   ELSE Organism_Name
-              END AS [Organism Name]
-       FROM ( SELECT [Name],
+              END AS [Organism Name],
+              [State]
+       FROM ( SELECT [Filename] As [Name],
                      [Type],
                      [Description],
                      [Source],
-                     Entries,
-                     Residues,
+                     NumProteins As Entries,
+                     NumResidues As Residues,
               CASE
                   WHEN [Type] IN ('Internal_Standard', 'contaminant', 'old_contaminant') THEN 1 ELSE 0 END AS IntStandardOrContaminant,
                      Organism_Name,
-                     ID
-              FROM S_V_Protein_Collection_Picker 
+                     Protein_Collection_ID As ID,
+                     State_Name As [State]
+              FROM S_V_Protein_Collections_by_Organism 
             ) AS CP 
      ) AS LookupQ
      LEFT JOIN dbo.T_Organisms Org
        ON LookupQ.[Organism Name] = Org.OG_Name
      LEFT OUTER JOIN T_Protein_Collection_Usage PCU
        ON LookupQ.ID = PCU.Protein_Collection_ID
-GROUP BY LookupQ.[Name], LookupQ.[Type], LookupQ.[Description], LookupQ.[Source], LookupQ.Entries, LookupQ.Residues,
-         LookupQ.[Organism Name], LookupQ.ID, LookupQ.IntStandardOrContaminant, PCU.Most_Recently_Used,
-         PCU.Job_Usage_Count, PCU.Job_Usage_Count_Last12Months, Org.OG_organismDBName
+GROUP BY LookupQ.ID, LookupQ.[Name], LookupQ.[Description], LookupQ.[Organism Name], 
+         LookupQ.[State], LookupQ.Entries, LookupQ.Residues,
+         PCU.Job_Usage_Count_Last12Months, PCU.Job_Usage_Count,
+         PCU.Most_Recently_Used, LookupQ.[Type], LookupQ.[Source], 
+         LookupQ.IntStandardOrContaminant, Org.OG_organismDBName
 
 
 GO
