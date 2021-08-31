0a4d9bd8ec3af901dc8c4a4dd14156e49bb464b9
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-31 10:17:53 -0700

    Change column order in clustered index

--- DMS5/T_EMSL_Instrument_Usage_Report.sql
+++ DMS5/T_EMSL_Instrument_Usage_Report.sql
@@ -37,9 +37,9 @@ GO
 /****** Object:  Index [IX_T_EMSL_Instrument_Usage_Report] ******/
 CREATE CLUSTERED INDEX [IX_T_EMSL_Instrument_Usage_Report] ON [dbo].[T_EMSL_Instrument_Usage_Report]
 (
-	[Instrument] ASC,
 	[Year] ASC,
-	[Month] ASC
+	[Month] ASC,
+	[Instrument] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_EMSL_Instrument_Usage_Report] ADD  CONSTRAINT [DF_T_EMSL_Instrument_Usage_Report_DMS_Inst_ID]  DEFAULT ((1)) FOR [DMS_Inst_ID]
