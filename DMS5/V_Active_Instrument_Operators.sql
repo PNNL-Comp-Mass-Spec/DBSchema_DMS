259f6565a5a5bd959d1cac1dcd365ca60f4eea19
alchemistmatt <matthew.monroe@pnnl.gov>
2021-09-29 18:09:16 -0700

    Add new view that shows active users permitted to add/update datasets

--- DMS5/V_Active_Instrument_Operators.sql
+++ DMS5/V_Active_Instrument_Operators.sql
@@ -6,15 +6,8 @@ GO
 
 CREATE VIEW [dbo].[V_Active_Instrument_Operators]
 As
-SELECT DISTINCT U.U_PRN AS [Payroll Num],
-                U.U_Name AS [Name]
-FROM T_Users AS U
-     INNER JOIN T_User_Operations_Permissions AS Ops_Permissions
-       ON U.ID = Ops_Permissions.U_ID
-     INNER JOIN T_User_Operations AS Ops
-       ON Ops_Permissions.Op_ID = Ops.ID
-WHERE U.U_Status = 'Active' AND
-      Ops.Operation IN ('DMS_Instrument_Operation', 'DMS_Infrastructure_Administration')
+SELECT [Payroll Num], [Name]
+FROM dbo.V_Active_Instrument_Users
 
 
 GO
---
+++ DMS5/V_Active_Instrument_Users.sql
@@ -0,0 +1,19 @@
+/****** Object:  View [dbo].[V_Active_Instrument_Users] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_Active_Instrument_Users]
+As
+SELECT DISTINCT U.U_PRN AS [Payroll Num],
+                U.U_Name AS [Name]
+FROM T_Users AS U
+     INNER JOIN T_User_Operations_Permissions AS Ops_Permissions
+       ON U.ID = Ops_Permissions.U_ID
+     INNER JOIN T_User_Operations AS Ops
+       ON Ops_Permissions.Op_ID = Ops.ID
+WHERE U.U_Status = 'Active' AND
+      Ops.Operation IN ('DMS_Instrument_Operation', 'DMS_Infrastructure_Administration', 'DMS_Dataset_Operation')
+
+GO
