ae93d1ceea2f6eb3daea0588e83c5817556edf9a
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-04 12:16:15 -0700

    2021-10-02 auto-commit DMS5

--- DMS5/T_EUS_UsageType_Data.sql
+++ DMS5/T_EUS_UsageType_Data.sql
@@ -7,6 +7,6 @@ INSERT INTO [T_EUS_UsageType] (ID, Name, Description, Enabled, Enabled_Campaign,
 INSERT INTO [T_EUS_UsageType] (ID, Name, Description, Enabled, Enabled_Campaign, Enabled_Prep_Request) VALUES (13,'BROKEN','Broken (out of Service)',1,0,0)
 INSERT INTO [T_EUS_UsageType] (ID, Name, Description, Enabled, Enabled_Campaign, Enabled_Prep_Request) VALUES (16,'USER','On-Site usage (legacy name)',0,0,0)
 INSERT INTO [T_EUS_UsageType] (ID, Name, Description, Enabled, Enabled_Campaign, Enabled_Prep_Request) VALUES (19,'USER_UNKNOWN','EMSL Usage - To be specified later (should rarely be used)',0,0,0)
-INSERT INTO [T_EUS_UsageType] (ID, Name, Description, Enabled, Enabled_Campaign, Enabled_Prep_Request) VALUES (20,'USER_ONSITE','Samples from onsite user',1,1,1)
-INSERT INTO [T_EUS_UsageType] (ID, Name, Description, Enabled, Enabled_Campaign, Enabled_Prep_Request) VALUES (21,'USER_REMOTE','Samples from remote  user',1,1,1)
+INSERT INTO [T_EUS_UsageType] (ID, Name, Description, Enabled, Enabled_Campaign, Enabled_Prep_Request) VALUES (20,'USER_ONSITE','Samples analyzed onsite by originating user; also, samples for Resource Owner proposals',1,1,1)
+INSERT INTO [T_EUS_UsageType] (ID, Name, Description, Enabled, Enabled_Campaign, Enabled_Prep_Request) VALUES (21,'USER_REMOTE','Samples analyzed by instrumentation staff for an EMSL user',1,1,1)
 SET IDENTITY_INSERT [T_EUS_UsageType] OFF
