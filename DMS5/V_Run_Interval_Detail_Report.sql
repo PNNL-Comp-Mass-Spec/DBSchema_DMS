888f35ea9a853bb0307e71cf55ea683c73c48a4f
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-27 16:58:41 -0800

    Show Last Affected on the run interval list report

--- DMS5/V_Run_Interval_Detail_Report.sql
+++ DMS5/V_Run_Interval_Detail_Report.sql
@@ -19,8 +19,11 @@ SELECT R.ID,
        'StaffNotAvailable:' + U.StaffNotAvailable + '%|' +
        'CapDev:' + U.CapDev + '%|' +
        'InstrumentAvailable:' + U.InstrumentAvailable + '%' AS Usage,
-       Entered
-FROM dbo.T_Run_Interval R LEFT OUTER JOIN V_Run_Interval_Usage U ON R.ID = U.ID
+       Entered,
+	   Last_Affected AS [Last Affected]
+FROM dbo.T_Run_Interval R 
+     LEFT OUTER JOIN V_Run_Interval_Usage U 
+       ON R.ID = U.ID
 
 
 GO
