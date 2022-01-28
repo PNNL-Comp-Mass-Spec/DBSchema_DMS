af9f5ce7e572f37ab17b0058251beb526c54d962
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-28 14:43:08 -0800

    Add index to T_EMSL_Instrument_Usage_Report

--- DMS5/T_EMSL_Instrument_Usage_Report.sql
+++ DMS5/T_EMSL_Instrument_Usage_Report.sql
@@ -42,6 +42,16 @@ CREATE CLUSTERED INDEX [IX_T_EMSL_Instrument_Usage_Report] ON [dbo].[T_EMSL_Inst
 	[Instrument] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_T_EMSL_Instrument_Usage_Report_Type_DatasetID] ******/
+CREATE NONCLUSTERED INDEX [IX_T_EMSL_Instrument_Usage_Report_Type_DatasetID] ON [dbo].[T_EMSL_Instrument_Usage_Report]
+(
+	[Type] ASC,
+	[Dataset_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
+GO
 ALTER TABLE [dbo].[T_EMSL_Instrument_Usage_Report] ADD  CONSTRAINT [DF_T_EMSL_Instrument_Usage_Report_DMS_Inst_ID]  DEFAULT ((1)) FOR [DMS_Inst_ID]
 GO
 ALTER TABLE [dbo].[T_EMSL_Instrument_Usage_Report] ADD  CONSTRAINT [DF_T_EMSL_Instrument_Usage_Report_Usage_Type]  DEFAULT ((1)) FOR [Usage_Type]
