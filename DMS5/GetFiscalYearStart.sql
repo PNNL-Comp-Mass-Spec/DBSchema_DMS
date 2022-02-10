ae2e290ad5a309848d67484a15b02593c7d5b1eb
alchemistmatt <matthew.monroe@pnnl.gov>
2022-02-10 10:10:09 -0800

    Fix logic for determining the fiscal year start date

--- DMS5/GetFiscalYearStart.sql
+++ DMS5/GetFiscalYearStart.sql
@@ -3,31 +3,36 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE FUNCTION dbo.GetFiscalYearStart
+
+CREATE FUNCTION [dbo].[GetFiscalYearStart]
 /****************************************************
 **
-**	Desc: 
-**  Returns starting date for fiscal year N years ago
+**  Desc:   Returns starting date for fiscal year N years ago
 **
-**	Return value: person
+**  Return value: Fiscal year start date
 **
-**	Parameters: 
-**
-**	Auth:	grk
-**	Date:	07/18/2011
+**  Auth:   grk
+**  Date:   07/18/2011 grk - Initial Version
+**          02/10/2022 mem - Update to work properly when running between January 1 and September 30
 **    
 *****************************************************/
 (
-	@numberOfRecentYears int
+    @numberOfRecentYears int
 )
 RETURNS Datetime
 AS
-	BEGIN
-	DECLARE @yr INT = DATEPART(YY, DATEADD(YY, -1 * @numberOfRecentYears, GETDATE()))
-	DECLARE @fy_start varchar(24) = '10/1/' + CONVERT(varchar(12), @yr);
-	DECLARE @dt DATETIME = CONVERT(DATETIME, @fy_start)
-	RETURN @dt
-	END
+BEGIN
+    Declare @referenceDate Datetime = GetDate()
+
+    Declare @targetYear Int = DATEPART(YY, DATEADD(YY, -1 * @numberOfRecentYears, @referenceDate))
+    If Month(@referenceDate) < 10
+        Set @targetYear = @targetYear - 1
+
+    Declare @startDateText varchar(24) = '10/1/' + Cast(@targetYear As varchar(12))
+    Declare @fiscalYearStart Datetime = Cast(@startDateText As Datetime)
+
+    Return @fiscalYearStart
+END
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[GetFiscalYearStart] TO [DDL_Viewer] AS [dbo]
