941297c92d6af158f0f19b1e2be8795cc6a72bbe
alchemistmatt <matthew.monroe@pnnl.gov>
2022-02-14 20:53:24 -0800

    Assure that msg is not an empty string when @mode is 'debug'

--- DMS5/AddBOMTrackingDatasets.sql
+++ DMS5/AddBOMTrackingDatasets.sql
@@ -3,116 +3,130 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE dbo.AddBOMTrackingDatasets
+
+CREATE PROCEDURE [dbo].[AddBOMTrackingDatasets]
 /****************************************************
 **
-**  Desc: 
-**    Adds new tracking dataset for the first of the month
-**    for all actively tracked instruments
-**    for the given year, month (or current year/month if blank)
+**  Desc:
+**      Adds new tracking dataset for the first of the month
+**      for all actively tracked instruments
+**      for the given year and month
 **
-**  Return values: 0: success, otherwise, error code
+**      If @month is 'next', adds a tracking dataset for the beginning of the next month
 **
-**  Parameters:
+**  Auth:   grk
+**  Date:   12/16/2012
+**          12/16/2012 grk - initial release
+**          02/23/2016 mem - Add set XACT_ABORT on
+**          04/12/2017 mem - Log exceptions to T_Log_Entries
+**          02/14/2022 mem - Assure that msg is not an empty string when @mode is 'debug'
 **
-**    Auth: grk
-**    Date: 12/16/2012 
-**			12/16/2012 grk - initial release
-**			02/23/2016 mem - Add set XACT_ABORT on
-**			04/12/2017 mem - Log exceptions to T_Log_Entries
-**    
 ** Pacific Northwest National Laboratory, Richland, WA
 ** Copyright 2012, Battelle Memorial Institute
 *****************************************************/
 (
-	@month VARCHAR(16) = '', -- current month, if blank
-	@year VARCHAR(16) = '', -- current year, if blank
-	@mode VARCHAR(12) = 'add', -- 'info', 'debug'
-   	@callingUser VARCHAR(128)  = 'D3E154' -- Ron Moore
+    @month varchar(16) = '',                -- current month, if blank
+    @year varchar(16) = '',                 -- current year, if blank
+    @mode varchar(12) = 'add',              -- 'add, 'info' (just show instrument names), or 'debug' (call AddBOMTrackingDataset and preview tracking datasets)
+    @callingUser varchar(128)  = 'D3E154'   -- Ron Moore
 )
 As
-	Set XACT_ABORT, nocount on
+    Set XACT_ABORT, nocount on
 
-	DECLARE   	               
-		@myError INT = 0,
-		@message VARCHAR(512) = ''
-	
-	---------------------------------------------------
-	-- temp table to hold list of tracked instruments
-	---------------------------------------------------
+    Declare @myError int = 0
+    Declare @message varchar(512) = ''
 
-	CREATE TABLE #TI (
-		seq int IDENTITY(1,1) NOT NULL,
-		inst VARCHAR(64),
-		result INT NULL,
-		msg VARCHAR(512) NULL
-	)
+    ---------------------------------------------------
+    -- temp table to hold list of tracked instruments
+    ---------------------------------------------------
 
-	BEGIN TRY 
+    CREATE TABLE #TI (
+        Entry_ID int IDENTITY(1,1) NOT NULL,
+        inst varchar(64),
+        result INT NULL,
+        msg varchar(512) NULL
+    )
 
-		---------------------------------------------------
-		-- get list of tracked instruments
-		---------------------------------------------------
-		
-		INSERT INTO #TI (inst)	                
-		SELECT VT.Name FROM dbo.V_Instrument_Tracked VT
-				                                             
-		---------------------------------------------------
-		-- loop through tracked instruments
-		-- and try to make BOM tracking dataset for each
-		---------------------------------------------------
+    BEGIN TRY
 
-		DECLARE 
-			@done TINYINT = 0,
-			@seq INT = 0,
-			@instrumentName VARCHAR(64),
-			@msg VARCHAR(512) = '',
-			@err VARCHAR(16) = ''										
+        ---------------------------------------------------
+        -- Get list of tracked instruments
+        ---------------------------------------------------
 
-		WHILE NOT @done = 1
-		BEGIN --<loop>
-			SET @instrumentName = ''
-								
-			SELECT TOP 1 
-				@instrumentName = inst,
-				@seq = seq		
-			FROM #TI
-			WHERE seq > @seq 
-			ORDER BY seq ASC 
-		
-			IF @instrumentName = ''
-				SET @done = 1
-			ELSE
-			BEGIN --<a>
-				IF @mode in ('debug', 'info') PRINT '->' + @instrumentName
-		
-				IF @mode in ('add', 'debug')
-				BEGIN --<b>
-					EXEC @myError = AddBOMTrackingDataset
-											@month  ,
-											@year  ,
-											@instrumentName ,
-											@mode  ,
-											@msg  OUTPUT,
-											@callingUser											                                     
-				END --<b>
-				
-				UPDATE #TI
-				SET
-					result = @myError, 
-					msg = @msg
-				WHERE seq = @seq												
-			END --<a>																								            
-		END --<loop>
-		
-		IF @mode in ('debug', 'info') SELECT * FROM #TI
+        INSERT INTO #TI (inst)
+        SELECT VT.Name
+        FROM dbo.V_Instrument_Tracked VT
 
-	END TRY
-	BEGIN CATCH 
-		EXEC FormatErrorMessage @message OUTPUT, @myError OUTPUT
-		Exec PostLogEntry 'Error', @message, 'AddBOMTrackingDatasets'
-	END CATCH
-	RETURN @myError
+        ---------------------------------------------------
+        -- Loop through tracked instruments
+        -- and try to make BOM tracking dataset for each
+        ---------------------------------------------------
+
+        Declare @continue tinyint = 1
+        Declare @entryID int = -1
+        Declare @instrumentName varchar(64)
+        Declare @msg varchar(512) = ''
+        Declare @err varchar(16) = ''
+        Declare @returnCode Int
+
+        While @continue > 0
+        Begin --<loop>
+            Set @instrumentName = ''
+
+            SELECT TOP 1
+                @instrumentName = inst,
+                @entryID = Entry_ID
+            FROM #TI
+            WHERE Entry_ID > @entryID
+            ORDER BY Entry_ID ASC
+
+            If @instrumentName = ''
+            Begin
+                Set @continue = 0
+            End
+            Else
+            Begin --<a>
+                If @mode in ('debug', 'info') 
+                Begin
+                    PRINT '->' + @instrumentName
+                End
+
+                If @mode in ('add', 'debug')
+                Begin --<b>
+                    EXEC @returnCode = AddBOMTrackingDataset
+                                            @month,
+                                            @year,
+                                            @instrumentName,
+                                            @mode,
+                                            @msg OUTPUT,
+                                            @callingUser
+
+                    If @mode = 'debug' And Coalesce(@msg, '') = ''
+                    Begin
+                        Set @msg = 'Called AddBOMTrackingDataset with @mode=''debug'''
+                    End
+                End --<b>
+
+                UPDATE #TI
+                SET result = @returnCode,
+                    msg = @msg
+                WHERE Entry_ID = @entryID
+
+            End --<a>
+        End --<loop>
+
+        If @mode in ('debug', 'info')
+        Begin
+            SELECT * FROM #TI
+        End
+
+    END TRY
+    BEGIN CATCH
+        EXEC FormatErrorMessage @message OUTPUT, @myError OUTPUT
+        Exec PostLogEntry 'Error', @message, 'AddBOMTrackingDatasets'
+    END CATCH
+
+    RETURN @myError
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[AddBOMTrackingDatasets] TO [DDL_Viewer] AS [dbo]
