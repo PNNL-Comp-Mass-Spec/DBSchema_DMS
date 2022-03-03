926d82221ba77bb179c81a773f398cf1fffc5ba6
alchemistmatt <matthew.monroe@pnnl.gov>
2022-03-02 18:14:19 -0800

    Rename parameter @datasetID to @datasetOrDataPackageId

--- DMS_Pipeline/CreateSignaturesForJobSteps.sql
+++ DMS_Pipeline/CreateSignaturesForJobSteps.sql
@@ -3,7 +3,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE dbo.CreateSignaturesForJobSteps
+
+CREATE PROCEDURE [dbo].[CreateSignaturesForJobSteps]
 /****************************************************
 **
 **  Desc:   Create signatures for job steps
@@ -14,24 +15,22 @@ CREATE PROCEDURE dbo.CreateSignaturesForJobSteps
 **          12/21/2009 mem - Added warning message if @debugMode is non-zero and a signature cannot be computed for a step tool
 **          03/22/2011 mem - Now using varchar(1024) when extracting the @Value from the XML parameters
 **          07/16/2014 mem - Updated capitalization of keywords
+**          03/02/2022 mem - Rename parameter @datasetID to @datasetOrDataPackageId
 **    
 *****************************************************/
 (
     @job int,
     @pXML xml,
-    @datasetID int,
+    @datasetOrDataPackageId int,
     @message varchar(512) output,
-    @DebugMode tinyint = 0
+    @debugMode tinyint = 0
 )
 As
     Set nocount on
-
-    Declare @myError int
-    Set @myError = 0
-
-    Declare @myRowCount int
-    Set @myRowCount = 0
-
+    
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
+    
     Set @message = ''
 
     ---------------------------------------------------
@@ -64,8 +63,8 @@ As
         Set @message = 'Error getting job parameters'
         goto Done
     End
-
-    If @DebugMode <> 0
+    
+    If @debugMode <> 0
         SELECT '@Job_Parameters' As [Table], *
         FROM @Job_Parameters
 
@@ -75,64 +74,63 @@ As
     ---------------------------------------------------
     --
     Declare @signature int
-    Declare @Shared int
+    Declare @shared int
     Declare @stepTool varchar(64)
     Declare @curStep int
     --
-    Declare @prevStep int
-    Set @prevStep = 0
+    Declare @prevStep Int = 0
     --
-    Declare @done tinyint
-    Set @done = 0
+    Declare @continue tinyint = 1
     --
-    While @done = 0
+    While @continue = 1
     Begin --<a>
-        -- get next step that requires signature
+        -- get next step that requires signature 
         --
         Set @curStep = 0
         --
         SELECT TOP 1 @curStep = Step_Number,
                      @stepTool = Step_Tool,
-                     @Shared = Shared_Result_Version
+                     @shared = Shared_Result_Version
         FROM #Job_Steps
         WHERE Job = @job AND
               (Shared_Result_Version + Filter_Version) > 0 AND
               Step_Number > @prevStep
         ORDER BY Step_Number
 
-
+        
         -- If none found, done, otherwise process
         If @curStep = 0
-            Set @done = 1
+        Begin
+            Set @continue = 0
+        End
         Else
         Begin --<b>
-            Set @prevStep = @curStep
+            Set @prevStep = @curStep            
             ---------------------------------------------------
             -- get signature for step
             -- rollup parameter names and values for sections
             -- associated with step's step tool into single string
             --
             -- to allow for more than one instance of a tool
-            -- in a single script, look at parameters in sections
+            -- in a single script, look at parameters in sections 
             -- that either are not locked to any step
             -- (step number is null) or are locked to the current step
             --
             Set @signature = 0
             --
-            Declare @s varchar(max)
-            Set @s = ''
+            Declare @s varchar(max) = ''
             --
             SELECT @s = @s + [Name] + '=' + [Value] + ';'
             FROM @Job_Parameters
             WHERE [Section] in (
                 SELECT xmlNode.value('@name', 'varchar(128)') SectionName
-                FROM   T_Step_Tools CROSS APPLY
+                FROM   T_Step_Tools CROSS APPLY 
                        Parameter_Template.nodes('//section') As R(xmlNode)
-                WHERE  [Name] = @stepTool AND
+                WHERE  [Name] = @stepTool AND 
                        ((Step_Number is null) OR (Step_Number = @curStep))
             )
             ORDER BY [Section], [Name]
-            --
+            -- 
             SELECT @myError = @@error, @myRowCount = @@rowcount
             --
             If @myError <> 0
@@ -140,35 +138,35 @@ As
                 Set @message = 'Error forming global signature string'
                 goto Done
             End
-
+            
             If @myRowCount > 0
             Begin --<c>
                 ---------------------------------------------------
                 -- get signature for rolled-up parameter string
                 --
-                exec @signature = GetSignature @s
+                exec @signature = GetSignature @s           
                 --
                 If @signature = 0
                 Begin
                     Set @message = 'Error calculating signature'
                     goto Done
                 End
-
-                If @DebugMode <> 0
+                
+                If @debugMode <> 0
                     SELECT @signature As Signature, @S As Settings
-
+                    
             End --<c>
             Else
             Begin
-                If @DebugMode <> 0
+                If @debugMode <> 0
                     SELECT 'Warning: Cannot compute signature since could not find a section named "' + @stepTool + '" in column Parameter_Template in table T_Step_Tools' As Message
             End
-
+            
             ---------------------------------------------------
             -- calculate shared folder name
             --
-            Declare @SharedResultsFolderName varchar(256)
-            Set @SharedResultsFolderName = @stepTool + '_' + convert(varchar(12), @Shared) + '_' + convert(varchar(12), @Signature) + '_' + convert(varchar(12), @DatasetID)
+            Declare @sharedResultsFolderName varchar(256)
+            Set @sharedResultsFolderName = @stepTool + '_' + convert(varchar(12), @shared) + '_' + convert(varchar(12), @signature) + '_' + convert(varchar(12), @datasetOrDataPackageId)
 
             ---------------------------------------------------
             -- Set signature (and shared results folder name for shared results steps)
@@ -176,12 +174,11 @@ As
             UPDATE #Job_Steps
             SET Signature = @signature,
                 Output_Folder_Name = CASE
-                                         WHEN @Shared > 0 THEN @SharedResultsFolderName
+                                         WHEN @shared > 0 THEN @sharedResultsFolderName
                                          ELSE Output_Folder_Name
                                      END
             WHERE Job = @job AND
                   Step_Number = @curStep
-
 
         End --<b>
     End --<a>
