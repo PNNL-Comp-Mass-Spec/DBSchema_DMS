fc3b592f2afc47afd935d91eee5ea97da8af586e
alchemistmatt <matthew.monroe@pnnl.gov>
2022-03-16 16:17:10 -0700

    Rename fields in views

--- DMS5/V_Operations_Tasks_Detail_Report.sql
+++ DMS5/V_Operations_Tasks_Detail_Report.sql
@@ -3,23 +3,25 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE VIEW [dbo].[V_Operations_Tasks_Detail_Report]
 AS
-SELECT  ID ,
-        Tab ,
-        Description ,
-        Assigned_Personal AS [Assigned Personal] ,
-        Comments ,
-        Status ,
-        Priority ,
+SELECT  ID,
+        Tab,
+        Description,
+        Requester,
+        Requested_Personnel AS [Requested Personnel],
+        Assigned_Personnel AS [Assigned Personnel],
+        Comments,
         Hours_Spent,
-        CASE WHEN Status IN ('Completed', 'Not Implemented') THEN DATEDIFF(DAY, Created, Closed) ELSE DATEDIFF(DAY, Created, GETDATE()) END  AS Days_In_Queue ,
-        Requestor ,
-        Requested_Personal AS [Requested Personal] ,
-        Work_Package ,
+        Status,
+        Priority,
+        Work_Package,
+        CASE WHEN Status IN ('Completed', 'Not Implemented') THEN DATEDIFF(DAY, Created, Closed) ELSE DATEDIFF(DAY, Created, GETDATE()) END AS Days_In_Queue,
         Created,
         Closed
 FROM    T_Operations_Tasks
+
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Operations_Tasks_Detail_Report] TO [DDL_Viewer] AS [dbo]
 GO
--- DMS5/V_Operations_Tasks_List_Report.sql
+++ DMS5/V_Operations_Tasks_List_Report.sql
@@ -3,29 +3,32 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE VIEW [dbo].[V_Operations_Tasks_List_Report]
 AS
-SELECT  ID ,
-        Tab ,
-        Description ,
-        Assigned_Personal AS [Assigned Personal] ,
-        Comments ,
-        Status ,
-        Priority ,
-        Hours_Spent,
-        CASE WHEN Status IN ('Completed', 'Not Implemented') THEN DATEDIFF(DAY, Created, Closed) ELSE DATEDIFF(DAY, Created, GETDATE()) END  AS Days_In_Queue ,
-        Work_Package ,
-        Created,
-        Closed,
-      Case 
-			When Status In ('Completed', 'Not Implemented') Then 0			-- Request is complete or closed
-			When DATEDIFF(DAY, Created, GETDATE()) <= 30 Then	30	-- Request is 0 to 30 days old
-			When DATEDIFF(DAY, Created, GETDATE()) <= 60 Then	60	-- Request is 30 to 60 days old
-			When DATEDIFF(DAY, Created, GETDATE()) <= 90 Then	90	-- Request is 60 to 90 days old
-			Else 120								-- Request is over 90 days old
-		End
-		AS #Age_Bracket    
-FROM    T_Operations_Tasks
+SELECT  OpsTask.ID,
+        OpsTask.Tab,
+        OpsTask.Description,
+        OpsTask.Assigned_Personnel AS [Assigned Personnel],
+        OpsTask.Comments,
+        OpsTask.Status,
+        OpsTask.Priority,
+        OpsTask.Hours_Spent,
+        CASE WHEN OpsTask.Status IN ('Completed', 'Not Implemented') 
+             THEN DATEDIFF(DAY, OpsTask.Created, OpsTask.Closed) 
+             ELSE DATEDIFF(DAY, OpsTask.Created, GETDATE()) END AS Days_In_Queue,
+        OpsTask.Work_Package,
+        OpsTask.Created,
+        OpsTask.Closed,
+        Case 
+            When OpsTask.Status In ('Completed', 'Not Implemented') Then 0     -- Request is complete or closed
+            When DATEDIFF(DAY, OpsTask.Created, GETDATE()) <= 30 Then    30    -- Request is 0 to 30 days old
+            When DATEDIFF(DAY, OpsTask.Created, GETDATE()) <= 60 Then    60    -- Request is 30 to 60 days old
+            When DATEDIFF(DAY, OpsTask.Created, GETDATE()) <= 90 Then    90    -- Request is 60 to 90 days old
+            Else 120                                -- Request is over 90 days old
+        End
+        AS #Age_Bracket    
+FROM T_Operations_Tasks OpsTask
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Operations_Tasks_List_Report] TO [DDL_Viewer] AS [dbo]
