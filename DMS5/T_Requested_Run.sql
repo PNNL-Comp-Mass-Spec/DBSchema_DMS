51f6ceada4c6a4d04997331162300cae4563e3bf
alchemistmatt <matthew.monroe@pnnl.gov>
2022-03-26 17:20:46 -0700

    Add index on Batch ID

--- DMS5/T_Requested_Run.sql
+++ DMS5/T_Requested_Run.sql
@@ -62,6 +62,13 @@ GRANT SELECT ON [dbo].[T_Requested_Run] TO [Limited_Table_Write] AS [dbo]
 GO
 GRANT UPDATE ON [dbo].[T_Requested_Run] TO [Limited_Table_Write] AS [dbo]
 GO
+/****** Object:  Index [IX_T_Requested_Run_BatchID_include_DatasetID] ******/
+CREATE NONCLUSTERED INDEX [IX_T_Requested_Run_BatchID_include_DatasetID] ON [dbo].[T_Requested_Run]
+(
+	[RDS_BatchID] ASC
+)
+INCLUDE([DatasetID]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
 /****** Object:  Index [IX_T_Requested_Run_BatchID_include_ExpID] ******/
 CREATE NONCLUSTERED INDEX [IX_T_Requested_Run_BatchID_include_ExpID] ON [dbo].[T_Requested_Run]
 (
