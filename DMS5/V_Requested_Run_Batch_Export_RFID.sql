52033e508868d22c36902008f28cfca43bafd0a7
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-05 14:56:33 -0700

    Fix encoding: replace invalid characters with spaces

--- DMS5/V_Requested_Run_Batch_Export_RFID.sql
+++ DMS5/V_Requested_Run_Batch_Export_RFID.sql
@@ -4,16 +4,14 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
 CREATE VIEW [dbo].[V_Requested_Run_Batch_Export_RFID]
 AS
 SELECT RRB.ID,
        RRB.Batch As Name,
 	   T_Users.U_Name AS Owner,
 	   RRB.Description,
-       RequestedRunStats.Requests,              -- Total requested runs in batch
-       RequestedRunStats.Active_Requests,       -- Active requested runs in batch (no dataset yet)       
+       RequestedRunStats.Requests,              -- Total requested runs in batch
+       RequestedRunStats.Active_Requests,       -- Active requested runs in batch (no dataset yet)
 	   RRB.Requested_Instrument AS Inst_Group,
        RRB.Created As Created,
 	   HexID
@@ -24,7 +22,7 @@ FROM T_Requested_Run_Batches AS RRB
                               COUNT(*) AS Requests,
                               Sum(Case When RDS_Status = 'Active' Then 1 Else 0 End) As Active_Requests
                        FROM T_Requested_Run AS RR1
-                       GROUP BY RDS_BatchID 
+                       GROUP BY RDS_BatchID
                      ) AS RequestedRunStats
        ON RequestedRunStats.BatchID = RRB.ID
 WHERE RRB.ID > 0
