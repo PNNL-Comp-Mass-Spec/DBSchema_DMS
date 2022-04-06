675570b50ecc4a49782bd39597e183d98cbcc3d7
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-05 21:40:16 -0700

    Use MyEMSL upload job (aka StatusNum) when TransactionID is null

--- DMS_Capture/V_MyEMSL_DatasetID_TransactionID.sql
+++ DMS_Capture/V_MyEMSL_DatasetID_TransactionID.sql
@@ -7,11 +7,13 @@ GO
 CREATE VIEW [dbo].[V_MyEMSL_DatasetID_TransactionID]
 AS
 SELECT Dataset_ID,
-       TransactionID,
+       -- TransactionID was deprecated 2019-05-21; use StatusNum (aka MyEMSL Upload ID) if null
+       Coalesce(TransactionID, StatusNum) As TransactionID,
        Verified,
        FileCountNew,
        FileCountUpdated
 FROM dbo.T_MyEMSL_Uploads
-WHERE Not TransactionID Is Null
+WHERE Not StatusNum Is Null Or 
+      Not TransactionID Is Null
 
 GO
