f17dcacfd07f32bd0286aaba01ef0b462197b4ff
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-07 01:05:06 -0700

    2022-05-07 auto-commit DMS_Data_Package

--- DMS_Data_Package/T_Properties_Data.sql
+++ DMS_Data_Package/T_Properties_Data.sql
@@ -1,6 +1,6 @@
 /****** Object:  Table [T_Properties] ******/
 /****** RowCount: 4 ******/
-/****** Columns: Property, Value ******/
+/****** Columns: Property, [Value] ******/
 INSERT INTO [T_Properties] VALUES ('MessageBroker1','prismdevii.pnl.gov')
 INSERT INTO [T_Properties] VALUES ('MessageBroker2','proto-10.pnl.gov')
 INSERT INTO [T_Properties] VALUES ('MessagePort','61613')
