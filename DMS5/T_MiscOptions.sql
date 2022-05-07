b36130ea3ea3ccd3c4938286e422437504b69ae2
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-07 01:04:59 -0700

    2022-05-07 auto-commit DMS5

--- DMS5/T_MiscOptions.sql
+++ DMS5/T_MiscOptions.sql
@@ -17,3 +17,9 @@ CREATE TABLE [dbo].[T_MiscOptions](
 GO
 GRANT VIEW DEFINITION ON [dbo].[T_MiscOptions] TO [DDL_Viewer] AS [dbo]
 GO
+/****** Object:  Index [ix_t_misc_options_id] ******/
+CREATE UNIQUE NONCLUSTERED INDEX [ix_t_misc_options_id] ON [dbo].[T_MiscOptions]
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
--- DMS5/T_MiscPaths_Data.sql
+++ DMS5/T_MiscPaths_Data.sql
@@ -1,14 +1,14 @@
 /****** Object:  Table [T_MiscPaths] ******/
 /****** RowCount: 10 ******/
 SET IDENTITY_INSERT [T_MiscPaths] ON
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (1,'na','DMS3_Xfer\','AnalysisXfer                    ','')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (8,'G:\SqlServerBackup\','G:\SqlServerBackup\','Database Backup Log Path        ','')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (6,'\\proto-8\DB_Backups\Gigasax_Backup\','\\proto-8\DB_Backups\Gigasax_Backup\','Database Backup Path            ','')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (5,'G:\DIM_Trigger\','','DIMTriggerFileDir               ','')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (10,'\\gigasax\DMS_Organism_Files\','F:\DMS_Organism_Files\','DMSOrganismFiles                ','')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (11,'\\gigasax\DMS_Parameter_Files\','F:\DMS_Parameter_Files\','DMSParameterFiles               ','The specific folder for each step tool is defiend in table AJT_parmFileStoragePath in DMS5 and in table T_Step_Tools in the DMS_Pipeline database')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (12,'EMSL-Prism.Users.DMS_Monitoring_Admins@pnnl.gov','n/a','Email_alert_admins              ','Used by PostEmailAlert')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (3,'G:\DMS_InstSourceDirScans\','','InstrumentSourceScanDir         ','')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (4,'','http://gigasax/LC_Cart_Config/','LCCartConfigDocs                ','')
-INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, Function, Comment) VALUES (9,'','','Redgate Backup Transfer Folder  ','')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (1,'na','DMS3_Xfer\','AnalysisXfer                    ','')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (8,'G:\SqlServerBackup\','G:\SqlServerBackup\','Database Backup Log Path        ','')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (6,'\\proto-8\DB_Backups\Gigasax_Backup\','\\proto-8\DB_Backups\Gigasax_Backup\','Database Backup Path            ','')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (5,'G:\DIM_Trigger\','','DIMTriggerFileDir               ','')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (10,'\\gigasax\DMS_Organism_Files\','F:\DMS_Organism_Files\','DMSOrganismFiles                ','')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (11,'\\gigasax\DMS_Parameter_Files\','F:\DMS_Parameter_Files\','DMSParameterFiles               ','The specific folder for each step tool is defiend in table AJT_parmFileStoragePath in DMS5 and in table T_Step_Tools in the DMS_Pipeline database')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (12,'EMSL-Prism.Users.DMS_Monitoring_Admins@pnnl.gov','n/a','Email_alert_admins              ','Used by PostEmailAlert')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (3,'G:\DMS_InstSourceDirScans\','','InstrumentSourceScanDir         ','')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (4,'','http://gigasax/LC_Cart_Config/','LCCartConfigDocs                ','')
+INSERT INTO [T_MiscPaths] (TMP_ID, Server, Client, [Function], Comment) VALUES (9,'','','Redgate Backup Transfer Folder  ','')
 SET IDENTITY_INSERT [T_MiscPaths] OFF
