160595606c491108e8466fee7b9f59e0aa856bf5
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-24 11:17:29 -0700

    Remove reference to T_Event_Log to drastically reduce query times

--- DMS5/V_Dataset_Check_Report.sql
+++ DMS5/V_Dataset_Check_Report.sql
@@ -3,18 +3,24 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW V_Dataset_Check_Report
+
+CREATE VIEW [dbo].[V_Dataset_Check_Report]
 AS
-SELECT     dbo.T_Dataset.Dataset_ID, dbo.T_Dataset.Dataset_Num AS Dataset, dbo.T_Dataset.DS_created AS Created, 
-                      dbo.T_DatasetStateName.DSS_name AS State, dbo.T_Event_Log.Entered AS [State Date], dbo.t_storage_path.SP_machine_name AS Storage, 
-                      dbo.T_Instrument_Name.IN_name AS Instrument, dbo.T_Dataset.DS_PrepServerName AS [Prep Server]
-FROM         dbo.T_Dataset INNER JOIN
-                      dbo.T_Event_Log ON dbo.T_Dataset.Dataset_ID = dbo.T_Event_Log.Target_ID AND 
-                      dbo.T_Dataset.DS_state_ID = dbo.T_Event_Log.Target_State INNER JOIN
-                      dbo.T_DatasetStateName ON dbo.T_Dataset.DS_state_ID = dbo.T_DatasetStateName.Dataset_state_ID INNER JOIN
-                      dbo.t_storage_path ON dbo.T_Dataset.DS_storage_path_ID = dbo.t_storage_path.SP_path_ID INNER JOIN
-                      dbo.T_Instrument_Name ON dbo.T_Dataset.DS_instrument_name_ID = dbo.T_Instrument_Name.Instrument_ID
-WHERE     (dbo.T_Event_Log.Target_Type = 4) AND (dbo.T_Dataset.DS_state_ID <> 3) AND (dbo.T_Dataset.DS_state_ID <> 4)
+SELECT DS.Dataset_ID,
+       DS.Dataset_Num AS Dataset,
+       DS.DS_created AS Created,
+       DSN.DSS_name AS State,
+       DS.DS_Last_Affected As Last_Affected,
+       SPath.SP_machine_name AS Storage,
+       InstName.IN_name AS Instrument
+FROM dbo.T_Dataset DS
+     INNER JOIN dbo.T_DatasetStateName DSN
+       ON DS.DS_state_ID = DSN.Dataset_state_ID
+     INNER JOIN dbo.t_storage_path SPath
+       ON DS.DS_storage_path_ID = SPath.SP_path_ID
+     INNER JOIN dbo.T_Instrument_Name InstName
+       ON DS.DS_instrument_name_ID = InstName.Instrument_ID
+WHERE DS.DS_created >= DateAdd(DAY, -120, GetDate())
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Dataset_Check_Report] TO [DDL_Viewer] AS [dbo]
