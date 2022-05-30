a8350e9e599a34428703fda69145930f18670a03
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-29 22:02:51 -0700

    Add identity column to T_Job_Step_Processing_Stats

--- DMS_Pipeline/T_Job_Step_Processing_Stats.sql
+++ DMS_Pipeline/T_Job_Step_Processing_Stats.sql
@@ -4,6 +4,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[T_Job_Step_Processing_Stats](
+	[Entry_ID] [int] IDENTITY(1,1) NOT NULL,
 	[Entered] [smalldatetime] NOT NULL,
 	[Job] [int] NOT NULL,
 	[Step] [int] NOT NULL,
@@ -24,4 +25,10 @@ CREATE TABLE [dbo].[T_Job_Step_Processing_Stats](
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[T_Job_Step_Processing_Stats] TO [DDL_Viewer] AS [dbo]
+GO
+/****** Object:  Index [IX_T_Job_Step_Processing_Stats_Entry_ID] ******/
+CREATE NONCLUSTERED INDEX [IX_T_Job_Step_Processing_Stats_Entry_ID] ON [dbo].[T_Job_Step_Processing_Stats]
+(
+	[Entry_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
