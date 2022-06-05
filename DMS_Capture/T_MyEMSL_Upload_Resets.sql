5f44674d610c4a99987a5bf3eacde87d2219c010
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-04 21:01:43 -0700

    Fix typo in index target

--- DMS_Capture/T_MyEMSL_Upload_Resets.sql
+++ DMS_Capture/T_MyEMSL_Upload_Resets.sql
@@ -22,7 +22,7 @@ GO
 /****** Object:  Index [IX_T_MyEMSL_Upload_Resets_Dataset_ID] ******/
 CREATE NONCLUSTERED INDEX [IX_T_MyEMSL_Upload_Resets_Dataset_ID] ON [dbo].[T_MyEMSL_Upload_Resets]
 (
-	[Job] ASC
+	[Dataset_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
 /****** Object:  Index [IX_T_MyEMSL_Upload_Resets_Entered] ******/
