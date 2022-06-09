a9a36ce40b23667ed038e9b23515ed5ca801a11d
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-09 11:58:11 -0700

    Use new column names

--- DMS5/V_EUS_Export_Data_Package_Metadata.sql
+++ DMS5/V_EUS_Export_Data_Package_Metadata.sql
@@ -3,7 +3,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW V_EUS_Export_Data_Package_Metadata
+
+CREATE VIEW [dbo].[V_EUS_Export_Data_Package_Metadata]
 AS
 SELECT ID,
        Name,
@@ -11,13 +12,13 @@ SELECT ID,
        Owner AS Owner_PRN,
        Team,
        State,
-       [Package Type],
+       Package_Type As [Package Type],
        Total AS Total_Items,
        Jobs,
        Datasets,
        Experiments,
        Biomaterial,
-       [Last Modified],
+       Last_Modified As [Last Modified],
        Created,
        Package_File_Folder,
        Storage_Path_Relative,
