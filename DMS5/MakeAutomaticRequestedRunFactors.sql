6717b6e9fe2599d05af8191d992d40d462015ec9
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-10 13:45:36 -0700

    Exit procedure if @batchID is 0 or null

--- DMS5/MakeAutomaticRequestedRunFactors.sql
+++ DMS5/MakeAutomaticRequestedRunFactors.sql
@@ -3,98 +3,91 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE MakeAutomaticRequestedRunFactors
+
+CREATE PROCEDURE [dbo].[MakeAutomaticRequestedRunFactors]
 /****************************************************
 **
-**	Desc: 
-**	Create reqeusted run factors from metadata values
+**  Desc: 
+**      Create reqeusted run factors from metadata values
 **
-**	Return values: 0: success, otherwise, error code
-**
-**	Parameters: 
-**
-**	Auth:	grk
-**	Date:	03/23/2010 grk - initial release
-**			11/08/2016 mem - Use GetUserLoginWithoutDomain to obtain the user's network login
-**			11/10/2016 mem - Pass '' to GetUserLoginWithoutDomain
+**  Auth:   grk
+**  Date:   03/23/2010 grk - initial release
+**          11/08/2016 mem - Use GetUserLoginWithoutDomain to obtain the user's network login
+**          11/10/2016 mem - Pass '' to GetUserLoginWithoutDomain
+**          06/10/2022 mem - Exit the procedure if @batchID is 0 or null
 **    
 *****************************************************/
 (
-	@batchID INT,
-	@mode VARCHAR(32) = 'all', -- 'all', 'actual_run_order'
-	@message varchar(512) OUTPUT,
-	@callingUser varchar(128) = ''
+    @batchID int,
+    @mode varchar(32) = 'all', -- 'all', 'actual_run_order'
+    @message varchar(512) OUTPUT,
+    @callingUser varchar(128) = ''
 )
 As
-	SET NOCOUNT ON 
+    SET NOCOUNT ON 
 
-	declare @myError int
-	declare @myRowCount int
-	set @myError = 0
-	set @myRowCount = 0
+    Declare @myError Int = 0
+    Declare @myRowCount int = 0
 
-	SET @message = ''
+    SET @message = ''
 
-	--
-	DECLARE @factorList VARCHAR(MAX)
-	SET @factorList = ''
+    If Coalesce(@batchID, 0) = 0
+    Begin
+        Set @message = 'Batch ID is zero; cannot create automatic factors'
+        Return 0
+    End
 
-	-----------------------------------------------------------
-	-- make factor list for actual run order
-	-- FUTURE: mode = 'actual_run_order' or 'all'
-	-----------------------------------------------------------
-	--
-	CREATE TABLE #REQ (
-		Request INT,
-		Seq INT IDENTITY(1,1) NOT NULL
-	)
-	--
-	-----------------------------------------------------------
-	-- 
-	INSERT INTO #REQ
-		( Request )
-	SELECT
-	  T_Requested_Run.ID
-	FROM
-	  T_Requested_Run
-	  INNER JOIN T_Dataset ON T_Requested_Run.DatasetID = T_Dataset.Dataset_ID
-	WHERE
-	  ( T_Requested_Run.RDS_BatchID = @batchID )
-	  AND ( NOT ( T_Dataset.Acq_Time_Start IS NULL )
-		  )
-	ORDER BY
-	  T_Dataset.Acq_Time_Start
-	--
-	-----------------------------------------------------------
-	--
-	 SELECT 
-		@factorList = @factorList +
-		'<r ' + 
-		'i="' + CONVERT(VARCHAR(12), Request) + '" ' + 
-		'f="Actual_Run_Order" ' +
-		'v="' + CONVERT(VARCHAR(12), Seq) + '" ' +
-		'/>' 
-	FROM #REQ
-	
+    DECLARE @factorList VARCHAR(MAX) = ''
 
-	-----------------------------------------------------------
-	-- update factors
-	-----------------------------------------------------------
-	--
-	IF @factorList = ''
-		RETURN @myError
-	--
-	IF @callingUser = ''
-	BEGIN 
-		SET @callingUser = dbo.GetUserLoginWithoutDomain('')
-	END
-	--
-	EXEC @myError = UpdateRequestedRunFactors
-							@factorList,
-							@message OUTPUT,
-							@callingUser
-							
-	RETURN @myError
+    -----------------------------------------------------------
+    -- Make factor list for actual run order
+    -- FUTURE: mode = 'actual_run_order' or 'all'
+    -----------------------------------------------------------
+
+    CREATE TABLE #REQ (
+        Request INT,
+        Seq INT IDENTITY(1,1) NOT NULL
+    )
+
+    INSERT INTO #REQ( Request )
+    SELECT T_Requested_Run.ID
+    FROM T_Requested_Run
+         INNER JOIN T_Dataset
+           ON T_Requested_Run.DatasetID = T_Dataset.Dataset_ID
+    WHERE (T_Requested_Run.RDS_BatchID = @batchID) AND
+          (NOT (T_Dataset.Acq_Time_Start IS NULL))
+    ORDER BY T_Dataset.Acq_Time_Start
+    --
+    SELECT 
+        @factorList = @factorList +
+        '<r ' + 
+        'i="' + CONVERT(VARCHAR(12), Request) + '" ' + 
+        'f="Actual_Run_Order" ' +
+        'v="' + CONVERT(VARCHAR(12), Seq) + '" ' +
+        '/>' 
+    FROM #REQ
+    
+    -----------------------------------------------------------
+    -- Update factors
+    -----------------------------------------------------------
+    --
+    If @factorList = ''
+    Begin
+        RETURN @myError
+    End
+    
+    If @callingUser = ''
+    BEGIN 
+        SET @callingUser = dbo.GetUserLoginWithoutDomain('')
+    END
+
+    EXEC @myError = UpdateRequestedRunFactors
+                            @factorList,
+                            @message OUTPUT,
+                            @callingUser
+                            
+    RETURN @myError
+
 GO
 GRANT VIEW DEFINITION ON [dbo].[MakeAutomaticRequestedRunFactors] TO [DDL_Viewer] AS [dbo]
 GO
