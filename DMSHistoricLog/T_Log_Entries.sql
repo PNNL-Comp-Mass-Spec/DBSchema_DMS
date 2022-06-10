e369915bb5135528ab3940844324df8938fc77a9
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-09 21:58:11 -0700

    Make Entry_ID the primary key

--- DMSHistoricLog/T_Log_Entries.sql
+++ DMSHistoricLog/T_Log_Entries.sql
@@ -9,7 +9,10 @@ CREATE TABLE [dbo].[T_Log_Entries](
 	[posting_time] [datetime] NOT NULL,
 	[type] [varchar](32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[message] [varchar](512) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[DBName] [varchar](64) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+ CONSTRAINT [PK_T_Log_Entries] PRIMARY KEY CLUSTERED 
+(
+	[Entry_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
