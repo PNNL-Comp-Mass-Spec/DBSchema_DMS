90e3172ab02c9a065ef990d7bb6a97295aa226ec
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-09 21:57:47 -0700

    Remove deleted column

--- DMSHistoricLog/V_Historic_Log_Report.sql
+++ DMSHistoricLog/V_Historic_Log_Report.sql
@@ -8,7 +8,7 @@ CREATE VIEW [dbo].[V_Historic_Log_Report]
 AS
 SELECT Entry_ID AS Entry, posted_by AS [Posted By], 
    posting_time AS [Posting Time], type AS Type, 
-   message AS Message, DBName AS [DB Name]
+   message AS Message
 FROM T_Log_Entries
 
 GO
