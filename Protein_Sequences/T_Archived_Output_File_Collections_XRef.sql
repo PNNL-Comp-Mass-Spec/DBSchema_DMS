d77dc191f9051051dd3e65dc7e9f7e8ef5ae3bb3
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-10 13:45:57 -0700

    Rename primary key column in T_Archived_Output_File_Collections_XRef

--- Protein_Sequences/T_Archived_Output_File_Collections_XRef.sql
+++ Protein_Sequences/T_Archived_Output_File_Collections_XRef.sql
@@ -4,12 +4,12 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[T_Archived_Output_File_Collections_XRef](
-	[Archived_File_Member_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Entry_ID] [int] IDENTITY(1,1) NOT NULL,
 	[Archived_File_ID] [int] NOT NULL,
 	[Protein_Collection_ID] [int] NOT NULL,
  CONSTRAINT [PK_T_Archived_Output_File_Collections_XRef] PRIMARY KEY CLUSTERED 
 (
-	[Archived_File_Member_ID] ASC
+	[Entry_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
