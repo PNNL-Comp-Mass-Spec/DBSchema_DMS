64bd6440b7fd19862e6f3b7885a439515ee5a9f8
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-11 15:55:41 -0700

    Query tables directly when finding stale campaigns

--- DMS5/V_Campaign_List_Stale.sql
+++ DMS5/V_Campaign_List_Stale.sql
@@ -6,23 +6,24 @@ GO
 
 CREATE VIEW [dbo].[V_Campaign_List_Stale]
 AS
-SELECT ID, 
-       Campaign,
-       State,
-       [Most Recent Activity],
-       [Most Recent Sample Prep Request],
-       [Most Recent Experiment],
-       [Most Recent Run Request],
-       [Most Recent Dataset],
-       [Most Recent Analysis Job],
-       Created
-FROM V_Campaign_Detail_Report_Ex
-WHERE (ISNULL([Most Recent Sample Prep Request], '1/1/2000') <= DATEADD(MONTH, -18, GETDATE())) AND
-      (ISNULL([Most Recent Experiment], '1/1/2000') <= DATEADD(MONTH, -18, GETDATE())) AND
-      (ISNULL([Most Recent Run Request], '1/1/2000') <= DATEADD(MONTH, -18, GETDATE())) AND
-      (ISNULL([Most Recent Dataset], '1/1/2000') <= DATEADD(MONTH, -18, GETDATE())) AND
-      (ISNULL([Most Recent Analysis Job], '1/1/2000') <= DATEADD(MONTH, -7, GETDATE())) AND
-      (Created < '1/1/2009')
+SELECT C.Campaign_ID,
+       C.Campaign_Num As Campaign,
+       C.CM_State As State,
+       CT.Most_Recent_Activity,
+       CT.Sample_Prep_Request_Most_Recent as most_recent_sample_prep_request,
+       CT.Experiment_Most_Recent as most_recent_experiment,
+       CT.Run_Request_Most_Recent as most_recent_run_request,
+       CT.Dataset_Most_Recent as most_recent_dataset,
+       CT.Job_Most_Recent as most_recent_analysis_job,
+       C.CM_created As Created
+   FROM T_Campaign C
+     LEFT JOIN T_Campaign_Tracking CT ON CT.C_ID = C.Campaign_ID
+WHERE COALESCE(CT.sample_prep_request_most_recent, '1/1/2000') <= DATEADD(MONTH, -18, GETDATE()) AND
+      COALESCE(CT.experiment_most_recent, '1/1/2000') <= DATEADD(MONTH, -18, GETDATE()) AND
+      COALESCE(CT.run_request_most_recent, '1/1/2000') <= DATEADD(MONTH, -18, GETDATE()) AND
+      COALESCE(CT.dataset_most_recent, '1/1/2000') <= DATEADD(MONTH, -18, GETDATE()) AND
+      COALESCE(CT.job_most_recent, '1/1/2000') <= DATEADD(MONTH, -18, GETDATE()) AND
+      C.CM_created < DATEADD(year, -7, GETDATE())
 
 
 GO
