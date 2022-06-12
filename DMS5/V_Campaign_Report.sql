77fdc4114023fc05051ef6ff953defb3a5426e7d
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-11 19:53:23 -0700

    Add state field to campaign view

--- DMS5/V_Campaign_Report.sql
+++ DMS5/V_Campaign_Report.sql
@@ -3,11 +3,18 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW dbo.V_Campaign_Report
+
+CREATE VIEW [dbo].[V_Campaign_Report]
 AS
-SELECT     Campaign_Num AS Campaign, CM_Project_Num AS Project, dbo.GetCampaignRolePerson(Campaign_ID, 'Project Mgr') AS ProjectMgr, 
-                      dbo.GetCampaignRolePerson(Campaign_ID, 'PI') AS PI, CM_comment AS Comment, CM_created AS Created
-FROM         T_Campaign  
+SELECT Campaign_Num AS Campaign,
+       CM_Project_Num AS Project,
+       dbo.GetCampaignRolePerson(Campaign_ID, 'Project Mgr') AS ProjectMgr,
+       dbo.GetCampaignRolePerson(Campaign_ID, 'PI') AS PI,
+       CM_comment AS Comment,
+       CM_created AS Created,
+       CM_State As State
+FROM T_Campaign
+
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Campaign_Report] TO [DDL_Viewer] AS [dbo]
