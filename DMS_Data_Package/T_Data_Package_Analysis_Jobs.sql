c4a2cd616954b440b1a74705de669d0951160629
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-25 01:05:00 -0700

    2022-06-25 auto-commit DMS_Data_Package

--- DMS_Data_Package/T_Data_Package_Analysis_Jobs.sql
+++ DMS_Data_Package/T_Data_Package_Analysis_Jobs.sql
@@ -32,7 +32,7 @@ CREATE NONCLUSTERED INDEX [IX_T_Data_Package_Analysis_Jobs_Dataset_ID_Data_Packa
 	[Dataset_ID] ASC,
 	[Data_Package_ID] ASC,
 	[Job] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 GO
 /****** Object:  Index [IX_T_Data_Package_Analysis_Jobs_Job_Include_Data_Package_ID] ******/
 CREATE NONCLUSTERED INDEX [IX_T_Data_Package_Analysis_Jobs_Job_Include_Data_Package_ID] ON [dbo].[T_Data_Package_Analysis_Jobs]
