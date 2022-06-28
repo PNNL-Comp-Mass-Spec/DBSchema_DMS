7d4068cbed710106ff12d687615832da953bd11e
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-27 21:57:44 -0700

    Add data repository names

---
+++ DMS_Data_Package/T_Data_Repository_Data.sql
@@ -0,0 +1,8 @@
+/****** Object:  Table [T_Data_Repository] ******/
+/****** RowCount: 4 ******/
+SET IDENTITY_INSERT [T_Data_Repository] ON
+INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (1,'ProteomeXchange')
+INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (2,'MassIVE')
+INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (3,'CPTAC')
+INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (4,'MoTrPAC')
+SET IDENTITY_INSERT [T_Data_Repository] OFF
