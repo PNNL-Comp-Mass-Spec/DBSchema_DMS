928c0b3fdfdd22e74394f6ac32a612ac5d14c606
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-29 10:52:24 -0700

    Include container name when logging error messages from UpdateMaterialContainers

--- DMS5/DoMaterialContainerOperation.sql
+++ DMS5/DoMaterialContainerOperation.sql
@@ -11,8 +11,6 @@ CREATE Procedure [dbo].[DoMaterialContainerOperation]
 **
 **  Return values: 0: success, otherwise, error code
 **
-**  Parameters:
-**
 **  Auth:   grk
 **  Date:   07/23/2008 grk - Initial version (ticket http://prismtrac.pnl.gov/trac/ticket/603)
 **          10/01/2009 mem - Expanded error message
@@ -22,6 +20,7 @@ CREATE Procedure [dbo].[DoMaterialContainerOperation]
 **          06/16/2017 mem - Restrict access using VerifySPAuthorized
 **          08/01/2017 mem - Use THROW if not authorized
 **          05/17/2018 mem - Prevent updating containers of type 'na'
+**          06/29/2022 mem - Include container name when logging error messages from UpdateMaterialContainers
 **    
 ** Pacific Northwest National Laboratory, Richland, WA
 ** Copyright 2008, Battelle Memorial Institute
@@ -38,8 +37,10 @@ As
     Declare @myError int = 0
     Declare @myRowCount int = 0
     
-    set @message = ''
+    Set @message = ''
+
     Declare @msg varchar(512) = ''
+    Declare @logErrors Tinyint = 0
 
     ---------------------------------------------------
     -- Verify that the user can execute this procedure from the given client host
@@ -48,9 +49,9 @@ As
     Declare @authorized tinyint = 0    
     Exec @authorized = VerifySPAuthorized 'DoMaterialContainerOperation', @raiseError = 1
     If @authorized = 0
-    Begin
+    Begin;
         THROW 51000, 'Access denied', 1;
-    End
+    End;
 
     BEGIN TRY 
 
@@ -63,13 +64,14 @@ As
 
     If Len(@name) = 0
     Begin
-        set @msg = 'Container name cannot be empty'
+        Set @msg = 'Container name cannot be empty'
         RAISERROR (@msg, 11, 1)
     End
 
     If Exists (Select * From V_Material_Containers_List_Report Where Container = @name And [Type] = 'na')
     Begin
-        set @msg = 'Container "' + @name + '" cannot be updated by the website; contact a DMS admin (see DoMaterialContainerOperation)'
+        Set @msg = 'Container "' + @name + '" cannot be updated by the website; contact a DMS admin (see DoMaterialContainerOperation)'
+        Set @logErrors = 1
         RAISERROR (@msg, 11, 1)
     End
 
@@ -78,18 +80,19 @@ As
     SELECT @tmpID = ID
     FROM T_Material_Containers
     WHERE Tag = @name
-    --
-    if @tmpID = 0
-    begin
-        set @msg = 'Could not find the container named "' + @name + '" (mode is ' + @mode + ')'
+
+    If @tmpID = 0
+    Begin
+        Set @msg = 'Could not find the container named "' + @name + '" (mode is ' + @mode + ')'
         RAISERROR (@msg, 11, 1)
-    end
-    else
-    begin
+    End
+    Else
+    Begin
         Declare @iMode varchar(32) = @mode
         Declare @containerList varchar(4096) = @tmpID
         Declare @newValue varchar(128) = ''
         Declare @comment varchar(512) = ''
+        Set @logErrors = 1
 
         exec @myError = UpdateMaterialContainers
                 @iMode,
@@ -99,23 +102,33 @@ As
                 @msg output,
                 @callingUser
 
-        if @myError <> 0
-        begin
+        If @myError <> 0
+        Begin
             RAISERROR (@msg, 11, 2)
-        end
-    end
+        End
+    End
 
     END TRY
     BEGIN CATCH 
         EXEC FormatErrorMessage @message output, @myError output
-        
-        -- rollback any open transactions
-        IF (XACT_STATE()) <> 0
-            ROLLBACK TRANSACTION;
+
+        -- Rollback any open transactions
+        If (XACT_STATE()) <> 0
+            Rollback TRANSACTION;
+
+        If @logErrors > 0
+        Begin
+            If CharIndex(@name, @message) > 0
+                Set @msg = @message
+            Else
+                Set @msg = @message + ' (container ' + @name + ')'
             
-        Exec PostLogEntry 'Error', @message, 'DoMaterialContainerOperation'
+            Exec PostLogEntry 'Error', @message, 'DoMaterialContainerOperation'
+        End
+
     END CATCH
-    return @myError
+
+    Return @myError
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[DoMaterialContainerOperation] TO [DDL_Viewer] AS [dbo]
