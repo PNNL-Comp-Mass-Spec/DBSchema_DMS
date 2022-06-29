d38ea1508b25f75e3667294b81fc322f2f1caae3
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-28 18:14:33 -0700

    Add unique index on data repository name and add data repositories

--- DMS_Data_Package/T_Data_Repository.sql
+++ DMS_Data_Package/T_Data_Repository.sql
@@ -13,3 +13,12 @@ CREATE TABLE [dbo].[T_Data_Repository](
 ) ON [PRIMARY]
 
 GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_T_Data_Repository_Repository_Name] ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_T_Data_Repository_Repository_Name] ON [dbo].[T_Data_Repository]
+(
+	[Repository_Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
--- DMS_Data_Package/T_Data_Repository_Data.sql
+++ DMS_Data_Package/T_Data_Repository_Data.sql
@@ -1,8 +1,9 @@
 /****** Object:  Table [T_Data_Repository] ******/
-/****** RowCount: 4 ******/
+/****** RowCount: 5 ******/
 SET IDENTITY_INSERT [T_Data_Repository] ON
-INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (1,'ProteomeXchange')
-INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (2,'MassIVE')
 INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (3,'CPTAC')
+INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (2,'MassIVE')
 INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (4,'MoTrPAC')
+INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (1,'ProteomeXchange')
+INSERT INTO [T_Data_Repository] (Repository_ID, Repository_Name) VALUES (5,'TEDDY')
 SET IDENTITY_INSERT [T_Data_Repository] OFF
