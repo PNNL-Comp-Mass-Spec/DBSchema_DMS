607b15c9611dcc2752fa131a35201dec545bbc35
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-03 16:45:10 -0700

    Remove package column since it duplicates the data package ID and is no longer used

--- DMS_Data_Package/V_Data_Package_Folder_Creation_Parameters.sql
+++ DMS_Data_Package/V_Data_Package_Folder_Creation_Parameters.sql
@@ -6,13 +6,12 @@ GO
 
 CREATE VIEW [dbo].[V_Data_Package_Folder_Creation_Parameters]
 AS
-SELECT CONVERT(varchar(12), DP.ID) AS package,
+SELECT DP.ID,
        DPS.Path_Local_Root AS local,
        DPS.Path_Shared_Root AS share,
        DP.Path_Year AS year,
        DP.Path_Team AS team,
-       DP.Package_File_Folder AS folder,
-       DP.ID
+       DP.Package_File_Folder AS folder
 FROM T_Data_Package DP
      INNER JOIN T_Data_Package_Storage DPS
        ON DP.Path_Root = DPS.ID
