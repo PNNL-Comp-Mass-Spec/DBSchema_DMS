4d30f6307b615d50bf89a3883b451c87030d8af0
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-06 13:37:57 -0700

    Cast to date directly

--- DMS5/V_Analysis_Job_scheduled_count_by_day.sql
+++ DMS5/V_Analysis_Job_scheduled_count_by_day.sql
@@ -4,16 +4,13 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-CREATE VIEW dbo.V_Analysis_Job_scheduled_count_by_day
+CREATE VIEW [dbo].[V_Analysis_Job_Scheduled_Count_by_Day]
 AS
-SELECT CONVERT(datetime, CONVERT(varchar(24), m) 
-   + '/' + CONVERT(varchar(24), d) + '/' + CONVERT(varchar(24), y)) 
-   AS date, COUNT(*) 
-   AS [Number of Analysis Jobs Scheduled]
-FROM V_Analysis_date_scheduled
-WHERE (state = 4)
-GROUP BY y, m, d
+SELECT Cast(AJ_created As Date) As Date, COUNT(*) AS [Number of Analysis Jobs Scheduled]
+FROM T_Analysis_Job
+WHERE AJ_StateID = 4
+GROUP By Cast(AJ_created As Date)
+
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Analysis_Job_Scheduled_Count_by_Day] TO [DDL_Viewer] AS [dbo]
 GO
