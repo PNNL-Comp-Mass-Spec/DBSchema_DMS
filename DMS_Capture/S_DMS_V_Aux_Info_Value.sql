acf8389d3ce2f42966e6b22f870a6b6979109f01
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-07 11:32:22 -0700

    Rename synonym and use new view name

---
+++ DMS_Capture/S_DMS_V_Aux_Info_Value.sql
@@ -0,0 +1,5 @@
+/****** Object:  Synonym [dbo].[S_DMS_V_Aux_Info_Value] ******/
+CREATE SYNONYM [dbo].[S_DMS_V_Aux_Info_Value] FOR [DMS5].[dbo].[V_Aux_Info_Value]
+GO
+GRANT VIEW DEFINITION ON [dbo].[S_DMS_V_Aux_Info_Value] TO [DDL_Viewer] AS [dbo]
+GO
--- DMS_Capture/V_DMS_Get_Experiment_Metadata.sql
+++ DMS_Capture/V_DMS_Get_Experiment_Metadata.sql
@@ -4,7 +4,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE VIEW V_DMS_Get_Experiment_Metadata
+CREATE VIEW [dbo].[V_DMS_Get_Experiment_Metadata]
 AS
 SELECT T.Experiment_Num,
        AI.Target,
@@ -13,7 +13,7 @@ SELECT T.Experiment_Num,
        AI.Item,
        AI.Value
 FROM S_DMS_T_Experiments AS T
-     INNER JOIN S_DMS_V_AuxInfo_Value AS AI
+     INNER JOIN S_DMS_V_Aux_Info_Value AS AI
        ON T.Exp_ID = AI.Target_ID
 WHERE (AI.Target = 'Experiment')
 
