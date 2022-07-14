57f518dc93a47fb0b89d1b38c229093cf3ae4212
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-14 11:24:33 -0700

    Rename columns, add/update comments, and remove unused columns

--- DMS5/V_Dataset_Report.sql
+++ DMS5/V_Dataset_Report.sql
@@ -3,7 +3,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
--- Note that this view is used by page family helper_dataset_ckbx
+
+-- Note that this view is used by page families helper_dataset and helper_dataset_ckbx
 
 CREATE VIEW [dbo].[V_Dataset_Report]
 AS
@@ -14,16 +15,14 @@ SELECT DS.Dataset_Num AS Dataset,
        InstName.IN_name AS Instrument,
        DS.DS_created AS Created,
        DS.DS_comment AS [Comment],
-	   DS.DateSortKey AS [Acq Start],
-       DS.Acq_Length_Minutes AS [Acq Length],
-       DS.DS_Oper_PRN AS [Oper.],
-       DTN.DST_Name AS [Type],
+       DS.DateSortKey AS Acq_Start,           -- Use DateSortKey here for speed, since it is indexed (and updated via a trigger); in contrast, the dataset list report and detail report use "ISNULL(DS.Acq_Time_Start, RR.RDS_Run_Start) AS Acq_Start"	   
+       DS.Acq_Length_Minutes AS Acq_Length,
+       DTN.DST_Name AS Dataset_Type,
        E.Experiment_Num AS Experiment,
 	   C.Campaign_Num AS Campaign,
-       RRH.ID AS Request,
-	   RRH.RDS_BatchID AS Batch,
-       ISNULL(SPath.SP_vol_name_client + SPath.SP_path + ISNULL(DS.DS_folder_name, DS.Dataset_Num), '') AS [Dataset Folder Path],
-       ISNULL(DAP.Archive_Path + '\' + ISNULL(DS.DS_folder_name, DS.Dataset_Num), '') AS [Archive Folder Path]
+       RR.ID AS Request,
+	   RR.RDS_BatchID AS Batch,
+       ISNULL(SPath.SP_vol_name_client + SPath.SP_path + ISNULL(DS.DS_folder_name, DS.Dataset_Num), '') AS Dataset_Folder_Path
 FROM dbo.T_DatasetStateName AS DSN
      INNER JOIN dbo.T_Dataset AS DS
        ON DSN.Dataset_state_ID = DS.DS_state_ID
@@ -39,8 +38,8 @@ FROM dbo.T_DatasetStateName AS DSN
        ON E.EX_campaign_ID = C.Campaign_ID
      INNER JOIN dbo.t_storage_path AS SPath
        ON DS.DS_storage_path_ID = SPath.SP_path_ID
-     LEFT OUTER JOIN dbo.T_Requested_Run AS RRH
-       ON DS.Dataset_ID = RRH.DatasetID
+     LEFT OUTER JOIN dbo.T_Requested_Run AS RR
+       ON DS.Dataset_ID = RR.DatasetID
      LEFT OUTER JOIN dbo.V_Dataset_Archive_Path AS DAP
        ON DS.Dataset_ID = DAP.Dataset_ID
 
