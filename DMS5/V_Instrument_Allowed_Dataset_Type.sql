ae8b7fe653151e511c61be8032ad0fe5854e05a1
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-14 18:32:13 -0700

    Update views to use cached stats

--- DMS5/V_Instrument_Allowed_Dataset_Type.sql
+++ DMS5/V_Instrument_Allowed_Dataset_Type.sql
@@ -4,23 +4,22 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_Instrument_Allowed_Dataset_Type]
-  AS
-SELECT IAGDT.Dataset_Type AS [Dataset Type],
-       SUM(CASE WHEN DS.Dataset_ID IS NULL THEN 0 ELSE 1 END) AS [Dataset Count],
-       DTN.DST_Description AS [Type Description],
-       IAGDT.Comment AS [Usage For This Instrument],
-       T_Instrument_Name.IN_name AS Instrument
-FROM T_Instrument_Group_Allowed_DS_Type IAGDT
-     INNER JOIN T_DatasetTypeName DTN
-       ON IAGDT.Dataset_Type = DTN.DST_Name
-     INNER JOIN T_Instrument_Name
-       ON IAGDT.IN_Group = T_Instrument_Name.IN_Group
-     LEFT OUTER JOIN T_Dataset DS
-       ON T_Instrument_Name.Instrument_ID = DS.DS_instrument_name_ID AND
-          DTN.DST_Type_ID = DS.DS_type_ID
-GROUP BY IAGDT.Dataset_Type, DTN.DST_Description, IAGDT.Comment, T_Instrument_Name.IN_name
+AS
+SELECT GT.Dataset_Type As [Dataset Type],
+       CachedUsage.Dataset_Usage_Count As [Dataset Count],
+       CachedUsage.Dataset_Usage_Last_Year As [Dataset Count Last Year],
+       DTN.DST_Description As [Type Description],
+       GT.Comment As [Usage For This Instrument],
+       InstName.IN_name As Instrument
+FROM t_instrument_group_allowed_ds_type GT
+     INNER JOIN T_DatasetTypeName  DTN
+       ON GT.Dataset_Type = DTN.DST_name
+     INNER JOIN t_instrument_name InstName
+       ON GT.IN_Group = InstName.IN_Group
+     LEFT OUTER JOIN T_Cached_Instrument_Dataset_Type_Usage CachedUsage
+       ON InstName.Instrument_ID = CachedUsage.instrument_id AND
+          DTN.DST_name = CachedUsage.dataset_type
 
 
 GO
--- DMS5/V_Instrument_Group_Allowed_Dataset_Type.sql
+++ DMS5/V_Instrument_Group_Allowed_Dataset_Type.sql
@@ -4,26 +4,17 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE VIEW [dbo].[V_Instrument_Group_Allowed_Dataset_Type]
 AS
-SELECT IADT.Dataset_Type AS [Dataset Type],
-       SUM(CASE
-               WHEN DS.Dataset_ID IS NULL THEN 0
-               ELSE 1
-           END) AS [Dataset Count],
+SELECT GT.Dataset_Type AS [Dataset Type],
+       GT.Dataset_Usage_Count AS [Dataset Count],
+       GT.Dataset_Usage_Last_Year AS [Dataset Count Last Year],
        DTN.DST_Description AS [Type Description],
-       IADT.[Comment] AS [Usage For This Group],
-       IADT.IN_Group AS [Instrument Group]
-FROM T_Instrument_Name InstName
-     RIGHT OUTER JOIN T_Instrument_Group_Allowed_DS_Type IADT
-                      INNER JOIN T_DatasetTypeName DTN
-                        ON IADT.Dataset_Type = DTN.DST_name
-       ON InstName.IN_Group = IADT.IN_Group
-     LEFT OUTER JOIN T_Dataset DS
-       ON InstName.Instrument_ID = DS.DS_instrument_name_ID AND
-          DTN.DST_Type_ID = DS.DS_type_ID
-GROUP BY IADT.Dataset_Type, DTN.DST_Description, IADT.[Comment], IADT.IN_Group
+       GT.[Comment] AS [Usage For This Group],
+       GT.IN_Group AS [Instrument Group]
+FROM T_Instrument_Group_Allowed_DS_Type GT
+     INNER JOIN T_DatasetTypeName DTN
+       ON GT.Dataset_Type = DTN.DST_name
 
 
 GO
