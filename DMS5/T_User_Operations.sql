b27ce667b863c95454f55aa4de82fe8a8d81f40c
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-22 20:21:43 -0700

    Add unique constraint index

--- DMS5/T_User_Operations.sql
+++ DMS5/T_User_Operations.sql
@@ -16,3 +16,12 @@ CREATE TABLE [dbo].[T_User_Operations](
 GO
 GRANT VIEW DEFINITION ON [dbo].[T_User_Operations] TO [DDL_Viewer] AS [dbo]
 GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_T_User_Operations_Unique_Operation] ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_T_User_Operations_Unique_Operation] ON [dbo].[T_User_Operations]
+(
+	[Operation] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
