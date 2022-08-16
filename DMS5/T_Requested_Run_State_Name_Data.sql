d7c1f27a013b8ba3b40f45044033c2e5e656a5f1
alchemistmatt <matthew.monroe@pnnl.gov>
2022-08-16 16:19:12 -0700

    Make the identity column the primary key for T_Requested_Run_State_Name

--- DMS5/T_Requested_Run_State_Name_Data.sql
+++ DMS5/T_Requested_Run_State_Name_Data.sql
@@ -1,9 +1,9 @@
 /****** Object:  Table [T_Requested_Run_State_Name] ******/
 /****** RowCount: 5 ******/
 SET IDENTITY_INSERT [T_Requested_Run_State_Name] ON
-INSERT INTO [T_Requested_Run_State_Name] (State_Name, State_ID) VALUES ('Active',1)
-INSERT INTO [T_Requested_Run_State_Name] (State_Name, State_ID) VALUES ('Completed',2)
-INSERT INTO [T_Requested_Run_State_Name] (State_Name, State_ID) VALUES ('Inactive',3)
-INSERT INTO [T_Requested_Run_State_Name] (State_Name, State_ID) VALUES ('Holding',4)
-INSERT INTO [T_Requested_Run_State_Name] (State_Name, State_ID) VALUES ('Fractionated',5)
+INSERT INTO [T_Requested_Run_State_Name] (State_ID, State_Name) VALUES (1,'Active')
+INSERT INTO [T_Requested_Run_State_Name] (State_ID, State_Name) VALUES (2,'Completed')
+INSERT INTO [T_Requested_Run_State_Name] (State_ID, State_Name) VALUES (5,'Fractionated')
+INSERT INTO [T_Requested_Run_State_Name] (State_ID, State_Name) VALUES (4,'Holding')
+INSERT INTO [T_Requested_Run_State_Name] (State_ID, State_Name) VALUES (3,'Inactive')
 SET IDENTITY_INSERT [T_Requested_Run_State_Name] OFF
