6939afe687cb74205cd5df559974abf5b897e54d
alchemistmatt <matthew.monroe@pnnl.gov>
2022-08-15 18:25:37 -0700

    Add an identity column to T_AuxInfo_Value and make it the primary key

--- DMS5/T_AuxInfo_Value.sql
+++ DMS5/T_AuxInfo_Value.sql
@@ -4,13 +4,13 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[T_AuxInfo_Value](
+	[Entry_ID] [int] IDENTITY(1,1) NOT NULL,
 	[Aux_Description_ID] [int] NOT NULL,
 	[Value] [varchar](512) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[Target_ID] [int] NOT NULL,
  CONSTRAINT [PK_T_AuxInfo_Value] PRIMARY KEY NONCLUSTERED 
 (
-	[Aux_Description_ID] ASC,
-	[Target_ID] ASC
+	[Entry_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 ) ON [PRIMARY]
 
@@ -29,6 +29,13 @@ CREATE CLUSTERED INDEX [IX_T_AuxInfo_Value_Target_ID] ON [dbo].[T_AuxInfo_Value]
 	[Target_ID] ASC,
 	[Aux_Description_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
+GO
+/****** Object:  Index [IX_T_AuxInfo_Value_uniq_Aux_Description_ID_Target_ID] ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_T_AuxInfo_Value_uniq_Aux_Description_ID_Target_ID] ON [dbo].[T_AuxInfo_Value]
+(
+	[Aux_Description_ID] ASC,
+	[Target_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[T_AuxInfo_Value]  WITH CHECK ADD  CONSTRAINT [FK_T_AuxInfo_Value_T_AuxInfo_Description] FOREIGN KEY([Aux_Description_ID])
 REFERENCES [dbo].[T_AuxInfo_Description] ([ID])
