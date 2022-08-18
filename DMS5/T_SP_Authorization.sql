0faf570f29d358fcfa87f9475bff7e7b238a1b73
alchemistmatt <matthew.monroe@pnnl.gov>
2022-08-18 16:57:34 -0700

    Add column Host_IP to the T_SP_Authorization tables; change the primary key to a new identity column

--- DMS5/T_SP_Authorization.sql
+++ DMS5/T_SP_Authorization.sql
@@ -4,14 +4,14 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[T_SP_Authorization](
-	[ProcedureName] [nvarchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[LoginName] [nvarchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[HostName] [nvarchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Entry_ID] [int] IDENTITY(1,1) NOT NULL,
+	[ProcedureName] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[LoginName] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[HostName] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Host_IP] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
  CONSTRAINT [PK_T_SP_Authorization] PRIMARY KEY CLUSTERED 
 (
-	[ProcedureName] ASC,
-	[LoginName] ASC,
-	[HostName] ASC
+	[Entry_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
@@ -33,4 +33,28 @@ CREATE NONCLUSTERED INDEX [IX_T_SP_Authorization_ProcName] ON [dbo].[T_SP_Author
 (
 	[ProcedureName] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_T_SP_Authorization_unique_Procedure_Login_Host_IP] ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_T_SP_Authorization_unique_Procedure_Login_Host_IP] ON [dbo].[T_SP_Authorization]
+(
+	[ProcedureName] ASC,
+	[LoginName] ASC,
+	[Host_IP] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_T_SP_Authorization_unique_Procedure_Login_Host_Name] ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_T_SP_Authorization_unique_Procedure_Login_Host_Name] ON [dbo].[T_SP_Authorization]
+(
+	[ProcedureName] ASC,
+	[LoginName] ASC,
+	[HostName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[T_SP_Authorization] ADD  CONSTRAINT [DF_T_SP_Authorization_Host_IP]  DEFAULT ('') FOR [Host_IP]
 GO
