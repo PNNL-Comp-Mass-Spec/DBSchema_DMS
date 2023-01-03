61affca7b387e523917e78af02d92436415a6054
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-02 20:13:30 -0800

    Sort by descending start and end dates

--- DMS5/V_NEXUS_Import_Proposals.sql
+++ DMS5/V_NEXUS_Import_Proposals.sql
@@ -7,15 +7,16 @@ GO
 CREATE VIEW [dbo].[V_NEXUS_Import_Proposals]
 AS
 (
-SELECT project_id, 
-       title, 
+SELECT project_id,
+       title,
        proposal_type,
        proposal_type_display,
-       actual_start_date, 
+       actual_start_date,
        actual_end_date,
        project_uuid,
-       Row_Number() Over (Partition By project_id Order By actual_start_date, actual_end_date) As IdRank
+       Row_Number() Over (Partition By project_id Order By actual_start_date Desc, actual_end_date Desc) As IdRank
 FROM openquery ( NEXUS, 'SELECT * FROM proteomics_views.vw_proposals' )
 )
+
 
 GO
