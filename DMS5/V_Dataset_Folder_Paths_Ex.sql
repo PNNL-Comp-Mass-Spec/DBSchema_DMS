e30048597a1814128f790077ec806cc62c233eee
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-10 11:51:14 -0800

    Add column alias to view

--- DMS5/V_Dataset_Folder_Paths_Ex.sql
+++ DMS5/V_Dataset_Folder_Paths_Ex.sql
@@ -6,20 +6,30 @@ GO
 
 CREATE VIEW [dbo].[V_Dataset_Folder_Paths_Ex]
 AS
-SELECT DFP.Dataset,
-       DFP.Dataset_ID,
-       DFP.Dataset_Folder_Path,
-       DFP.Archive_Folder_Path,
-       DFP.Dataset_URL,
-       DFP.Instrument_Data_Purged,
-       InstName.IN_Name AS Instrument,
-       DS.DS_Created AS Dataset_Created,
-       Convert(varchar(8), DatePart(YEAR, DS.DS_Created)) + '_' + Convert(varchar(4), DatePart(quarter, DS.DS_Created)) AS Dataset_YearQuarter
-FROM dbo.T_Dataset DS
-     INNER JOIN dbo.V_Dataset_Folder_Paths DFP
-       ON DS.Dataset_ID = DFP.Dataset_ID
-     INNER JOIN T_Instrument_Name InstName
-       ON DS.DS_instrument_name_ID = InstName.Instrument_ID
+SELECT dataset,
+       dataset_id,
+       dataset_folder_path,
+       archive_folder_path,
+       dataset_url,
+       instrument_data_purged,
+       instrument,
+       dataset_created,
+       dataset_yearquarter AS dataset_year_quarter,
+       dataset_yearquarter
+FROM ( SELECT DFP.Dataset,
+              DFP.Dataset_ID,
+              DFP.Dataset_Folder_Path,
+              DFP.Archive_Folder_Path,
+              DFP.Dataset_URL,
+              DFP.Instrument_Data_Purged,
+              InstName.IN_Name AS Instrument,
+              DS.DS_Created AS Dataset_Created,
+              Convert(varchar(8), DatePart(YEAR, DS.DS_Created)) + '_' + Convert(varchar(4), DatePart(quarter, DS.DS_Created)) AS Dataset_YearQuarter
+       FROM dbo.T_Dataset DS
+            INNER JOIN dbo.V_Dataset_Folder_Paths DFP
+              ON DS.Dataset_ID = DFP.Dataset_ID
+            INNER JOIN T_Instrument_Name InstName
+              ON DS.DS_instrument_name_ID = InstName.Instrument_ID ) SourceQ
 
 
 GO
