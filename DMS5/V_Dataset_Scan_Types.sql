131fca4678403624493032a7cd77756fbc903790
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-12 21:10:24 -0800

    Rename columns in view

--- DMS5/V_Dataset_Scan_Types.sql
+++ DMS5/V_Dataset_Scan_Types.sql
@@ -3,19 +3,20 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE VIEW [dbo].[V_Dataset_Scan_Types]
 AS
 SELECT DST.Entry_ID,
        DST.Dataset_ID,
        DS.Dataset_Num AS Dataset,
-       DST.ScanType,
-       DST.ScanCount,
-       DST.ScanFilter,
-       DS.Scan_Count,
+       DST.ScanType AS Scan_Type,
+       DST.ScanCount AS Scan_Count,
+       DST.ScanFilter AS Scan_Filter,
+       DS.Scan_Count AS Scan_Count_Total,
        DS.File_Size_Bytes,
        DS.Acq_Length_Minutes,
        DS.Acq_Time_Start,
-       DS.DS_created
+       DS.DS_created AS Created
 FROM T_Dataset_ScanTypes DST
      INNER JOIN T_Dataset DS
        ON DST.Dataset_ID = DS.Dataset_ID
