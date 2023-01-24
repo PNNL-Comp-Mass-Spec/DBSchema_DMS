ed0b864a8d16ed5de168a1c1faa888b8b9ccb25a
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-24 08:33:19 -0800

    Use lowercase column names in @colList

--- DMS5/GetRequestedRunParametersAndFactors.sql
+++ DMS5/GetRequestedRunParametersAndFactors.sql
@@ -4,105 +4,106 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE dbo.GetRequestedRunParametersAndFactors
+CREATE PROCEDURE [dbo].[GetRequestedRunParametersAndFactors]
 /****************************************************
 **
 **  Desc:
-**		Returns the run parameters and factors associated with the run requests in the input list
+**      Returns the run parameters and factors associated with the run requests in the input list
 **
-**		This is used by http://dms2.pnl.gov/requested_run_batch_blocking/grid
+**      This is used by https://dms2.pnl.gov/requested_run_batch_blocking/grid
 **
 **  Auth:   grk
 **  Date:   03/28/2013 grk - Cloned from GetFactorCrosstabByBatch
 **          01/05/2023 mem - Add view name to comment
+**          01/24/2023 bcg - Use lowercase column names in @colList
 **
 *****************************************************/
 (
-	@itemList TEXT,
-	@infoOnly tinyint = 0,
-	@message varchar(512)='' OUTPUT
+    @itemList text,
+    @infoOnly tinyint = 0,
+    @message varchar(512) = '' OUTPUT
 )
 AS
-	Set NoCount On
+    Set NoCount On
 
-	Declare @myRowCount int	= 0
-	Declare @myError int	= 0
+    Declare @myRowCount int = 0
+    Declare @myError int = 0
 
-	Declare @msg varchar(256)
+    Declare @msg varchar(256)
 
-	Declare @Sql varchar(max)
-	Declare @CrossTabSql varchar(max)
-	Declare @FactorNameList varchar(max)
+    Declare @Sql varchar(max)
+    Declare @CrossTabSql varchar(max)
+    Declare @FactorNameList varchar(max)
 
-	-----------------------------------------
-	-- temp tables to hold list of requests
-	-- and factors
-	-----------------------------------------
-	--
-	CREATE TABLE #REQS (
-		Request int
-	)
-	--
-	CREATE Table #FACTORS (
-		FactorID INT,
-		FactorName VARCHAR(128) NULL
-	)
+    -----------------------------------------
+    -- Temp tables to hold list of requests
+    -- and factors
+    -----------------------------------------
+    --
+    CREATE TABLE #REQS (
+        Request int
+    )
+    --
+    CREATE Table #FACTORS (
+        FactorID INT,
+        FactorName VARCHAR(128) NULL
+    )
 
-	-----------------------------------------
-	-- populate temp table from request list
-	-----------------------------------------
-	--
-	INSERT INTO #REQS (Request)
-	SELECT Item
-	FROM dbo.MakeTableFromList(@itemList)
+    -----------------------------------------
+    -- Populate temp table from request list
+    -----------------------------------------
+    --
+    INSERT INTO #REQS (Request)
+    SELECT Item
+    FROM dbo.MakeTableFromList(@itemList)
 
 /*
-	If IsNull(@NameContains, '') <> ''
-	Begin
-		-----------------------------------------
-		-- filter by request name
-		-----------------------------------------
-		--
-		DELETE FROM
-			#REQS
-		WHERE
-			NOT EXISTS (
-				SELECT ID
-				FROM T_Requested_Run
-				WHERE
-					ID = Request AND
-					RDS_Name LIKE '%' + @NameContains + '%'
-			)
-		--
-		SELECT @myRowCount = @@rowcount, @myError = @@error
-	End
+    If IsNull(@NameContains, '') <> ''
+    Begin
+        -----------------------------------------
+        -- filter by request name
+        -----------------------------------------
+        --
+        DELETE FROM
+            #REQS
+        WHERE
+            NOT EXISTS (
+                SELECT ID
+                FROM T_Requested_Run
+                WHERE
+                    ID = Request AND
+                    RDS_Name LIKE '%' + @NameContains + '%'
+            )
+        --
+        SELECT @myRowCount = @@rowcount, @myError = @@error
+    End
 */
 
-	-----------------------------------------
-	-- Build the Sql for obtaining the factors for the requests
-	--
-	-- These columns correspond to view V_Requested_Run_Unified_List_Ex
-	-----------------------------------------
-	--
-	EXEC @myError = MakeFactorCrosstabSQL_Ex
-					@colList = N'Request, Name, Status, Batch, Experiment, Dataset, Instrument, Cart, LC_Col, Block, Run_Order',
-					@Sql = @Sql OUTPUT,
-					@message = @message OUTPUT
-	--
-	IF @myError <> 0
-		RETURN @myError
+    -----------------------------------------
+    -- Build the SQL for obtaining the factors for the requests
+    --
+    -- These columns correspond to view V_Requested_Run_Unified_List_Ex
+    -----------------------------------------
+    --
+    EXEC @myError = MakeFactorCrosstabSQL_Ex
+                    @colList = N'request, name, status, batch, experiment, dataset, instrument, cart, lc_col, block, run_order',
+                    @Sql = @Sql OUTPUT,
+                    @message = @message OUTPUT
+    --
+    IF @myError <> 0
+        RETURN @myError
 
-	-----------------------------------------
-	-- run dynamic SQL, or dump it
-	-----------------------------------------
-	--
-	If @infoOnly <> 0
-		Print @Sql
-	Else
-		Exec (@Sql)
+    -----------------------------------------
+    -- run dynamic SQL, or dump it
+    -----------------------------------------
+    --
+    If @infoOnly <> 0
+        Print @Sql
+    Else
+        Exec (@Sql)
 
-	--
-	return @myError
+    --
+    return @myError
 
 
 GO
