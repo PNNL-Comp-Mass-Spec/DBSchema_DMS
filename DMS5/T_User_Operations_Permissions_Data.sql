2540f0c23e40a843b53eb248800fd3354da45859
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-28 01:05:09 -0800

    2023-01-28 auto-commit DMS5

--- DMS5/T_User_Operations_Permissions_Data.sql
+++ DMS5/T_User_Operations_Permissions_Data.sql
@@ -1,5 +1,5 @@
 /****** Object:  Table [T_User_Operations_Permissions] ******/
-/****** RowCount: 394 ******/
+/****** RowCount: 396 ******/
 /****** Columns: U_ID, Op_ID ******/
 INSERT INTO [T_User_Operations_Permissions] VALUES (4,17)
 INSERT INTO [T_User_Operations_Permissions] VALUES (6,17)
@@ -395,3 +395,5 @@ INSERT INTO [T_User_Operations_Permissions] VALUES (3525,25)
 INSERT INTO [T_User_Operations_Permissions] VALUES (3530,17)
 INSERT INTO [T_User_Operations_Permissions] VALUES (3530,26)
 INSERT INTO [T_User_Operations_Permissions] VALUES (3536,25)
+INSERT INTO [T_User_Operations_Permissions] VALUES (3538,32)
+INSERT INTO [T_User_Operations_Permissions] VALUES (3539,25)
