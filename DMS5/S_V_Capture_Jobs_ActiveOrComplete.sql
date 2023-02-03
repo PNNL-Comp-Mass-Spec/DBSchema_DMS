64737dbc55c4229e6f25f582bb636a6d97d4dc5d
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-03 15:58:38 -0800

    Remove renamed synonym

--- DMS5/S_V_Capture_Jobs_ActiveOrComplete.sql
+++
@@ -1,5 +0,0 @@
-/****** Object:  Synonym [dbo].[S_V_Capture_Jobs_ActiveOrComplete] ******/
-CREATE SYNONYM [dbo].[S_V_Capture_Jobs_ActiveOrComplete] FOR [DMS_Capture].[dbo].[V_Capture_Jobs_ActiveOrComplete]
-GO
-GRANT VIEW DEFINITION ON [dbo].[S_V_Capture_Jobs_ActiveOrComplete] TO [DDL_Viewer] AS [dbo]
-GO
