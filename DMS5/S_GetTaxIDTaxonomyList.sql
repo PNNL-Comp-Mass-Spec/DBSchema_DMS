9c3243377d5875714a96323e856fc519308d129c
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2023-02-23 00:17:27 -0800

    Add missed permissions

--- DMS5/S_GetTaxIDTaxonomyList.sql
+++ DMS5/S_GetTaxIDTaxonomyList.sql
@@ -1,3 +1,5 @@
 /****** Object:  Synonym [dbo].[S_GetTaxIDTaxonomyList] ******/
 CREATE SYNONYM [dbo].[S_GetTaxIDTaxonomyList] FOR [Ontology_Lookup].[dbo].[get_taxid_taxonomy_list]
 GO
+GRANT VIEW DEFINITION ON [dbo].[S_GetTaxIDTaxonomyList] TO [DDL_Viewer] AS [dbo]
+GO
--- DMS5/S_GetTaxIDTaxonomyTable.sql
+++ DMS5/S_GetTaxIDTaxonomyTable.sql
@@ -1,3 +1,5 @@
 /****** Object:  Synonym [dbo].[S_GetTaxIDTaxonomyTable] ******/
 CREATE SYNONYM [dbo].[S_GetTaxIDTaxonomyTable] FOR [Ontology_Lookup].[dbo].[get_taxid_taxonomy_table]
 GO
+GRANT VIEW DEFINITION ON [dbo].[S_GetTaxIDTaxonomyTable] TO [DDL_Viewer] AS [dbo]
+GO
