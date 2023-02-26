4fc655518d1f76d5376911270183a0760a4f7c90
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-25 16:21:24 -0800

    Rename variables and update messages

--- DMS5/do_biomaterial_operation.sql
+++ DMS5/do_biomaterial_operation.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE [dbo].[do_biomaterial_operation]
 /****************************************************
 **
 **  Desc:
-**      Perform cell culture operation defined by 'mode'
+**      Perform biomaterial (cell culture) operation defined by 'mode'
 **
 **  Return values: 0: success, otherwise, error code
 **
@@ -18,6 +18,7 @@ CREATE PROCEDURE [dbo].[do_biomaterial_operation]
 **          08/01/2017 mem - Use THROW if not authorized
 **          10/13/2022 mem - Fix misspelled words
 **          02/23/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
+**          02/25/2023 mem - Rename variables and update messages
 **
 *****************************************************/
 (
@@ -29,12 +30,12 @@ CREATE PROCEDURE [dbo].[do_biomaterial_operation]
 AS
     set nocount on
 
-    declare @myError int = 0
-    declare @myRowCount int = 0
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
 
     set @message = ''
 
-    declare @result int
+    Declare @result int
 
     ---------------------------------------------------
     -- Verify that the user can execute this procedure from the given client host
@@ -43,47 +44,45 @@ AS
     Declare @authorized tinyint = 0
     Exec @authorized = verify_sp_authorized 'do_biomaterial_operation', @raiseError = 1
     If @authorized = 0
-    Begin
+    Begin;
         THROW 51000, 'Access denied', 1;
-    End
+    End;
 
     ---------------------------------------------------
-    -- get cell culture ID
+    -- Get cell biomaterial ID
     ---------------------------------------------------
 
-    declare @ccID int
-    set @ccID = 0
-    --
-    SELECT
-        @ccID = CC_ID
+    Declare @biomaterialID int = 0
+
+    SELECT @biomaterialID = CC_ID
     FROM T_Cell_Culture
     WHERE (CC_Name = @biomaterial)
     --
     SELECT @myError = @@error, @myRowCount = @@rowcount
-    --
-    if @myError <> 0 or @ccID = 0
+
+    if @myError <> 0 or @biomaterialID = 0
     begin
-        set @message = 'Could not get Id for cell culture "' + @biomaterial + '"'
+        set @message = 'Could not get Id for biomaterial "' + @biomaterial + '"'
         RAISERROR (@message, 10, 1)
         return 51140
     end
 
     ---------------------------------------------------
-    -- Delete cell culture if it is in "new" state only
+    -- Delete biomaterial if it is in "new" state only
     ---------------------------------------------------
 
     if @mode = 'delete'
     begin
         ---------------------------------------------------
-        -- verify that cell culture is not used by any experiments
+        -- Verify that biomaterial is not used by any experiments
         ---------------------------------------------------
 
-        declare @exps int
+        Declare @exps int
         set @exps = 1
         --
         SELECT @exps = COUNT(*)
         FROM  T_Experiment_Cell_Cultures
-        WHERE  (CC_ID = @ccID)
+        WHERE  (CC_ID = @biomaterialID)
         --
         SELECT @myError = @@error, @myRowCount = @@rowcount
         --
@@ -96,23 +95,23 @@ AS
         --
         if @exps > 0
         begin
-            set @message = 'Cannot delete cell culture that is referenced by any experiments'
+            set @message = 'Cannot delete biomaterial that is referenced by any experiments'
             RAISERROR (@message, 10, 1)
             return 51141
         end
 
         ---------------------------------------------------
-        -- delete the cell culture
+        -- Delete the biomaterial
         ---------------------------------------------------
 
         DELETE FROM T_Cell_Culture
-        WHERE CC_ID = @ccID
+        WHERE CC_ID = @biomaterialID
         --
         SELECT @myError = @@error, @myRowCount = @@rowcount
         --
         if @myError <> 0
         begin
-            RAISERROR ('Could not delete cell culture "%s"',
+            RAISERROR ('Could not delete biomaterial "%s"',
                 10, 1, @biomaterial)
             return 51142
         end
@@ -123,7 +122,7 @@ AS
             Declare @stateID int
             Set @stateID = 0
 
-            Exec alter_event_log_entry_user 2, @ccID, @stateID, @callingUser
+            Exec alter_event_log_entry_user 2, @biomaterialID, @stateID, @callingUser
         End
 
         return 0
@@ -136,6 +135,7 @@ AS
     set @message = 'Mode "' + @mode +  '" was unrecognized'
     RAISERROR (@message, 10, 1)
     return 51222
+
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[do_biomaterial_operation] TO [DDL_Viewer] AS [dbo]
