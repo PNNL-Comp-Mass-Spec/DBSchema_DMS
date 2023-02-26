2b7584674ae69b6e9dacc324bb2ce6d7f8e94487
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2023-02-25 22:38:35 -0800

    Change view column name to lowercase

--- DMS_Pipeline/V_Pipeline_Script_Entry.sql
+++ DMS_Pipeline/V_Pipeline_Script_Entry.sql
@@ -3,7 +3,6 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW [dbo].[V_Pipeline_Script_Entry]
 AS
 -- When the length of data in an XML column for a given row surpasses a threshold, 
@@ -21,9 +20,8 @@ SELECT id,
        Case When Backfill_to_DMS = 0 Then 'N' Else 'Y' End AS backfill_to_dms,
        Cast(contents As Varchar(Max)) As contents,      -- Cast the XML to varchar(max)
        Cast(parameters As Varchar(Max)) As parameters,  -- Cast the XML to varchar(max)
-       Cast(fields As Varchar(Max)) As Fields           -- Cast the XML to varchar(max)
+       Cast(fields As Varchar(Max)) As fields           -- Cast the XML to varchar(max)
 FROM dbo.T_Scripts
-
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Pipeline_Script_Entry] TO [DDL_Viewer] AS [dbo]
