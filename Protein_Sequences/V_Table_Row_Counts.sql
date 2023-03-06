03c0972eb513160e3ddf16bd8d4757a2a56aa3e3
alchemistmatt <matthew.monroe@pnnl.gov>
2023-03-06 14:19:28 -0800

    Add columns to views

--- Protein_Sequences/V_Table_Row_Counts.sql
+++ Protein_Sequences/V_Table_Row_Counts.sql
@@ -3,14 +3,19 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW V_Table_Row_Counts
+
+CREATE VIEW [dbo].[V_Table_Row_Counts]
 AS
-SELECT TOP 100 PERCENT o.name AS TableName, 
-    i.rowcnt AS TableRowCount
-FROM dbo.sysobjects o INNER JOIN
-    dbo.sysindexes i ON o.id = i.id
-WHERE (o.type = 'u') AND (i.indid < 2) AND 
-    (o.name <> 'dtproperties')
-ORDER BY o.name
+SELECT s.Name As schema_name,
+       o.name AS table_name, 
+       i.rowcnt AS table_row_count
+FROM sys.objects o 
+     INNER JOIN dbo.sysindexes i 
+       ON o.object_id = i.id 
+     INNER JOIN sys.schemas s 
+       ON s.schema_id = o.schema_id
+WHERE o.type = 'u' AND 
+      i.indid < 2 AND 
+      o.name <> 'dtproperties'
 
 GO
--- Protein_Sequences/V_Table_Size_Summary.sql
+++ Protein_Sequences/V_Table_Size_Summary.sql
@@ -3,31 +3,35 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-create VIEW V_Table_Size_Summary
+
+CREATE VIEW [dbo].[V_Table_Size_Summary]
 AS
-WITH Table_Space_Summary
-(Schema_Name, Table_Name, Space_Used_MB, Space_Reserved_MB, Table_Row_Count)
-AS
-(
- SELECT Schema_Name, Table_Name, 
-       	SUM(Space_Used_MB), 
-		SUM(Space_Reserved_MB), 
-		MAX(Table_Row_Count)
- FROM dbo.V_Table_Index_Sizes
- GROUP BY Schema_Name, Table_Name
+WITH Table_Space_Summary (schema_name, table_name, space_used_mb, space_reserved_mb, table_row_count, table_size_bytes, index_count)
+As (
+    SELECT schema_name, 
+           table_name, 
+           SUM(space_used_mb) As space_used_mb, 
+           SUM(space_reserved_mb) As space_reserved_mb, 
+           MAX(table_row_count) As table_row_count,
+           Max(table_size_bytes) As table_size_bytes,
+           COUNT(*) As index_count
+    FROM dbo.V_Table_Index_Sizes
+    GROUP BY schema_name, table_name
 )
-SELECT TOP 100 PERCENT S.Schema_Name, S.Table_Name,
-	S.Space_Used_MB,
-    ROUND(S.Space_Used_MB / CONVERT(real, TotalsQ.TotalUsedMB) * 100, 2) AS Percent_Total_Used_MB, 
-	S.Space_Reserved_MB,
-    ROUND(S.Space_Reserved_MB / CONVERT(real, TotalsQ.TotalReservedMB) * 100, 2) AS Percent_Total_Reserved_MB, 
-    S.Table_Row_Count, 
-    ROUND(S.Table_Row_Count / CONVERT(real, TotalsQ.TotalRows) * 100, 2) AS Percent_Total_Rows
+SELECT S.schema_name, 
+       S.table_name,
+       S.table_row_count, 
+       S.space_used_mb,
+       S.table_size_bytes As size_bytes,
+       S.index_count,
+       ROUND(S.space_used_mb / CONVERT(real, TotalsQ.total_used_mb) * 100, 2) AS percent_total_used_mb, 
+       S.space_reserved_mb,
+       ROUND(S.Space_Reserved_MB / CONVERT(real, TotalsQ.total_reserved_mb) * 100, 2) AS percent_total_reserved_mb, 
+       ROUND(S.Table_Row_Count / CONVERT(real, TotalsQ.total_rows) * 100, 2) AS percent_total_rows
 FROM Table_Space_Summary S CROSS JOIN
-        (SELECT SUM(Space_Used_MB) AS TotalUsedMB,
-				SUM(Space_Reserved_MB) AS TotalReservedMB,
-				SUM(Table_Row_Count) AS TotalRows
-		 FROM Table_Space_Summary) TotalsQ
-ORDER BY S.Space_Used_MB DESC
+     (SELECT SUM(Space_Used_MB) AS total_used_mb,
+             SUM(Space_Reserved_MB) AS total_reserved_mb,
+             SUM(Table_Row_Count) AS total_rows
+      FROM Table_Space_Summary) TotalsQ
 
 GO
