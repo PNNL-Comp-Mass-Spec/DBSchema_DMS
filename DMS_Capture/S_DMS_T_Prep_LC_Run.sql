5a4b82f571ed0317b2ca045c4b5f2b74f114b1a0
alchemistmatt <matthew.monroe@pnnl.gov>
2023-03-08 17:44:32 -0800

    Remove old synonyms

--- DMS_Capture/S_DMS_T_Prep_LC_Run.sql
+++
@@ -1,5 +0,0 @@
-/****** Object:  Synonym [dbo].[S_DMS_T_Prep_LC_Run] ******/
-CREATE SYNONYM [dbo].[S_DMS_T_Prep_LC_Run] FOR [DMS5].[dbo].[T_Prep_LC_Run]
-GO
-GRANT VIEW DEFINITION ON [dbo].[S_DMS_T_Prep_LC_Run] TO [DDL_Viewer] AS [dbo]
-GO
--- DMS_Data_Package/S_Prep_LC_Run_List.sql
+++
@@ -1,5 +0,0 @@
-/****** Object:  Synonym [dbo].[S_Prep_LC_Run_List] ******/
-CREATE SYNONYM [dbo].[S_Prep_LC_Run_List] FOR [DMS5].[dbo].[T_Prep_LC_Run]
-GO
-GRANT VIEW DEFINITION ON [dbo].[S_Prep_LC_Run_List] TO [DDL_Viewer] AS [dbo]
-GO
