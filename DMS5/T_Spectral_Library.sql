dccbef94ebf640831d9b1a55154cecc25ac94182
alchemistmatt <matthew.monroe@pnnl.gov>
2023-03-18 20:02:02 -0700

    Replace index

--- DMS5/T_Spectral_Library.sql
+++ DMS5/T_Spectral_Library.sql
@@ -49,12 +49,12 @@ GO
 SET ANSI_PADDING ON
 
 GO
-/****** Object:  Index [IX_T_Spectral_Library_Library_Name_Settings_Hash] ******/
-CREATE UNIQUE NONCLUSTERED INDEX [IX_T_Spectral_Library_Library_Name_Settings_Hash] ON [dbo].[T_Spectral_Library]
+/****** Object:  Index [IX_T_Spectral_Library_Settings_Hash] ******/
+CREATE NONCLUSTERED INDEX [IX_T_Spectral_Library_Settings_Hash] ON [dbo].[T_Spectral_Library]
 (
-	[Library_Name] ASC,
 	[Settings_Hash] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
 GO
 ALTER TABLE [dbo].[T_Spectral_Library] ADD  CONSTRAINT [DF_T_Spectral_Library_Created]  DEFAULT (getdate()) FOR [Created]
 GO
