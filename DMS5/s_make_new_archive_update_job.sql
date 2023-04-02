2e80caa533dba68a821ce837819b1e86eb9483e9
alchemistmatt <matthew.monroe@pnnl.gov>
2023-04-01 20:14:18 -0700

    Remove renamed synonym

--- DMS5/s_make_new_archive_update_job.sql
+++
@@ -1,5 +0,0 @@
-/****** Object:  Synonym [dbo].[s_make_new_archive_update_job] ******/
-CREATE SYNONYM [dbo].[s_make_new_archive_update_job] FOR [DMS_Capture].[dbo].[make_new_archive_update_task]
-GO
-GRANT VIEW DEFINITION ON [dbo].[s_make_new_archive_update_job] TO [DDL_Viewer] AS [dbo]
-GO
