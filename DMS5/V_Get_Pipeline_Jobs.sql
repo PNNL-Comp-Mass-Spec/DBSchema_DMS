14882ea634a562bd41af3f10979294636419baa6
alchemistmatt <matthew.monroe@pnnl.gov>
2023-04-26 16:12:55 -0700

    Use Left Outer Join instead of Cross Join

--- DMS5/V_Get_Pipeline_Jobs.sql
+++ DMS5/V_Get_Pipeline_Jobs.sql
@@ -16,7 +16,7 @@ SELECT AJ.AJ_jobID AS job,
        SPath.SP_vol_name_client + 'DMS3_XFER\' + DS.Dataset_Num + '\' AS transfer_folder_path,
        AJ.AJ_Comment AS comment,
        AJ.AJ_specialProcessing as special_processing,
-       AJ.AJ_Owner AS owner
+       AJ.AJ_Owner AS Owner
 FROM dbo.T_Analysis_Job AS AJ
      INNER JOIN dbo.T_Analysis_Tool AnTool
        ON AJ.AJ_analysisToolID = AnTool.AJT_toolID
@@ -26,7 +26,7 @@ FROM dbo.T_Analysis_Job AS AJ
        ON DS.DS_storage_path_ID = SPath.SP_path_ID
      LEFT OUTER JOIN dbo.T_Dataset_Archive AS DA
        ON AJ.AJ_datasetID = DA.AS_Dataset_ID
-     CROSS JOIN (SELECT Value As ArchiveDisabled FROM dbo.T_MiscOptions WHERE Name = 'ArchiveDisabled') As ArchiveStatus
+     LEFT OUTER JOIN (SELECT Value As ArchiveDisabled FROM dbo.T_MiscOptions WHERE Name = 'ArchiveDisabled') As ArchiveStatus ON 1=1
 WHERE (AJ.AJ_StateID IN (1, 8)) AND
       (
         -- Ideally we only allow a job to start processing if the dataset is archived (states 3 or 10)
@@ -43,7 +43,7 @@ WHERE (AJ.AJ_StateID IN (1, 8)) AND
         (DA.AS_state_ID IN (7, 8) And DA.AS_state_Last_Affected < DateAdd(minute, -60, GetDate()))
         Or
         -- If option ArchiveDisabled is non-zero, datasets will not have entries in T_Dataset_Archive             
-        ArchiveStatus.ArchiveDisabled > 0
+        Coalesce(ArchiveStatus.ArchiveDisabled, 0) > 0
         Or
         -- Lastly, let QC_Shew and QC_Mam datasets start if they have been dispositioned (DS_Rating >= 1) and the archive state changed more than 15 minutes ago
         -- However, exclude QC datasets with an archive state of 6 (Operation Failed) or 7 (Purge In Progress)
