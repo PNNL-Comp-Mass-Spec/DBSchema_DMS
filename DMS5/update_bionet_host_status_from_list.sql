4c7143a51b582695fecb061873e1778156778f52
alchemistmatt <matthew.monroe@pnnl.gov>
2023-05-09 18:55:19 -0700

    Add arguments @message and @returnCode
    Use @message to return a vertical bar delimited list of host names to add or update

--- DMS5/update_bionet_host_status_from_list.sql
+++ DMS5/update_bionet_host_status_from_list.sql
@@ -7,8 +7,13 @@ CREATE PROCEDURE [dbo].[update_bionet_host_status_from_list]
 /****************************************************
 **
 **  Desc:
-**      Updates the Last_Online column in T_Bionet_Hosts
-**      for the computers in @hostNames
+**      Updates the Last_Online column in T_Bionet_Hosts for the computers in @hostNames
+**
+**  Arguments:
+**    @hostNames        Comma separated list of computer names; optionally include IP address with each host name using the format Host@IP
+**    @addMissingHosts  If true, add missing hosts
+**    @infoOnly         When true, preview updates
+**    @message          Output message; when @infoOnly is 1, will contain a vertical bar delimited list of host names to add or update
 **
 **  Auth:   mem
 **  Date:   12/03/2015 mem - Initial version
@@ -16,20 +21,21 @@ CREATE PROCEDURE [dbo].[update_bionet_host_status_from_list]
 **                         - Add support for including IP addresses, for example ltq_orb_3@192.168.30.78
 **          03/17/2017 mem - Pass this procedure's name to parse_delimited_list
 **          02/23/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
+**          05/09/2023 mem - Add arguments @message and @returnCode
 **
 *****************************************************/
 (
     @hostNames varchar(8000),   -- Comma separated list of computer names.  Optionally include IP address with each host name using the format host@IP
     @addMissingHosts tinyint = 0,
-    @infoOnly tinyint = 0
+    @infoOnly tinyint = 0,
+    @message varchar(4000) = '' Output,
+    @returnCode varchar(512) = '' Output
 )
 AS
     Set NoCount On
 
-    Declare @myRowCount int
-    Declare @myError int
-    Set @myRowCount = 0
-    Set @myError = 0
+    Declare @myRowCount Int = ''
+    Declare @myError Int = ''
 
     -----------------------------------------
     -- Validate the input parameters
@@ -39,13 +45,17 @@ AS
     Set @addMissingHosts = IsNull(@addMissingHosts, 0)
     Set @infoOnly = IsNull(@infoOnly, 0)
 
+    Set @message = ''
+    Set @returnCode = ''
+
     -----------------------------------------
     -- Create a temporary table
     -----------------------------------------
 
     CREATE TABLE #Tmp_Hosts (
         Host varchar(80) not null,      -- Could have Host and IP, encoded as Host@IP
-        IP varchar(15) null
+        IP varchar(15) Null,
+        Entry_ID Int Identity(1,1) Not Null
     )
 
     CREATE CLUSTERED INDEX #IX_Tmp_Hosts ON #Tmp_Hosts (Host)
@@ -70,12 +80,13 @@ AS
     UPDATE #Tmp_Hosts
     SET Host = SubString(FilterQ.HostAndIP, 1, AtSignLoc - 1),
         IP = SubString(FilterQ.HostAndIP, AtSignLoc + 1, 16)
-    FROM ( SELECT Host AS HostAndIP,
+    FROM ( SELECT Entry_ID,
+                  Host As HostAndIP,
                   CharIndex('@', Host) AS AtSignLoc
            FROM #Tmp_Hosts
            WHERE Host LIKE '%@[0-9]%' ) FilterQ
          INNER JOIN #Tmp_Hosts
-           ON FilterQ.HostAndIP = #Tmp_Hosts.Host
+           ON FilterQ.Entry_id = #Tmp_Hosts.Entry_id
     --
     SELECT @myRowCount = @@rowcount, @myError = @@error
 
@@ -117,6 +128,24 @@ AS
         ORDER BY Src.Host
         --
         SELECT @myRowCount = @@rowcount, @myError = @@error
+                
+        If @addMissingHosts > 0
+            Set @message = 'Hosts to add or update';
+        Else
+            Set @message = 'Hosts to update';
+
+        SELECT @message = @message + ' | ' + Src.Host + 
+                CASE
+                    WHEN Target.Host IS NULL AND
+                        @addMissingHosts = 0 THEN ' (host not found; will be skipped)'
+                    WHEN Target.Host IS NULL AND
+                        @addMissingHosts <> 0 THEN ' (host not found; will be added)'
+                    ELSE ''
+                END
+        FROM #Tmp_Hosts Src
+             LEFT OUTER JOIN T_Bionet_Hosts Target
+               ON Target.Host = Src.Host
+        ORDER BY Src.Host
 
     End
     Else
@@ -141,7 +170,7 @@ AS
             INSERT INTO T_Bionet_Hosts( Host,
                                         IP,
                                         Entered,
-                                  Last_Online )
+                                        Last_Online )
             SELECT Src.Host,
                    Src.IP,
                    GetDate(),
@@ -156,9 +185,6 @@ AS
         End
 
     End
-
-
-Done:
 
     Return @myError
 
