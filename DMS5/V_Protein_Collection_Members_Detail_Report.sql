510997887db46449655e115f404780a4b39b2ec6
alchemistmatt <matthew.monroe@pnnl.gov>
2023-05-18 18:02:14 -0700

    Fix typo in message

--- DMS5/V_Protein_Collection_Members_Detail_Report.sql
+++ DMS5/V_Protein_Collection_Members_Detail_Report.sql
@@ -5,8 +5,8 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE VIEW [dbo].[V_Protein_Collection_Members_Detail_Report]
 AS
-SELECT 'Use V_Protein_Collection_Members_List_Report or use stored procedure get_protein_collection_member_detail' AS Message,
-       'When using S_V_Protein_Collection_Members, always use as Where clause' AS Warning
+SELECT 'Use V_Protein_Collection_Members_List_Report or use stored procedure get_protein_collection_member_detail' AS message,
+       'When using S_V_Protein_Collection_Members, always use a Where clause' AS warning
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Protein_Collection_Members_Detail_Report] TO [DDL_Viewer] AS [dbo]
