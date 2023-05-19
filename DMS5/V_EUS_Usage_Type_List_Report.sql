3aaae8868073a55d8831373710bc1f453f0c5d6b
alchemistmatt <matthew.monroe@pnnl.gov>
2023-05-18 17:19:20 -0700

    Rename column to eus_usage_type and use lowercase column names

--- DMS5/V_EUS_Usage_Type_List_Report.sql
+++ DMS5/V_EUS_Usage_Type_List_Report.sql
@@ -3,17 +3,15 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW [dbo].[V_EUS_Usage_Type_List_Report]
 AS
-SELECT ID,
-       Name,
-       Description,
-       Enabled,
-       Enabled_Campaign,
-       Enabled_Prep_Request
+SELECT id,
+       name AS eus_usage_type,
+       description,
+       enabled,
+       enabled_campaign,
+       enabled_prep_request
 FROM T_EUS_UsageType
-
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_EUS_Usage_Type_List_Report] TO [DDL_Viewer] AS [dbo]
