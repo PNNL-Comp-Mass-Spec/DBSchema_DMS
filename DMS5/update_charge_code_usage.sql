27cf4b43a8d10616641125323161a663eba6766c
alchemistmatt <matthew.monroe@pnnl.gov>
2023-05-24 16:57:42 -0700

    Move preview queries to the start of the procedure

--- DMS5/update_charge_code_usage.sql
+++ DMS5/update_charge_code_usage.sql
@@ -32,85 +32,13 @@ AS
     -----------------------------------------------------------
 
 
-    If @infoOnly = 0
+    If @infoOnly <> 0
     Begin
-        --
-        -- Look for Charge Codes with a non-zero sample prep usage value, but are no longer associated with a sample prep request
-        --
-        UPDATE T_Charge_Code
-        SET Usage_SamplePrep = 0
-        WHERE Usage_SamplePrep > 0 AND
-              Charge_Code IN ( SELECT CC.Charge_Code
-                               FROM T_Charge_Code CC
-                               WHERE Usage_SamplePrep > 0 AND
-                                     NOT EXISTS ( SELECT 1
-                                                  FROM ( SELECT CC.Charge_Code,
-                                                                COUNT(*) AS SPR_Usage
-                                                         FROM T_Sample_Prep_Request SPR
-                                                              INNER JOIN T_Charge_Code CC
-                                                                ON SPR.Work_Package_Number = CC.Charge_Code
-                                                         GROUP BY CC.Charge_Code ) StatsQ
-                                                  WHERE StatsQ.SPR_Usage > 0 AND
-                                                        StatsQ.Charge_Code = CC.Charge_Code ) )
 
-        --
-        -- Look for Charge Codes with a non-zero requested run usage value, but are no longer associated with a requested run
-        --
-        UPDATE T_Charge_Code
-        SET Usage_RequestedRun = 0
-        WHERE Usage_RequestedRun > 0 AND
-              Charge_Code IN ( SELECT CC.Charge_Code
-                               FROM T_Charge_Code CC
-                               WHERE Usage_RequestedRun > 0 AND
-                                     NOT EXISTS ( SELECT 1
-                                                  FROM ( SELECT CC.Charge_Code,
-                                                                COUNT(*) AS RR_Usage
-                                                         FROM T_Charge_Code CC
-                                                              INNER JOIN T_Requested_Run RR
-                                                                ON CC.Charge_Code = RR.RDS_WorkPackage
-                                                         GROUP BY CC.Charge_Code ) StatsQ
-                                                  WHERE StatsQ.RR_Usage > 0 AND
-                                                        StatsQ.Charge_Code = CC.Charge_Code ) )
-        --
-        -- Update sample prep request usage stats
-        --
-        UPDATE T_Charge_Code
-        SET Usage_SamplePrep = SPR_Usage
-        FROM T_Charge_Code Target
-                INNER JOIN ( SELECT CC.Charge_Code,
-                                    COUNT(*) AS SPR_Usage
-                            FROM T_Sample_Prep_Request SPR
-                                INNER JOIN T_Charge_Code CC
-                                    ON SPR.Work_Package_Number = CC.Charge_Code
-                            GROUP BY CC.Charge_Code
-                           ) StatsQ
-                ON Target.Charge_Code = StatsQ.Charge_Code
-        WHERE IsNull(Target.Usage_SamplePrep, 0) <> SPR_Usage
-        --
-        SELECT @myError = @@error, @myRowCount = @@rowcount
+        ---------------------------------------------------
+        -- Preview Updates
+        ---------------------------------------------------
 
-        --
-        -- Update requested run usage stats
-        --
-        UPDATE T_Charge_Code
-        SET Usage_RequestedRun = RR_Usage
-        FROM T_Charge_Code Target
-                INNER JOIN ( SELECT CC.Charge_Code,
-                                    COUNT(*) AS RR_Usage
-                            FROM T_Charge_Code CC
-                                INNER JOIN T_Requested_Run RR
-                                    ON CC.Charge_Code = RR.RDS_WorkPackage
-                            GROUP BY CC.Charge_Code
-                           ) StatsQ
-                ON Target.Charge_Code = StatsQ.Charge_Code
-        WHERE IsNull(Target.Usage_RequestedRun, 0) <> RR_Usage
-        --
-        SELECT @myError = @@error, @myRowCount = @@rowcount
-
-    End
-    Else
-    Begin
-        --
         -- Show usage stats for all work packages
         --
         SELECT ISNULL(A.Charge_Code, B.Charge_Code) AS Charge_Code,
@@ -132,7 +60,6 @@ AS
              ) B
                ON A.Charge_Code = B.Charge_Code
 
-        --
         -- Look for Charge Codes with a non-zero sample prep usage value, but are no longer associated with a sample prep request
         --
         SELECT CC.Charge_Code,
@@ -153,8 +80,7 @@ AS
                                   GROUP BY CC.Charge_Code ) StatsQ
                            WHERE StatsQ.SPR_Usage > 0 AND
                                  StatsQ.Charge_Code = CC.Charge_Code )
-        
-        --
+
         -- Look for Charge Codes with a non-zero requested run usage value, but are no longer associated with a requested run
         --
         SELECT CC.Charge_Code,
@@ -175,9 +101,87 @@ AS
                                   GROUP BY CC.Charge_Code ) StatsQ
                            WHERE StatsQ.RR_Usage > 0 AND
                                  StatsQ.Charge_Code = CC.Charge_Code )
+
+        RETURN
     End
 
-    Return @myError
+    ---------------------------------------------------
+    -- Update usage stats
+    ---------------------------------------------------
+
+    -- Look for Charge Codes with a non-zero sample prep usage value, but are no longer associated with a sample prep request
+    --
+    UPDATE T_Charge_Code
+    SET Usage_SamplePrep = 0
+    WHERE Usage_SamplePrep > 0 AND
+          Charge_Code IN ( SELECT CC.Charge_Code
+                           FROM T_Charge_Code CC
+                           WHERE Usage_SamplePrep > 0 AND
+                                 NOT EXISTS ( SELECT 1
+                                              FROM ( SELECT CC.Charge_Code,
+                                                            COUNT(*) AS SPR_Usage
+                                                     FROM T_Sample_Prep_Request SPR
+                                                          INNER JOIN T_Charge_Code CC
+                                                            ON SPR.Work_Package_Number = CC.Charge_Code
+                                                     GROUP BY CC.Charge_Code ) StatsQ
+                                              WHERE StatsQ.SPR_Usage > 0 AND
+                                                    StatsQ.Charge_Code = CC.Charge_Code ) )
+
+    -- Look for Charge Codes with a non-zero requested run usage value, but are no longer associated with a requested run
+    --
+    UPDATE T_Charge_Code
+    SET Usage_RequestedRun = 0
+    WHERE Usage_RequestedRun > 0 AND
+          Charge_Code IN ( SELECT CC.Charge_Code
+                           FROM T_Charge_Code CC
+                           WHERE Usage_RequestedRun > 0 AND
+                                 NOT EXISTS ( SELECT 1
+                                              FROM ( SELECT CC.Charge_Code,
+                                                            COUNT(*) AS RR_Usage
+                                                     FROM T_Charge_Code CC
+                                                          INNER JOIN T_Requested_Run RR
+                                                            ON CC.Charge_Code = RR.RDS_WorkPackage
+                                                     GROUP BY CC.Charge_Code ) StatsQ
+                                              WHERE StatsQ.RR_Usage > 0 AND
+                                                    StatsQ.Charge_Code = CC.Charge_Code ) )
+    --
+    SELECT @myError = @@error, @myRowCount = @@rowcount
+
+    -- Update sample prep request usage stats
+    --
+    UPDATE T_Charge_Code
+    SET Usage_SamplePrep = SPR_Usage
+    FROM T_Charge_Code Target
+            INNER JOIN ( SELECT CC.Charge_Code,
+                                COUNT(*) AS SPR_Usage
+                        FROM T_Sample_Prep_Request SPR
+                            INNER JOIN T_Charge_Code CC
+                                ON SPR.Work_Package_Number = CC.Charge_Code
+                        GROUP BY CC.Charge_Code
+                       ) StatsQ
+            ON Target.Charge_Code = StatsQ.Charge_Code
+    WHERE IsNull(Target.Usage_SamplePrep, 0) <> SPR_Usage
+    --
+    SELECT @myError = @@error, @myRowCount = @@rowcount
+
+    -- Update requested run usage stats
+    --
+    UPDATE T_Charge_Code
+    SET Usage_RequestedRun = RR_Usage
+    FROM T_Charge_Code Target
+            INNER JOIN ( SELECT CC.Charge_Code,
+                                COUNT(*) AS RR_Usage
+                        FROM T_Charge_Code CC
+                            INNER JOIN T_Requested_Run RR
+                                ON CC.Charge_Code = RR.RDS_WorkPackage
+                        GROUP BY CC.Charge_Code
+                       ) StatsQ
+            ON Target.Charge_Code = StatsQ.Charge_Code
+    WHERE IsNull(Target.Usage_RequestedRun, 0) <> RR_Usage
+    --
+    SELECT @myError = @@error, @myRowCount = @@rowcount
+
+    Return 0
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[update_charge_code_usage] TO [DDL_Viewer] AS [dbo]
