3ab9851da5a8e187fae7be0b1044e5e1b5c0bc01
alchemistmatt <matthew.monroe@pnnl.gov>
2023-05-24 15:43:14 -0700

    Look for work packages that have non-zero usage values but are not actually in use

--- DMS5/update_charge_code_usage.sql
+++ DMS5/update_charge_code_usage.sql
@@ -15,6 +15,7 @@ CREATE PROCEDURE [dbo].[update_charge_code_usage]
 **  Auth:   mem
 **  Date:   06/04/2013 mem - Initial version
 **          02/23/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
+**          05/24/2023 mem - Look for work packages that have non-zero usage values but are not actually in use
 **
 *****************************************************/
 (
@@ -23,10 +24,8 @@ CREATE PROCEDURE [dbo].[update_charge_code_usage]
 AS
     set nocount on
 
-    declare @myError int
-    declare @myRowCount int
-    set @myError = 0
-    set @myRowCount = 0
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
 
     -----------------------------------------------------------
     -- Create the table to hold the data
@@ -35,7 +34,46 @@ AS
 
     If @infoOnly = 0
     Begin
+        --
+        -- Look for Charge Codes with a non-zero sample prep usage value, but are no longer associated with a sample prep request
+        --
+        UPDATE T_Charge_Code
+        SET Usage_SamplePrep = 0
+        WHERE Usage_SamplePrep > 0 AND
+              Charge_Code IN ( SELECT CC.Charge_Code
+                               FROM T_Charge_Code CC
+                               WHERE Usage_SamplePrep > 0 AND
+                                     NOT EXISTS ( SELECT 1
+                                                  FROM ( SELECT CC.Charge_Code,
+                                                                COUNT(*) AS SPR_Usage
+                                                         FROM T_Sample_Prep_Request SPR
+                                                              INNER JOIN T_Charge_Code CC
+                                                                ON SPR.Work_Package_Number = CC.Charge_Code
+                                                         GROUP BY CC.Charge_Code ) StatsQ
+                                                  WHERE StatsQ.SPR_Usage > 0 AND
+                                                        StatsQ.Charge_Code = CC.Charge_Code ) )
 
+        --
+        -- Look for Charge Codes with a non-zero requested run usage value, but are no longer associated with a requested run
+        --
+        UPDATE T_Charge_Code
+        SET Usage_RequestedRun = 0
+        WHERE Usage_RequestedRun > 0 AND
+              Charge_Code IN ( SELECT CC.Charge_Code
+                               FROM T_Charge_Code CC
+                               WHERE Usage_RequestedRun > 0 AND
+                                     NOT EXISTS ( SELECT 1
+                                                  FROM ( SELECT CC.Charge_Code,
+                                                                COUNT(*) AS RR_Usage
+                                                         FROM T_Charge_Code CC
+                                                              INNER JOIN T_Requested_Run RR
+                                                                ON CC.Charge_Code = RR.RDS_WorkPackage
+                                                         GROUP BY CC.Charge_Code ) StatsQ
+                                                  WHERE StatsQ.RR_Usage > 0 AND
+                                                        StatsQ.Charge_Code = CC.Charge_Code ) )
+        --
+        -- Update sample prep request usage stats
+        --
         UPDATE T_Charge_Code
         SET Usage_SamplePrep = SPR_Usage
         FROM T_Charge_Code Target
@@ -51,7 +89,9 @@ AS
         --
         SELECT @myError = @@error, @myRowCount = @@rowcount
 
-
+        --
+        -- Update requested run usage stats
+        --
         UPDATE T_Charge_Code
         SET Usage_RequestedRun = RR_Usage
         FROM T_Charge_Code Target
@@ -70,7 +110,9 @@ AS
     End
     Else
     Begin
-
+        --
+        -- Show usage stats for all work packages
+        --
         SELECT ISNULL(A.Charge_Code, B.Charge_Code) AS Charge_Code,
                A.SPR_Usage,
                B.RR_Usage
@@ -89,9 +131,50 @@ AS
                                GROUP BY CC.Charge_Code
              ) B
                ON A.Charge_Code = B.Charge_Code
-        --
-        SELECT @myError = @@error, @myRowCount = @@rowcount
 
+        --
+        -- Look for Charge Codes with a non-zero sample prep usage value, but are no longer associated with a sample prep request
+        --
+        SELECT CC.Charge_Code,
+               CC.Usage_SamplePrep AS Old_Usage,
+               0 AS New_Usage,
+               CC.WBS_Title,
+               CC.Charge_Code_Title,
+               CC.SubAccount_Title,
+               CC.Setup_Date
+        FROM T_Charge_Code CC
+        WHERE Usage_SamplePrep > 0 AND
+              NOT EXISTS ( SELECT 1
+                           FROM ( SELECT CC.Charge_Code,
+                                         COUNT(*) AS SPR_Usage
+                                  FROM T_Sample_Prep_Request SPR
+                                       INNER JOIN T_Charge_Code CC
+                                         ON SPR.Work_Package_Number = CC.Charge_Code
+                                  GROUP BY CC.Charge_Code ) StatsQ
+                           WHERE StatsQ.SPR_Usage > 0 AND
+                                 StatsQ.Charge_Code = CC.Charge_Code )
+        
+        --
+        -- Look for Charge Codes with a non-zero requested run usage value, but are no longer associated with a requested run
+        --
+        SELECT CC.Charge_Code,
+               CC.Usage_RequestedRun AS Old_Usage,
+               0 AS New_Usage,
+               CC.WBS_Title,
+               CC.Charge_Code_Title,
+               CC.SubAccount_Title,
+               CC.Setup_Date
+        FROM T_Charge_Code CC
+        WHERE Usage_RequestedRun > 0 AND
+              NOT EXISTS ( SELECT 1
+                           FROM ( SELECT CC.Charge_Code,
+                                         COUNT(*) AS RR_Usage
+                                  FROM T_Charge_Code CC
+                                       INNER JOIN T_Requested_Run RR
+                                         ON CC.Charge_Code = RR.RDS_WorkPackage
+                                  GROUP BY CC.Charge_Code ) StatsQ
+                           WHERE StatsQ.RR_Usage > 0 AND
+                                 StatsQ.Charge_Code = CC.Charge_Code )
     End
 
     Return @myError
