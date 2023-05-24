1cbff51767394aa02daf00d7e87db97292122bc9
alchemistmatt <matthew.monroe@pnnl.gov>
2023-05-23 18:09:47 -0700

    Add missing error message and additional validation
    Change @returnCode to varchar(64)
    Remove unused variables

--- DMS5/add_requested_run_batch_location_scan.sql
+++ DMS5/add_requested_run_batch_location_scan.sql
@@ -6,33 +6,42 @@ GO
 CREATE PROCEDURE [dbo].[add_requested_run_batch_location_scan]
 /****************************************************
 **
-**  Desc: Adds a location scan for requested run batch
+**  Desc:
+**      Adds a location scan for one or more requested run batches
+**
+**  Arguments:
+**    @locationId           Location ID (row in in t_material_locations)
+**    @scanDate             Scan date/time
+**    @batchIdList          Requested run batch IDs (comma separated list)
+**    @message              Error message (output); empty string if no error
+**    @returnCode           Return code (duplicates the integer returned by this procedure; varchar for compatibility with Postgres error codes)
 **
 **  Return values: 0: success, otherwise, error code
 **
 **  Auth:   bcg
-**  Date:   05/19/2023 - initial version
+**  Date:   05/19/2023 bcg - Initial version
+**          05/23/2023 mem - Add missing error message and additional validation
 **
 *****************************************************/
 (
     @locationId int,
     @scanDate datetime,
-    @batchIdList varchar(max),                 -- Requested run batch IDs
-    @message varchar(512) = '' Output,
-    @returnCode tinyint = 1 Output
+    @batchIdList varchar(max),
+    @message varchar(512) = '' output,
+    @returnCode varchar(64) = '' output
 )
 AS
     Set XACT_ABORT, nocount on
 
     Declare @myError int = 0
     Declare @myRowCount int = 0
+
     Declare @useRaiseError tinyint = 1
+    Declare @logErrors tinyint = 0
+    Declare @matchCount int
 
     Set @message = ''
-    Set @useRaiseError = IsNull(@useRaiseError, 1)
-
-    Declare @logErrors tinyint = 0
-    Declare @existingBatchGroupID int = Null
+    Set @returnCode = ''
 
     ---------------------------------------------------
     -- Verify that the user can execute this procedure from the given client host
@@ -51,21 +60,20 @@ AS
     -- Validate input fields
     ---------------------------------------------------
 
-    Declare @instrumentGroupToUse varchar(64)
-    Declare @userID int = 0
-    Declare @matchCount int = 0
-    Set @locationId = IsNull(@locationId, 0)
-    
-    SELECT @matchCount = COUNT(ID)
-    FROM T_Material_Locations
-    WHERE ID = @locationId
+    Set @locationId  = Coalesce(@locationId, 0)
+    Set @scanDate    = Coalesce(@scanDate, GetDate())
+    Set @batchIdList = Coalesce(@batchIdList, '')
 
-    If @matchCount <> 1
+    If Not Exists (SELECT ID FROM T_Material_Locations WHERE ID = @locationId)
     Begin
+        Set @message = 'Location ID not found in T_Material_Locations: ' + Cast(@locationId As varchar(12))
+        Set @myError = 50001
+        Set @returnCode = Cast(@myError As varchar(64))
+
         If @useRaiseError > 0
             RAISERROR (@message, 11, 1)
         Else
-            Return @myError
+            RETURN @myError
     End
 
     Set @logErrors = 1
@@ -76,8 +84,8 @@ AS
     --
     CREATE TABLE #Tmp_BatchIDs (
         BatchIDText varchar(128) NULL,
-        Batch_ID [int] NULL,
-        Valid [tinyint] DEFAULT 0
+        Batch_ID int NULL,
+        Valid tinyint DEFAULT 0
     )
     --
     SELECT @myError = @@error, @myRowCount = @@rowcount
@@ -85,11 +93,13 @@ AS
     If @myError <> 0
     Begin
         Set @message = 'Failed to create temporary table for batch IDs'
+        Set @myError = 50002
+        Set @returnCode = Cast(@myError As varchar(64))
 
         If @useRaiseError > 0
             RAISERROR (@message, 11, 22)
         Else
-            Return 50002
+            RETURN @myError
     End
 
     ---------------------------------------------------
@@ -105,11 +115,13 @@ AS
     If @myError <> 0
     Begin
         Set @message = 'Failed to populate temporary table for batch IDs'
+        Set @myError = 50003
+        Set @returnCode = Cast(@myError As varchar(64))
 
         If @useRaiseError > 0
             RAISERROR (@message, 11, 23)
         Else
-            Return 50003
+            RETURN @myError
     End
 
     ---------------------------------------------------
@@ -127,31 +139,31 @@ AS
         FROM #Tmp_BatchIDs
         WHERE Batch_ID Is Null
 
-
         Set @logErrors = 0
-        Set @message = 'Batch IDs must be integers, not names; first invalid item: ' + IsNull(@firstInvalid, '')
+        Set @message = 'Batch IDs must be integers, not names; first invalid item: ' + Coalesce(@firstInvalid, '')
+        Set @myError = 50004
+        Set @returnCode = Cast(@myError As varchar(64))
 
         If @useRaiseError > 0
             RAISERROR (@message, 11, 30)
         Else
-            Return 50004
+            RETURN @myError
     End
 
     ---------------------------------------------------
     -- Check status of supplied batch IDs
     ---------------------------------------------------
-    Declare @count int
 
     -- Do all batch IDs in list actually exist?
     --
-    Set @count = 0
+    Set @matchCount = 0
     --
     UPDATE #Tmp_BatchIDs
     SET #Tmp_BatchIDs.Valid = 1
     FROM T_Requested_Run_Batches rrb
     WHERE #Tmp_BatchIDs.Batch_ID = rrb.ID
 
-    SELECT @count = COUNT(*)
+    SELECT @matchCount = COUNT(*)
     FROM #Tmp_BatchIDs
     WHERE Valid = 0
     --
@@ -160,14 +172,16 @@ AS
     If @myError <> 0
     Begin
         Set @message = 'Failed trying to check existence of batch IDs in list'
+        Set @myError = 50005
+        Set @returnCode = Cast(@myError As varchar(64))
 
         If @useRaiseError > 0
             RAISERROR (@message, 11, 24)
         Else
-            Return 50005
+            RETURN @myError
     End
 
-    If @count <> 0
+    If @matchCount > 0
     Begin
 
         Declare @invalidIDs varchar(64) = null
@@ -180,19 +194,21 @@ AS
 
         Set @logErrors = 0
         Set @message = 'Batch ID list contains batch IDs that do not exist: ' + @invalidIDs
+        Set @myError = 50006
+        Set @returnCode = Cast(@myError As varchar(64))
 
-        If @useRaiseError > 0 -- TODO: do not fire the error! Just drop batch IDs that don't exist (amd report them via @message).
+        If @useRaiseError > 0
             RAISERROR (@message, 11, 25)
         Else
-            Return 50006
+            RETURN @myError
 
     End
-    
+
     -- Start transaction
     --
     Declare @transName varchar(32) = 'AddBatchLocationScan'
 
-    Begin transaction @transName
+    Begin Transaction @transName
 
     MERGE T_Requested_Run_Batch_Location_History AS t
     USING (SELECT @locationID AS location_id, batch_id FROM #Tmp_BatchIDs) AS s
@@ -201,32 +217,34 @@ AS
         INSERT (batch_id, location_id, first_scan_date)
         VALUES (s.batch_id, s.location_id, @scanDate)
     WHEN MATCHED AND (t.first_scan_date < @scanDate OR t.last_scan_date IS NULL OR t.last_scan_date < @scanDate) THEN
-        UPDATE 
+        UPDATE
             SET last_scan_date = CASE
                                      WHEN t.last_scan_date IS NULL AND @scanDate < t.first_scan_date THEN t.first_scan_date
                                      WHEN t.last_scan_date IS NULL OR t.last_scan_date < @scanDate THEN @scanDate
                                      ELSE t.last_scan_date
                                  END,
                 first_scan_date = CASE
-                                     WHEN @scanDate < t.first_scan_date THEN @scanDate 
-                                     ELSE t.first_scan_date 
+                                     WHEN @scanDate < t.first_scan_date THEN @scanDate
+                                     ELSE t.first_scan_date
                                   END;
-            
+
     --
     SELECT @myError = @@error, @myRowCount = @@rowcount
     --
     If @myError <> 0
     Begin
-        Set @message = 'Merge operation failed while adding updated batch ID locations'
+        Set @message = 'Merge operation failed while adding/updating batch location history'
+        Set @myError = 50007
+        Set @returnCode = Cast(@myError As varchar(64))
 
         If @useRaiseError > 0
             RAISERROR (@message, 11, 26)
         Else
-            Return 50007
+            RETURN @myError
     End
 
-    commit transaction @transName
-    
+    Commit Transaction @transName
+
     END TRY
     BEGIN CATCH
         EXEC format_error_message @message output, @myError output
@@ -239,6 +257,7 @@ AS
             Exec post_log_entry 'Error', @message, 'add_requested_run_batch_location_scan'
     END CATCH
 
-    return @myError
+    Set @returnCode = Cast(@myError As varchar(64))
+    RETURN @myError
 
 GO
