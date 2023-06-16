54bcf48133ad7b3996ab18f2434a12fff0dd4e94
alchemistmatt <matthew.monroe@pnnl.gov>
2023-06-15 20:17:57 -0700

    Use new column names

--- DMS5/V_Project_Usage_Stats2.sql
+++ DMS5/V_Project_Usage_Stats2.sql
@@ -9,8 +9,8 @@ CREATE VIEW [dbo].[V_Project_Usage_Stats2]
 AS
 -- Show project stats for this week and the previous week, filtering out Maintenance and Cap_Dev that are not associated with a user proposal
 SELECT 	Entry_ID,
-		StartDate,
-		EndDate,
+		Start_Date,
+		End_Date,
 		[Year],
 		[Week],
 		Proposal_ID,
@@ -25,12 +25,12 @@ SELECT 	Entry_ID,
 		Proposal_Title,
 		Instrument_First,
 		Instrument_Last,
-		JobTool_First,
-		JobTool_Last,
+		Job_Tool_First,
+		Job_Tool_Last,
 		Proposal_Start_Date,
 		Proposal_End_Date,
 		Proposal_Type,
-		SortKey
+		Sort_Key
 FROM V_Project_Usage_Stats
 WHERE [Year] = DATEPART(year, GETDATE()) AND [Week] >= DATEPART(week, GETDATE()) - 1 AND 
    (NOT (Usage_Type IN ('CAP_DEV', 'Maintenance') AND Project_Type = 'Unknown'))
