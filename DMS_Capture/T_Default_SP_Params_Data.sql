1cde16edbc2daa5fb4d11346765216c5b648dee8
alchemistmatt <matthew.monroe@pnnl.gov>
2023-06-20 19:50:59 -0700

    Use new procedure names in options tables

--- DMS_Capture/T_Default_SP_Params_Data.sql
+++ DMS_Capture/T_Default_SP_Params_Data.sql
@@ -1,9 +1,9 @@
 /****** Object:  Table [T_Default_SP_Params] ******/
 /****** RowCount: 6 ******/
 /****** Columns: SP_Name, ParamName, ParamValue, Description ******/
-INSERT INTO [T_Default_SP_Params] VALUES ('MakeNewJobsFromAnalysisBroker','bypassDatasetArchive','1','waive the requirement that there be an existing complete dataset archive job in broker')
-INSERT INTO [T_Default_SP_Params] VALUES ('MakeNewJobsFromAnalysisBroker','datasetIDFilterMax','0','If non-zero, then will be used to filter the candidate datasets')
-INSERT INTO [T_Default_SP_Params] VALUES ('MakeNewJobsFromAnalysisBroker','datasetIDFilterMin','0','If non-zero, then will be used to filter the candidate datasets')
-INSERT INTO [T_Default_SP_Params] VALUES ('MakeNewJobsFromAnalysisBroker','importWindowDays','10','Max days to go back in DMS archive table looking for results transfers')
-INSERT INTO [T_Default_SP_Params] VALUES ('MakeNewJobsFromAnalysisBroker','loggingEnabled','0','Set to 1 to enable SP logging')
-INSERT INTO [T_Default_SP_Params] VALUES ('MakeNewJobsFromAnalysisBroker','timeWindowToRequireExisingDatasetArchiveJob','30','Days')
+INSERT INTO [T_Default_SP_Params] VALUES ('make_new_tasks_from_analysis_broker','bypassDatasetArchive','1','waive the requirement that there be an existing complete dataset archive job in broker')
+INSERT INTO [T_Default_SP_Params] VALUES ('make_new_tasks_from_analysis_broker','datasetIDFilterMax','0','If non-zero, then will be used to filter the candidate datasets')
+INSERT INTO [T_Default_SP_Params] VALUES ('make_new_tasks_from_analysis_broker','datasetIDFilterMin','0','If non-zero, then will be used to filter the candidate datasets')
+INSERT INTO [T_Default_SP_Params] VALUES ('make_new_tasks_from_analysis_broker','importWindowDays','10','Max days to go back in DMS archive table looking for results transfers')
+INSERT INTO [T_Default_SP_Params] VALUES ('make_new_tasks_from_analysis_broker','loggingEnabled','0','Set to 1 to enable SP logging')
+INSERT INTO [T_Default_SP_Params] VALUES ('make_new_tasks_from_analysis_broker','timeWindowToRequireExisingDatasetArchiveJob','30','Days')
--- DMS_Capture/T_Process_Step_Control_Data.sql
+++ DMS_Capture/T_Process_Step_Control_Data.sql
@@ -1,11 +1,12 @@
 /****** Object:  Table [T_Process_Step_Control] ******/
-/****** RowCount: 8 ******/
+/****** RowCount: 9 ******/
 /****** Columns: Processing_Step_Name, enabled, Last_Affected, Entered_By ******/
-INSERT INTO [T_Process_Step_Control] VALUES ('CreateJobSteps',1,'9/5/2009 8:58:09 AM','PNL\D3J410')
-INSERT INTO [T_Process_Step_Control] VALUES ('MakeNewArchiveJobsFromDMS',1,'1/15/2010 9:02:59 AM','PNL\D3J408')
-INSERT INTO [T_Process_Step_Control] VALUES ('MakeNewAutomaticJobs',1,'9/11/2009 3:53:40 PM','PNL\D3J410')
-INSERT INTO [T_Process_Step_Control] VALUES ('MakeNewJobsFromAnalysisBroker',1,'1/14/2010 1:20:50 PM','PNL\D3J408')
-INSERT INTO [T_Process_Step_Control] VALUES ('MakeNewJobsFromDMS',1,'1/20/2010 1:49:08 PM','PNL\D3J408')
+INSERT INTO [T_Process_Step_Control] VALUES ('create_task_steps',1,'9/5/2009 8:58:09 AM','PNL\D3J410')
+INSERT INTO [T_Process_Step_Control] VALUES ('LogLevel',1,'6/19/2023 8:34:43 PM','PNL\D3L243')
+INSERT INTO [T_Process_Step_Control] VALUES ('make_new_archive_tasks_from_dms',1,'1/15/2010 9:02:59 AM','PNL\D3J408')
+INSERT INTO [T_Process_Step_Control] VALUES ('make_new_automatic_tasks',1,'9/11/2009 3:53:40 PM','PNL\D3J410')
+INSERT INTO [T_Process_Step_Control] VALUES ('make_new_tasks_from_analysis_broker',1,'1/14/2010 1:20:50 PM','PNL\D3J408')
+INSERT INTO [T_Process_Step_Control] VALUES ('make_new_tasks_from_dms',1,'1/20/2010 1:49:08 PM','PNL\D3J408')
+INSERT INTO [T_Process_Step_Control] VALUES ('update_task_state',1,'9/11/2009 9:54:58 AM','PNL\D3J410')
+INSERT INTO [T_Process_Step_Control] VALUES ('update_task_step_states',1,'9/5/2009 12:29:30 PM','PNL\D3J410')
 INSERT INTO [T_Process_Step_Control] VALUES ('UpdateDMS',1,'1/4/2010 11:29:41 AM','PNL\D3J408')
-INSERT INTO [T_Process_Step_Control] VALUES ('UpdateJobState',1,'9/11/2009 9:54:58 AM','PNL\D3J410')
-INSERT INTO [T_Process_Step_Control] VALUES ('UpdateStepStates',1,'9/5/2009 12:29:30 PM','PNL\D3J410')
