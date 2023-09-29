b0b6660684abbf7a91e65e006f6e41f67ab220e4
alchemistmatt <matthew.monroe@pnnl.gov>
2023-09-28 19:03:13 -0700

    Obtain tool names from T_Analysis_Tool

--- DMS_Data_Package/check_data_package_dataset_job_coverage.sql
+++ DMS_Data_Package/check_data_package_dataset_job_coverage.sql
@@ -15,6 +15,7 @@ CREATE FUNCTION [dbo].[check_data_package_dataset_job_coverage]
 **          02/15/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
 **          08/17/2023 mem - Use renamed column data_pkg_id in data package item tables
 **          09/26/2023 mem - Obtain dataset names from T_Dataset
+**          09/28/2023 mem - Obtain tool names from T_Analysis_Tool
 **
 *****************************************************/
 (
@@ -33,14 +34,19 @@ BEGIN
         INSERT INTO @table_variable ( Dataset, Num )
         SELECT DS.Dataset_Num AS Dataset,
                NULL AS job_count
-        FROM T_Data_Package_Datasets AS TD
+        FROM T_Data_Package_Datasets AS DPD
              INNER JOIN S_Dataset DS
-               ON TD.Dataset_ID = DS.Dataset_ID
-             LEFT OUTER JOIN T_Data_Package_Analysis_Jobs AS TA
-               ON TD.Dataset_ID = TA.Dataset_ID AND
-                  TD.Data_Pkg_ID = TA.Data_Pkg_ID AND
-                  TA.tool = @tool
-        WHERE TD.Data_Pkg_ID = @packageID AND TA.job Is Null;
+               ON DPD.Dataset_ID = DS.Dataset_ID
+             LEFT OUTER JOIN T_Data_Package_Analysis_Jobs AS DPJ
+                             INNER JOIN S_analysis_job AJ
+                               ON AJ.AJ_JobID = DPJ.Job
+                             INNER JOIN S_analysis_tool T
+                               ON AJ.AJ_AnalysisToolID = T.AJT_ToolID AND
+                                  T.AJT_ToolName = @tool
+               ON DPD.Dataset_ID = DPJ.Dataset_ID AND
+                  DPD.Data_Pkg_ID = DPJ.Data_Pkg_ID
+        WHERE DPD.Data_Pkg_ID = @packageID AND
+              DPJ.job Is Null;
     END
 
     -- Package datasets with no DMS jobs for tool
@@ -50,14 +56,14 @@ BEGIN
         INSERT INTO @table_variable ( Dataset, Num )
         SELECT DS.Dataset_Num AS Dataset,
                NULL AS job_count
-        FROM T_Data_Package_Datasets AS TD
+        FROM T_Data_Package_Datasets AS DPD
              INNER JOIN S_Dataset DS
-               ON TD.Dataset_ID = DS.Dataset_ID
-        WHERE TD.Data_Pkg_ID = @packageID AND
+               ON DPD.Dataset_ID = DS.Dataset_ID
+        WHERE DPD.Data_Pkg_ID = @packageID AND
               NOT EXISTS ( SELECT J.Dataset_ID
                            FROM S_V_Analysis_Job_List_Report_2 AS J
                            WHERE J.Tool = @tool AND
-                                 J.Dataset_ID = TD.Dataset_ID
+                                 J.Dataset_ID = DPD.Dataset_ID
                          );
     END
 
@@ -67,19 +73,20 @@ BEGIN
     BEGIN
         INSERT INTO @table_variable ( Dataset, Num )
         SELECT DS.Dataset_Num AS Dataset,
-               SUM(CASE
-                       WHEN TJ.Job IS NULL THEN 0
-                       ELSE 1
-                   END) AS job_count
-        FROM T_Data_Package_Datasets AS TD
+               SUM(CASE WHEN DPJ.Job IS NULL THEN 0 ELSE 1 END) AS job_count
+        FROM T_Data_Package_Datasets AS DPD
              INNER JOIN S_Dataset DS
-               ON TD.Dataset_ID = DS.Dataset_ID
-             LEFT OUTER JOIN T_Data_Package_Analysis_Jobs AS TJ
-               ON TD.Dataset_ID = TJ.Dataset_ID AND
-                  TD.Data_Pkg_ID = TJ.Data_Pkg_ID AND
-                  TJ.Tool = @tool
-        WHERE TD.Data_Pkg_ID = @packageID
-        GROUP BY TD.Data_Pkg_ID, DS.Dataset_Num, TJ.Tool;
+               ON DPD.Dataset_ID = DS.Dataset_ID
+             LEFT OUTER JOIN T_Data_Package_Analysis_Jobs AS DPJ
+                             INNER JOIN S_analysis_job AJ
+                               ON AJ.AJ_JobID = DPJ.Job
+                             INNER JOIN S_analysis_tool T
+                               ON AJ.AJ_AnalysisToolID = T.AJT_ToolID AND
+                                  T.AJT_ToolName = @tool
+               ON DPD.Dataset_ID = DPJ.Dataset_ID AND
+                  DPD.Data_Pkg_ID = DPJ.Data_Pkg_ID
+        WHERE DPD.Data_Pkg_ID = @packageID
+        GROUP BY DS.Dataset_Num;
     END
 
     RETURN
