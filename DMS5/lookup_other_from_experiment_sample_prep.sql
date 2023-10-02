c199ff798933082bc989bfbe4d56aa8f94ff8a44
alchemistmatt <matthew.monroe@pnnl.gov>
2023-10-01 20:12:41 -0700

    Rename variable and update comments

--- DMS5/lookup_other_from_experiment_sample_prep.sql
+++ DMS5/lookup_other_from_experiment_sample_prep.sql
@@ -8,20 +8,21 @@ CREATE PROCEDURE [dbo].[lookup_other_from_experiment_sample_prep]
 **
 **  Desc:
 **      Get values for misc fields from the sample prep
-**      request associated with the given experiment (if there is one)
+**      Lookup the work package defined for the sample prep request associated with the given experiment (if there is one)
 **
-**      This procedure is used by add_update_requested_run and
-**      the error messages assume that this is the case
+**      This procedure is used by add_update_requested_run and the error messages it returns assume that this is the case
 **
-**  Return values: 0: success, otherwise, error code
-**
-**  Parameters:
+**  Arguments:
+**    @experimentName   Experiment name
+**    @workPackage      Input/output: work package provided by the user; will be updated by this procedure if it is '' or '(lookup)' and the experiment has a sample prep request
+**    @message          Error message
 **
 **  Auth:   grk
 **  Date:   06/03/2009 grk - Initial release (Ticket #499)
 **          01/23/2017 mem - Provide clearer error messages
 **          06/13/2017 mem - Fix failure to update @workPackage using sample prep request's work package
 **          02/23/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
+**          10/01/2023 mem - Rename variable and update comments
 **
 *****************************************************/
 (
@@ -47,15 +48,15 @@ AS
     ---------------------------------------------------
     -- Find associated sample prep request for experiment
     ---------------------------------------------------
-    --
-    Declare @samPrepID int = 0
-    --
-    SELECT @samPrepID = EX_sample_prep_request_ID
+
+    Declare @prepRequestID int = 0
+
+    SELECT @prepRequestID = EX_sample_prep_request_ID
     FROM T_Experiments
     WHERE Experiment_Num = @experimentName
     --
     SELECT @myError = @@error, @myRowCount = @@rowcount
-    --
+
     If @myError <> 0
     Begin
         Set @message = 'Error trying to find sample prep request for the experiment'
@@ -64,9 +65,12 @@ AS
 
     ---------------------------------------------------
     -- If there is no associated sample prep request we are done
-    -- However, do not allow the workpackage to be (lookup)
+    -- However, do not allow the work package to be blank or '(lookup)'
+    --
+    -- Note that experiments without a sample prep request will have sample_prep_request_id = 0
     ---------------------------------------------------
-    If @samPrepID = 0
+
+    If Coalesce(@prepRequestID, 0) = 0
     Begin
         If @workPackage = '' Or @workPackage = @ovr
         Begin
@@ -94,7 +98,7 @@ AS
 
         SELECT @newWorkPackage = ISNULL(Work_Package_Number, '')
         FROM T_Sample_Prep_Request
-        WHERE ID = @samPrepID
+        WHERE ID = @prepRequestID
         --
         SELECT @myError = @@error, @myRowCount = @@rowcount
         --
