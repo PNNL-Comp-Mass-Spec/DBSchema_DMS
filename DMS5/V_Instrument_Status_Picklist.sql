6692c6e2404cb9e572c3bf109879003b26f65506
alchemistmatt <matthew.monroe@pnnl.gov>
2023-10-13 16:33:29 -0700

    Update pick list view to obtain status names from T_Instrument_State_Name

--- DMS5/V_Instrument_Status_Picklist.sql
+++ DMS5/V_Instrument_Status_Picklist.sql
@@ -3,16 +3,10 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW [dbo].[V_Instrument_Status_Picklist]
 AS
-SELECT 'active' AS val
-UNION
-SELECT 'inactive' AS val
-UNION
-SELECT 'offline' AS val
-UNION
-SELECT 'broken' AS val
+SELECT state_name AS val
+FROM T_Instrument_State_Name
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Instrument_Status_Picklist] TO [DDL_Viewer] AS [dbo]
