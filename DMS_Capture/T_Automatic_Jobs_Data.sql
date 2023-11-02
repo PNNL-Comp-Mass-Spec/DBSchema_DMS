ba42b8a8caf132bcbb6c670541f2b0305239fd6b
alchemistmatt <matthew.monroe@pnnl.gov>
2023-11-01 20:47:32 -0700

    Add automatic job for 'LCDatasetCapture' script

--- DMS_Capture/T_Automatic_Jobs_Data.sql
+++ DMS_Capture/T_Automatic_Jobs_Data.sql
@@ -1,6 +1,7 @@
 /****** Object:  Table [T_Automatic_Jobs] ******/
-/****** RowCount: 3 ******/
+/****** RowCount: 4 ******/
 /****** Columns: Script_For_Completed_Job, Script_For_New_Job, Enabled ******/
 INSERT INTO [T_Automatic_Jobs] VALUES ('DatasetArchive','MyEMSLVerify',1)
 INSERT INTO [T_Automatic_Jobs] VALUES ('DatasetArchive','SourceFileRename',1)
 INSERT INTO [T_Automatic_Jobs] VALUES ('DatasetCapture','LCDatasetCapture',0)
+INSERT INTO [T_Automatic_Jobs] VALUES ('LCDatasetCapture','ArchiveUpdate',1)
