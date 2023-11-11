dc70821c1bdd14cc251061c56919a87fee765d38
alchemistmatt <matthew.monroe@pnnl.gov>
2023-11-11 01:05:38 -0800

    2023-11-11 auto-commit DMS_Capture

--- DMS_Capture/T_Automatic_Jobs_Data.sql
+++ DMS_Capture/T_Automatic_Jobs_Data.sql
@@ -3,5 +3,5 @@
 /****** Columns: Script_For_Completed_Job, Script_For_New_Job, Enabled ******/
 INSERT INTO [T_Automatic_Jobs] VALUES ('DatasetArchive','MyEMSLVerify',1)
 INSERT INTO [T_Automatic_Jobs] VALUES ('DatasetArchive','SourceFileRename',1)
-INSERT INTO [T_Automatic_Jobs] VALUES ('DatasetCapture','LCDatasetCapture',0)
+INSERT INTO [T_Automatic_Jobs] VALUES ('DatasetCapture','LCDatasetCapture',1)
 INSERT INTO [T_Automatic_Jobs] VALUES ('LCDatasetCapture','ArchiveUpdate',1)
