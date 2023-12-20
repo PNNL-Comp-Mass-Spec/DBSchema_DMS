23e1ffd7920fe6e55b1f158fb364d64e7060f1c9
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2023-12-20 14:09:38 -0800

    Add a view for RFID labels for material containers

---
+++ DMS5/V_Material_Containers_Active_Export_RFID.sql
@@ -0,0 +1,26 @@
+/****** Object:  View [dbo].[V_Material_Containers_Active_Export_RFID] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[V_Material_Containers_Active_Export_RFID]
+AS
+SELECT MC.Tag AS container,
+       MC.Comment AS comment,
+       CASE 
+         WHEN C.Campaign_Num IS NULL OR C.Campaign_Num = 'NOT SET' THEN
+           ''
+         ELSE 
+           C.Campaign_Num
+       END AS campaign,
+       MC.Created AS created,
+       --MC.Status AS status,
+       MC.Researcher AS researcher,
+       MC.ID AS id,
+       MC.RFID_Hex_ID AS hex_id
+FROM T_Material_Containers MC
+     LEFT OUTER JOIN T_Campaign C
+       ON MC.Campaign_ID = C.Campaign_ID
+WHERE status = 'Active'
+
+GO
