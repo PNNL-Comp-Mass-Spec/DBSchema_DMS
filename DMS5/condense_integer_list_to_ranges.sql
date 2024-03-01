7637b412055a8a47bf27c84519bb9f19f97f7d0f
alchemistmatt <matthew.monroe@pnnl.gov>
2024-03-01 14:52:42 -0800

    Add support for duplicate values when condensing a list of integers to ranges

--- DMS5/condense_integer_list_to_ranges.sql
+++ DMS5/condense_integer_list_to_ranges.sql
@@ -7,8 +7,7 @@ CREATE PROCEDURE [dbo].[condense_integer_list_to_ranges]
 /****************************************************
 **
 **  Desc:
-**      Given a list of integers in a temporary table, condenses
-**      the list into a comma and dash separted list
+**      Given a list of integers in a temporary table, condense the list into a comma and dash separted list
 **
 **      Leverages code from Dwain Camps
 **      https://www.simple-talk.com/sql/database-administration/condensing-a-delimited-list-of-integers-in-sql-server/
@@ -16,35 +15,46 @@ CREATE PROCEDURE [dbo].[condense_integer_list_to_ranges]
 **      The calling procedure must create two temporary tables
 **      The #Tmp_ValuesByCategory table must be populated with the integers
 **
-**      Create Table #Tmp_ValuesByCategory (
+**      CREATE TABLE #Tmp_ValuesByCategory (
 **          Category varchar(512),
-**          Value int Not null
+**          Value int
 **      )
 **
-**      Create Table #Tmp_Condensed_Data (
+**      CREATE TABLE #Tmp_Condensed_Data (
 **          Category varchar(512),
 **          ValueList varchar(max)
 **      )
 **
-**      Example data:
+**      Example usage:
 **
-**          Populate #Tmp_ValuesByCategory with:
-**          Category  Value
-**          Job       100
-**          Job       101
-**          Job       102
-**          Job       114
-**          Job       115
-**          Job       118
+**          INSERT INTO #Tmp_ValuesByCategory
+**          VALUES ('Job', 100),
+**                 ('Job', 101),
+**                 ('Job', 102),
+**                 ('Job', 114),
+**                 ('Job', null),
+**                 ('Job', 115),
+**                 ('Job', 118),
+**                 ('Dataset', 500),
+**                 ('Dataset', 505),
+**                 ('Dataset', 506),
+**                 ('Dataset', 507),
+**                 ('Dataset', 508),
+**                 ('Dataset', 512);
 **
-**      After calling this procedure, #Tmp_Condensed_Data will have:
+**          EXEC condense_integer_list_to_ranges
+**
+**          SELECT * FROM #Tmp_Condensed_Data
+**
 **          Category  ValueList
+**          --------  ---------------------
+**          Dataset   500, 505-508, 512
 **          Job       100-102, 114-115, 118
-**
 **
 **  Auth:   mem
 **  Date:   07/01/2014 mem - Initial version
 **          02/23/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
+**          03/01/2024 mem - Add support for duplicate values
 **
 *****************************************************/
 (
@@ -80,20 +90,23 @@ AS
 
     WITH Islands AS (
         SELECT Category, MIN(Value) AS StartValue, MAX(Value) AS EndValue
-        FROM (
-            SELECT Category, Value
-                -- This rn represents the "staggered rows"
-                ,rn=Value-ROW_NUMBER() OVER (PARTITION BY Category ORDER BY Value)
-            FROM #Tmp_ValuesByCategory) a
-        GROUP BY Category, rn
+        FROM (SELECT V.Category, V.Value,
+                     -- This rn represents the "staggered rows"
+                     rn = V.Value - ROW_NUMBER() OVER (PARTITION BY V.Category ORDER BY V.Value)
+              FROM (SELECT DISTINCT VC.Category, VC.Value
+                    FROM #Tmp_ValuesByCategory VC
+                   ) V
+              WHERE NOT V.Value IS NULL
+             ) RankQ
+        GROUP BY RankQ.Category, RankQ.rn
     )
     UPDATE a
-    SET ValueList=STUFF((
-        SELECT ', ' +
-            CASE -- Include either a single Item or the range (hyphenated)
-                WHEN StartValue = EndValue THEN CAST(StartValue AS VARCHAR(12))
-                ELSE CAST(StartValue AS VARCHAR(12)) + '-' + CAST(EndValue AS VARCHAR(12))
-                END
+    SET ValueList = STUFF((
+          SELECT ', ' +
+                 CASE -- Include either a single Item or the range (hyphenated)
+                     WHEN StartValue = EndValue THEN CAST(StartValue AS VARCHAR(12))
+                     ELSE CAST(StartValue AS VARCHAR(12)) + '-' + CAST(EndValue AS VARCHAR(12))
+                 END
         FROM Islands b
         WHERE a.Category = b.Category
         ORDER BY StartValue
@@ -103,10 +116,13 @@ AS
     If @debugMode <> 0
     Begin
         SELECT Category, MIN(Value) AS StartValue, MAX(Value) AS EndValue
-        FROM (
-            SELECT Category, Value,
-                   rn=Value-ROW_NUMBER() OVER (PARTITION BY Category ORDER BY Value)
-            FROM #Tmp_ValuesByCategory) a
+        FROM (SELECT Category, Value,
+                     rn = Value - ROW_NUMBER() OVER (PARTITION BY Category ORDER BY Value)
+              FROM (SELECT DISTINCT VC.Category, VC.Value
+                    FROM #Tmp_ValuesByCategory VC
+                   ) V
+              WHERE NOT V.Value IS NULL
+             ) RankQ
         GROUP BY Category, rn
         ORDER BY Category, rn
 
@@ -114,7 +130,7 @@ AS
     End
 
 Done:
-    return 0
+    Return 0
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[condense_integer_list_to_ranges] TO [DDL_Viewer] AS [dbo]
