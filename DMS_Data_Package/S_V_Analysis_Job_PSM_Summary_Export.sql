c279a9d46da7de4767ed5a295fc12a60b925d9bf
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-18 01:05:16 -0700

    2024-05-18 auto-commit DMS_Data_Package

--- DMS_Data_Package/S_V_Analysis_Job_PSM_Summary_Export.sql
+++ DMS_Data_Package/S_V_Analysis_Job_PSM_Summary_Export.sql
@@ -1,5 +1,3 @@
 /****** Object:  Synonym [dbo].[S_V_Analysis_Job_PSM_Summary_Export] ******/
 CREATE SYNONYM [dbo].[S_V_Analysis_Job_PSM_Summary_Export] FOR [DMS5].[dbo].[V_Analysis_Job_PSM_Summary_Export]
 GO
-GRANT VIEW DEFINITION ON [dbo].[S_V_Analysis_Job_PSM_Summary_Export] TO [DDL_Viewer] AS [dbo]
-GO
