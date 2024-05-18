505c1574846fb9e544bb98559c4dbda2c780f8be
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-17 18:35:36 -0700

    Trim leading and trailing whitespace from @chargeCodeList

--- DMS5/add_new_charge_code.sql
+++ DMS5/add_new_charge_code.sql
@@ -8,6 +8,7 @@ CREATE PROCEDURE [dbo].[add_new_charge_code]
 **
 **  Desc:
 **      Adds a charge code (work package) to T_Charge_Code
+**
 **      Useful when a work package is not auto-adding to the table
 **      (charge codes are auto-added if the owner is a DMS user or DMS guest)
 **
@@ -16,39 +17,38 @@ CREATE PROCEDURE [dbo].[add_new_charge_code]
 **  Auth:   mem
 **          08/13/2015 mem - Initial Version
 **          02/23/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
+**          05/17/2024 mem - Trim leading and trailing whitespace from @chargeCodeList
 **
 *****************************************************/
 (
-    @chargeCodeList varchar(2000),
+    @chargeCodeList varchar(2000),          -- Comma-separated list of work packages
     @infoOnly tinyint = 0,
     @message varchar(512)='' output
 )
 AS
-    set nocount on
+    Set nocount on
 
-    declare @myError int
-    declare @myRowCount int
-    set @myError = 0
-    set @myRowCount = 0
+    Declare @myError int = 0
+    Declare @myRowCount int = 0
 
     ----------------------------------------------------------
     -- Validate the inputs
     ----------------------------------------------------------
 
     Set @infoOnly = IsNull(@infoOnly, 0)
-    Set @ChargeCodeList = IsNull(@ChargeCodeList, '')
+    Set @ChargeCodeList = LTrim(RTrim(IsNull(@ChargeCodeList, '')))
     Set @message = ''
 
     If @ChargeCodeList = ''
     Begin
-        set @message = '@ChargeCodeList is empty; nothing to do'
-        print @message
+        Set @message = '@ChargeCodeList is empty; nothing to do'
+        Print @message
     End
     Else
     Begin
-        exec update_charge_codes_from_warehouse @infoOnly=@infoOnly, @updateAll=0, @ExplicitChargeCodeList=@ChargeCodeList, @message=@message output
+        Exec @myError = update_charge_codes_from_warehouse @infoOnly=@infoOnly, @updateAll=0, @ExplicitChargeCodeList=@ChargeCodeList, @message=@message output
 
-        if @message <> ''
+        If @message <> ''
             Print @message
     End
 
@@ -56,7 +56,7 @@ AS
     -- Done
     ---------------------------------------------------
 Done:
-    return @myError
+    Return @myError
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[add_new_charge_code] TO [DDL_Viewer] AS [dbo]
