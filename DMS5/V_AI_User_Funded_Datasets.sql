b3f796623317b3b6abb1769a87a4a389ccfc4c8c
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-22 16:04:38 -0700

    Directly query T_Experiments instead of using V_Experiment_Detail_Report_Ex

--- DMS5/V_AI_User_Funded_Datasets.sql
+++ DMS5/V_AI_User_Funded_Datasets.sql
@@ -3,22 +3,25 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW [dbo].[V_AI_User_Funded_Datasets]
 AS
-SELECT dbo.T_Dataset.Dataset_Num As dataset,
-   dbo.T_Instrument_Name.IN_name AS instrument,
-   dbo.T_Dataset.DS_created AS date, T.experiment,
-   AI.Value AS proposal_number
-FROM dbo.V_Experiment_Detail_Report_Ex T INNER JOIN
-   dbo.V_Aux_Info_Value AI ON T.ID = AI.Target_ID INNER JOIN
-   dbo.T_Dataset ON T.ID = dbo.T_Dataset.Exp_ID INNER JOIN
-   dbo.T_Instrument_Name ON
-   dbo.T_Dataset.DS_instrument_name_ID = dbo.T_Instrument_Name.Instrument_ID
-WHERE (AI.Target = 'Experiment') AND (AI.Category = 'Accounting')
-   AND (AI.Subcategory = 'Funding') AND
-   (AI.Item = 'Proposal Number') AND (AI.Value <> '')
-
+SELECT DS.Dataset_Num AS dataset,
+       InstName.IN_name AS instrument,
+       DS.DS_created AS DATE,
+       E.Experiment_Num AS experiment,
+       AI.Value AS proposal_number
+FROM dbo.T_Experiments E
+     INNER JOIN dbo.V_Aux_Info_Value AI
+       ON E.Exp_ID = AI.Target_ID
+     INNER JOIN dbo.T_Dataset DS
+       ON E.Exp_ID = DS.Exp_ID
+     INNER JOIN dbo.T_Instrument_Name InstName
+       ON DS.DS_instrument_name_ID = InstName.Instrument_ID
+WHERE AI.Target = 'Experiment' AND
+      AI.Category = 'Accounting' AND
+      AI.Subcategory = 'Funding' AND
+      AI.Item = 'Proposal Number' AND
+      AI.Value <> ''
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_AI_User_Funded_Datasets] TO [DDL_Viewer] AS [dbo]
