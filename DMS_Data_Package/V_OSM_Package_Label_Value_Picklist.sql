fa1d8d03e05355083cae1c8fb1e952fa11c37e04
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-23 16:38:54 -0700

    Rename views

--- DMS_Data_Package/V_OSM_Package_Label_Value_Picklist.sql
+++ DMS_Data_Package/V_OSM_Package_Label_Value_Picklist.sql
@@ -1,14 +1,12 @@
-/****** Object:  View [dbo].[V_OSM_Package_Picklist] ******/
+/****** Object:  View [dbo].[V_OSM_Package_Label_Value_Picklist] ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE VIEW [dbo].[V_OSM_Package_Picklist]
+CREATE VIEW [dbo].[V_OSM_Package_Label_Value_Picklist]
 AS
-SELECT CONVERT(VARCHAR(12), ID) + CHAR(32) + Name AS label, 
-       ID AS value 
+SELECT CONVERT(VARCHAR(12), ID) + CHAR(32) + Name AS label,
+       ID AS value
 FROM T_OSM_Package
-
 
 GO
