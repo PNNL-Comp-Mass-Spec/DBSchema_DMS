368c0463deef8011c5e7a785cce632da03de9f74
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-22 21:12:49 -0700

    Add table and procedure for tracking query row counts

---
+++ DMS5/T_Query_Row_Counts.sql
@@ -0,0 +1,43 @@
+/****** Object:  Table [dbo].[T_Query_Row_Counts] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[T_Query_Row_Counts](
+	[Query_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Object_Name] [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Where_Clause] [varchar](4000) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Row_Count] [bigint] NOT NULL,
+	[Last_Used] [datetime] NOT NULL,
+	[Last_Refresh] [datetime] NOT NULL,
+	[Refresh_Interval_Hours] [numeric](9, 3) NOT NULL,
+	[Entered] [datetime] NOT NULL,
+ CONSTRAINT [PK_T_Query_Row_Counts] PRIMARY KEY CLUSTERED 
+(
+	[Query_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_T_Query_Row_Counts_Object_Name_include_Where_Clause] ******/
+CREATE NONCLUSTERED INDEX [IX_T_Query_Row_Counts_Object_Name_include_Where_Clause] ON [dbo].[T_Query_Row_Counts]
+(
+	[Object_Name] ASC
+)
+INCLUDE([Where_Clause]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[T_Query_Row_Counts] ADD  CONSTRAINT [DF_T_Query_Row_Counts_Where_Clause]  DEFAULT ('') FOR [Where_Clause]
+GO
+ALTER TABLE [dbo].[T_Query_Row_Counts] ADD  CONSTRAINT [DF_T_Query_Row_Counts_Row_Count]  DEFAULT ((0)) FOR [Row_Count]
+GO
+ALTER TABLE [dbo].[T_Query_Row_Counts] ADD  CONSTRAINT [DF_T_Query_Row_Counts_Last_Used]  DEFAULT (getdate()) FOR [Last_Used]
+GO
+ALTER TABLE [dbo].[T_Query_Row_Counts] ADD  CONSTRAINT [DF_T_Query_Row_Counts_Last_Refresh]  DEFAULT (getdate()) FOR [Last_Refresh]
+GO
+ALTER TABLE [dbo].[T_Query_Row_Counts] ADD  CONSTRAINT [DF_T_Query_Row_Counts_Refresh_Interval_Hours]  DEFAULT ((4)) FOR [Refresh_Interval_Hours]
+GO
+ALTER TABLE [dbo].[T_Query_Row_Counts] ADD  CONSTRAINT [DF_T_Query_Row_Counts_Entered]  DEFAULT (getdate()) FOR [Entered]
+GO
