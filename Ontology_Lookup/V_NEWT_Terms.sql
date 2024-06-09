7e45b25f004d391f2e13613c232d9726aa59b927
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-08 20:18:37 -0700

    Query table directly instead of using view

--- Ontology_Lookup/V_NEWT_Terms.sql
+++ Ontology_Lookup/V_NEWT_Terms.sql
@@ -3,22 +3,21 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE VIEW [dbo].[V_NEWT_Terms]
 AS
-SELECT term_name,
-       identifier,
-       term_pk,
-       is_leaf,
-       parent_term_name,
-       parent_term_identifier,
-       grandparent_term_name,
-       grandparent_term_identifier
-FROM V_Term_Lineage
-WHERE ontology = 'NEWT' AND
-      is_obsolete = 0 AND
-      identifier LIKE '[0-9]%'
-
+SELECT N.term_name,
+       N.identifier,
+       N.term_pk,
+       N.is_leaf,
+       N.rank,
+       N.parent_term_name,
+       N.parent_term_id as parent_term_identifier,
+       N.grandparent_term_name,
+       N.grandparent_term_id AS grandparent_term_identifier,
+       N.common_name,
+       N.synonym,
+       N.mnemonic
+FROM T_CV_NEWT N
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_NEWT_Terms] TO [DDL_Viewer] AS [dbo]
