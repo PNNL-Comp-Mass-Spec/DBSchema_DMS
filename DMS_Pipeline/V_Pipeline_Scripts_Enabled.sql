f63a91cefeaede822dcd8984c99c9732964a1313
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-09 17:08:02 -0700

    Rename view to V_Pipeline_Scripts_Enabled

--- DMS_Pipeline/V_Pipeline_Scripts_Enabled.sql
+++ DMS_Pipeline/V_Pipeline_Scripts_Enabled.sql
@@ -1,15 +1,17 @@
-/****** Object:  View [dbo].[V_Pipeline_Script_With_Parameters] ******/
+/****** Object:  View [dbo].[V_Pipeline_Scripts_Enabled] ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+CREATE VIEW [dbo].[V_Pipeline_Scripts_Enabled]
+AS
+-- This view is used by DMS website page families pipeline_jobs and pipeline_jobs_history in the utility_queries section
 
-CREATE VIEW [dbo].[V_Pipeline_Script_With_Parameters] as 
 SELECT Script
 FROM T_Scripts
 WHERE Enabled = 'Y' AND
       Pipeline_Job_Enabled > 0
 
 GO
-GRANT VIEW DEFINITION ON [dbo].[V_Pipeline_Script_With_Parameters] TO [DDL_Viewer] AS [dbo]
+GRANT VIEW DEFINITION ON [dbo].[V_Pipeline_Scripts_Enabled] TO [DDL_Viewer] AS [dbo]
 GO
