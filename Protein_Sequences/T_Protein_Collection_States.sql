50951672f68f78a00b544d2b7cfed3d6f7df26a7
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-22 16:14:09 -0700

    Make protein description fields Not Null and expand column size

--- Protein_Sequences/T_Protein_Collection_States.sql
+++ Protein_Sequences/T_Protein_Collection_States.sql
@@ -5,8 +5,8 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[T_Protein_Collection_States](
 	[Collection_State_ID] [tinyint] NOT NULL,
-	[State] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Description] [varchar](128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[State] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Description] [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
  CONSTRAINT [PK_T_Protein_Collection_States] PRIMARY KEY CLUSTERED 
 (
 	[Collection_State_ID] ASC
