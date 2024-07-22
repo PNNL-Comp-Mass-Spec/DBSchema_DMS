e7d480fdd36cd5860f70a7362ba3666bdc495e74
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-22 16:14:32 -0700

    Add new protein collection state and update descriptions

--- Protein_Sequences/T_Protein_Collection_States_Data.sql
+++ Protein_Sequences/T_Protein_Collection_States_Data.sql
@@ -1,9 +1,10 @@
 /****** Object:  Table [T_Protein_Collection_States] ******/
-/****** RowCount: 6 ******/
+/****** RowCount: 7 ******/
 /****** Columns: Collection_State_ID, State, Description ******/
-INSERT INTO [T_Protein_Collection_States] VALUES (0,'Unknown','Protein collection does not exist')
-INSERT INTO [T_Protein_Collection_States] VALUES (1,'New','Newly entered, in development')
-INSERT INTO [T_Protein_Collection_States] VALUES (2,'Provisional','In Review before release to production')
-INSERT INTO [T_Protein_Collection_States] VALUES (3,'Production','Currently in use for analyses')
-INSERT INTO [T_Protein_Collection_States] VALUES (4,'Retired','No longer used for analyses, kept for legacy reasons')
-INSERT INTO [T_Protein_Collection_States] VALUES (5,'Proteins_Deleted','Protein names, descriptions, and sequences are no longer in the database')
+INSERT INTO [T_Protein_Collection_States] VALUES (0,'Unknown','protein collection does not exist')
+INSERT INTO [T_Protein_Collection_States] VALUES (1,'New','newly entered, in development')
+INSERT INTO [T_Protein_Collection_States] VALUES (2,'Provisional','in review before release to production')
+INSERT INTO [T_Protein_Collection_States] VALUES (3,'Production','currently in use by analysis jobs')
+INSERT INTO [T_Protein_Collection_States] VALUES (4,'Retired','no longer used for analyses, kept for legacy reasons')
+INSERT INTO [T_Protein_Collection_States] VALUES (5,'Proteins_Deleted','protein names, descriptions, and sequences have been deleted from the database, and we do not have the corresponding FASTA file')
+INSERT INTO [T_Protein_Collection_States] VALUES (6,'Offline','protein names and sequences are no longer in the database; contact an admin to restore this protein collection using the FASTA file')
