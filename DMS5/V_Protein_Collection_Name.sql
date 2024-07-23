5688c096540f11ce1da2c0b6eaa66bfce068399b
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-22 17:09:16 -0700

    Add sort weight column to the view used by the protein collection helper

--- DMS5/V_Protein_Collection_Name.sql
+++ DMS5/V_Protein_Collection_Name.sql
@@ -6,8 +6,8 @@ GO
 CREATE VIEW [dbo].[V_Protein_Collection_Name]
 AS
 SELECT LookupQ.name,
-       LookupQ.type,
-       CASE WHEN ISNULL(Org.og_organismdbname, '') = LookupQ.name
+       LookupQ.state,
+       CASE WHEN ISNULL(Org.OG_organismDBName, '') = LookupQ.name
             THEN CASE WHEN ISNULL(LookupQ.description, '') = '' THEN 'PREFERRED' ELSE 'PREFERRED: ' + LookupQ.Description END
             ELSE LookupQ.description
             END AS description,
@@ -25,9 +25,11 @@ SELECT LookupQ.name,
             END AS most_recent_usage,
        LookupQ.entries,
        LookupQ.organism_name,
+       LookupQ.type,
+       LookupQ.sort_weight,
        LookupQ.id
 FROM ( SELECT PC.Name,
-              PC.Type,
+              PC.State_Name AS State,
               PC.Description,
               PC.Entries,
               CASE
@@ -35,19 +37,27 @@ FROM ( SELECT PC.Name,
                   ELSE Org.OG_name
               END AS Organism_Name,
               PC.ID,
-              Case
-                  WHEN PC.Type = 'Internal_Standard' THEN 1
-                  WHEN PC.Type In ('contaminant','old_contaminant') THEN 2
-                  ELSE 0
-              END AS TypeSortOrder
+              CASE WHEN PC.State_Name IN ('New', 'Provisional', 'Production') THEN
+                  CASE WHEN PC.Type = 'internal_standard' THEN 3
+                       WHEN PC.Type In ('contaminant', 'old_contaminant') THEN 2
+                       ELSE 1
+                  END
+              ELSE
+                  CASE WHEN PC.Type = 'internal_standard' THEN 6
+                       WHEN PC.Type In ('contaminant', 'old_contaminant') THEN 5
+                       ELSE 4
+                 END
+              END AS Sort_Weight,
+              PC.Type
        FROM T_Cached_Protein_Collections PC INNER JOIN
 	        T_Organisms Org ON PC.Organism_ID = Org.Organism_ID
-       WHERE IsNull(PC.State_Name, '') <> 'Retired'
+       WHERE Not IsNull(PC.State_Name, '') IN ('Unknown', 'Retired', 'Proteins_Deleted')
        ) LookupQ
        LEFT JOIN dbo.T_Organisms Org ON LookupQ.Organism_Name = Org.OG_Name
        LEFT OUTER JOIN T_Protein_Collection_Usage PCU ON LookupQ.ID = PCU.Protein_Collection_ID
-GROUP BY LookupQ.Name, LookupQ.Type, LookupQ.Description, LookupQ.Entries, LookupQ.Organism_Name,
-         LookupQ.ID, LookupQ.TypeSortOrder, PCU.Most_Recently_Used, PCU.Job_Usage_Count, PCU.Job_Usage_Count_Last12Months, Org.OG_organismDBName
+GROUP BY LookupQ.Name, LookupQ.State, Org.OG_organismDBName, LookupQ.description,
+         PCU.Job_Usage_Count, PCU.Job_Usage_Count_Last12Months, PCU.Most_Recently_Used, LookupQ.Entries,
+         LookupQ.Organism_Name, LookupQ.Type, LookupQ.Sort_Weight, LookupQ.ID
 
 GO
 GRANT VIEW DEFINITION ON [dbo].[V_Protein_Collection_Name] TO [DDL_Viewer] AS [dbo]
